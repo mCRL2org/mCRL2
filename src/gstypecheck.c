60d0c10ec31865c51bf5320de2fb6edeab725ca3
Yaroslav Usenko <y.s.usenko@tue.nl>
2005-02-15 14:39:26 +0000

    Infrastructure 1
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@158 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/gstypecheck.c
+++ src/gstypecheck.c
@@ -1,5 +1,6 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <string.h>
@@ -12,8 +13,95 @@ extern "C" {
 #include "gstypecheck.h"
 #include "gsfunc.h"
 
-ATermAppl gsTypeCheck(ATermAppl spec)
+// Static data 
+
+// Static functions declaration
+static ATermAppl gstcReadIn (ATermAppl);
+static ATermAppl gstcCheck (ATermAppl);
+static ATermAppl gstcTransform (ATermAppl);
+
+// Main function
+ATermAppl
+gsTypeCheck (ATermAppl input)
+{	
+	ATermAppl Result = input;
+	gsVerboseMsg ("type checking fase started\n");
+
+	Result = gstcReadIn(Result);
+    if (Result == NULL) {
+      throw;
+    }
+
+ 	Result = gstcCheck(Result);
+    if (Result == NULL) {
+      throw;
+    }
+
+ 	Result = gstcTransform(Result);
+    if (Result == NULL) {
+      throw;
+    }
+
+	gsVerboseMsg ("type checking fase finished\n");
+	
+finally:
+    if (Result != NULL) {
+      gsDebugMsg("return %t\n", Result);
+    } else {
+      gsDebugMsg("return NULL\n");
+    }
+    return Result;
+}
+
+// Static functions
+static ATermAppl
+gstcReadIn (ATermAppl input)
 {
-  gsVerboseMsg("type checking is not yet implemented\n");
-  return spec;
+	ATermAppl Result = input;
+	gsDebugMsg ("type checking read-in fase started\n");
+	
+	Result=NULL;
+	
+	gsDebugMsg ("type checking read-in fase finished\n");
+finally:
+    if (Result != NULL) {
+      gsDebugMsg("return %t\n", Result);
+    } else {
+      gsDebugMsg("return NULL\n");
+    }
+    return Result;
+}
+
+static ATermAppl
+gstcCheck (ATermAppl input)
+{
+	ATermAppl Result = input;
+	gsDebugMsg ("type checking check fase started\n");
+
+
+	gsDebugMsg ("type checking check fase finished\n");
+finally:
+    if (Result != NULL) {
+      gsDebugMsg("return %t\n", Result);
+    } else {
+      gsDebugMsg("return NULL\n");
+    }
+    return Result;
+}
+
+static ATermAppl
+gstcTransform (ATermAppl input)
+{
+	ATermAppl Result = input;
+	gsDebugMsg ("type checking transform fase started\n");
+
+
+	gsDebugMsg ("type checking transform fase finished\n");
+finally:
+    if (Result != NULL) {
+      gsDebugMsg("return %t\n", Result);
+    } else {
+      gsDebugMsg("return NULL\n");
+    }
+    return Result;
 }
