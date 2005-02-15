7052ef470ce5bf863251e8631c60eee95509f978
Yaroslav Usenko <y.s.usenko@tue.nl>
2005-02-15 20:49:35 +0000

    Infra 3
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@160 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/gstypecheck.c
+++ src/gstypecheck.c
@@ -14,8 +14,19 @@ extern "C"
 #include "gsfunc.h"
 
 // Static data 
+typedef struct { 
+     ATermIndexedSet basic_sorts;	
+     ATermTable defined_sorts;		//name -> sort expression
+     ATermTable constructors;		//name -> Set(sort expression)
+     ATermTable mappings;		    //name -> Set(sort expression)
+     ATermTable actions;	        //name -> Set(List(sort expression)) because of action polymorphism
+     ATermTable processes;	        //name -> List(sort expression) process names are unique.
+} Context;
+static Context context;
 
 // Static functions declaration
+void gstcContextDataInit(void);
+void gstcContextDataDestroy(void);
 static ATermAppl gstcReadIn (ATermAppl);
 static ATermAppl gstcCheck (ATermAppl);
 static ATermAppl gstcTransform (ATermAppl);
@@ -27,6 +38,8 @@ gsTypeCheck (ATermAppl input)
 	ATermAppl Result = input;
 	gsVerboseMsg ("type checking fase started\n");
 
+	gstcContextDataInit();
+	
 	Result = gstcReadIn(Result);
     if (Result == NULL) {
       throw;
@@ -50,15 +63,42 @@ finally:
     } else {
       gsDebugMsg("return NULL\n");
     }
+	gstcContextDataDestroy();
     return Result;
 }
 
 // Static functions
+void gstcContextDataInit(void){
+	ATprotect((ATerm* )&context.basic_sorts);
+    ATprotect((ATerm* )&context.defined_sorts);	
+    ATprotect((ATerm* )&context.constructors);
+    ATprotect((ATerm* )&context.mappings);
+    ATprotect((ATerm* )&context.actions);	
+    ATprotect((ATerm* )&context.processes);
+	
+	context.basic_sorts=ATindexedSetCreate(63,50);
+    context.defined_sorts=ATtableCreate(63,50);
+    context.constructors=ATtableCreate(63,50);
+    context.mappings=ATtableCreate(63,50);
+    context.actions=ATtableCreate(63,50);
+    context.processes=ATtableCreate(63,50);
+}
+
+void gstcContextDataDestroy(void){
+	ATindexedSetDestroy(context.basic_sorts);
+    ATtableDestroy(context.defined_sorts);
+    ATtableDestroy(context.constructors);
+    ATtableDestroy(context.mappings);
+    ATtableDestroy(context.actions);
+    ATtableDestroy(context.processes);
+}
+
 static ATermAppl
 gstcReadIn (ATermAppl input)
 {
 	ATermAppl Result = input;
 	gsDebugMsg ("type checking read-in fase started\n");
+	
 	
 	
 	gsDebugMsg ("type checking read-in fase finished\n");
