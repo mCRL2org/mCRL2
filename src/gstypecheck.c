333d47c88d4a40f3e8f0d386614ce177eb63ac13
Yaroslav Usenko <y.s.usenko@tue.nl>
2005-02-18 16:28:41 +0000

    More read-in.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@166 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/gstypecheck.c
+++ src/gstypecheck.c
@@ -17,8 +17,8 @@ extern "C"
 typedef struct { 
      ATermIndexedSet basic_sorts;	
      ATermTable defined_sorts;		//name -> sort expression
-     ATermTable constructors;		//name -> Set(sort expression)
-     ATermTable mappings;		    //name -> Set(sort expression)
+     ATermTable constants;			//name -> Set(sort expression)
+     ATermTable functions;		    //name -> Set(sort expression)
      ATermTable actions;	        //name -> Set(List(sort expression)) because of action polymorphism
      ATermTable processes;	        //name -> List(sort expression) process names are unique.
 } Context;
@@ -30,6 +30,8 @@ void gstcContextDataDestroy(void);
 static ATermAppl gstcReadIn (ATermAppl);
 static ATermAppl gstcCheck (ATermAppl);
 static ATermAppl gstcTransform (ATermAppl);
+static inline ATbool gstcInTypes(ATermAppl, ATermList);
+static ATbool gstcEqTypes(ATermAppl, ATermAppl);
 
 // Main function
 ATermAppl
@@ -71,15 +73,15 @@ finally:
 void gstcContextDataInit(void){
 	ATprotect((ATerm* )&context.basic_sorts);
     ATprotect((ATerm* )&context.defined_sorts);	
-    ATprotect((ATerm* )&context.constructors);
-    ATprotect((ATerm* )&context.mappings);
+    ATprotect((ATerm* )&context.constants);
+    ATprotect((ATerm* )&context.functions);
     ATprotect((ATerm* )&context.actions);	
     ATprotect((ATerm* )&context.processes);
 	
 	context.basic_sorts=ATindexedSetCreate(63,50);
     context.defined_sorts=ATtableCreate(63,50);
-    context.constructors=ATtableCreate(63,50);
-    context.mappings=ATtableCreate(63,50);
+    context.constants=ATtableCreate(63,50);
+    context.functions=ATtableCreate(63,50);
     context.actions=ATtableCreate(63,50);
     context.processes=ATtableCreate(63,50);
 }
@@ -87,20 +89,68 @@ void gstcContextDataInit(void){
 void gstcContextDataDestroy(void){
 	ATindexedSetDestroy(context.basic_sorts);
     ATtableDestroy(context.defined_sorts);
-    ATtableDestroy(context.constructors);
-    ATtableDestroy(context.mappings);
+    ATtableDestroy(context.constants);
+    ATtableDestroy(context.functions);
     ATtableDestroy(context.actions);
     ATtableDestroy(context.processes);
 }
 
 static ATermAppl
-gstcReadIn (ATermAppl input)
-{
+gstcReadIn (ATermAppl input){	
+	ATbool new;
 	ATermAppl Result = input;
 	gsDebugMsg ("type checking read-in fase started\n");
 	
+	// Read-in sorts
+	ATermList Sorts = ATLgetArgument(ATAgetArgument(Result,0),0);
+	for(;!ATisEmpty(Sorts);Sorts=ATgetNext(Sorts)){
+		ATermAppl Sort=ATAgetFirst(Sorts);
+		ATermAppl SortName=ATAgetArgument(Sort,0);
+		if(ATindexedSetGetIndex(context.basic_sorts, (ATerm)SortName)>=0 
+			|| ATAtableGet(context.defined_sorts, (ATerm)SortName)!=NULL){
+				ThrowM("Double declaration of sort %t\n", SortName);
+		}				
+		if(gsIsSortId(Sort)) ATindexedSetPut(context.basic_sorts, (ATerm)SortName, &new);
+		else
+		if(gsIsSortRef(Sort)) ATtablePut(context.defined_sorts, (ATerm)SortName, (ATerm)ATAgetArgument(Sort,1));
+	}
 	
+	// Check soorts for loops
+	// Unwind sorts to enable equiv and subtype relations
 	
+	//gsWarningMsg("Basic: %t\n Defined: %t\n", ATindexedSetElements(context.basic_sorts), ATtableKeys(context.defined_sorts));
+
+    // Read-in functions and constants
+	ATermList Funcs = ATconcat(ATLgetArgument(ATAgetArgument(Result,1),0),ATLgetArgument(ATAgetArgument(Result,2),0));
+	for(;!ATisEmpty(Funcs);Funcs=ATgetNext(Funcs)){
+		ATermAppl Func=ATAgetFirst(Funcs);
+		ATermAppl FuncName=ATAgetArgument(Func,0);
+		ATermAppl FuncType=ATAgetArgument(Func,1);
+		bool is_constant=!gsIsSortArrowProd(FuncType);
+		
+		if(ATAtableGet(context.constants, (ATerm)FuncName)!=NULL){
+			ThrowM("Double declaration of constant %t\n", FuncName);
+		}
+		ATermList Types=ATLtableGet(context.functions, (ATerm)FuncName);
+		if(is_constant && Types!=NULL){
+			ThrowM("Double declaration of constant %t\n", FuncName);
+		}
+		if(is_constant){
+			ATtablePut(context.constants, (ATerm)FuncName, (ATerm)FuncType);
+		}
+		else{
+			// the table context.functions contains a list of types for each
+			// function name. We need to check if there is already such a type 
+			// in the list. If so -- error, otherwise -- add
+			if (Types!=NULL && gstcInTypes(FuncType, Types)){
+				ThrowM("Double declaration of constant %t\n", FuncName);
+			}
+			else{
+				Types=(Types==NULL)?ATmakeList1((ATerm)FuncType):ATinsert(Types,(ATerm)FuncType);
+				ATtablePut(context.functions, (ATerm)FuncName, (ATerm)Types);
+			}
+		}	
+	}	
 	gsDebugMsg ("type checking read-in fase finished\n");
 finally:
     if (Result != NULL) {
@@ -116,7 +166,6 @@ gstcCheck (ATermAppl input)
 {
 	ATermAppl Result = input;
 	gsDebugMsg ("type checking check fase started\n");
-
 
 	gsDebugMsg ("type checking check fase finished\n");
 finally:
@@ -143,4 +192,14 @@ finally:
       gsDebugMsg("return NULL\n");
     }
     return Result;
+}
+
+static inline ATbool gstcInTypes(ATermAppl Type, ATermList Types){
+	for(;!ATisEmpty(Types);Types=ATgetNext(Types))
+    	if(gstcEqTypes(ATAgetFirst(Types),Type)) return ATtrue;
+	return ATfalse;
+}
+
+static ATbool gstcEqTypes(ATermAppl Type1, ATermAppl Type2){
+	return ATfalse;
 }
