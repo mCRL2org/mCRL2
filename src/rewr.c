75bf3171f34230437a7bd67be0b7f3bde97dd86e
Muck van Weerdenburg <m.j.van.weerdenburg@tue.nl>
2005-04-06 13:25:31 +0000

    - Added support for output parameter - Added options --help and --write-aterm - Changed ad hoc implementation for something decent
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@275 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/rewr.c
+++ src/rewr.c
@@ -17,42 +17,154 @@ static bool gsIsLPESpec(ATermAppl Spec)
 	return gsIsLPE(ATAgetArgument(Spec,5));
 }
 
+static ATermAppl rewrite_proc(ATermAppl p)
+{
+	ATermList l,m;
+
+	if ( gsIsAction(p) || gsIsProcess(p) )
+	{
+		return ATmakeAppl2(ATgetAFun(p),ATgetArgument(p,0),(ATerm) gsRewriteTerms(ATLgetArgument(p,1)));
+	} else if ( gsIsAtTime(p) )
+	{
+		return ATmakeAppl2(ATgetAFun(p),ATgetArgument(p,0),(ATerm) gsRewriteTerm(ATAgetArgument(p,1)));
+	} else if ( gsIsSum(p) )
+	{
+		return ATmakeAppl2(ATgetAFun(p),ATgetArgument(p,0),(ATerm) rewrite_proc(ATAgetArgument(p,1)));
+	} else if ( gsIsCond(p) )
+	{
+		return gsMakeCond(gsRewriteTerm(ATAgetArgument(p,0)),rewrite_proc(ATAgetArgument(p,1)),rewrite_proc(ATAgetArgument(p,2)));
+	} else {
+		l = ATgetArguments(p);
+		m = ATmakeList0();
+		for (; !ATisEmpty(l); l=ATgetNext(l))
+		{
+			m = ATinsert(m,(ATerm) rewrite_proc(ATAgetFirst(l)));
+		}
+		return ATmakeApplList(ATgetAFun(p),ATreverse(m));
+	}
+}
+
 static ATermAppl rewrite_nolpe(ATermAppl Spec)
 {
-	Spec = ATsetArgument(Spec,(ATerm) gsRewriteTerm(ATAgetArgument(Spec,5)),5);
-	Spec = ATsetArgument(Spec,(ATerm) gsRewriteTerm(ATAgetArgument(Spec,6)),6);
+	ATermAppl a;
+	ATermList l,m;
+
+	l = ATLgetArgument(ATAgetArgument(Spec,5),0);
+	m = ATmakeList0();
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		a = ATAgetArgument(ATAgetFirst(l),3);
+		a = rewrite_proc(a);
+		m = ATinsert(m,(ATerm) ATsetArgument(ATAgetFirst(l),(ATerm) a,3));
+	}
+	m = ATreverse(m);
+	Spec = ATsetArgument(Spec,(ATerm) gsMakeProcEqnSpec(m),5);
+
+	a = ATAgetArgument(ATAgetArgument(Spec,6),1);
+	a = rewrite_proc(a);
+	Spec = ATsetArgument(Spec,(ATerm) gsMakeInit(ATLgetArgument(ATAgetArgument(Spec,6),0),a),6);
 
 	return Spec;
 }
 
 static ATermAppl rewrite_lpe(ATermAppl Spec)
 {
-	return rewrite_nolpe(Spec);
+	ATermAppl a,b,c;
+	ATermList l,m,n,o,d;
+
+	l = ATLgetArgument(ATAgetArgument(Spec,5),2);
+	m = ATmakeList0();
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		a = ATAgetArgument(ATAgetFirst(l),1);
+		a = rewrite_proc(a);
+		b = ATAgetArgument(ATAgetFirst(l),2);
+		if ( gsIsMultAct(b) )
+		{
+			n = ATLgetArgument(b,0);
+			o = ATmakeList0();
+			for (; !ATisEmpty(n); n=ATgetNext(n))
+			{
+				o = ATinsert(o,(ATerm) rewrite_proc(ATAgetFirst(n)));
+			}
+			b = gsMakeMultAct(ATreverse(o));
+		}
+		c = ATAgetArgument(ATAgetFirst(l),3);
+		if ( !gsIsNil(c) )
+		{
+			c = rewrite_proc(c);
+		}
+		d = ATLgetArgument(ATAgetFirst(l),4);
+		n = ATmakeList0();
+		for (; !ATisEmpty(d); d=ATgetNext(d))
+		{
+			n = ATinsert(n,(ATerm) gsMakeAssignment(ATAgetArgument(ATAgetFirst(d),0),gsRewriteTerm(ATAgetArgument(ATAgetFirst(d),1))));
+		}
+		d = ATreverse(n);
+		m = ATinsert(m,(ATerm) gsMakeLPESummand(ATLgetArgument(ATAgetFirst(l),0),a,b,c,d));
+	}
+	m = ATreverse(m);
+	Spec = ATsetArgument(Spec,(ATerm) ATsetArgument(ATAgetArgument(Spec,5),(ATerm) m,2),5);
+
+	l = ATLgetArgument(ATAgetArgument(Spec,6),1);
+	m = ATmakeList0();
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		m = ATinsert(m,(ATerm) gsMakeAssignment(ATAgetArgument(ATAgetFirst(l),0),gsRewriteTerm(ATAgetArgument(ATAgetFirst(l),1))));
+	}
+	Spec = ATsetArgument(Spec,(ATerm) gsMakeLPEInit(ATLgetArgument(ATAgetArgument(Spec,6),0),ATreverse(m)),6);
+
+	return Spec;
+}
+
+void print_help(FILE *f)
+{
+	fprintf(f,"Usage: %s OPTIONS [SPECFILE [OUTFILE]]\n",NAME);
+	fprintf(f,"Rewrite data expressions in SPECFILE and save the result to\n"
+	          "OUTFILE. If OUTFILE is not present, stdout is used. If\n"
+	          "SPECFILE is not present, stdin is used. To use stdin and\n"
+	          "save the output into a file, use '-' for SPECFILE.\n"
+	          "\n"
+	          "The OPTIONS that can be used are:\n"
+	          "-h, --help               display this help message\n"
+	          "-r, --read-aterm         SPECFILE is an ATerm\n"
+	          "-w, --write-aterm        OUTFILE should be an ATerm\n"
+	          "-i, --inner              Use innermost rewriter (default)\n"
+	       );
 }
 
 int main(int argc, char **argv)
 {
-	FILE *SpecStream;
+	FILE *SpecStream, *OutStream;
 	ATerm stackbot;
 	ATermAppl Spec;
-	#define sopts "ai"
+	#define sopts "hawi"
 	struct option lopts[] = {
+		{ "help",		no_argument,	NULL,	'h' },
 		{ "read-aterm",		no_argument,	NULL,	'a' },
+		{ "write-aterm",	no_argument,	NULL,	'w' },
 		{ "inner",		no_argument,	NULL,	'i' },
 		{ 0, 0, 0, 0 }
 	};
-	int opt,read_aterm,strat;
+	int opt,read_aterm,write_aterm,strat;
 
 	ATinit(argc,argv,&stackbot);
 
 	read_aterm = 0;
+	write_aterm = 0;
 	strat = GS_REWR_INNER;
 	while ( (opt = getopt_long(argc,argv,sopts,lopts,NULL)) != -1 )
 	{
 		switch ( opt )
 		{
+			case 'h':
+				print_help(stderr);
+				return 0;
 			case 'a':
 				read_aterm = 1;
+				break;
+			case 'w':
+				write_aterm = 1;
 				break;
 			case 'i':
 				strat = GS_REWR_INNER;
@@ -62,17 +174,26 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if ( argc-optind < 1 )
+	if ( optind < argc && strcmp(argv[optind],"-") )
 	{
-/*		fprintf(stderr,"%s: no input file supplied\n",NAME);
-		return 1;*/
-		SpecStream = stdin;
-	} else {
 		if ( (SpecStream = fopen(argv[optind],"r")) == NULL )
 		{
 			perror(NAME);
 			return 1;
 		}
+	} else {
+		SpecStream = stdin;
+	}
+
+	if ( optind+1 < argc )
+	{
+		if ( (OutStream = fopen(argv[optind+1],"w")) == NULL )
+		{
+			perror(NAME);
+			return 1;
+		}
+	} else {
+		OutStream = stdout;
 	}
 
 	if ( read_aterm )
@@ -93,5 +214,10 @@ int main(int argc, char **argv)
 		Spec = rewrite_nolpe(Spec);
 	}
 
-	gsPrintSpecification(stdout,Spec);
+	if ( write_aterm )
+	{
+		ATwriteToTextFile((ATerm) Spec,OutStream);
+	} else {
+		gsPrintSpecification(OutStream,Spec);
+	}
 }
