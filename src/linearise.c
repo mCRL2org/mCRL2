57060d936aa860cc8ce67aa9c98341968cceb5ad
Aad Mathijssen <a.h.j.mathijssen@tue.nl>
2005-04-24 13:47:23 +0000

    Reduced the number of warnings for the lineariser that are generated by more recent versions of gcc: in order to avoid type punned pointer warnings, calls to ATprotects((ATerm)* t) are replaced by calls to ATprotectAppl(t) or ATprotectList(t). These functions are defined in gslowlevel.h or in the ATerm library.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@287 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/linearise.c
+++ src/linearise.c
@@ -16,6 +16,7 @@
 #define MAIN
 #include "linearise.h"
 #include "libgsparse.h"
+#include "gslowlevel.h"
 #include "gstypecheck.h"
 #include "gsdataimpl.h"
 #include "libgsrewrite.h"
@@ -906,12 +907,12 @@ static void initialize_symbols(void)
   ATprotectAFun(tuple_symbol);
   initprocspec_symbol=ATmakeAFun("initprocspec",3,ATfalse);
   ATprotectAFun(initprocspec_symbol);
-  ATprotect((ATerm *)&terminationAction);
+  ATprotectAppl(terminationAction);
   terminationAction=gsMakeMultAct(
              ATinsertA(
                ATempty,
                gsMakeAction(gsMakeActId(fresh_name("Terminate"),ATempty),ATempty)));
-  ATprotect((ATerm *)&terminatedProcId);
+  ATprotectAppl(terminatedProcId);
   terminatedProcId=gsMakeProcVarId(gsString2ATermAppl("Terminated**"),ATempty);
   insertProcDeclaration(
            terminatedProcId,
@@ -6546,7 +6547,7 @@ static ATermAppl generateLPEmCRL(
 
 static void initialize_data(void)
 { 
-  ATprotect((ATerm *)&pcrlprocesses);
+  ATprotectList(pcrlprocesses);
   pcrlprocesses=ATempty;
   gsEnableConstructorFunctions();
   objectIndexTable=ATindexedSetCreate(1024,75);
@@ -6557,8 +6558,8 @@ static void initialize_data(void)
   enumeratedtypes=NULL;
   enumeratedtypelist=NULL;
   stacklist=NULL;
-  ATprotect((ATerm *)&sumlist);
-  ATprotect((ATerm *)&localequationvariables);
+  ATprotectList(sumlist);
+  ATprotectList(localequationvariables);
   
 }
 
@@ -7130,7 +7131,7 @@ static int main2(int argc, char *argv[],ATerm *stack_bottom)
     ATerror("Option -newstate can only be used with -regular or -regular2\n");
 
   ATinit(argc,argv,stack_bottom);
-  ATprotect((ATerm *)&result);
+  ATprotectAppl(result);
   if (((argc < 2)||(sname==NULL)))
       usage();
   initialize_data();
