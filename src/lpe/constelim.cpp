a732ef0344e63ebfcd9f0c2400b7e60a68e732fa
Frank Stappers <f.p.m.stappers@tue.nl>
2005-09-14 09:30:52 +0000

    Added file "constelim.cpp"
    Framework only
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@506 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ src/lpe/constelim.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include "atermpp/aterm.h"
+#include "mcrl2/mcrl2_visitor.h"
+#include "mcrl2/lpe.h"
+#include <boost/program_options.hpp>
+
+using namespace std;
+using namespace mcrl2;
+using namespace atermpp;
+
+
+namespace po = boost::program_options;
+po::variables_map vm;
+
+//Constanten
+string version = "Version 0.1";
+
+
+int const_main(string filename, int opt)
+{
+  aterm_appl t = read_from_named_file(filename).to_appl();
+  if (!t)
+    cerr << "could not read file!" << endl;
+  return 0;
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<T>(cout, " ")); 
+    return os;
+}
+
+int main(int ac, char* av[])
+{
+      string filename;
+      int opt = 0;
+
+      try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help,h", "produce help message")
+            ("version,v", "get the version number of the current release of the mCRL2 tools")
+        ;
+	
+	po::options_description hidden("Hidden options");
+	hidden.add_options()
+	    ("input-file", po::value<string>(), "input file" )
+	;
+	
+	po::options_description cmdline_options;
+	cmdline_options.add(desc).add(hidden);
+	
+	po::options_description visible("Allowed options");
+	visible.add(desc);
+	
+	po::positional_options_description p;
+	p.add("input-file", -1);
+	
+	po::variables_map vm;
+        po::store(po::command_line_parser(ac, av).
+                  options(cmdline_options).positional(p).run(), vm);
+        po::notify(vm);
+        
+	// If no arguments are ac==1, so print help
+        if (vm.count("help") || ac == 1) {
+            cout << "Usage: constelim [options] input-file\n";
+            cout << desc;
+            return 0;
+        }
+        
+        if (vm.count("version")) {
+	    cout << version << endl;
+	    return 0;
+	}
+
+        if (vm.count("input-file"))
+        {
+          filename = vm["input-file"].as<string>();
+	  cout << filename << endl;
+	}
+
+        const_main(filename, opt);       
+	
+    }
+    catch(exception& e)
+    {
+        cout << e.what() << "\n";
+        return 1;
+    }    
+    return 0;
+}
