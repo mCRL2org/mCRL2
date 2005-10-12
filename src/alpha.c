51db731a3d5dadb7039431f49a2b6122098060cc
Yaroslav Usenko <y.s.usenko@tue.nl>
2005-10-12 17:42:57 +0000

    Added -q, etc. options.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@657 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/alpha.c
+++ src/alpha.c
@@ -2,11 +2,14 @@
 extern "C" {
 #endif
 
-#define NAME "alpha"
+#define NAME      "alpha"
+#define LVERSION  "0.1"
+#define AUTHOR    "Yaroslav S. Usenko and Muck van Weerdenburg"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <getopt.h>
 #include <aterm2.h>
 #include "gslowlevel.h"
@@ -18,31 +21,50 @@ extern "C" {
 extern ATermAppl gsParse(FILE *);
 extern ATermAppl gsTypeCheck(ATermAppl);
 
-void print_help(FILE *f, char *Name)
+void PrintUsage(FILE *f, char *Name)
 {
 	fprintf(f,"Usage: %s OPTIONS [SPECFILE [OUTFILE]]\n",Name);
-	fprintf(f,"Apply alphabet axioms to processes in SPECFILE and save the result to OUTFILE.\n"
-		  "If OUTFILE is not present, stdout is used. If SPECFILE is not present, stdin is\n"
-		  "used. To use stdin and save the output into a file, use '-' for SPECFILE.\n"
-	          "\n"
-	          "The OPTIONS that can be used are:\n"
-	          "  -h, --help             display this help message\n"
-	          "  -a, --read-aterm       SPECFILE is an ATerm\n"
-	          "  -w, --write-aterm      OUTFILE should be an ATerm\n"
+        fprintf(f,"Apply alphabet axioms to processes in SPECFILE and save the result to OUTFILE.\n"
+                  "If OUTFILE is not present, stdout is used. If SPECFILE is not present, stdin is\n"
+                  "used. To use stdin and save the output into a file, use '-' for SPECFILE.\n"
+                  "\n"
+                  "The OPTIONS that can be used are:\n"
+		  "-a, --read-aterm         SPECFILE is an ATerm\n"
+		  "-w, --write-aterm        OUTFILE should be an ATerm\n"
+		  "-h, --help             display this help\n"
+		  "    --version          display version information\n"
+		  "-q, --quiet            do not display warning messages\n"
+		  "-v, --verbose          turn on the display of short intermediate messages\n"
+		  "-d, --debug            turn on the display of detailed intermediate messages\n"
 	       );
 }
+
+void PrintMoreInfo(FILE *Stream, char *Name) {
+  fprintf(Stream, "Try \'%s --help\' for more information.\n", Name);
+}
+
+void PrintVersion(FILE *Stream) {
+  fprintf(Stream, "%s %s\nWritten by %s.\n",
+    NAME, LVERSION, AUTHOR);
+}
+
 
 int main(int argc, char **argv)
 {
 	FILE *SpecStream, *OutStream;
 	ATerm stackbot;
 	ATermAppl Spec;
-	#define sopts "haw"
+        #define sopts      "aw:hqvd"
+        #define VersionOption     CHAR_MAX + 1
 	struct option lopts[] = {
-		{ "help",		no_argument,	NULL,	'h' },
-		{ "read-aterm",		no_argument,	NULL,	'a' },
-		{ "write-aterm",	no_argument,	NULL,	'w' },
-		{ 0, 0, 0, 0 }
+	  { "read-aterm" , no_argument,	      NULL, 'a' },
+	  { "write-aterm", no_argument,	      NULL, 'w' },
+	  { "help"       , no_argument,       NULL, 'h'},
+	  { "version"    , no_argument,       NULL, VersionOption},
+	  { "quiet"      , no_argument,       NULL, 'q'},
+	  { "verbose"    , no_argument,       NULL, 'v'},
+	  { "debug"      , no_argument,       NULL, 'd'},
+	  { 0, 0, 0, 0 }
 	};
 	int opt,read_aterm,write_aterm;
 
@@ -51,22 +73,34 @@ int main(int argc, char **argv)
 	read_aterm = 0;
 	write_aterm = 0;
 	while ( (opt = getopt_long(argc,argv,sopts,lopts,NULL)) != -1 )
-	{
-		switch ( opt )
-		{
-			case 'h':
-				print_help(stderr, argv[0]);
-				return 0;
-			case 'a':
-				read_aterm = 1;
-				break;
-			case 'w':
-				write_aterm = 1;
-				break;
-			default:
-				break;
-		}
-	}
+	  {
+	    switch ( opt )
+	      {
+	      case 'a':
+		read_aterm = 1;
+		break;
+	      case 'w':
+		write_aterm = 1;
+		break;
+	      case 'h':
+		PrintUsage(stdout, argv[0]);
+		return 0;
+	      case VersionOption:
+		PrintVersion(stdout);
+		return 0;
+	      case 'q':
+		gsSetQuietMsg();
+		break;
+	      case 'v':
+		gsSetVerboseMsg();
+		break;
+	      case 'd':
+		gsSetDebugMsg();
+		break;
+	      default:
+		break;
+	      }
+	  }
 
 	SpecStream = stdin;
 	if ( optind < argc && strcmp(argv[optind],"-") )
