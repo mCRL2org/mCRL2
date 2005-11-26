c2821edb6327c76ced00b0c0ee78d18dcb2d849e
Frank Stappers <f.p.m.stappers@tue.nl>
2005-11-26 14:40:34 +0000

    Removed bug from framework: member name from action class displays correctly the name of an action.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@880 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/lpe/action.h
+++ src/lpe/action.h
@@ -47,7 +47,7 @@ class action: public aterm_wrapper
     ///
     std::string name() const
     {
-      return m_name.to_string();
+      return unquote(aterm_appl(ATAgetArgument(m_name,0)).to_string());
     }
 
     /// Returns the sequence of arguments.
