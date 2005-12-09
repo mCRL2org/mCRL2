f89952cd7c74e02e6d88b5448991cefdf4611220
Muck van Weerdenburg <m.j.van.weerdenburg@tue.nl>
2005-12-09 13:45:15 +0000

    Apparently did not add some files.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@949 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ src/nextstate_standard.cpp
@@ -0,0 +1,637 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <aterm2.h>
+#include "liblowlevel.h"
+#include "libstruct.h"
+#include "libprint_c.h"
+#include "libenum.h"
+#include "librewrite.h"
+#include "nextstate_standard.h"
+
+#define ATAgetFirst(x) ((ATermAppl) ATgetFirst(x))
+#define ATLgetFirst(x) ((ATermList) ATgetFirst(x))
+#define ATAgetArgument(x,y) ((ATermAppl) ATgetArgument(x,y))
+#define ATLgetArgument(x,y) ((ATermList) ATgetArgument(x,y))
+
+#define ATisList(x) (ATgetType(x) == AT_LIST)
+
+
+static void fill_tree_init(bool *init, int n, int l)
+{
+	if ( l == 0 )
+		return;
+
+	if ( n > 2 )
+	{
+		fill_tree_init(init,n/2,l/2);
+		fill_tree_init(init+n/2,n-n/2,l-l/2);
+	} else /* n == 2 */ {
+		init[1] = true;
+	}
+}
+ATerm NextStateStandard::buildTree(ATerm *args)
+{
+	int n,m;
+
+	if ( info.statelen == 0 )
+		return (ATerm) info.nil;
+
+	if ( tree_init == NULL )
+	{
+		tree_init = (bool *) malloc(info.statelen*sizeof(bool));
+		for (int i=0; i<info.statelen; i++)
+			tree_init[i] = false;
+		n = 1;
+		while ( n < info.statelen )
+		{
+			n *= 2;
+		}
+		n /= 2;
+		fill_tree_init(tree_init,info.statelen,info.statelen-n);
+	}
+
+	n = 0;
+	m = 0;
+	while ( n < info.statelen )
+	{
+		if ( tree_init[n] )
+		{
+			args[m-1] = (ATerm) ATmakeAppl2(info.pairAFun,args[m-1],args[n]);
+		} else {
+			args[m] = args[n];
+			m++;
+		}
+		n++;
+	}
+
+	n = m;
+	while ( n > 1 )
+	{
+		for (int i=0; i<n; i+=2)
+		{
+			args[i/2] = (ATerm) ATmakeAppl2(info.pairAFun,args[i],args[i+1]);
+		}
+		
+		n /= 2;
+	}
+	
+	return args[0];
+}
+
+ATerm NextStateStandard::getTreeElement(ATerm tree, int index)
+{
+	int n = info.statelen;
+	int m = 0;
+	int t = n/2;
+
+	while ( 1 )
+	{
+		if ( index < t )
+		{
+			tree = ATgetArgument((ATermAppl) tree,0);
+			n = t;
+		} else {
+			tree = ATgetArgument((ATermAppl) tree,1);
+			m = t;
+		}
+
+		if ( (t == 1) || (t == index) )
+			break;
+
+		t = (n+m)/2;
+	}
+	while ( (ATgetType(tree) == AT_APPL) && ATisEqualAFun(ATgetAFun((ATermAppl) tree),info.pairAFun) )
+	{
+		tree = ATgetArgument((ATermAppl) tree,0);
+	}
+	return tree;
+}
+
+int NextStateStandard::getStateLength()
+{
+	return info.statelen;
+}
+
+ATermAppl NextStateStandard::getStateArgument(ATerm state, int index)
+{
+	switch ( info.stateformat )
+	{
+		case GS_STATE_VECTOR:
+			return info.rewr_obj->fromRewriteFormat(ATgetArgument((ATermAppl) state,index));
+		case GS_STATE_TREE:
+			return info.rewr_obj->fromRewriteFormat(getTreeElement(state,index));
+		default:
+			return NULL;
+	}
+}
+
+ATermAppl NextStateStandard::makeStateVector(ATerm state)
+{
+	// XXX can be done more efficiently in some cases
+	for (int i=0; i<info.statelen; i++)
+	{
+		stateargs[i] = (ATerm) getStateArgument(state,i);
+	}
+	return ATmakeApplArray(info.stateAFun,stateargs);
+}
+
+ATermAppl NextStateStandard::FindDummy(ATermAppl sort)
+{
+	ATermList l;
+
+	if ( gsIsSortArrow(sort) )
+	{
+		gsErrorMsg("cannot generate dummies for function sorts (%T)\n",sort);
+		exit(1);
+	}
+
+	l = ATLgetArgument(ATAgetArgument(current_spec,1),0);
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		ATermAppl conssort = ATAgetArgument(ATAgetFirst(l),1);
+		if ( ATisEqual(gsGetSortExprResult(conssort),sort) )
+		{
+			ATermList domain = gsGetSortExprDomain(conssort);
+			ATermAppl t = ATAgetFirst(l);
+
+			if ( ATindexOf(domain,(ATerm) sort,0) == -1)
+			{
+				for (; !ATisEmpty(domain); domain=ATgetNext(domain))
+				{
+					t = gsMakeDataAppl(t,FindDummy(ATAgetFirst(domain)));
+				}
+
+				return t;
+			}
+		}
+	}
+
+	l = ATLgetArgument(ATAgetArgument(current_spec,2),0);
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		ATermAppl mapsort = ATAgetArgument(ATAgetFirst(l),1);
+		if ( ATisEqual(gsGetSortExprResult(mapsort),sort) )
+		{
+			ATermList domain = gsGetSortExprDomain(mapsort);
+			ATermAppl t = ATAgetFirst(l);
+
+			for (; !ATisEmpty(domain); domain=ATgetNext(domain))
+			{
+				t = gsMakeDataAppl(t,FindDummy(ATAgetFirst(domain)));
+			}
+
+			return t;
+		}
+	}
+
+	gsfprintf(stderr,"Could not find dummy of type %T\n",sort);
+	exit(1);
+}
+
+ATerm NextStateStandard::SetVars(ATerm a, ATermList free_vars)
+{
+	ATermList l,m;
+
+	if ( !usedummies )
+	{
+		return a;
+	}
+
+	if ( ATisList(a) )
+	{
+		l = (ATermList) a;
+		m = ATmakeList0();
+		for (; !ATisEmpty(l); l=ATgetNext(l))
+		{
+			m = ATinsert(m,SetVars(ATgetFirst(l),free_vars));
+		}
+		return (ATerm) ATreverse(m);
+	} else if ( gsIsDataVarId((ATermAppl) a) )
+	{
+		if ( ATindexOf(free_vars,a,0) >= 0 )
+		{
+			return (ATerm) FindDummy(ATAgetArgument((ATermAppl) a,1));
+		} else {
+			return a;
+		}
+	} else if ( gsIsDataAppl((ATermAppl) a) )
+	{
+		return (ATerm) gsMakeDataAppl((ATermAppl) SetVars(ATgetArgument((ATermAppl) a,0),free_vars),(ATermAppl) SetVars(ATgetArgument((ATermAppl) a,1),free_vars));
+	} else {
+		return a;
+	}
+}
+
+ATermList NextStateStandard::ListToFormat(ATermList l,ATermList free_vars)
+{
+	if ( ATisEmpty(l) )
+	{
+		return l;
+	} else {
+		return ATinsert(ListToFormat(ATgetNext(l),free_vars),info.rewr_obj->toRewriteFormat((ATermAppl) SetVars(ATgetFirst(l),free_vars)));
+	}
+}
+
+#define LISTFROMFORMAT_IMPLEMENTATION \
+	if ( ATisEmpty(l) ) \
+	{ \
+		return l; \
+	} else { \
+		return ATinsert(ListFromFormat(ATgetNext(l)),(ATerm) info.rewr_obj->fromRewriteFormat(ATgetFirst(l))); \
+	}
+ATermList NextStateStandard::ListFromFormat(ATermList l)
+{
+	LISTFROMFORMAT_IMPLEMENTATION
+}
+
+ATermList NextStateGeneratorStandard::ListFromFormat(ATermList l)
+{
+	LISTFROMFORMAT_IMPLEMENTATION
+}
+
+ATermAppl NextStateStandard::ActionToRewriteFormat(ATermAppl act, ATermList free_vars)
+{
+	ATermList l = ATLgetArgument(act,0);
+	ATermList m = ATmakeList0();
+
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		ATermAppl a = ATAgetFirst(l);
+		a = gsMakeAction(ATAgetArgument(a,0),ListToFormat(ATLgetArgument(a,1),free_vars));
+		m = ATinsert(m,(ATerm) a);
+	}
+	m = ATreverse(m);
+
+	return gsMakeMultAct(m);
+}
+
+ATerm NextStateStandard::AssignsToRewriteFormat(ATermList assigns, ATermList free_vars)
+{
+/*	ATermList l = ATmakeList0();
+	for (; !ATisEmpty(assigns); assigns=ATgetNext(assigns))
+	{
+		ATermAppl a = ATAgetFirst(assigns);
+		l = ATinsert(l,(ATerm) ATmakeAppl2(ATgetAFun(a),rewr_obj->toRewriteFormat(ATAgetArgument(a,0)),rewr_obj->toRewriteFormat(ATAgetArgument(a,1))));
+	}
+	return ATreverse(l); */
+
+	int i = 0;
+	for (ATermList l=pars; !ATisEmpty(l); l=ATgetNext(l),i++)
+	{
+		bool set = false;
+
+		for (ATermList m=assigns; !ATisEmpty(m); m=ATgetNext(m))
+		{
+			if ( ATisEqual(ATAgetArgument(ATAgetFirst(m),0),ATAgetFirst(l)) )
+			{
+				stateargs[i] = info.rewr_obj->toRewriteFormat((ATermAppl) SetVars(ATgetArgument(ATAgetFirst(m),1),free_vars));
+				set = true;
+				break;
+			}
+		}
+		if ( !set )
+		{
+			stateargs[i] = (ATerm) gsMakeNil();
+		}
+	}
+
+	return (ATerm) ATmakeApplArray(info.stateAFun,stateargs);
+}
+
+NextStateStandard::NextStateStandard(ATermAppl spec, bool allow_free_vars, int state_format, Enumerator *e, bool clean_up_enumerator, NextStateStrategy strategy)
+{
+	ATermList l,m,n,free_vars;
+
+	info.enum_obj = e;
+	clean_up_enum_obj = clean_up_enumerator;
+	info.rewr_obj = e->getRewriter();
+
+	usedummies = !allow_free_vars;
+
+	tree_init = NULL;
+
+	current_spec = spec;
+	ATprotectAppl(&current_spec);
+
+	info.stateformat = state_format;
+	info.pairAFun = ATmakeAFun("@STATE_PAIR@",2,ATfalse);
+	ATprotectAFun(info.pairAFun);
+
+	info.nil = gsMakeNil();
+	ATprotectAppl(&info.nil);
+
+	free_vars = ATLgetArgument(ATAgetArgument(current_spec,5),0);
+
+	pars = ATLgetArgument(ATAgetArgument(current_spec,5),1);
+	ATprotectList(&pars);
+
+	info.statelen = ATgetLength(pars);
+	info.stateAFun = ATmakeAFun("STATE",info.statelen,ATfalse);
+	ATprotectAFun(info.stateAFun);
+
+	info.procvars = ATLgetArgument(ATAgetArgument(current_spec,5),1);
+	ATprotectList(&info.procvars);
+	
+	stateargs = (ATerm *) malloc(info.statelen*sizeof(ATerm));
+	for (int i=0; i<info.statelen; i++)
+	{
+		stateargs[i] = NULL;
+	}
+	ATprotectArray(stateargs,info.statelen);
+
+	smndAFun = ATmakeAFun("@SMND@",4,ATfalse);
+	ATprotectAFun(smndAFun);
+	ATermList sums = ATLgetArgument(ATAgetArgument(current_spec,5),2);
+	l = ATmakeList0();
+	for (bool b=true; !ATisEmpty(sums); sums=ATgetNext(sums))
+	{
+		if ( b && !gsIsNil(ATAgetArgument(ATAgetFirst(sums),3)) )
+		{
+			fprintf(stderr,"warning: specification uses time, which is (currently) not supported; ignoring timing\n");
+			b = false;
+		}
+		if ( !gsIsDelta(ATAgetArgument(ATAgetFirst(sums),2)) )
+		{
+			l = ATinsert(l,ATgetFirst(sums));
+		}
+	}
+	sums = ATreverse(l);
+	info.num_summands = ATgetLength(sums);
+	info.summands = (ATermAppl *) malloc(info.num_summands*sizeof(ATermAppl));
+	for (int i=0; i<info.num_summands; i++)
+	{
+		info.summands[i] = NULL;
+	}
+	ATprotectArray((ATerm *) info.summands,info.num_summands);
+	for (int i=0; !ATisEmpty(sums); sums=ATgetNext(sums),i++)
+	{
+		info.summands[i] = ATmakeAppl4(smndAFun,ATgetArgument(ATAgetFirst(sums),0),info.rewr_obj->toRewriteFormat((ATermAppl) SetVars(ATgetArgument(ATAgetFirst(sums),1),free_vars)),(ATerm) ActionToRewriteFormat(ATAgetArgument(ATAgetFirst(sums),2),free_vars),(ATerm) AssignsToRewriteFormat(ATLgetArgument(ATAgetFirst(sums),4),free_vars));
+	}
+
+	l = pars;
+	free_vars = ATLgetArgument(ATAgetArgument(current_spec,6),0);
+	m = ATLgetArgument(ATAgetArgument(current_spec,6),1);
+
+	for (int i=0; !ATisEmpty(l); l=ATgetNext(l), i++)
+	{
+		n = m;
+		bool set = false;
+		for (; !ATisEmpty(n); n=ATgetNext(n))
+		{
+			if ( ATisEqual(ATAgetArgument(ATAgetFirst(n),0),ATAgetFirst(l)) )
+			{
+				stateargs[i] = info.rewr_obj->rewriteInternal(info.rewr_obj->toRewriteFormat((ATermAppl) SetVars(ATgetArgument(ATAgetFirst(n),1),free_vars)));
+				set = true;
+				break;
+			}
+		}
+		if ( !set )
+		{
+			gsErrorMsg("Parameter '%T' does not have an initial value.",ATgetArgument(ATAgetFirst(l),0));
+			initial_state = NULL;
+			ATprotect(&initial_state);
+			return;
+		}
+	}
+
+	switch ( info.stateformat )
+	{
+		case GS_STATE_VECTOR:
+			initial_state = (ATerm) ATmakeApplArray(info.stateAFun,stateargs);
+			break;
+		case GS_STATE_TREE:
+			initial_state = (ATerm) buildTree(stateargs);
+			break;
+		default:
+			initial_state = NULL;
+			break;
+	}
+	ATprotect(&initial_state);
+}
+
+NextStateStandard::~NextStateStandard()
+{
+	ATunprotect(&initial_state);
+
+	ATunprotectArray(stateargs);
+
+	ATunprotectAppl(&current_spec);
+	ATunprotectAppl(&info.nil);
+
+	if ( clean_up_enum_obj )
+	{
+		delete info.enum_obj;
+	}
+	
+	ATunprotectAFun(info.pairAFun);
+
+	ATunprotectList(&pars);
+	ATunprotectAFun(info.stateAFun);
+
+	ATunprotectList(&info.procvars);
+	
+	ATunprotectAFun(smndAFun);
+	ATunprotectArray((ATerm *) info.summands);
+	free(info.summands);
+
+	free(tree_init);
+}
+
+ATerm NextStateStandard::getInitialState()
+{
+	return initial_state;
+}
+
+NextStateGenerator *NextStateStandard::getNextStates(ATerm state, NextStateGenerator *old)
+{
+	if ( old == NULL )
+	{
+		return new NextStateGeneratorStandard(state,info);
+	} else {
+		((NextStateGeneratorStandard *) old)->reset(state);
+		return old;
+	}
+}
+
+
+
+
+ATerm NextStateGeneratorStandard::makeNewState(ATerm old, ATerm assigns)
+{
+	ATermList l;
+
+	l = info.procvars;
+	for (int i=0; i<info.statelen; i++)
+	{
+		ATerm a = ATgetArgument((ATermAppl) assigns,i);
+		if ( ATisEqual(a,info.nil) )
+		{
+			switch ( info.stateformat )
+			{
+				case GS_STATE_VECTOR:
+					stateargs[i] = ATgetArgument((ATermAppl) old,i);
+					break;
+				case GS_STATE_TREE:
+//					stateargs[i] = getTreeElement(old,i);
+					stateargs[i] = info.rewr_obj->getSubstitution((ATermAppl) ATgetFirst(l));
+					break;
+			}
+		} else {
+			stateargs[i] = info.rewr_obj->rewriteInternal(a);
+//			stateargs[i] = rewr_obj->rewriteInternal(SetVars(a));
+		}
+		l = ATgetNext(l);
+	}
+	switch ( info.stateformat )
+	{
+		case GS_STATE_VECTOR:
+			return (ATerm) ATmakeApplArray(info.stateAFun,stateargs);
+		case GS_STATE_TREE:
+			return info.parent->buildTree(stateargs);
+		default:
+			return NULL;
+	}
+}
+
+ATermAppl NextStateGeneratorStandard::rewrActionArgs(ATermAppl act)
+{
+	ATermList l = ATLgetArgument(act,0);
+	ATermList m = ATmakeList0();
+
+	for (; !ATisEmpty(l); l=ATgetNext(l))
+	{
+		ATermAppl a = ATAgetFirst(l);
+		a = gsMakeAction(ATAgetArgument(a,0),ListFromFormat(info.rewr_obj->rewriteInternalList(ATLgetArgument(a,1))));
+		m = ATinsert(m,(ATerm) a);
+	}
+	m = ATreverse(m);
+
+	return gsMakeMultAct(m);
+}
+
+void NextStateGeneratorStandard::SetTreeStateVars(ATerm tree, ATermList *vars)
+{
+	if ( (ATgetType(tree) == AT_APPL) && ATisEqualAFun(ATgetAFun((ATermAppl) tree),info.pairAFun) )
+	{
+		SetTreeStateVars(ATgetArgument((ATermAppl) tree,0),vars);
+		SetTreeStateVars(ATgetArgument((ATermAppl) tree,1),vars);
+	} else {
+		info.rewr_obj->setSubstitution((ATermAppl) ATgetFirst(*vars),tree);
+		*vars = ATgetNext(*vars);
+	}
+}
+
+NextStateGeneratorStandard::NextStateGeneratorStandard(ATerm State, ns_info &Info)
+{
+	info = Info;
+
+	cur_state = NULL;
+	cur_act = NULL;
+	cur_nextstate = NULL;
+	ATprotect(&cur_state);
+	ATprotect(&cur_act);
+	ATprotect(&cur_nextstate);
+
+	stateargs = (ATerm *) malloc(info.statelen*sizeof(ATerm));
+	for (int i=0; i<info.statelen; i++)
+	{
+		stateargs[i] = NULL;
+	}
+	ATprotectArray(stateargs,info.statelen);
+
+	sols = NULL;
+
+	reset(State);
+}
+
+NextStateGeneratorStandard::~NextStateGeneratorStandard()
+{
+	delete sols;
+
+	ATunprotectArray(stateargs);
+	free(stateargs);
+
+	ATunprotect(&cur_nextstate);
+	ATunprotect(&cur_act);
+	ATunprotect(&cur_state);
+}
+
+void NextStateGeneratorStandard::reset(ATerm State)
+{
+	error = false;
+
+	ATermList l = info.procvars;
+	switch ( info.stateformat )
+	{
+		case GS_STATE_VECTOR:
+			for (int i=0; !ATisEmpty(l); l=ATgetNext(l),i++)
+			{
+				ATerm a = ATgetArgument((ATermAppl) State,i);
+
+				if ( !ATisEqual(a,info.nil) )
+				{
+					info.rewr_obj->setSubstitution((ATermAppl) ATgetFirst(l),a);
+				}
+			}
+			break;
+		case GS_STATE_TREE:
+			SetTreeStateVars(State,&l);
+			break;
+	}
+
+	cur_state = State;
+
+	if ( info.num_summands == 0 )
+	{
+		sols = info.enum_obj->findSolutions(ATmakeList0(),info.rewr_obj->toRewriteFormat(gsMakeDataExprFalse()),sols);
+	} else {
+		cur_act = ATgetArgument(info.summands[0],2);
+		cur_nextstate = ATgetArgument(info.summands[0],3);
+		sols = info.enum_obj->findSolutions(ATLgetArgument(info.summands[0],0),ATgetArgument(info.summands[0],1),sols);
+	}
+	sum_idx = 1;
+}
+
+bool NextStateGeneratorStandard::next(ATermAppl *Transition, ATerm *State)
+{
+	ATermList sol;
+
+	while ( !sols->next(&sol) && !error && (sum_idx < info.num_summands) )
+	{
+		error |= sols->errorOccurred();
+		if ( !error )
+		{
+			cur_act = ATgetArgument(info.summands[sum_idx],2);
+			cur_nextstate = ATgetArgument(info.summands[sum_idx],3);
+
+			sols = info.enum_obj->findSolutions(ATLgetArgument(info.summands[sum_idx],0),ATgetArgument(info.summands[sum_idx],1),sols);
+		
+			sum_idx++;
+		}
+	}
+	error |= sols->errorOccurred();
+
+	if ( sol != NULL )
+	{
+		for (ATermList m=sol; !ATisEmpty(m); m=ATgetNext(m))
+		{
+			info.rewr_obj->setSubstitution((ATermAppl) ATgetArgument((ATermAppl) ATgetFirst(m),0),ATgetArgument((ATermAppl) ATgetFirst(m),1));
+		}
+		*Transition = rewrActionArgs((ATermAppl) cur_act);
+		*State = (ATerm) makeNewState(cur_state,cur_nextstate);
+		for (ATermList m=sol; !ATisEmpty(m); m=ATgetNext(m))
+		{
+			info.rewr_obj->clearSubstitution((ATermAppl) ATgetArgument((ATermAppl) ATgetFirst(m),0));
+		}
+		return true;
+	} else {
+		*Transition = NULL;
+		*State = NULL;
+		return false;
+	}
+}
+
+bool NextStateGeneratorStandard::errorOccurred()
+{
+	return error;
+}
---
+++ src/nextstate_standard.h
@@ -0,0 +1,97 @@
+#ifndef _NEXTSTATE_STANDARD_H
+#define _NEXTSTATE_STANDARD_H
+
+#include <aterm2.h>
+#include "libnextstate.h"
+#include "libenum.h"
+
+class NextStateStandard;
+
+typedef struct {
+	NextStateStandard *parent;
+	Enumerator *enum_obj;
+	Rewriter *rewr_obj;
+
+	int num_summands;
+	ATermAppl *summands;
+	ATermList procvars;
+	int stateformat;
+	ATermAppl nil;
+	AFun pairAFun;
+	int statelen;
+	AFun stateAFun;
+} ns_info;
+
+class NextStateGeneratorStandard : public NextStateGenerator
+{
+	public:
+		NextStateGeneratorStandard(ATerm State, ns_info &Info);
+		~NextStateGeneratorStandard();
+
+		bool next(ATermAppl *Transition, ATerm *State);
+
+		bool errorOccurred();
+		
+		void reset(ATerm State);
+
+	private:
+		ns_info info;
+
+		bool error;
+
+		int sum_idx;
+
+		ATerm cur_state;
+		ATerm cur_act;
+		ATerm cur_nextstate;
+
+		ATerm *stateargs;
+
+		EnumeratorSolutions *sols;
+
+		void SetTreeStateVars(ATerm tree, ATermList *vars);
+		ATermAppl rewrActionArgs(ATermAppl act);
+		ATerm makeNewState(ATerm old, ATerm assigns);
+		ATermList ListFromFormat(ATermList l);
+};
+
+class NextStateStandard : public NextState
+{
+	friend class NextStateGeneratorStandard;
+	public:
+		NextStateStandard(ATermAppl spec, bool allow_free_vars, int state_format, Enumerator *e, bool clean_up_enumerator, NextStateStrategy strategy = nsStandard);
+		~NextStateStandard();
+
+		ATerm getInitialState();
+		NextStateGenerator *getNextStates(ATerm state, NextStateGenerator *old = NULL);
+
+		int getStateLength();
+		ATermAppl getStateArgument(ATerm state, int index);
+		ATermAppl makeStateVector(ATerm state);
+
+	private:
+		ns_info info;
+
+		bool clean_up_enum_obj;
+		bool usedummies;
+
+		AFun smndAFun;
+		bool *tree_init;
+		ATerm *stateargs;
+
+		ATermAppl current_spec;
+		ATermList pars;
+		ATerm initial_state;
+
+		ATerm buildTree(ATerm *args);
+		ATerm getTreeElement(ATerm tree, int index);
+
+		ATermAppl FindDummy(ATermAppl sort);
+		ATerm SetVars(ATerm a, ATermList free_vars);
+		ATermList ListToFormat(ATermList l,ATermList free_vars);
+		ATermList ListFromFormat(ATermList l);
+		ATermAppl ActionToRewriteFormat(ATermAppl act, ATermList free_vars);
+		ATerm AssignsToRewriteFormat(ATermList assigns, ATermList free_vars);
+};
+
+#endif
