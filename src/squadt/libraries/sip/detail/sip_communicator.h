4dbf372802baef2fe8079c916e0ca1f29e732fcf
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-02-10 15:19:40 +0000

    Introduced a constant for the build directory shared by all Jamfiles. This solves the remaining squadt build problem.
    project-root.jam:
     - cleaned up
    
    *.h:
     - Introduced another namespace to group types (classes) related to
       sip_communicator
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@1124 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/squadt/libraries/sip/detail/sip_communicator.h
+++ src/squadt/libraries/sip/detail/sip_communicator.h
@@ -12,64 +12,66 @@
 
 namespace sip {
 
-  class sip_communicator;
-
-  /* Abstract communicator class that divides an incoming data stream in messages */
-  class sip_communicator : public transport::transporter {
-    public:
-
-      typedef boost::shared_ptr < message > message_ptr;
-
-      sip_communicator();
-
-      /** Queues incoming messages */
-      void deliver(std::istream&);
-
-      /** Queues incoming messages */
-      void deliver(std::string&);
-
-      /* Wait until the next message arrives */
-      void await_message();
-
-      /** Send a message */
-      inline void send_message(message&);
-
-      /** Pops the first message of the queue */
-      inline message pop_message();
-
-      inline size_t number_of_messages();
-
-    protected:
-
-      /** The current message queue */
-      std::deque < message_ptr > message_queue;
-
-      /** Buffer that holds content until a message is complete */
-      std::string      buffer;
-
-      /** For barrier synchronisation (used with function await_message) */
-      boost::barrier   wait_lock;
-
-      bool             using_buffer;
-      bool             using_lock;
-  };
-
-  /* Send a message */
-  inline void sip_communicator::send_message(message& m) {
-    send(m.to_xml());
-  }
-
-  /* A precondition is that the queue is not empty */
-  inline message sip_communicator::pop_message() {
-    message_ptr m = message_queue.front();
-
-    message_queue.pop_front();
-
-    return (*m);
-  }
-
-  inline size_t sip_communicator::number_of_messages() {
-    return (message_queue.size());
+  namespace communicator {
+    class sip_communicator;
+ 
+    /* Abstract communicator class that divides an incoming data stream in messages */
+    class sip_communicator : public transport::transporter {
+      public:
+ 
+        typedef boost::shared_ptr < message > message_ptr;
+ 
+        sip_communicator();
+ 
+        /** Queues incoming messages */
+        void deliver(std::istream&);
+ 
+        /** Queues incoming messages */
+        void deliver(std::string&);
+ 
+        /* Wait until the next message arrives */
+        void await_message();
+ 
+        /** Send a message */
+        inline void send_message(message&);
+ 
+        /** Pops the first message of the queue */
+        inline message pop_message();
+ 
+        inline size_t number_of_messages();
+ 
+      protected:
+ 
+        /** The current message queue */
+        std::deque < message_ptr > message_queue;
+ 
+        /** Buffer that holds content until a message is complete */
+        std::string      buffer;
+ 
+        /** For barrier synchronisation (used with function await_message) */
+        boost::barrier   wait_lock;
+ 
+        bool             using_buffer;
+        bool             using_lock;
+    };
+ 
+    /* Send a message */
+    inline void sip_communicator::send_message(message& m) {
+      send(m.to_xml());
+    }
+ 
+    /* A precondition is that the queue is not empty */
+    inline message sip_communicator::pop_message() {
+      message_ptr m = message_queue.front();
+ 
+      message_queue.pop_front();
+ 
+      return (*m);
+    }
+ 
+    inline size_t sip_communicator::number_of_messages() {
+      return (message_queue.size());
+    }
   }
 }
 
