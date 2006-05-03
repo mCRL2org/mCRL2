8ac0a9f50949058e128d0bad34c48797fa106059
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-05-03 21:35:46 +0000

    Changes:  - adapted maximum_revision such that it can either generate anew or    conditionally update an existing header file that contains the computed    revision number  - ltsgraph is installed properly now  - the tools that depend on mcrl2_revision.h are no longer forcibly rebuild    every time, but only when any of the source or header files actually changes  - building and installing squadt is better integrated.
        That is files are not compiled more than once anymore (due to the use of
        both make and bjam)
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@1455 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- utility/maximum_revision.cpp
+++ utility/maximum_revision.cpp
@@ -1,46 +1,58 @@
-#include <getopt.h>
-#include <string.h>
-#include <iostream>
-#include <fstream>
 #include <cstdio>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include <getopt.h>
 
 #include <boost/filesystem/operations.hpp>
 
-#define PROGRAM_NAME    "maximum SVN revision"
+#define PROGRAM_NAME    "maximum_revision"
 #define PROGRAM_VERSION "1.0.2"
 
 /* Works under the assumption of valid XML as input */
 
 void usage() {
-  std::cout << "Usage: " << PROGRAM_NAME << " [options] [directory]\n";
-  std::cout << std::endl;
-  std::cout << "Where options is a combination of:" << std::endl;
-  std::cout << std::endl;
-  std::cout << "      --cpp-define   output CPP definition\n";
-  std::cout << "  -h, --help         show this information\n";
-  std::cout << "      --version      show program version\n";
-  std::cout << std::endl;
-  std::cout << "Prints the maximum svn revision of any file in <directory>\n";
-  std::cout << "and otherwise current directory, or any of its subdirectories.\n";
-  std::cout << std::endl;
-  std::cout << "Report bugs to J.v.d.Wulp@tue.nl.\n";
+  std::cout << "Usage: " << PROGRAM_NAME << " [options] [directory]\n"
+            << std::endl
+            << " Recursively computes the maximum revision number from any"
+            << " subdirectory of a working copy of a subversion repository."
+            << std::endl
+            << "Where options is a combination of:" << std::endl
+            << std::endl
+            << "      --cpp-define [ name ]   output CPP definition\n"
+            << "      --cpp-file < filename > create or update CPP header file\n"
+            << "  -h, --help                  show this information\n"
+            << "      --version               show program version\n"
+            << std::endl
+            << "Prints the maximum SVN revision of any file in <directory>\n"
+            << "and otherwise current directory, or any of its subdirectories.\n"
+            << std::endl
+            << "Report bugs to J.v.d.Wulp@tue.nl.\n";
 
   exit(0);
 }
 
 namespace bf = boost::filesystem;
 
-bf::path target_directory = bf::current_path();
-
 /* Whether the output is a CPP define or not */
 bool        output_cpp_definition = false;
+bool        output_cpp_file       = false;
 
-/* The output is a CPP define or not */
+/* The name of the CPP macro to define as the maximum revision number */
 std::string cpp_variable_name     = "REVISION";
+
+/* The name of the file to update with the maximum revision number */
+std::string cpp_file_name         = "revision.h";
+
+bf::path    target_directory = bf::current_path();
 
 void process_command_line_options (const int argc, char** argv) {
   static struct option long_options[] = {
     { "cpp-define" , optional_argument, 0, 0   },
+    { "cpp-file"   , required_argument, 0, 0   },
     { "version"    , no_argument,       0, 0   },
     { "help"       , no_argument,       0, 'h' },
     { 0            , 0,                 0, 0   }
@@ -62,6 +74,12 @@ void process_command_line_options (const int argc, char** argv) {
 
               break;
             case 1:
+              output_cpp_file = true;
+
+              cpp_file_name.assign(optarg);
+
+              break;
+            case 2:
               unsigned int svn_revision;
 
               sscanf("$Revision$", "$%*s %u $", &svn_revision);
@@ -96,7 +114,7 @@ unsigned int explore_path(bf::path apath) {
       if (bf::is_directory(*i)) {
         if ((*i).leaf() == ".svn") {
           try {
-            std::fstream file((*i / bf::path("entries")).string().c_str(), std::ios_base::in);
+            std::ifstream file((*i / bf::path("entries")).string().c_str());
 
             while (file) {
               size_t i = 0;
@@ -153,9 +171,48 @@ unsigned int explore_path(bf::path apath) {
   return (return_value);
 }
 
-/*****************************************************************
- * Validation with respect to the XML schema file is assumed
- *****************************************************************/
+/**
+ * Tries to extract the revision number and returns it, otherwise returns 0
+ **/
+unsigned int extract_revision(bf::path& f) {
+  unsigned int revision = 0;
+
+  std::ifstream file(f.string().c_str());
+
+  while (file) {
+    std::string::iterator i = cpp_variable_name.begin();
+
+    char c;
+    
+    file.get(c);
+  
+    while (file && *i != '\0' && c == *i) {
+      ++i;
+  
+      file.get(c);
+    }
+  
+    if (i == cpp_variable_name.end()) {
+      /* We have a match, read the number */
+      revision = 0;
+
+      while (file && !isdigit(c) && !iscntrl(c)) {
+        file.get(c);
+      }
+
+      while (file && isdigit(c)) {
+        revision = revision * 10 + (c - '0');
+
+        file.get(c);
+      }
+    }
+  }
+
+  file.close();
+           
+  return (revision);
+}
+
 int main(int argc, char **argv) {
   process_command_line_options(argc, argv);
 
@@ -167,7 +224,36 @@ int main(int argc, char **argv) {
 
   unsigned int revision = explore_path(target_directory);
 
-  if (output_cpp_definition) {
+  if (output_cpp_file) {
+    bf::path target_file(cpp_file_name);
+
+    if (bf::exists(target_file)) {
+      /* File exists, try to extract the revision number */
+      if (revision <= extract_revision(target_file)) {
+        output_cpp_file = false;
+      }
+    }
+
+    if (output_cpp_file) {
+      /* Create or overwrite file */
+      std::ofstream f(cpp_file_name.c_str(), std::ios::out | std::ios::trunc);
+
+      std::string guard = bf::path(cpp_file_name).leaf();
+
+      std::replace(guard.begin(), guard.end(), '.', '_');
+      std::replace(guard.begin(), guard.end(), '/', '_');
+      std::replace(guard.begin(), guard.end(), '\\', '_');
+      std::transform(guard.begin(), guard.end(), guard.begin(), toupper);
+
+      f << "#ifndef " << guard << std::endl
+        << "# define " << guard << std::endl
+        << "# define " << cpp_variable_name << " " << revision << std::endl
+        << "#endif" << std::endl;
+
+      f.close(); 
+    }
+  }
+  else if (output_cpp_definition) {
     std::cout << "#define " << cpp_variable_name << " " << revision << std::endl;
   }
   else {
