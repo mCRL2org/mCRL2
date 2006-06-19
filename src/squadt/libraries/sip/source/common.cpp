85830b7cbd121f2c1be6740e2140889019d96bb7
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-06-19 13:34:13 +0000

    Changes:  - fix for building with gcc 4.*
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@1676 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/squadt/libraries/sip/source/common.cpp
+++ src/squadt/libraries/sip/source/common.cpp
@@ -4,11 +4,16 @@
 #include <sip/detail/common.h>
 
 namespace sip {
+  namespace messaging {
 
-  /** \brief The type identifier for messages of which the type is not known */
-  const sip::message_identifier_t message::message_unknown = sip::message_unknown;
+    /** \brief The type identifier for messages of which the type is not known */
+    template < >
+    const sip::message_identifier_t message< sip::message_identifier_t, sip::message_unknown, sip::message_any >::message_unknown = sip::message_unknown;
 
-  /** \brief The type identifier for messages of any type */
-  const sip::message_identifier_t message::message_any     = sip::message_any;
+    /** \brief The type identifier for messages of any type */
+    template < >
+    const sip::message_identifier_t message< sip::message_identifier_t, sip::message_unknown, sip::message_any >::message_any     = sip::message_any;
+
+  }
 }
 
