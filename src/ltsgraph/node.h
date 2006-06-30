8b117b5457bf0742dd26de6155f5060f7573951a
dlelann <dlelann@21252fe5-60e8-0310-8ff0-c1ae1001594e>
2006-06-30 14:50:17 +0000

    - Fix a commit bug
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@1728 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/ltsgraph/node.h
+++ src/ltsgraph/node.h
@@ -1,116 +1,56 @@
-#include "node.h"
+#ifndef node_h
+#define node_h
 
-const wxColour borderColor     = "BLACK";
-const wxColour borderColorInit = "RED";
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
 
-Node::Node(unsigned int _num, double _posX, double _posY, wxString _lbl, bool _initState) : 
-             posX(_posX), posY(_posY), lbl(_lbl), num(_num), initState(_initState)
+#include "wx/wxprec.h"
+#include "wx/dataobj.h"
+#include <wx/dcps.h>
+#include <string>
+
+#define POS_NODE_LBL_X 3.0
+#define POS_NODE_LBL_Y 5.0
+#define FONT_SIZE 9
+#define CIRCLE_RADIUS 10
+
+
+using namespace std;
+
+class Node
 {
-  locked = false;
-	labelsVisible = true;
-}
+public:
+  Node(unsigned int _num, double _posX, double _posY, wxString _lbl, bool _initState);
+  void OnPaint(wxDC * ptrDC);
+  unsigned int Get_num();
+  double GetX(); 
+  double GetY();
+	string Get_lbl();
+  void SetXY(double, double);
+	void forceSetXY(double, double);//set the x and y even if the node is locked
+  bool IsLocked();
+  void Lock();
+  void Unlock();
+	bool LabelVisible();
+	void ShowLabels();
+	void HideLabels();
+	void SetRadius(int);
+	bool IsInitState();
 
-void Node::OnPaint(wxDC * ptrDC) 
-{
-  wxColour color;
-  if (!locked)
-    color = wxSystemSettings::GetColour(wxSYS_COLOUR_BTNFACE);
-  else 
-    color = wxSystemSettings::GetColour(wxSYS_COLOUR_HIGHLIGHT);
+private:
+	int radius;
+  bool locked;
+	bool labelsVisible;
+  double posX;
+  double posY;
+  wxString lbl;
+  unsigned int num;
+	bool initState;
+    
 
-    //Circle
-		if (initState) {
-			wxPen pen = wxPen(borderColorInit, 2, wxSOLID);
-			ptrDC->SetPen(pen);
-		}
-		else {
-			wxPen pen = wxPen(borderColor, 1, wxSOLID);
-			ptrDC->SetPen(pen);
-		}
-    wxBrush myBrush(color,wxSOLID );
-    ptrDC->SetBrush(myBrush);
-    ptrDC->DrawCircle((wxCoord)posX,(wxCoord)posY,radius);
+};
 
-    //Label
-		if (labelsVisible) {
-			wxFont MyFont(FONT_SIZE, wxSWISS , wxNORMAL, wxNORMAL, false, wxT("Arial"));
-			ptrDC->SetFont(MyFont);
-	
-			double x= posX-POS_NODE_LBL_X;;
-			//Adjust label x position according to the length
-			switch (lbl.Length()) {
-				case 1:  x = posX-POS_NODE_LBL_X;  break;
-				case 2:  x = posX-POS_NODE_LBL_X-3;break;
-				case 3:  x = posX-POS_NODE_LBL_X-6;break;
-				default: x = posX-POS_NODE_LBL_X;  break;
-			}
-	
-			double y=posY-POS_NODE_LBL_Y;
-	
-			ptrDC->DrawRotatedText(lbl,(int) round(x),(int) round(y),0.0);
-		}
-}
 
-unsigned int Node::Get_num() {
-    return num;
-}
 
-double Node::GetX() {
-    return posX;
-}
-
-double Node::GetY() {
-    return posY;
-}
-
-string Node::Get_lbl() {
-	string st_lbl = string(lbl.fn_str());
-	return st_lbl;
-}
-
-bool Node::IsLocked(){
-  return locked;
-}
-
-bool Node::IsInitState(){
-  return initState;
-}
-
-void Node::Lock() {
-  locked = true;
-}
-
-void Node::Unlock() {
-  locked = false;
-}
-
-bool Node::LabelVisible() {
-	return labelsVisible;
-}
-
-void Node::ShowLabels() {
-		labelsVisible = true;
-} 
-
-void Node::HideLabels() {
-		labelsVisible = false;
-} 
-
-void Node::SetXY(double _x, double _y) {
-  if (!locked) {
-    posX = _x;
-    posY = _y;
-  }
-  else 
-    wxBell();
-}
-
-void Node::forceSetXY(double _x, double _y) {
-    posX = _x;
-    posY = _y;
-}
-
-void Node::SetRadius(int newRadius) {
-	radius = newRadius;
-}
-
+#endif //node_h
