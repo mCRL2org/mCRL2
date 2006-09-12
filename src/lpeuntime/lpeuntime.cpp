8b43f9827ee61769a88b35178c6f03dda4ee9b95
Jeroen Keiren <j.j.a.keiren@tue.nl>
2006-09-12 11:01:08 +0000

    - Created the untime function, which takes an lpe::specification argument and returns an lpe::specification, untiming the lpe. - Added error check for saving output file
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@1899 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/lpeuntime/lpeuntime.cpp
+++ src/lpeuntime/lpeuntime.cpp
@@ -5,8 +5,8 @@
 // ----------------------------------------------------------------------
 //
 // file          : lpeuntime 
-// date          : 08-09-2006
-// version       : 0.1
+// date          : 11-09-2006
+// version       : 0.2
 //
 // author(s)     : Jeroen Keiren <j.j.a.keiren@student.tue.nl>
 //
@@ -34,20 +34,22 @@
 #include <lpe/lpe.h>
 #include <lpe/specification.h>
 
+//Lowlevel library for gsErrorMsg
+#include <libprint_c.h>
+
 using namespace std;
 using namespace atermpp;
 using namespace lpe;
 
 namespace po = boost::program_options;
 
-#define VERSION "0.1"
+#define VERSION "0.2"
 
 /* Verbosity switch */
 bool        verbose = false;
 
-/* Name of the file to read input from (or standard input: "-") */
-std::string input_file;
-std::string output_file;
+std::string input_file; // Name of the file to read input from
+std::string output_file; // Name of the file to write output to (or stdout)
 
 void parse_command_line(int ac, char** av) {
   po::options_description desc;
@@ -98,6 +100,135 @@ void parse_command_line(int ac, char** av) {
   verbose  = 0 < vm.count("verbose");
   input_file = (0 < vm.count("INFILE")) ? vm["INFILE"].as< string >() : "-";
   output_file = (0 < vm.count("OUTFILE")) ? vm["OUTFILE"].as< string >() : "-";
+}
+
+///Returns an LPE specification in which the timed arguments have been rewritten
+lpe::specification untime(const lpe::specification specification) {
+  // TODO: Strip use of gs functions as much as possible; everything that's possible through these
+  // should also be available through the LPE library!
+  // TODO: Check with Wieger if the Append and Insert style functions can be made available through
+  // the LPE library. This would make the code much more readable, and less complex
+  // declarations
+  lpe::specification untime_specification; // Updated specification
+  lpe::LPE lpe; // Original lpe
+  lpe::LPE untime_lpe; // Updated lpe
+  lpe::summand_list untime_summand_list; // Updated summand list
+  lpe::data_variable_list untime_process_parameters; // Updated process parameters
+  lpe::data_variable last_action_time; // Extra parameter to display the last action time
+ 
+  // init
+  untime_summand_list = lpe::summand_list();
+  lpe = specification.lpe();
+
+  // Create extra parameter last_action_time and add it to the list of process parameters,
+  // last_action_time is used later on in the code
+  last_action_time = data_variable("last_action_time", lpe::sort("Real"));
+  untime_process_parameters = ATappend(lpe.process_parameters(), ATerm(ATermAppl(last_action_time)));
+      
+  // Transpose the original summand list, and see if there are summands with time
+  // If a summand has time, remove it, create new conditions for time, and add it to the new summand list (untime_summand_list)
+  // If a summand does not contain time, first introduce time, and then untime it.
+
+  // NOTE: For efficiency reasons we use ATinsert instead of ATappend; ATappend is implemented by using
+  // ATinsert followed by ATreverse. Therefore it is more efficient to ATinsert everything, then reverse
+  // all at once when we are done.
+  // ATinsert and ATreverse should really have an equivalent in the LPE library.
+  for (lpe::summand_list::iterator i = lpe.summands().begin(); i != lpe.summands().end(); ++i)
+  { 
+    // Declarations within scope of for-loop
+    lpe::data_variable time_var;
+    lpe::data_variable_list untime_summation_variables;
+    lpe::data_expression untime_condition;
+    lpe::data_assignment_list untime_assignments;
+    lpe::LPE_summand untime_summand;
+ 
+    if (i->has_time()) 
+    { 
+      // The summand is already timed, therefor there is no need to add an extra summation variable for time
+      untime_summation_variables = i->summation_variables();   
+
+      // Extend the original condition with an additional argument t.i(d,e.i)>last_action_time
+      untime_condition = gsMakeDataExprAnd(i->condition(), 
+                                           gsMakeDataExprGT(i->time(), 
+                                                            last_action_time.to_expr()
+                                                           )
+                                           );
+
+      // Extend original assignments to include t.i(d,e.i)
+      untime_assignments = ATappend(i->assignments(),
+                                    ATerm(ATermAppl(data_assignment(last_action_time,i->time())))
+                                    );
+    }
+    else
+    {
+          
+      // Add a new summation variable (this is allowed because according to an axiom the following equality holds):
+      // c -> a . X == sum t:Real . c -> a@t . X
+      time_var = data_variable("t", lpe::sort("Real")); // TODO: See if we can auto-name the variable (in order to prevent name collisions)
+      untime_summation_variables = ATappend(i->summation_variables(), ATerm(ATermAppl(time_var)));
+ 
+      // Extend the original condition with an additional argument
+      untime_condition = gsMakeDataExprAnd(i->condition(),
+                                           gsMakeDataExprGT(time_var,
+                                                            last_action_time.to_expr()
+                                                           )
+                                           );
+
+
+      // Extend original assignments to include t.i(d,e.i)
+      untime_assignments = ATappend(i->assignments(),
+                                    ATerm(ATermAppl(data_assignment(last_action_time, time_var.to_expr())))
+                                   );
+    } // i->has_time()
+
+    // Create a new summand with the changed parameters
+    untime_summand = lpe::LPE_summand(untime_summation_variables,
+					untime_condition,
+					i->is_delta(),
+					i->actions(),
+					gsMakeNil(), // new time
+					untime_assignments
+					);
+
+    // Add the new summand to the list
+    untime_summand_list = ATinsert(untime_summand_list, ATerm(ATermAppl(untime_summand)));
+
+  }
+ 
+  // Revert summand list, because it is the wrong way round now.
+  untime_summand_list = ATreverse(untime_summand_list);
+      
+  // Create new LPE, this equals lpe, except for the new summand list and the additional process parameter.
+  untime_lpe = lpe::LPE(lpe.free_variables(), untime_process_parameters, untime_summand_list, lpe.actions());
+
+
+  // FIXME:
+  // Create new initial_variables and initial_state in order to correctly initialize.
+  // NOTE: This is done assuming that the initial assignments are calculated at creation 
+  // time by "zipping" the initial_variables and the initial_state
+  // TODO: Move to decs section once functioning!
+  lpe::data_variable_list untime_initial_variables;
+  lpe::data_expression_list untime_initial_state;
+
+  //FIXME: The commented out changes here segfaultm they are needed to update the initialization.
+  //untime_initial_variables = ATappend(specification.initial_variables(), ATerm(ATermAppl(last_action_time)));
+  untime_initial_variables = specification.initial_variables();
+  //untime_initial_state = ATappend(specification.initial_state(),ATerm(gsMakeDataExprInt2Real(gsMakeDataExprInt(0)))); 
+  untime_initial_state = specification.initial_state();
+
+  // Create new specification, this equals original specification, except for the new LPE.
+  untime_specification = lpe::specification(specification.sorts(), 
+						specification.constructors(),
+						specification.mappings(),
+						specification.equations(),
+						specification.actions(),
+						untime_lpe, //new LPE
+						specification.initial_free_variables(),
+						untime_initial_variables, //new initial variables
+						untime_initial_state // new initial state
+						);
+
+  return untime_specification;
 }
         
 int main(int ac, char** av) {
@@ -162,7 +293,6 @@ int main(int ac, char** av) {
   lpe::specification lpe_specification;
 
   if (lpe_specification.load(input_file)) {
-    lpe::LPE lpe = lpe_specification.lpe();
 
 // TODO: Change the following to follow lpeuntime output instead of lpeinfo    
 #ifdef ENABLE_SQUADT_CONNECTIVITY
@@ -215,133 +345,13 @@ int main(int ac, char** av) {
     else {
 */
 #endif
-      // TODO: Strip use of gs functions as much as possible; everything that's possible through these
-      // should also be available through the LPE library!
-      // TODO: Check with Wieger if the Append and Insert style functions can be made available through
-      // the LPE library. This would make the code much more readable, and less complex
 
-      // Create a new lpe specification with an lpe which has the time removed;
-
-      // declarations
-      lpe::specification untime_specification; // Updated specification
-      lpe::LPE untime_lpe; // Updated lpe
-      lpe::summand_list untime_summand_list; // Updated summand list
-      lpe::data_variable_list untime_process_parameters; // Updated process parameters
-      lpe::data_variable last_action_time; // Extra parameter to display the last action time
- 
-      // init
-      untime_summand_list = lpe::summand_list();
-
-      // Create extra parameter last_action_time and add it to the list of process parameters,
-      // last_action_time is used later on in the code
-      last_action_time = data_variable("last_action_time", lpe::sort("Real"));
-      untime_process_parameters = ATappend(lpe.process_parameters(), ATerm(ATermAppl(last_action_time)));
-      
-      // Transpose the original summand list, and see if there are summands with time
-      // If a summand has time, remove it, create new conditions for time, and add it to the new summand list (untime_summand_list)
-      // If a summand does not contain time, first introduce time, and then untime it.
-
-      // NOTE: For efficiency reasons we use ATinsert instead of ATappend; ATappend is implemented by using
-      // ATinsert followed by ATreverse. Therefore it is more efficient to ATinsert everything, then reverse
-      // all at once when we are done.
-      // ATinsert and ATreverse should really have an equivalent in the LPE library.
-      for (lpe::summand_list::iterator i = lpe.summands().begin(); i != lpe.summands().end(); ++i)
-      { 
-        // Declarations within scope of for-loop
-        lpe::data_variable time_var;
-        lpe::data_variable_list untime_summation_variables;
-        lpe::data_expression untime_condition;
-        lpe::data_assignment_list untime_assignments;
-        lpe::LPE_summand untime_summand;
- 
-        if (i->has_time()) 
-        { 
-          // The summand is already timed, therefor there is no need to add an extra summation variable for time
-          untime_summation_variables = i->summation_variables();   
-
-          // Extend the original condition with an additional argument t.i(d,e.i)>last_action_time
-          untime_condition = gsMakeDataExprAnd(i->condition(), 
-                                               gsMakeDataExprGT(i->time(), 
-                                                                last_action_time.to_expr()
-                                                               )
-                                               );
-
-          // Extend original assignments to include t.i(d,e.i)
-          untime_assignments = ATappend(i->assignments(),
-                                        ATerm(ATermAppl(data_assignment(last_action_time,i->time())))
-                                        );
-
-        }
-        else
-        {
-          
-          // Add a new summation variable (this is allowed because according to an axiom the following equality holds):
-          // c -> a . X == sum t:Real . c -> a@t . X
-          time_var = data_variable("t", lpe::sort("Real")); // TODO: See if we can auto-name the variable (in order to prevent name collisions)
-          untime_summation_variables = ATappend(i->summation_variables(), ATerm(ATermAppl(time_var)));
-
-          // Extend the original condition with an additional argument
-          untime_condition = gsMakeDataExprAnd(i->condition(),
-                                               gsMakeDataExprGT(time_var,
-                                                                last_action_time.to_expr()
-                                                               )
-                                               );
-
-
-          // Extend original assignments to include t.i(d,e.i)
-          untime_assignments = ATappend(i->assignments(),
-                                        ATerm(ATermAppl(data_assignment(last_action_time, time_var.to_expr())))
-                                        );
-        } // i->has_time()
-
-        // Create a new summand with the changed parameters
-        untime_summand = lpe::LPE_summand(untime_summation_variables,
-					untime_condition,
-					i->is_delta(),
-					i->actions(),
-					gsMakeNil(), // new time
-					untime_assignments
-					);
-
-        // Add the new summand to the list
-        untime_summand_list = ATinsert(untime_summand_list, ATerm(ATermAppl(untime_summand)));
-
+      // Untime lpe_specification and save the output to a binary file
+      if (!untime(lpe_specification).save(output_file, true)) 
+      {
+        // An error occurred when saving
+        gsErrorMsg("Could not save to '%s'\n", output_file.c_str());
       }
- 
-      // Revert summand list, because it is the wrong way round now.
-      untime_summand_list = ATreverse(untime_summand_list);
-      
-      // Create new LPE, this equals lpe, except for the new summand list and the additional process parameter.
-      untime_lpe = lpe::LPE(lpe.free_variables(), untime_process_parameters, untime_summand_list, lpe.actions());
-
-
-      // FIXME:
-      // Create new initial_variables and initial_state in order to correctly initialize.
-      // NOTE: This is done assuming that the initial assignments are calculated at creation 
-      // time by "zipping" the initial_variables and the initial_state
-      // TODO: Move to decs section once functioning!
-      lpe::data_variable_list untime_initial_variables;
-      lpe::data_expression_list untime_initial_state;
-
-      //FIXME: The commented out changes here segfaultm they are needed to update the initialization.
-      //untime_initial_variables = ATappend(lpe_specification.initial_variables(), ATerm(ATermAppl(last_action_time)));
-      untime_initial_variables = lpe_specification.initial_variables();
-      //untime_initial_state = ATappend(lpe_specification.initial_state(),ATerm(gsMakeDataExprInt2Real(gsMakeDataExprInt(0)))); 
-      untime_initial_state = lpe_specification.initial_state();
-
-      // Create new specification, this equals original specification, except for the new LPE.
-      untime_specification = lpe::specification(lpe_specification.sorts(), 
-						lpe_specification.constructors(),
-						lpe_specification.mappings(),
-						lpe_specification.equations(),
-						lpe_specification.actions(),
-						untime_lpe, //new LPE
-						lpe_specification.initial_free_variables(),
-						untime_initial_variables, //new initial variables
-						untime_initial_state // new initial state
-						);
-
-      untime_specification.save(output_file, true); // Save as binary file
 
 #ifdef ENABLE_SQUADT_CONNECTIVITY
 /*
