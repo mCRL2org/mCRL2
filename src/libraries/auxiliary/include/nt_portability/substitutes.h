b1502d2d0ecad5a9c5741ab5767273eaff67a3bc
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-10-23 09:27:47 +0000

    Changes:  - added more macros to define things that are missing in Windows; everything    should be there now except for an implementation of fork()
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2057 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/libraries/auxiliary/include/nt_portability/substitutes.h
+++ src/libraries/auxiliary/include/nt_portability/substitutes.h
@@ -6,16 +6,24 @@
  * Windows environment.
  */
 
-# define snprintf _snprintf
-# define getpid   _getpid
-
-typedef int pid_t;
-
 #include <assert.h>
 #include <stdio.h>
 #include <process.h>
 #include <errno.h>
+#include <io.h>
 #include <unistd.h>
+
+#define snprintf _snprintf
+#define getpid   _getpid
+#define pipe     _pipe
+#define dup      _dup
+#define dup2     _dup2
+
+typedef int pid_t;
+
+#define STDIN_FILENO  _fileno(stdin)
+#define STDOUT_FILENO _fileno(stdout)
+#define STDERR_FILENO _fileno(stderr)
 
 #define WNOHANG 0x00000001
 
