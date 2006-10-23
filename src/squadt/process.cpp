57f741e2e4233474d6422d6b2c2533e0e3760e25
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-10-23 10:41:18 +0000

    Changes:  - changed process to use _spawnv under Windows  - created a process wrapper used by the process class that changes the    working directory and then executes, using _execv a new process or returns    with a non-zero value
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2059 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/squadt/process.cpp
+++ src/squadt/process.cpp
@@ -18,6 +18,7 @@
 
 #include "task_monitor.h"
 #include "command.h"
+#include "setup.h"
 
 namespace squadt {
   namespace execution {
@@ -39,16 +40,26 @@ namespace squadt {
      * @param[in] c the command to execute
      **/
     void process::operator() (const command& c) {
+#if (defined(_WIN32) || defined(__WIN32__) || defined(WIN32))
+      command cc(c);
+
+      cc.prepend_argument(c.working_directory);
+      cc.prepend_argument(c.executable);
+      cc.set_executable(TOOL_DIRECTORY + "/process_wrapper");
+
+      identifier = *_spawnv(_P_NOWAIT, cc.executable.c_str(), cc.get_argument_array());
+#else
       boost::shared_array < char const* > arguments(c.get_argument_array());
 
       identifier = fork();
 
       if (identifier == 0) {
         /* Change working directory to the project directory */
-        chdir(c.get_working_directory().c_str());
+        chdir(c.working_directory.c_str());
 
         execv(c.executable.c_str(), const_cast < char* const* > (arguments.get()));
       }
+#endif
 
       current_status = (identifier < 0) ? aborted : running;
 
---
+++ src/squadt/process_wrapper.cpp
@@ -0,0 +1,23 @@
+#include <io.h>
+#include <process.h>
+
+/*
+ * Process wrapper that changes the working directory and then executes another program
+ *
+ * Command line arguments
+ *
+ *  1    working directory
+ *  2    executable
+ *  3... command arguments to pass along
+ */
+int main(int argc, char** argv) {
+
+  if (3 < argc) {
+    if (chdir(argv[0]) == 0) {
+      execv(argv[1], const_cast < char* const* > (argv + 2));
+    }
+  }
+
+  return (1);
+}
+
