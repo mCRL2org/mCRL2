12637fbfef7fc8fc757ab846617762a49e4f701a
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2006-10-23 08:42:12 +0000

    Changes:  - added define for WNOHANG
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2056 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/libraries/auxiliary/include/nt_portability/substitutes.h
+++ src/libraries/auxiliary/include/nt_portability/substitutes.h
@@ -17,10 +17,10 @@ typedef int pid_t;
 #include <errno.h>
 #include <unistd.h>
 
+#define WNOHANG 0x00000001
+
 inline pid_t waitpid(pid_t pid, int* status, int options) {
 
-  assert (options == 0);
-    
   return _cwait (status, pid, _WAIT_CHILD);
 }
 
