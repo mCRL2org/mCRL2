4985fc20b0538cadc55c2c31978bec447f422ebd
Alexander van Dam <avandam@damdonk.nl>
2006-11-23 14:52:08 +0000

    Changes: - Added functionality: show the binding variables (left hand side variables of the equations of the PBES) and their signature.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2198 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- src/pbesinfo/pbesinfo.cpp
+++ src/pbesinfo/pbesinfo.cpp
@@ -2,7 +2,7 @@
 //
 // file          : pbesinfo 
 // date          : 21-11-2005
-// version       : 0.0.2
+// version       : 0.0.3
 //
 // author(s)     : Alexander van Dam <avandam@damdonk.nl>
 //
@@ -105,6 +105,7 @@ int main(int argc, char** argv)
 	/// If PBES can be loaded from file_name, then
 	///	- Show number of equations
 	/// - Show number of mu's / nu's.
+	/// - Show Binding variables and their type
 	/// else 
 	/// - Give error
 	if (pbes_specification.load(file_name))
@@ -117,14 +118,18 @@ int main(int argc, char** argv)
 		int mu = 0;
 		int nu = 0;
 		int fp_errors = 0;
-		for (lpe::equation_system::iterator i = eqsys.begin(); i != eqsys.end(); i++)
+		for (lpe::equation_system::iterator fp_i = eqsys.begin(); fp_i != eqsys.end(); fp_i++)
 		{
-			if (i->symbol().is_mu())
+			if (fp_i->symbol().is_mu())
+			{
 				mu++;						// The fixpoint is a mu
-			else if (i->symbol().is_nu())
+			}
+			else if (fp_i->symbol().is_nu())
+			{
 				nu++;						// The fixpoint is a nu
+			}
 			else
-				fp_errors++;					// The fixpoint is not a mu and not a nu. Probably an error has occurred.
+				fp_errors++;				// The fixpoint is not a mu and not a nu. Probably an error has occurred.
 		}
 		// Show an error-message if there were some errors when reading the fixpoints.
 		if (fp_errors != 0)
@@ -134,14 +139,43 @@ int main(int argc, char** argv)
 		
 		cout << "Input read from '" << ((file_name == "-") ? "standard input" : file_name) << "'" << endl << endl;
 		
+		// Show number of equations
 		cout << "Number of equations: " << eqsys.size() << endl;
-		cout << "Number of mu's: " << mu << endl;
-		cout << "Number of nu's: " << nu << endl;
+		
+		// Show number of mu's, nu's
+		cout << "Number of mu's:      " << mu << endl;
+		cout << "Number of nu's:      " << nu << endl;
+		
+		// Show Binding variables with their type
+		int nr_predvar = 1;
+		string sort_bool = "Bool";
+		for (atermpp::set<propositional_variable>::iterator bv_i = eqsys.binding_variables().begin(); bv_i != eqsys.binding_variables().end(); bv_i++)
+		{
+			int size = bv_i->parameters().size();
+			int nr_sorts = 1;
+			if (nr_predvar == 1)
+				cout << "Binding variables:   " << bv_i->name() << " :: ";
+			else
+				cout << "                     " << bv_i->name() << " :: ";
+			for (atermpp::term_list_iterator<lpe::data_variable> dv_i = bv_i->parameters().begin(); dv_i != bv_i->parameters().end(); dv_i++)
+			{
+				cout << dv_i->type();
+				if (nr_sorts < size)
+				{
+					cout << " x ";
+					nr_sorts++;
+				}
+				else
+					cout << " -> "<< lpe::sort(sort_bool);
+			}
+			cout << endl;
+			nr_predvar++;
+		}
 	}
 	else
 	{
 		cerr << "Unable to load PBES from '" << file_name << "'" << endl;
 	}
-		
+
 	return 0;
 }
