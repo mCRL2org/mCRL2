84c2720afcf348c1f7cb141a7679529085dd5b6b
Muck van Weerdenburg <m.j.van.weerdenburg@tue.nl>
2006-11-29 11:16:57 +0000

    Forgot to add header file for FSM parser.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2222 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ src/libraries/lts/source/fsmlexer.h
@@ -0,0 +1,28 @@
+#ifndef __FSMLEXER_H
+#define __FSMLEXER_H
+
+#include <aterm2.h>
+#include "lts/liblts.h"
+
+class fsm_lexer
+{
+  public:
+    ATermList stateVector;
+    ATermList valueTable;
+    ATermList stateId;
+    ATermList typeValues;
+    ATermAppl typeId;
+    ATermTable labelTable;
+    
+    AFun const_ATtypeid;
+    AFun const_ATparmid;
+    AFun const_ATvalue;
+    AFun const_ATstate;
+    AFun const_ATparam;
+
+    mcrl2::lts::lts *fsm_lts;
+};
+
+extern fsm_lexer *fsm_lexer_obj;
+
+#endif
