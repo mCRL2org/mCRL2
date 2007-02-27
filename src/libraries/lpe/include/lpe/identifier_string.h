8565bafa5d60abeb6ac1e37a203b24d7d1dbebd0
Wieger Wesselink <j.w.wesselink@tue.nl>
2007-02-27 08:59:03 +0000

    *) Forgot to add this file in the last commit.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@2628 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ src/libraries/lpe/include/lpe/identifier_string.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file identifier_string.h
+
+#ifndef LPE_IDENTIFIER_STRING_H
+#define LPE_IDENTIFIER_STRING_H
+
+#include "atermpp/aterm_string.h"
+
+namespace lpe {
+
+/// String type of the LPE library.
+typedef atermpp::aterm_string identifier_string;
+
+} // namespace lpe
+
+#endif // LPE_IDENTIFIER_STRING_H
