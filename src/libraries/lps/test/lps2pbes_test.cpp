5b4d516f7d85be5fd48a9d47225abba17473a448
Wieger Wesselink <j.w.wesselink@tue.nl>
2007-06-01 15:48:45 +0000

    *) Rewritten the lps2pbes test program. Due to the patch to mcrl22lps the tests no longer need to be run as separate program calls, which results in a speedup with a factor of more than 20.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@3010 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ src/libraries/lps/test/lps2pbes_test.cpp
@@ -0,0 +1,64 @@
+// Test program for timed lps2pbes.
+
+#include <iostream>
+#include <iterator>
+#include <boost/test/minimal.hpp>
+#include <boost/algorithm/string.hpp>
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+#include "lps/pbes.h"
+#include "lps/detail/tools.h"
+#include "lps/detail/read_text.h"
+#include "test_specifications.h"
+
+using namespace std;
+using namespace lps;
+using namespace lps::detail;
+namespace fs = boost::filesystem;
+
+int test_main(int argc, char* argv[])
+{
+  aterm bottom_of_stack;
+  aterm_init(bottom_of_stack);
+  gsEnableConstructorFunctions();
+
+  BOOST_CHECK(argc > 1);
+  
+  // The dummy file test.test is used to extract the full path of the test directory.
+  fs::path dummy_path = fs::system_complete( fs::path( argv[1], fs::native ) );
+  fs::path dir = dummy_path.branch_path();
+  BOOST_CHECK(fs::is_directory(dir));
+
+  fs::directory_iterator end_iter;
+  for ( fs::directory_iterator dir_itr(dir); dir_itr != end_iter; ++dir_itr )
+  {
+    if ( fs::is_regular( dir_itr->status() ) )
+    {
+      std::string filename = dir_itr->path().file_string();
+      if (boost::ends_with(filename, ".form"))
+      {       
+        std::string timed_result_file   = filename.substr(0, filename.find_last_of('.') + 1) + ".expected_timed_result";
+        std::string untimed_result_file = filename.substr(0, filename.find_last_of('.') + 1) + ".expected_untimed_result";
+        std::string formula = read_text(filename);
+        if (fs::exists(timed_result_file))
+        {
+          pbes result = lps2pbes(SPEC1, formula, true);
+          pbes expected_result;
+          expected_result.load(timed_result_file);
+          cerr << "checking " << timed_result_file << endl;
+          BOOST_CHECK(result == expected_result);
+        }
+        if (fs::exists(untimed_result_file))
+        {
+          pbes result = lps2pbes(SPEC1, formula, false);
+          pbes expected_result;
+          expected_result.load(untimed_result_file);
+          cerr << "checking " << untimed_result_file << endl;
+          BOOST_CHECK(result == expected_result);
+        }
+      }
+    }
+  }
+
+  return 0;
+}
