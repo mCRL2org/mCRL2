4c92445199b8ffb4166387ea573407f1f3635caa
Muck van Weerdenburg <m.j.van.weerdenburg@tue.nl>
2007-06-30 13:59:29 +0000

    Uncommented implement_data_action_rename and added assert(0) as body; the function is needed when building with --enable-experimental.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@3188 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/parser/source/dataimpl.cpp
+++ libraries/parser/source/dataimpl.cpp
@@ -369,11 +369,12 @@ ATermAppl implement_data_state_frm(ATermAppl state_frm,
   return impl_exprs(state_frm, lps_spec);
 }
 
-//ATermAppl implement_data_action_rename(ATermAppl action_rename,
-//  lps::specification &lps_spec)
-//{
-  //return impl_exprs(action_rename, lps_spec);
-//}
+ATermAppl implement_data_action_rename(ATermAppl action_rename,
+  lps::specification &lps_spec)
+{
+  assert(0);
+  return NULL;/*impl_exprs(action_rename, lps_spec);*/
+}
 
 ATermAppl impl_exprs_spec(ATermAppl spec)
 {
