2eb421f2907dd1dcebde3da3750fb03b8d0ad782
Jan Friso Groote <j.f.groote@tue.nl>
2007-11-06 11:52:07 +0000

    Started documenting the trace library.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@3709 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/trace/include/mcrl2/libtrace.h
+++ libraries/trace/include/mcrl2/libtrace.h
@@ -1,33 +1,71 @@
-// Author(s): Muck van Weerdenburg
+// Author(s): Muck van Weerdenburg. Documentation Jan Friso Groote.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 /// \file libtrace.h
+/// \brief This library allows to flexibly manipulate traces.
+/// This library allows to build, traverse and store traces.
+/// Traces are sequences of state-action-time triples.
+/// The state is a vector of data values, the action is the outgoing
+/// action in this state, and the time is an absolute
+/// real number indicating the current time or NULL if the trace 
+/// is untimed.
 
 #include <aterm2.h>
 #include <iostream>
 #include <string>
 
+////////////////////////////////////////////////////////////
+//
+/// \brief Formats in which traces can be saved on disk
+/// There are several formats for traces.
+/// The tfMcrl2 format saves a trace as an mcrl2 term in ATerm internal.
+/// This is a compact but unreadable format.
+/// The tfPlain format is an ascii representation of the trace, which is
+/// human readable. tfUnknown is only used to read traces, when it is
+/// not known what the format is. In this case it is determined based
+/// on the format of the input file.
 enum TraceFormat { tfMcrl2, tfPlain, tfUnknown };
 
 class Trace
 {
 	public:
+/// \brief Default constructor for an empty trace
 		Trace();
+/// \brief Construct the trace in the basis of an input stream
 		Trace(std::istream &is, TraceFormat tf = tfUnknown);
+/// \brief Construct the trace on the basis of an input file
 		Trace(std::string const& filename, TraceFormat tf = tfUnknown);
+/// \brief Destructor for traces
 		~Trace();
 
+/// \brief Set the current position back to the beginning of the trace
 		void resetPosition();
+
+/// \brief Set the current position after the Pos'th action of the trace
+/// Set the current position after the Pos'th action of the trace. The
+/// initial position corresponds to Pos=0. If Pos is beyond the last
+/// action in the trace, no new position is set.
 		void setPosition(unsigned int pos);
+
+/// \brief Get the current position in the trace
 		unsigned int getPosition();
+
+/// \brief Get the current position in the trace
+/// Get the length of the trace. If getLength()=n, valid positions
+/// in the trace are smaller than n.
 		unsigned int getLength();
 
+/// \brief Get the state at the current position in the trace
 		ATermAppl currentState();
-		ATermAppl currentTime();
+/// \brief Get the outgoing action from the current state in the trace
 		ATermAppl nextAction();
+/// \brief Get the time of the current action from the current state in the trace
+/// Get the time of the current action from the current state in the trace. If 
+/// the current action is untimed, NULL is returned.
+		ATermAppl currentTime();
 
 		void truncate();
 
