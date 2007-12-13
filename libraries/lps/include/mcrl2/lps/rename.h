711b8967ece1d39aabca9c3fe864d83926b42dad
Wieger Wesselink <j.w.wesselink@tue.nl>
2007-12-13 22:49:17 +0000

    - Fixed a bug in specification rename_process_parameters(const specification& spec, IdentifierGenerator& generator), noted by Aad. I forgot to exclude the data specification in spec from the renaming.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@3889 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/include/mcrl2/lps/rename.h
+++ libraries/lps/include/mcrl2/lps/rename.h
@@ -12,6 +12,7 @@
 
 #include <set>
 #include <vector>
+#include <utility>
 #include <boost/iterator/transform_iterator.hpp>
 #include "atermpp/make_list.h"
 #include "atermpp/algorithm.h"
@@ -26,8 +27,14 @@
 namespace lps {
 
 /// \internal
-template <typename IdentifierGenerator, typename Term>
-aterm_appl rename_process_parameters_helper(const linear_process& p, Term& t, IdentifierGenerator& generator)
+/// Generates a renaming of process parameters of the process p. The parameters are
+/// renamed according to the given identifier generator, and well typedness constraints
+/// are taken into account. The result consists of two vectors (src, dest) that specify
+/// the renaming src[i] := dest[i].
+///
+template <typename IdentifierGenerator>
+std::pair<std::vector<data_variable>, std::vector<data_variable> >
+rename_process_parameters_helper(const linear_process& p, IdentifierGenerator& generator)
 {
   std::set<identifier_string> forbidden_names = detail::set_union(
     detail::free_variable_names(p),
@@ -48,42 +55,15 @@ aterm_appl rename_process_parameters_helper(const linear_process& p, Term& t, Id
       dest.push_back(data_variable(new_name, i->sort()));
     }
   }
-  return atermpp::partial_replace(t, detail::make_data_variable_replacer(src, dest));
+  return std::make_pair(src, dest);
 }
-
-/*
-/// Renames the process parameters in the process p using the given identifier generator.
-template <typename IdentifierGenerator>
-linear_process rename_process_parameters(const linear_process& p, IdentifierGenerator& generator)
-{
-  std::set<identifier_string> forbidden_names = detail::set_union(
-    detail::free_variable_names(p),
-    detail::summand_variable_names(p)
-  );
-  
-  std::vector<data_variable> src;  // contains the variables that need to be renamed
-  std::vector<data_variable> dest; // contains the corresponding replacements
-  generator.add_identifiers(forbidden_names);
-
-  for (data_variable_list::iterator i = p.process_parameters().begin(); i != p.process_parameters().end(); ++i)
-  {
-    identifier_string new_name = generator(i->name());
-    if (new_name != i->name())
-    {
-      // save the old and new value in the src and dest arrays
-      src.push_back(*i);
-      dest.push_back(data_variable(new_name, i->sort()));
-    }
-  }
-  return atermpp::partial_replace(p, detail::make_data_variable_replacer(src, dest));
-}
-*/
 
 /// Renames the process parameters in the process p using the given identifier generator.
 template <typename IdentifierGenerator>
 linear_process rename_process_parameters(const linear_process& p, IdentifierGenerator& generator)
 {
-  return rename_process_parameters_helper(p, p, generator);
+  std::pair<std::vector<data_variable>, std::vector<data_variable> > r = rename_process_parameters_helper(p, generator);
+  return atermpp::partial_replace(p, detail::make_data_variable_replacer(r.first, r.second));
 }
 
 /// Renames the process parameters in the process p, such that none of them
@@ -100,7 +80,17 @@ linear_process rename_process_parameters(const linear_process& p, const std::set
 template <typename IdentifierGenerator>
 specification rename_process_parameters(const specification& spec, IdentifierGenerator& generator)
 {
-  return rename_process_parameters_helper(spec.process(), spec, generator);
+  std::pair<std::vector<data_variable>, std::vector<data_variable> > r = rename_process_parameters_helper(spec.process(), generator);
+
+  linear_process new_process              = atermpp::partial_replace(spec.process()        , detail::make_data_variable_replacer(r.first, r.second));
+  action_label_list new_action_labels     = atermpp::partial_replace(spec.action_labels()  , detail::make_data_variable_replacer(r.first, r.second));
+  process_initializer new_initial_process = atermpp::partial_replace(spec.initial_process(), detail::make_data_variable_replacer(r.first, r.second));
+    
+  specification result = spec;
+  result = set_lps(result, new_process);
+  result = set_action_labels(result, new_action_labels);
+  result = set_initial_process(result, new_initial_process);
+  return result;
 }
 
 /// Renames the process parameters in the specification spec, such that none of them
