cf43981358420c5c501caffa96f9d44547016d36
Jan Friso Groote <j.f.groote@tue.nl>
2008-01-13 08:56:23 +0000

    Made a few more changes to the documentation of the trace library. Changed a spelling error, added dots, and removed duplicates in the text for the \brief and \details section as I erroneously had assumed that the \brief section is not repeated in the full description of a routine.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4000 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/trace/include/mcrl2/trace.h
+++ libraries/trace/include/mcrl2/trace.h
@@ -29,7 +29,7 @@ namespace trace {
 ////////////////////////////////////////////////////////////
 //
 /// \brief Formats in which traces can be saved on disk
-/// \details Formats in which traces can be saved on disk. There are several formats for traces.
+/// \details There are several formats for traces.
 /// The tfMcrl2 format saves a trace as an mCRL2 term in ATerm internal format.
 /// This is a compact but unreadable format.
 /// The tfPlain format is an ascii representation of the trace, which is
@@ -37,6 +37,7 @@ namespace trace {
 /// tfUnknown is only used to read traces, when it is
 /// not known what the format is. In this case it is determined based
 /// on the format of the input file.
+
 enum TraceFormat 
   { tfMcrl2,  /**< Format is stored as an ATerm */
     tfPlain,  /**< Format is stored in plain text. In this format there are only actions */
@@ -44,7 +45,7 @@ enum TraceFormat
   };
 
 /// \brief This class contains a trace consisting of a sequence of (timed) actions possibly with intermediate states
-/// \details This class contains a trace. A trace is a sequence of actions. Actions can have a time
+/// \details A trace is a sequence of actions. Actions can have a time
 /// tag as a positive real number. Between two actions, before the first action and after the last
 /// action there can be a state. In the current version of the trace library an action, a state and
 /// a time tag are arbitrary expressions of sort AtermAppl. It is expected that this will change
@@ -64,74 +65,88 @@ class Trace
 {
 	public:
 /// \brief Default constructor for an empty trace.
-/// \details This is the default constructor for an empty trace. The current position
+/// \details The current position
 /// and length of trace are set to 0. The initial state and time are set to NULL.
+
 		Trace();
+
 /// \brief Construct the trace in the basis of an input stream.
 /// \details A trace is read from the input stream. If the format is tfUnknown it
 /// is automatically determined what the format of the trace is.
 /// \param[in] is The input stream from which the trace is read.
 /// \param[in] tf The format in which the trace was stored. Default: '''tfUnknown'''.
 /// \exception std::runtime_error message in case of failure
+
 		Trace(std::istream &is, TraceFormat tf = tfUnknown);
+
 /// \brief Construct the trace on the basis of an input file.
 /// \details A trace is read from the input file. If the format is tfUnknown it
 /// is automatically determined what the format of the trace is.
 /// \param[in] filename The name of the file from which the trace is read.
 /// \param[in] tf The format in which the trace was stored. Default: '''tfUnknown'''.
 /// \exception std::runtime_error message in case of failure
+
 		Trace(std::string const& filename, TraceFormat tf = tfUnknown);
+
 /// \brief Destructor for the trace.
-/// \details Destructor for the trace. It frees all the associated memory of the trace.
+/// \details This destructor frees all the associated memory of the trace.
+
 		~Trace();
 
 /// \brief Set the current position back to the beginning of the trace
-/// \details Set the current position back to the beginning of the trace. The 
-/// trace itself remains unaltered. 
+/// \details The trace itself remains unaltered. 
+
 		void resetPosition();
 
-/// \brief Set the current position after the pos'th action of the trace
-/// \details Set the current position after the pos'th action of the trace. The
-/// initial position corresponds to pos=0. If pos is larger than
+/// \brief Set the current position after the pos'th action of the trace.
+/// \details The initial position corresponds to pos=0. If pos is larger than
 /// the length of the trace, no new position is set.
 /// \param[in] pos The new position in the trace.
+
 		void setPosition(unsigned int pos);
 
-/// \brief Get the current position in the trace
-/// \details Get the current position of the trace as a non negative number
+/// \brief Get the current position in the trace.
+/// \details The current position of the trace is a non negative number
 /// smaller than the length of the trace.
 /// \return The current position of the trace.
+
 		unsigned int getPosition();
 
-/// \brief Get the current length of the trace
-/// \details Get the length of the trace.
+/// \brief Get the current length of the trace.
 /// \return A positive number indicating the number of actions in the trace.
+
 		unsigned int getLength();
 
-/// \brief Get the state at the current position in the trace
-/// \details Get the state at the current position. The state at
+/// \brief Get the state at the current position in the trace.
+/// \details The state at
 /// position 0 is the initial state. If no state is defined at
 /// the current position NULL is returned.
 /// \return The state at the current position of the trace.
+
 		ATermAppl currentState();
+
 /// \brief Get the outgoing action from the current position in the trace and
-///  move to the next position
+///  move to the next position.
 /// \details This routine returns the action at the current position of the
 /// trace and moves to the next position in the trace. When the current position
 /// is at the end of the trace, nothing happens and NULL is returned.
 /// \return An ATermAppl representing the action at the current position of the
 /// trace. This is NULL when at the end of the trace.
+
 		ATermAppl nextAction();
-/// \brief Get the time of the current state in the trace
-/// \details Get the time of the current state in the trace. This is the time at which
+
+/// \brief Get the time of the current state in the trace.
+/// \details This is the time at which
 /// the last action occurred (if any).
 /// \return An ATermAppl representing the current time, or NULL if the time is not defined.
+
 		ATermAppl currentTime();
 
 /// \brief Truncates the trace at the current position.
-/// \details This function removes the mext action at the current position and all
+/// \details This function removes the next action at the current position and all
 /// subsequent actions, times and states. The state and the time at the current
 /// position remain untouched.
+
 		void truncate();
 
 /// \brief Add an action to the current trace.
@@ -141,13 +156,15 @@ class Trace
 /// \param [in] action The action to be stored in the trace.
 /// \param [in] time The time to be associated with the current action and state.
 /// \return
+
 		void addAction(ATermAppl action, ATermAppl time = NULL);
 
 /// \brief Set the state at the current position.
-/// \details Set the state at the current position. If an action is set, the state
+/// \details If an action is set, the state
 /// that is reached is set to NULL. If this state is NULL it can be set once.
 /// \param [in] state The new state.
 /// \return The return value indicates whether the state is set.
+
 		bool setState(ATermAppl state);
 
 /// \brief Determine whether a state can still be set.
@@ -155,8 +172,8 @@ class Trace
 /// returns whether a state can still be set to a new value.
 /// \retval true The state can be set to a new value.
 /// \retval false The state can not be set to a new value.
-		bool canSetState();
 
+		bool canSetState();
 
 /// \brief Replace the trace with the content of the stream.
 /// \details The trace is replaced with the trace in the stream.
@@ -165,6 +182,7 @@ class Trace
 /// \param [in] is The stream from which the trace is read.
 /// \param [in] tf The expected format of the trace in the stream (default: tfUnknown).
 /// \exception std::runtime_error message in case of failure
+
 		void load(std::istream &is, TraceFormat tf = tfUnknown);
 
 /// \brief Replace the trace with the trace in the file.
