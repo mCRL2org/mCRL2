2c6400faa2511d1c2673feb52eb9a0e16f0d25be
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2008-01-20 13:34:13 +0000

    Changes:  - Reverted previous changes
        When switching between toolsets e.g. MSVC, Cygwin, MinGW, run remove build/bin/bjam' and built it again with the same toolset as the toolset.
    
     - added specialised workarounds header file for Cygwin
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4095 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ build/workarounds/cygwin/workarounds.h
@@ -0,0 +1,16 @@
+#ifndef _CYGWIN_SUBSTITUTES_H_
+#define _CYGWIN_SUBSTITUTES_H_
+
+/*
+ * Windows MinGW Compatibility Functions -- functions to support portability to the
+ * Windows environment.
+ */
+
+#include <cassert>
+#include <cerrno>
+#include <unistd.h>
+#include <process.h>
+
+#include "../workarounds.h"
+
+#endif
