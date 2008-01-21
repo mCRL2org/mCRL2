b53409e1d10a61b25e97ee9c71db98e0d57cca65
Wieger Wesselink <j.w.wesselink@tue.nl>
2008-01-21 15:37:26 +0000

    - Cleaned up the configuration files form Visual C++, and commented out the wxWidgets settings.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4107 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- build/msvc/setup.h
+++ build/msvc/setup.h
@@ -1,30 +1,7 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-// Locations
+// The following directory is the search path used by the xsim tool.
 #define PLUGIN_DIRECTORY "C:/mcrl2/lib/mcrl2/plugins"
-#define DATA_DIRECTORY   "C:/mcrl2/share/mcrl2"
-#define TOOL_DIRECTORY   "C:/mcrl2/bin"
-
-// Build options
-#define CC               "gcc"
-#define CXX              "g++"
-#define CFLAGS           "-Wno-unused -O3 -std=c99"
-#define CXXFLAGS         "-Wno-unused -O3 -std=c++98"
-#define CPPFLAGS         "-I/usr/local/include/cvc3"
-#define LDFLAGS          "-bundle -undefined dynamic_lookup"
-
-// Shared object compilation
-#define SCFLAGS          "-fPIC"
-#define SCXXFLAGS        "-fPIC"
-
-// Linking to shared library
-#define SLDFLAGS         ""
-
-// Preprocessor and linker options for the ATerm library
-#define ATERM_CPPFLAGS   "-m32 -I\"c:/mcrl2/include/mcrl2/aterm\""
-#define ATERM_LDFLAGS    "-L\"c:/mcrl2/lib/mcrl2\" -lATerm"
-
-//#define MCRL2_BCG
 
 #endif
