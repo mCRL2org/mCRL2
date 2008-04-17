8fcbd7184a9d3e28cea58032fe748b5819c490a5
Jeroen Keiren <j.j.a.keiren@tue.nl>
2008-04-17 14:35:42 +0000

    - Add testcases for lpssumelm to the test suite for the lps library, and remove   the now superfluous mcrl2 files of these tests.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4447 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/test/sumelm_test.cpp
+++ libraries/lps/test/sumelm_test.cpp
@@ -18,8 +18,10 @@ using namespace atermpp;
 using namespace mcrl2::data;
 using namespace mcrl2::lps;
 
-///Test case which tries to test all possibilities for substitutions. This is a
-///test for issue #367
+/*
+ * Test case which tries to test all possibilities for substitutions. This is a
+ * test for issue #367
+ */
 void test_case_1()
 {
   const std::string text(
@@ -41,11 +43,245 @@ void test_case_1()
   }
 }
 
+/// Sum variable y does not occur in the summand, and therefore must be removed.
+void test_case_2()
+{
+  const std::string text(
+    "act a,b;\n"
+    "proc P = sum y:Int . a . b . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    BOOST_CHECK(i->summation_variables().empty());
+  }
+}
+
+/* 
+ * The sum variable y is equal to 4 in the condition, hence 4 should be
+ * substituted for y in the summand, and sum y should be removed
+ * In this case, this boils down to removing sum y, and removing 4==y from the
+ * condition. The rest of the specification can be left untouched
+ */
+void test_case_3()
+{
+  const std::string text(
+    "act a;\n"
+    "proc P = sum y:Int . (4 == y) -> a . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    BOOST_CHECK(i->summation_variables().empty());
+    BOOST_CHECK(find_all_data_variables(i->condition()).empty());
+  }
+}
+
+/* 
+ * This is the same as test case 3, except with the equality in different order.
+ */
+void test_case_4()
+{
+  const std::string text(
+    "act a;\n"
+    "proc P = sum y:Int . (y == 4) -> a . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    BOOST_CHECK(i->summation_variables().empty());
+    BOOST_CHECK(find_all_data_variables(i->condition()).empty());
+  }
+}
+
+/* 
+ * Test whether sum variables are correctly removed from actions and timing.
+ */
+void test_case_5()
+{
+  const std::string text(
+    "act a,b:Int;\n"
+    "proc P = sum y:Int . (y == 4) -> a(y)@y . b(y*2)@(y+1) . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  std::set<data_variable> parameters = find_all_data_variables(s1.process().process_parameters());
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    BOOST_CHECK(i->summation_variables().empty());
+
+    // Check that the only data variables in the condition and time
+    // are process parameters
+    std::set<data_variable> condition_vars = find_all_data_variables(i->condition());
+    for (std::set<data_variable>::iterator j = condition_vars.begin()
+        ; j != condition_vars.end()
+        ; ++j)
+    {
+      BOOST_CHECK(parameters.find(*j) != parameters.end());
+    }
+
+    if (i->has_time())
+    {
+      std::set<data_variable> time_vars = find_all_data_variables(i->time());
+      for (std::set<data_variable>::iterator j = time_vars.begin()
+          ; j != time_vars.end()
+          ; ++j)
+      {
+        BOOST_CHECK(parameters.find(*j) != parameters.end());
+      }
+    }
+  }
+}
+
+/* 
+ * Test that a sum variable is not removed when it occurs in both sides of a
+ * variable. 
+ */
+void test_case_6()
+{
+  const std::string text(
+    "act a;\n"
+    "proc P = sum y:Int . (y == y + 1) -> a . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  int sumvar_count = 0;
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    if (!i->summation_variables().empty())
+    {
+      ++sumvar_count;
+    }
+  }
+  BOOST_CHECK(sumvar_count == 1);
+  BOOST_CHECK(s0 == s1);
+}
+
+/*
+ * The sum variable e occurs in the condition in the following ways:
+ * - As righthandside of an equality
+ * - Both as lefthandside and righthandside of an equality
+ * - As lefthandside of an equality
+ * The sum variable d occurs in the lefthandside of an equality in the
+ * condition.
+ * The sum variable f occurs in the righthandside of an equality in the
+ * condition.
+ *
+ * Result:
+ * - The summation over d is removed by substituting e for d
+ * - The summation over e is removed by substituting f for e
+ * This should leave only a sum f:D . f == g(f) -> a . X(f).
+ */
+void test_case_7()
+{
+  const std::string text(
+    "sort D = struct d1 | d2 | d3;\n"
+    "map g : D -> D;\n"
+    "act a;\n"
+    "proc P(c:D) = sum d:D . sum e:D . sum f:D . (d == e && e == g(e) && e == f) -> a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  int sumvar_count = 0;
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    if (!i->summation_variables().empty())
+    {
+      ++sumvar_count;
+    }
+  }
+  BOOST_CHECK(sumvar_count == 1);
+}
+
+/*
+ * Sum variable d occurs twice in the lefthandside of an equality
+ * The strongest set of substitutions possible is e := d; f := d, hence removing
+ * two summations.
+ * (Test introduced after a bugreport by M. Voorhoeve)
+ */
+void test_case_8()
+{
+  const std::string text(
+    "sort D = struct d1 | d2 | d3;\n"
+    "act a;\n"
+    "proc P(c:D) = sum d:D . sum e:D . sum f:D . (d == e && d == f) -> a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  int sumvar_count = 0;
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    if (!i->summation_variables().empty())
+    {
+      ++sumvar_count;
+      BOOST_CHECK(find_all_data_variables(i->condition()).empty());
+    }
+  }
+  BOOST_CHECK(sumvar_count == 1);
+}
+
+/*
+ * Test introduced after a bug report by Bas Ploeger. In contrary to what was
+ * assumed before, sum variables may not be removed from delta summands.
+ */
+void test_case_9()
+{
+  const std::string text(
+    "proc P = sum y:Bool . y -> delta . P;\n"
+    "init P;\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  specification s1 = sumelm(s0);
+  summand_list summands1 = s1.process().summands();
+  int sumvar_count = 0;
+  for(summand_list::iterator i = summands1.begin(); i != summands1.end(); ++i)
+  {
+    if (!i->summation_variables().empty())
+    {
+      ++sumvar_count;
+    }
+  }
+  BOOST_CHECK(sumvar_count == 1);
+}
+
 int test_main(int ac, char** av)
 {
   MCRL2_ATERM_INIT(ac, av)
 
   test_case_1();
+  test_case_2();
+  test_case_3();
+  test_case_4();
+  test_case_5();
+  test_case_6();
+  test_case_7();
+  test_case_8();
+  test_case_9();
 
   return 0;
 }
