32d21aefbf0a78a44420580630561490d1907c0a
Jeroen Keiren <j.j.a.keiren@tue.nl>
2008-04-17 18:55:13 +0000

    - Add automated tests for lpsbinary to the test suite. We might want to think of ways to improve these tests, but this already improves a lot on the automated tests we had before. (Let that be none). - Remove the old mcrl2 test files.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4450 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lps/test/binary_test.cpp
@@ -0,0 +1,246 @@
+// Author(s): Jeroen Keiren
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file binary_test.cpp
+/// \brief Some simple tests for the binary algorithm.
+
+#include <iostream>
+#include <string>
+#include <boost/test/minimal.hpp>
+#include <mcrl2/lps/specification.h>
+#include <mcrl2/lps/binary.h>
+#include <mcrl2/lps/mcrl22lps.h>
+
+using namespace atermpp;
+using namespace mcrl2::data;
+using namespace mcrl2::lps;
+
+///All process parameters of sort D should have been translated to
+///parameters of sort Bool. This leaves only parameters of sort Bool and Pos.
+void test_case_1()
+{
+  const std::string text(
+    "sort D = struct d1|d2;\n"
+    "act a;\n"
+    "proc P(e:D) = sum d:D . a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 1);
+}
+
+/*
+ * Sort D has got 8 constructors, which can therefore be encoded exactly in
+ * a vector of 3 boolean variables. This means that all combinations of the
+ * boolean variables encode a constructor.
+ * Process parameter e should be replaced by a vector of boolean variables,
+ * and should be replaced in nextstate.
+ * The initial state should be altered accordingly.
+ */
+void test_case_2()
+{
+  const std::string text(
+    "sort D = struct d1|d2|d3|d4|d5|d6|d7|d8;\n"
+    "act a;\n"
+    "proc P(e:D) = sum d:D . a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 3);
+}
+
+/*
+ * Sort D has got 7 constructors, which can therefore be encoded in
+ * a vector of 3 boolean variables. This means that there is one combination
+ * of the boolean variables that does not encode a constructor.
+ * Process parameter e should be replaced by a vector of boolean variables,
+ * and should be replaced in nextstate.
+ * The initial state should be altered accordingly.
+ */
+void test_case_3()
+{
+  const std::string text(
+    "sort D = struct d1|d2|d3|d4|d5|d6|d7;\n"
+    "act a;\n"
+    "proc P(e:D) = sum d:D . a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 3);
+}
+
+/*
+ * Sort D has got 2 constructors, and should be encoded into one boolean
+ * variable. Process parameter e should be replaced by a single boolean variable
+ * and the action a(e) and the initial state should be altered accordingly.
+ *
+ * Note there is parameter of sort Pos because of linearisation.
+ */
+void test_case_4()
+{
+  const std::string text(
+    "sort D = struct d1|d2;\n"
+    "act a,b:D;\n"
+    "proc P(e:D) = sum d:D . a(e) . b(d) . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::pos() || i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 2);
+}
+
+/*
+ * Sort D has got 9 constructors, therefore it should be encoded in
+ * a vector of 4 boolean variables. Note that this does leave a lot of
+ * combinations of booleans that are not used (7 to be precise).
+ * Process parameter e should be replaced by a vector of boolean variables,
+ * and should be replaced in nextstate.
+ * The initial state should be altered accordingly.
+ */
+void test_case_5()
+{
+  const std::string text(
+    "sort D = struct d1|d2|d3|d4|d5|d6|d7|d8|d9;\n"
+    "act a;\n"
+    "proc P(e:D) = sum d:D . a . P(d);\n"
+    "init P(d1);\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 4);
+}
+
+/*
+ * Sort D is a sort with 4 constructors. It is a recursive construct of two
+ * constructors parameterized with sort E, which is in turn a sort which has
+ * got two simple constructors.
+ * Sort D can be coded in a vector of 2 boolean variables, in which all
+ * combinations are used.
+ * Process parameter e should be replaced by a vector of boolean variables,
+ * and should be replaced in nextstate.
+ * The initial state should be altered accordingly.
+ */
+void test_case_6()
+{
+  const std::string text(
+    "sort D = struct d1(E) | d2(E);\n"
+    "     E = struct e1 | e2;\n"
+    "act a;\n"
+    "proc P(e:D) = sum d:D . a . P(d);\n"
+    "init P(d1(e1));\n"
+  );
+
+  specification s0 = mcrl22lps(text);
+  Rewriter* r = createRewriter(s0.data());
+  specification s1 = binary(s0, *r);
+  summand_list summands1 = s1.process().summands();
+
+  int bool_param_count = 0;
+  for (data_variable_list::iterator i = s1.process().process_parameters().begin();
+       i != s1.process().process_parameters().end();
+       ++i)
+  {
+    BOOST_CHECK(i->sort() == sort_expr::bool_());
+    if (i->sort() == sort_expr::bool_())
+    {
+      ++bool_param_count;
+    }
+  }
+  BOOST_CHECK(bool_param_count == 2);
+}
+
+int test_main(int ac, char** av)
+{
+  MCRL2_ATERM_INIT(ac, av)
+
+  test_case_1();
+  test_case_2();
+  test_case_3();
+  test_case_4();
+  test_case_5();
+  test_case_6();
+
+  return 0;
+}
+
