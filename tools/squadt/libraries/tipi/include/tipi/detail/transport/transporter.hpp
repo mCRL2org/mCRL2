f53393bee8fea05c831feacad9848ea9b940078a
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2008-05-18 19:18:24 +0000

    Changes  - corrected substitution mistake in file headers
        the copyright notice was missing the URI to the Boost license
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4563 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/squadt/libraries/tipi/include/tipi/detail/transport/transporter.hpp
+++ tools/squadt/libraries/tipi/include/tipi/detail/transport/transporter.hpp
@@ -3,7 +3,7 @@
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
-// LICENSE_1_0.txt)
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 /// \file transport/transporter.hpp
 
--- tools/txt2pbes/txt2pbes.cpp
+++ tools/txt2pbes/txt2pbes.cpp
@@ -1,7 +1,7 @@
 // Author(s): Aad Mathijssen and Simona Orzan. Distributed under
 // Copyright: see the accompanying file COPYING.
 // the Boost (See accompanying file LICENSE_1_0.txt or copy at
-// LICENSE_1_0.txt)
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 /// \file ./txt2pbes.cpp
 
