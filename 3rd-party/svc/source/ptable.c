3f78869110308c222c65134b1df56abb787a78fb
Aad Mathijssen <a.h.j.mathijssen@tue.nl>
2008-07-07 16:40:15 +0000

    Changed license of the SVC library from the GNU General Public License, version 2 (or newer), to the GNU Lesser General Public License, version 2.1 (or newer).
    This license change has been formally approved by Wan Fokkink.
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4824 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- 3rd-party/svc/source/ptable.c
+++ 3rd-party/svc/source/ptable.c
@@ -5,8 +5,8 @@
                        The  Netherlands
 
    This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1
    of the License, or (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
