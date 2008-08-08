5aecc2988f4c04c0975c111aaaa0a34914387d5b
Carst Tankink <c.tankink@student.tue.nl>
2008-08-08 12:40:40 +0000

    Preliminary version of the font_rendering utility library. This library provides a class that can render (anti-aliased) text on a (wx)GL context.
    The library's functionality is mainly based on font rendering functions of Diagraphica, with some generalisations, and some inspiration from Grape.
    
    At the moment the new LTSgraph version uses this library.
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4867 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/utilities/source/font_renderer.cpp
@@ -0,0 +1,266 @@
+// Author(s): Carst Tankink, based on font renderers by VitaminB100 and 
+//            A. J. (Hannes) Pretorius
+//            
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file font_renderer.cpp
+//
+// \brief Implements the font renderer class.
+
+#include "font_renderer.h"
+#include "font/character_set.xpm"
+#include <wx/image.h>
+
+namespace mcrl2 {
+  namespace utilities {
+    
+    font_renderer::font_renderer()
+    {
+      // Create textures.
+      //
+      int red = 0;
+      int green = 0;
+      int blue = 0;
+
+      // Allocate texture memory
+      glGenTextures(CHARSETSIZE, tex_char_id);
+
+      // Create the textures from the character set
+      for(size_t i = 0; i < CHARSETSIZE; ++i)
+      {
+        // Bind texture
+        glBindTexture(GL_TEXTURE_2D, tex_char_id[i]);
+
+        // Create image from xpm
+        wxImage image(characters[i]);
+        // Read in rgb-value of texture
+        int count = 0;
+
+        for (size_t h = 0; h < CHARHEIGHT; ++h)
+        {
+          for (size_t w = 0; w < CHARWIDTH; ++w)
+          {   
+            red   = (GLubyte)image.GetRed(w, h);
+            green = (GLubyte)image.GetGreen(w, h);  
+            blue  = (GLubyte)image.GetBlue(w, h);
+            
+            tex_char[i][count] = (GLubyte)(255.0 - (red + green + blue) / 3.0);
+
+            ++count;
+          } 
+        }
+        
+        // Build a MIP map for the texture. This gives faster renders and better
+        // anti-aliasing effects. See http://en.wikipedia.org/wiki/Mipmap for an
+        // introduction to the technique.
+        
+        gluBuild2DMipmaps(
+          GL_TEXTURE_2D,
+          GL_ALPHA,
+          CHARWIDTH, 
+          CHARHEIGHT,
+          GL_ALPHA,
+          GL_UNSIGNED_BYTE,
+          tex_char[i] );
+        
+      }
+    };
+
+
+    font_renderer::~font_renderer()
+    {
+      // Free texture resources
+    };
+
+    void font_renderer::draw_text(
+      const std::string s,
+      const double x, 
+      const double y,
+      const double scale,
+      const Alignment align_horizontal,
+      const Alignment align_vertical)
+    {
+      // Render text at the specified location
+      double xSLft;
+      double ySBot;
+
+      switch(align_horizontal)
+      {
+        case al_left: 
+        {
+          xSLft = x - (s.length() * CHARWIDTH * scale);
+          break;
+        }
+        case al_center:
+        {
+          xSLft = x - (0.5 * s.length() * CHARWIDTH * scale);
+          break;
+        }
+
+        case al_right: // Fall through to default case
+        default: 
+        {
+          xSLft = x;
+          break;
+        }
+      }
+
+      switch(align_vertical)
+      {
+        case al_top: 
+        { 
+          ySBot = y; 
+          break;
+        }
+        case al_bottom:
+        {
+          ySBot = y - (CHARHEIGHT * scale); 
+          break;
+        }
+
+        case al_center: // Fall through to default case
+        default:
+        {
+          ySBot = y - (.5 * CHARHEIGHT * scale);
+          break;
+        }
+      }
+
+      // Check if there actually is a label to render.
+      if(s.size() > 0)
+      {
+        // Enable texture mapping
+        glEnable( GL_TEXTURE_2D);
+        
+
+
+        for( size_t i = 0; i < s.length(); ++i)
+        {
+          double xLft = xSLft + i * scale * CHARWIDTH;
+          double xRgt = xSLft + (i+1) * scale * CHARWIDTH;
+          double yTop = ySBot + .5 * scale * CHARHEIGHT;
+          double yBot = ySBot - .5 * scale * CHARHEIGHT;
+          
+          size_t index = index_from_char(s[i]);
+          glBindTexture(GL_TEXTURE_2D, tex_char_id[index]);
+
+          // Setup texture parameters
+          glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+          glTexParameterf(GL_TEXTURE_2D, 
+                          GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+          glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+          glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+          glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+        
+          glEnable(GL_BLEND);
+          glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+          glBegin(GL_QUADS);
+            glTexCoord2f(0.0, 0.0); glVertex3f(xLft, yTop, 0.5);
+            glTexCoord2f(0.0, 1.0); glVertex3f(xLft, yBot, 0.5);
+            glTexCoord2f(1.0, 1.0); glVertex3f(xRgt, yBot, 0.5);
+            glTexCoord2f(1.0, 0.0); glVertex3f(xRgt, yTop, 0.5);
+          glEnd();
+          glDisable(GL_BLEND);
+        }
+      }
+    }
+
+    void font_renderer::draw_bounding_box(
+      const std::string s,
+      const double x, 
+      const double y,
+      const double scale,
+      const Alignment align_horizontal,
+      const Alignment align_vertical,
+      const bool draw_border)
+    {
+      // Render text's bb at the specified location
+    }
+       
+    void font_renderer::draw_cropped_text(
+      const std::string s,
+      const double x, 
+      const double y,
+      const double xLft, 
+      const double xRgt,
+      const double yTop, 
+      const double yBot,
+      const double scale,
+      const Alignment align_horizontal,
+      const Alignment align_vertical)
+    {
+      // Crop text to fit into bounding box.
+    }
+
+    size_t font_renderer::index_from_char(const char & c)
+    {
+      size_t result = 80; // Question mark
+      size_t ascii = static_cast<int>(c);
+
+      // lowercase
+      if (97 <= ascii & ascii <= 122)
+      {
+        result = ascii - 97;
+      }
+
+      // uppercase
+      else if (65  <= ascii && ascii <= 90)
+      {
+        result = ascii -65 + 26;
+      }
+      
+      // Numers 0 ... 9
+      else if ( 48 <= ascii && ascii <= 58)
+      {
+        result = ascii - 48 + 52;
+      }
+
+      // Special characters
+      else
+      {
+        switch (c)
+        {
+          case ' ': result = 62; break;
+          case '_':  result = 63; break;
+          case '-':  result = 64; break;
+          case '|':  result = 65; break;
+          case '/': result = 66; break;
+          case '\\': result = 67; break;
+          case '"':  result = 68; break;
+          case '\'': result = 69; break;
+          case ':':  result = 70; break;
+          case ';':  result = 71; break;
+          case '.':  result = 72; break;
+          case ',':  result = 73; break;
+          case '(':  result = 74; break;
+          case ')':  result = 75; break;
+          case '[':  result = 76; break;
+          case ']':  result = 77; break;
+          case '{':  result = 78; break;
+          case '}':  result = 79; break;
+          case '?':  result = 80; break;
+          case '<':  result = 81; break;
+          case '>':  result = 82; break;
+          case '+':  result = 83; break;
+          case '=':  result = 84; break;
+          case '*':  result = 85; break;
+          case '&':  result = 86; break;
+          case '^':  result = 87; break;
+          case '%':  result = 88; break;
+          case '$':  result = 89; break;
+          case '#':  result = 90; break;
+          case '@':  result = 91; break;
+          case '!':  result = 92; break;
+          case '`':  result = 93; break;
+          case '~':  result = 94; break;
+          default :  result = 80; break;
+        }
+      }
+
+      return result;
+    }
+  } // namespace utilities
+} // namespace mcrl2
