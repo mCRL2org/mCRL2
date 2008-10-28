203bbc969e654ba74645855a75bca8e614fede40
Jeroen Keiren <j.j.a.keiren@tue.nl>
2008-10-28 08:56:05 +0000

    - Copy over the data specification parser from the old data library, and adjust   it to skip the data implementation phase for the new data library. - Add a simple test to trigger the parser code - Long forgotten fix for ltsmin Jamfile
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/new-data-lib@5031 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/data/test/parser_test.cpp
@@ -0,0 +1,42 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file compatibility_test.cpp
+/// \brief Regression test for the transformations between new and old data
+///       format.
+
+#include <iostream>
+#include <boost/range/iterator_range.hpp>
+#include <boost/test/minimal.hpp>
+
+#include "mcrl2/data/parser.h"
+
+using namespace mcrl2;
+
+void parser_test()
+{
+  const std::string text(
+    "sort S;\n"
+    "cons s:S;\n"
+    "map f:S -> List(S);\n"
+  );
+
+  data::data_specification spec(data::parse_data_specification(text));
+  BOOST_CHECK(spec.sorts().size() == 1);
+  BOOST_CHECK(spec.constructors().size() == 1);
+  BOOST_CHECK(spec.functions().size() == 1);
+}
+
+int test_main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv);
+
+  parser_test();
+
+  return EXIT_SUCCESS;
+}
