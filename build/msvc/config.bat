3ed2a17e670a8d0bddf0fa826f55c4f70c1fe851
Frank Stappers <f.p.m.stappers@tue.nl>
2008-11-14 12:23:11 +0000

    modified config.bat so that it properly executes with windows command-line prompt.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5102 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- build/msvc/config.bat
+++ build/msvc/config.bat
@@ -1,10 +1,10 @@
-echo off
 echo Configuring source tree...
 
 copy config.jam ..\config.jam
 
 echo done!
 echo Bootstrapping bjam (as build\bin\bjam)
-..\..\3rd-party\boost\tools\jam\build.bat msvc
-
+cd ..\..\3rd-party\boost\tools\jam\
+call build.bat msvc
+cd ..\..\..\..\build\msvc
 pause
