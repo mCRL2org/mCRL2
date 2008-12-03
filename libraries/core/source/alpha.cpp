66c52737189e72188f3c812d698818e3e4c63b39
Aad Mathijssen <a.h.j.mathijssen@tue.nl>
2008-12-03 18:22:40 +0000

    Corrected copy-paste error.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5238 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/source/alpha.cpp
+++ libraries/core/source/alpha.cpp
@@ -6,7 +6,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-/// \file pnml2mcrl2.cpp
+/// \file alpha.cpp
 
 #include <cstdio>
 #include <cstdlib>
