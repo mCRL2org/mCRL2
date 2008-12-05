a2b5c20c30a160ec851ba7d0dcab7f63289d6e9f
Wieger Wesselink <j.w.wesselink@tue.nl>
2008-12-05 09:40:40 +0000

    - Made a new implementation of pbesrewr, using the filter_tool_with_rewriter class. It is still in the pbes/example directory, but it can be used in the tool directory without any modifications. I leave it up to Jan Friso to actually make this change.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5257 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/example/pbesrewr.cpp
+++ libraries/pbes/example/pbesrewr.cpp
@@ -1,109 +1,74 @@
-// Author(s): Wieger Wesselink
+// Author(s): Jan Friso Groote, Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 /// \file pbesrewr.cpp
-/// \brief Add your file description here.
 
-#include <cstdlib>
+#include "boost.hpp" // precompiled headers
+
 #include <iostream>
 #include <string>
-#include <stdexcept>
-#include <boost/program_options.hpp>
-#include "mcrl2/core/messaging.h"
-#include "mcrl2/pbes/rewriter.h"
+#include "mcrl2/utilities/filter_tool_with_rewriter.h"
+#include "mcrl2/data/identifier_generator.h"
+#include "mcrl2/data/enumerator.h"
+#include "mcrl2/data/rewriter.h"
+#include "mcrl2/pbes/pbes.h"
 #include "mcrl2/pbes/pbesrewr.h"
+#include "mcrl2/pbes/rewriter.h"
 
 using namespace mcrl2;
 using namespace mcrl2::pbes_system;
-namespace po = boost::program_options;
+using namespace mcrl2::core;
+using namespace mcrl2::utilities;
+
+class pbes_rewr_tool: public utilities::filter_tool_with_rewriter
+{
+  public:
+    pbes_rewr_tool()
+      : filter_tool_with_rewriter(
+          "pbesrewr",
+          "Jan friso Groote, Wieger Wesselink",
+          "Rewrite the PBES in INFILE, remove quantified variables and write the resulting PBES to OUTFILE. "
+          "If INFILE is not present, stdin is used. If OUTFILE is not present, stdout is used."
+        )
+    {}
+
+    bool run()
+    {
+      if (mcrl2::core::gsVerbose)
+      {
+        std::cout << "pbesrewr parameters:" << std::endl;
+        std::cout << "  input file:         " << m_input_filename << std::endl;
+        std::cout << "  output file:        " << m_output_filename << std::endl;
+      }
+    
+      // load the pbes
+      pbes<> p;
+      p.load(m_input_filename);
+      
+      // data rewriter
+      data::rewriter datar = create_rewriter(p.data());
+     
+      // pbes rewriter
+      simplifying_rewriter<pbes_system::pbes_expression, data::rewriter> pbesr(datar);    
+      
+      // apply the rewriter
+      pbesrewr(p, pbesr);
+      
+      // save the result
+      p.save(m_output_filename);
+      
+      return true;
+    }
+};
 
 int main(int argc, char* argv[])
 {
   MCRL2_ATERMPP_INIT(argc, argv)
-
-  typedef data::rewriter my_data_rewriter;
-  typedef simplifying_rewriter<pbes_system::pbes_expression, data::rewriter> my_pbes_rewriter;
-
-  std::string infile;            // location of pbes
-  std::string outfile;           // location of result
-
-  try {
-    //--- pbesrewr options ---------
-    boost::program_options::options_description pbesrewr_options(
-      "Usage: pbesrewr INFILE OUTFILE\n"
-      "\n"
-      "Reads a file containing a pbes, and applies a rewriter to it.\n"
-      "\n"
-      "Options"
-    );
-    pbesrewr_options.add_options()
-      ("help,h", "display this help")
-      ("verbose,v", "display short intermediate messages")
-      ;
-
-    //--- hidden options ---------
-    po::options_description hidden_options;
-    hidden_options.add_options()
-      ("input-file", po::value<std::string>(&infile), "input file")
-      ("output-file", po::value<std::string>(&outfile), "output file")
-    ;
-
-    //--- positional options ---------
-    po::positional_options_description positional_options;
-    positional_options.add("input-file", 1);
-    positional_options.add("output-file", 1);
-
-    //--- command line options ---------
-    po::options_description cmdline_options;
-    cmdline_options.add(pbesrewr_options).add(hidden_options);
-
-    po::variables_map var_map;
-    po::store(po::command_line_parser(argc, argv).
-        options(cmdline_options).positional(positional_options).run(), var_map);
-    po::notify(var_map);
-
-    if (var_map.count("help")) {
-      std::cout << pbesrewr_options << "\n";
-      return 1;
-    }
-    if (var_map.count("debug")) {
-      mcrl2::core::gsSetDebugMsg();
-    }
-    if (var_map.count("verbose")) {
-      mcrl2::core::gsSetVerboseMsg();
-    }
-
-    // load the pbes
-    pbes<> p;
-    p.load(infile);
-
-    // data rewriter
-    my_data_rewriter datar(p.data());
-
-    // pbes rewriter
-    my_pbes_rewriter pbesr(datar);    
-
-    // apply the rewriter
-    pbesrewr(p, pbesr);
-    
-    // save the result
-    p.save(outfile);
-  }
-  catch(mcrl2::runtime_error e)
-  {
-    std::cerr << "runtime error: " << e.what() << std::endl;
-    std::exit(1);
-  }
-  catch(std::exception& e) {
-    std::cerr << "error: " << e.what() << "\n";
-    return 1;
-  }
-  catch(...) {
-    std::cerr << "exception of unknown type!\n";
-  }
-
-  return 0;
+  pbes_rewr_tool tool;
+  return tool.execute(argc, argv);
 }
