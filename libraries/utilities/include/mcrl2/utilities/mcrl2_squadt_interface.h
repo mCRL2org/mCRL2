mCRL2org/mCRL2

commit e6953c2997d78f864d9822a80d5088f108ba99ba

Performed task #514; the mCRL2 LTS format can now store a data, action and parameter specification. This removes the need to supply an additional LPS file for conversion to FSM and enables tools like ltsview to display the correct parameter names. This extra information is automatically added by lps2lts on creation of an mCRL2 LTS.
Also, from now on the extension of mCRL2 LTS is .lts instead of .svc and
.svc now defaults to LTSs in the muCRL format. Old mCRL2 LTSs can still be
used without any problem and without renaming them. Only things like lps2lts
abp.lps abp.svc will no longer generate an mCRL2 LTS but an muCRL LTS. Also,
the mime type has been changed to remove the mentioning of SVC, so it might
be the case that old mCRL2 LTSs in a Squadt project are no longer usable.

I also noticed that FSM files without state information were not parsed
correctly to an mCRL2 or muCRL LTS (with an LPS); all states were mapped to
one single state. This is fixed now.


git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5213 21252fe5-60e8-0310-8ff0-c1ae1001594e

tools/lps2lts/squadt_interactor.ipp
@@ -156,7 +156,7 @@ class squadt_interactor::storage_configuration {
             mt = tipi::mime_type("application/svc+mcrl");
             break;
           case mcrl2::lts::lts_mcrl2:
-            mt = tipi::mime_type("application/svc+mcrl2");
+            mt = tipi::mime_type("application/mcrl2-lts");
             break;
 #ifdef USE_BCG
           case mcrl2::lts::lts_bcg:
@@ -174,7 +174,7 @@ class squadt_interactor::storage_configuration {
             throw mcrl2::runtime_error("unsupported LTS format used for output");
         }
       } else {
-        outfile = c.get_output_name(".svc");
+        outfile = c.get_output_name(".lts");
       }
 
       if (c.output_exists(squadt_interactor::lts_file_for_output)) {
@@ -275,7 +275,7 @@ void squadt_interactor::user_interactive_configuration(tipi::configuration& c)
                     append(lts_type_selector.associate(mcrl2::lts::lts_fsm, "FSM")).
                     append(lts_type_selector.associate(mcrl2::lts::lts_svc, "SVC")).
                     append(lts_type_selector.associate(mcrl2::lts::lts_mcrl, "SVC/mCRL")).
-                    append(lts_type_selector.associate(mcrl2::lts::lts_mcrl2, "SVC/mCRL2", true)));
+                    append(lts_type_selector.associate(mcrl2::lts::lts_mcrl2, "mCRL2", true)));
     }
 
     boost::shared_ptr < storage_configuration > storage_controls(new storage_configuration(c, d, m));

tools/ltsconvert/ltsconvert.cpp
@@ -155,8 +155,6 @@ class t_tool_options {
             gsWarningMsg("no output format set or detected; using default (mcrl2)\n");
             outtype = lts_mcrl2;
           }
-        } else if ( (outtype == lts_fsm) && lpsfile.empty() ) {
-          gsWarningMsg("parameter names are unknown (use -l/--lps option)\n");
         }
       }
     }
@@ -222,8 +220,9 @@ t_tool_options parse_command_line(int ac, char** av) {
 #endif
     "  'dot' for the GraphViz format (output only),\n"
     "  'fsm' for the Finite State Machine format,\n"
-    "  'mcrl' for the mCRL SVC format, or\n"
-    "  'mcrl2' for the mCRL2 SVC format (default)"
+    "  'mcrl' for the mCRL SVC format,\n"
+    "  'mcrl2' for the mCRL2 format (default), or"
+    "  'svc' for the (generic) SVC format\n"
   );
 
   clinterface.add_option("no-reach",
@@ -375,7 +374,7 @@ static const char* option_selected_output_format             = "selected_output_
 static const char* option_no_reachability_check              = "no_reachability_check";                 ///< do not check reachability of input LTS
 static const char* option_no_state_information               = "no_state_information";                  ///< dot format output specific option to not save state information
 static const char* option_tau_actions                        = "tau_actions";                           ///< the actions that should be recognised as tau
-static const char* option_add_bisimulation_equivalence_class = "add_bisimulation_equivalence_class";    ///< adds bisimulation equivalence class to the state information of a state instead of actually reducing modulo bisimulation [mCRL2 SVC specific]
+static const char* option_add_bisimulation_equivalence_class = "add_bisimulation_equivalence_class";    ///< adds bisimulation equivalence class to the state information of a state instead of actually reducing modulo bisimulation [mCRL2 specific]
 
 class squadt_interactor : public mcrl2::utilities::squadt::mcrl2_tool_interface {
 
@@ -402,7 +401,7 @@ class squadt_interactor : public mcrl2::utilities::squadt::mcrl2_tool_interface 
 
 void squadt_interactor::set_capabilities(tipi::tool::capabilities& c) const {
   c.add_input_configuration(lts_file_for_input, tipi::mime_type("aut", tipi::mime_type::text), tipi::tool::category::conversion);
-  c.add_input_configuration(lts_file_for_input, tipi::mime_type("svc+mcrl2", tipi::mime_type::application), tipi::tool::category::conversion);
+  c.add_input_configuration(lts_file_for_input, tipi::mime_type("mcrl2-lts", tipi::mime_type::application), tipi::tool::category::conversion);
   c.add_input_configuration(lts_file_for_input, tipi::mime_type("svc+mcrl", tipi::mime_type::application), tipi::tool::category::conversion);
   c.add_input_configuration(lts_file_for_input, tipi::mime_type("svc", tipi::mime_type::application), tipi::tool::category::conversion);
   c.add_input_configuration(lts_file_for_input, tipi::mime_type("fsm", tipi::mime_type::text), tipi::tool::category::conversion);
@@ -428,7 +427,7 @@ void squadt_interactor::user_interactive_configuration(tipi::configuration& c) {
                 append(d.create< label >().set_text("Output format : ")).
                 append(format_selector.associate(mcrl2::lts::lts_aut, "Aldebaran",true)).
                 append(format_selector.associate(mcrl2::lts::lts_mcrl, "SVC/mCRL")).
-                append(format_selector.associate(mcrl2::lts::lts_mcrl2, "SVC/mCRL2")).
+                append(format_selector.associate(mcrl2::lts::lts_mcrl2, "mCRL2")).
                 append(format_selector.associate(mcrl2::lts::lts_svc, "SVC")).
 #ifdef USE_BCG
                 append(format_selector.associate(mcrl2::lts::lts_bcg, "BCG")).
@@ -542,7 +541,7 @@ void squadt_interactor::user_interactive_configuration(tipi::configuration& c) {
   if ((format_selector.get_selection() == lts_fsm && (
          c.get_input(lts_file_for_input).type().sub_type() == "svc" ||
          c.get_input(lts_file_for_input).type().sub_type() == "svc+mcrl" ||
-         c.get_input(lts_file_for_input).type().sub_type() == "svc+mcrl2"))
+         c.get_input(lts_file_for_input).type().sub_type() == "mcrl2-lts"))
    || (format_selector.get_selection() == lts_mcrl2 && (
          c.get_input(lts_file_for_input).type().sub_type() == "aut" ||
          c.get_input(lts_file_for_input).type().sub_type() == "svc" ||

libraries/lts/include/mcrl2/lts/detail/liblts_private.h
@@ -83,6 +83,7 @@
       ATerm *label_values;
       transition *transitions;
       unsigned int init_state;
+      ATerm extra_data;
 
       p_lts(lts *l);
 

libraries/lts/include/mcrl2/lts/lts.h
@@ -23,6 +23,7 @@
 #include <iostream>
 #include <aterm2.h>
 #include <mcrl2/atermpp/set.h>
+#include <mcrl2/data/data_specification.h>
 #include <mcrl2/lps/specification.h>
 
 #ifdef USE_BCG
@@ -176,6 +177,19 @@ namespace lts
    * \param[in] time The time part of the timed action that will be created.
    * \return A timed action with label \a action and time \a time. */
   ATermAppl make_timed_pair(ATermAppl action, ATermAppl time = NULL);
+
+  /** \brief Add an mCRL2 data specification, parameter list and action
+   *         specification to a mCRL2 LTS in SVC format.
+   * \param[in] filename   The file name of the mCRL2 LTS.
+   * \param[in] data_spec  The data specification to add in mCRL2 internal
+   *                       format (or NULL for none).
+   * \param[in] params     The list of state(/process) parameters in mCRL2
+   *                       internal format (or NULL for none).
+   * \param[in] act_spec   The action specification to add in mCRL2 internal
+   *                       format (or NULL for none).
+   * \pre                  The LTS in filename is a mCRL2 SVC without extra
+   *                       information. */
+  void add_extra_mcrl2_svc_data(std::string const &filename, ATermAppl data_spec, ATermList params, ATermAppl act_spec);
   
   /** \cond */
   #include "detail/liblts_private.h"
@@ -720,6 +734,32 @@ namespace lts
        * \return           The pretty-printed representation of value.
        * \pre              value occurs as state parameter value in this LTS. */
       std::string pretty_print_state_parameter_value(ATerm value);
+
+
+      /** \brief Return the extra information stored in this LTS.
+       * \return The extra information stored in this LTS or NULL if there
+       *         is none. */
+      ATerm get_extra_data();
+      
+      /** \brief Set (or remove) the extra information for this LTS.
+       * \param[in] data  The extra information for this LTS or NULL to
+       *                  remove any possible extra information. */
+      void set_extra_data(ATerm data);
+      
+      /** \brief Check whether this LTS has an mCRL2 data specification.
+       * \return A boolean indicating whether this LTS has an mCRL2 data
+       *         specification. */
+      bool has_data_specification();
+      
+      /** \brief Return the mCRL2 data specification of this LTS.
+       * \return The mCRL2 data specification of this LTS.
+       * \pre    This LTS has an mCRL2 data specification. */
+      data::data_specification get_data_specification();
+      
+      /** \brief Set the mCRL2 data specification of this LTS.
+       * \param[in] spec  The mCRL2 data specification for this LTS.
+       * \pre             This is an mCRL2 LTS. */
+      void set_data_specification(data::data_specification spec);
 
       /** \brief Sorts the transitions using a sort style.
        * \param[in] ts The sort style to use. */

libraries/lts/source/bsimdata.cpp
@@ -677,5 +677,14 @@ int WriteDataAddParam(lts &l, bool is_branching)
     set_new_state(l,i);
   }
 
+  ATermAppl extra = (ATermAppl) l.get_extra_data();
+  if ( extra != NULL && !gsIsNil(ATAgetArgument(extra,1)) )
+  {
+    ATermList params = ATLgetArgument(ATAgetArgument(extra,1),0);
+    params = ATappend(params,(ATerm) gsMakeDataVarId(gsString2ATermAppl("bb_class"),gsMakeSortIdNat()));
+    extra = ATsetArgument(extra,(ATerm) ATsetArgument(ATAgetArgument(extra,1),(ATerm) params,0),1);
+    l.set_extra_data((ATerm) extra);
+  }
+
   return 1;
 }

libraries/lts/source/liblts.cpp
@@ -172,6 +172,7 @@ lts::~lts()
   {
     ATunprotectArray(label_values);
   }
+  ATunprotect(&extra_data);
 
   free(state_values);
   free(taus);
@@ -198,6 +199,9 @@ void p_lts::init(bool state_info, bool label_info)
   transitions_size = 0;
   ntransitions = 0;
   transitions = NULL;
+
+  extra_data = NULL;
+  ATprotect(&extra_data);
   
   this->type = lts_none;
   this->state_info = state_info;
@@ -220,6 +224,9 @@ void p_lts::init(p_lts const &l)
   type = l.type;
   state_info = l.state_info;
   label_info = l.label_info;
+
+  extra_data = l.extra_data;
+  ATprotect(&extra_data);
  
   if ( state_info )
   {
@@ -270,6 +277,7 @@ void p_lts::clear(bool state_info, bool label_info)
   clear_labels();
   clear_transitions();
   clear_type();
+  extra_data = NULL;
 
   this->state_info = state_info;
   this->label_info = label_info;
@@ -953,7 +961,12 @@ bool lts::write_to(string const& filename, lts_type type, lts_extra extra)
         case le_mcrl2:
           return write_to_fsm(filename,*extra.get_mcrl2_spec());
         default:
-          return write_to_fsm(filename);
+          if ( this->type == lts_mcrl2 && extra_data != NULL )
+          {
+            return write_to_fsm(filename,lts_mcrl2,ATLgetArgument(ATAgetArgument((ATermAppl) extra_data,1),0));
+          } else {
+            return write_to_fsm(filename);
+          }
       }
     case lts_dot:
       if ( extra.get_type() == le_dot )
@@ -996,7 +1009,12 @@ bool lts::write_to(ostream &os, lts_type type, lts_extra extra)
         case le_mcrl2:
           return write_to_fsm(os,*extra.get_mcrl2_spec());
         default:
-          return write_to_fsm(os);
+          if ( this->type == lts_mcrl2 && extra_data != NULL && !gsIsNil(ATAgetArgument((ATermAppl) extra_data,1)) )
+          {
+            return write_to_fsm(os,lts_mcrl2,ATLgetArgument(ATAgetArgument((ATermAppl) extra_data,1),0));
+          } else {
+            return write_to_fsm(os);
+          }
       }
     case lts_dot:
       if ( extra.get_type() == le_dot )
@@ -1293,7 +1311,7 @@ string p_lts::p_state_value_str(unsigned int state)
   if ( state_info )
   {
     ATerm value = state_values[state];
-    if ( ATisAppl(value) ) // XXX better check for mCRL2
+    if ( ATisAppl(value) && !strcmp(ATgetName(ATgetAFun((ATermAppl) value)),"STATE") )
     {
       s = "(";
       ATermList args = ATgetArguments((ATermAppl) value);
@@ -1480,9 +1498,14 @@ ATerm lts::state_parameter_name(unsigned int idx)
 {
   if ( type == lts_mcrl2 )
   {
-    char s[2+sizeof(unsigned int)*3];
-    sprintf(s,"p%u",idx);
-    return (ATerm) gsMakeDataVarId(gsString2ATermAppl(s),(ATermAppl) state_parameter_sort(idx));
+    if ( extra_data != NULL && !gsIsNil(ATAgetArgument((ATermAppl) extra_data,1)) )
+    {
+      return ATelementAt(ATLgetArgument(ATAgetArgument((ATermAppl) extra_data,1),0),idx);
+    } else {
+      char s[2+sizeof(unsigned int)*3];
+      sprintf(s,"p%u",idx);
+      return (ATerm) gsMakeDataVarId(gsString2ATermAppl(s),(ATermAppl) state_parameter_sort(idx));
+    }
   } else if ( type == lts_mcrl )
   {
     char s[2+sizeof(unsigned int)*3];
@@ -1501,9 +1524,14 @@ std::string lts::state_parameter_name_str(unsigned int idx)
 {
   if ( type == lts_mcrl2 || type == lts_mcrl )
   {
-    char s[2+sizeof(unsigned int)*3];
-    sprintf(s,"p%u",idx);
-    return s;
+    if ( extra_data != NULL && !gsIsNil(ATAgetArgument((ATermAppl) extra_data,1)) )
+    {
+      return ATgetName(ATgetAFun(ATAgetArgument(ATAelementAt(ATLgetArgument(ATAgetArgument((ATermAppl) extra_data,1),0),idx),0)));
+    } else {
+      char s[2+sizeof(unsigned int)*3];
+      sprintf(s,"p%u",idx);
+      return s;
+    }
   } else if ( type == lts_fsm )
   {
     return ATgetName(ATgetAFun((ATermAppl) state_parameter_name(idx)));
@@ -1678,6 +1706,43 @@ std::string lts::pretty_print_state_parameter_value(ATerm value)
   return "";
 }
 
+ATerm lts::get_extra_data()
+{
+  return extra_data;
+}
+
+void lts::set_extra_data(ATerm data)
+{
+  extra_data = data;
+}
+
+bool lts::has_data_specification()
+{
+  return extra_data != NULL &&
+         ATisAppl(extra_data) &&
+         !strcmp("mCRL2LTS1",ATgetName(ATgetAFun((ATermAppl) extra_data))) &&
+         !gsIsNil(ATAgetArgument((ATermAppl) extra_data,0));
+}
+
+data::data_specification lts::get_data_specification()
+{
+  assert(has_data_specification());
+
+  return data::data_specification(ATAgetArgument((ATermAppl) extra_data,0));
+}
+
+void lts::set_data_specification(data::data_specification spec)
+{
+  assert( type == lts_mcrl2 );
+
+  if ( extra_data == NULL )
+  {
+    extra_data = (ATerm) ATmakeAppl3(ATmakeAFun("mCRL2LTS1",3,ATfalse),(ATerm)(ATermAppl) spec, (ATerm) gsMakeNil(), (ATerm) gsMakeNil());
+  } else {
+    extra_data = (ATerm) ATsetArgument((ATermAppl) extra_data,(ATerm)(ATermAppl) spec,0);
+  }
+}
+
 void p_lts::p_remove_state_values()
 {
   state_info = false;
@@ -1686,6 +1751,10 @@ void p_lts::p_remove_state_values()
     ATunprotectArray(state_values);
     free(state_values);
     state_values = NULL;
+  }
+  if ( type == lts_mcrl2 && extra_data != NULL )
+  {
+    extra_data = (ATerm) ATsetArgument((ATermAppl) extra_data,(ATerm) gsMakeNil(),1);
   }
 }
 
@@ -2062,24 +2131,28 @@ lts_type lts::guess_format(string const& s) {
 
     if ( ext == "aut" )
     {
-      gsVerboseMsg("detected AUT extension\n");
+      gsVerboseMsg("detected Aldebaran extension\n");
       return lts_aut;
+    } else if ( ext == "lts" )
+    {
+      gsVerboseMsg("detected mCRL2 extension\n");
+      return lts_mcrl2;
     } else if ( ext == "svc" )
     {
-      gsVerboseMsg("detected SVC extension; assuming mCRL2 format\n");
-      return lts_mcrl2;
+      gsVerboseMsg("detected SVC extension; assuming mCRL format\n");
+      return lts_mcrl;
     } else if ( ext == "fsm" )
     {
-      gsVerboseMsg("detected FSM extension\n");
+      gsVerboseMsg("detected Finite State Machine extension\n");
       return lts_fsm;
     } else if ( ext == "dot" )
     {
-      gsVerboseMsg("detected dot extension\n");
+      gsVerboseMsg("detected GraphViz extension\n");
       return lts_dot;
 #ifdef USE_BCG
     } else if ( ext == "bcg" )
     {
-      gsVerboseMsg("detected BCG extension\n");
+      gsVerboseMsg("detected Binary Coded Graph extension\n");
       return lts_bcg;
 #endif
     }
@@ -2095,7 +2168,7 @@ lts_type lts::parse_format(std::string const& s) {
   } else if ( s == "mcrl" || s == "svc+mcrl")
   {
     return lts_mcrl;
-  } else if ( s == "mcrl2" || s == "svc+mcrl2")
+  } else if ( s == "mcrl2" || s == "mcrl2-lts")
   {
     return lts_mcrl2;
   } else if ( s == "svc" )
@@ -2119,7 +2192,7 @@ lts_type lts::parse_format(std::string const& s) {
 
 std::string p_lts::type_strings[]      = { "unknown", "mCRL2", "AUT", "mCRL", "SVC", "FSM", "dot", "BCG" };
                                                                                                       
-std::string p_lts::extension_strings[] = { "", "svc", "aut", "svc", "svc", "fsm", "dot", "bcg" };
+std::string p_lts::extension_strings[] = { "", "lts", "aut", "svc", "svc", "fsm", "dot", "bcg" };
 
 std::string lts::string_for_type(const lts_type type) {
   return (type_strings[type]);
@@ -2190,6 +2263,51 @@ std::string lts::string_for_preorder(const lts_preorder pre)
 std::string lts::name_of_preorder(const lts_preorder pre)
 {
   return preorder_desc_strings[pre];
+}
+
+void add_extra_mcrl2_svc_data(std::string const &filename, ATermAppl data_spec, ATermList params, ATermAppl act_spec)
+{
+  FILE *f = fopen(filename.c_str(),"ab");
+  if ( f == NULL )
+  {
+    gsErrorMsg("could not open file '%s' to add extra LTS information\n",filename.c_str());
+    return;
+  }
+
+  ATerm arg1 = (ATerm) ((data_spec == NULL)?gsMakeNil():data_spec);
+  ATerm arg2 = (ATerm) ((params == NULL)?gsMakeNil():ATmakeAppl1(ATmakeAFun("ParamSpec",1,ATfalse),(ATerm) params));
+  ATerm arg3 = (ATerm) ((act_spec == NULL)?gsMakeNil():act_spec);
+  ATerm data = (ATerm) ATmakeAppl3(ATmakeAFun("mCRL2LTS1",3,ATfalse),arg1,arg2,arg3);
+
+  long position;
+  if ( (position = ftell(f)) == -1 )
+  {
+    gsErrorMsg("could not determine file size of '%s'; not adding extra information\n",filename.c_str());
+    fclose(f);
+    return;
+  }
+
+  if ( ATwriteToBinaryFile(data,f) == ATfalse )
+  {
+    gsErrorMsg("error writing extra LTS information to '%s', file could be corrupted\n",filename.c_str());
+    fclose(f);
+    return;
+  }
+
+  unsigned char buf[8+12+1] = "XXXXXXXX   1STL2LRCm";
+  for (unsigned int i=0; i<8; i++)
+  {
+    buf[i] = position % 0x100;
+    position /= 0x100;
+  }
+  if ( fwrite(buf,1,8+12,f) != 8+12 )
+  {
+    gsErrorMsg("error writing extra LTS information to '%s', file could be corrupted\n",filename.c_str());
+    fclose(f);
+    return;
+  }
+
+  fclose(f);
 }
 
 }

libraries/lts/source/liblts_fsm.cpp
@@ -140,25 +140,39 @@ bool p_lts::read_from_fsm(std::istream &is, lts_type type, lps::specification *s
           return false;
         }
       }
-      for (unsigned int i=0; i<nstates; i++)
+      if ( nstates > 0 && ATgetLength((ATermList) state_values[0]) == 0 )
       {
-        state_values[i] = parse_mcrl2_state(state_values[i],*spec);
-        if ( state_values[i] == NULL )
+        p_remove_state_values();
+      } else {
+        for (unsigned int i=0; i<nstates; i++)
         {
-          return false;
+          state_values[i] = parse_mcrl2_state(state_values[i],*spec);
+          if ( state_values[i] == NULL )
+          {
+            return false;
+          }
         }
       }
+      extra_data = (ATerm) ATmakeAppl3(ATmakeAFun("mCRL2LTS1",3,ATfalse),
+              (ATerm)(ATermAppl) spec->data(),
+              (ATerm) ATmakeAppl1(ATmakeAFun("ParamSpec",1,ATfalse),(ATerm)(ATermList) spec->process().process_parameters()),
+              ATgetArgument((ATermAppl) *spec,1));
       this->type = lts_mcrl2;
     } else if ( type == lts_mcrl ) {
-      for (unsigned int i=0; i<nstates; i++)
+      if ( nstates > 0 && ATgetLength((ATermList) state_values[0]) == 0 )
       {
-        ATermList m = ATmakeList0();
-        for (ATermList l=ATreverse((ATermList) state_values[i]); !ATisEmpty(l); l=ATgetNext(l))
+        p_remove_state_values();
+      } else {
+        for (unsigned int i=0; i<nstates; i++)
         {
-          ATerm a = ATmake(ATgetName(ATgetAFun(ATAgetArgument(ATAgetFirst(l),0))));
-          m = ATinsert(m,a);
+          ATermList m = ATmakeList0();
+          for (ATermList l=ATreverse((ATermList) state_values[i]); !ATisEmpty(l); l=ATgetNext(l))
+          {
+            ATerm a = ATmake(ATgetName(ATgetAFun(ATAgetArgument(ATAgetFirst(l),0))));
+            m = ATinsert(m,a);
+          }
+          state_values[i] = (ATerm) m;
         }
-        state_values[i] = (ATerm) m;
       }
       this->type = lts_mcrl;
     } else {
@@ -197,6 +211,7 @@ bool p_lts::write_to_fsm(std::ostream &os, lts_type type, ATermList params)
     }
   } else {
     num_params = 0;
+    gsWarningMsg("parameter names are unknown (use LTS with extra information or supply the source LPS)\n");
   }
 
   // create set per parameter containing all used values
@@ -263,9 +278,9 @@ bool p_lts::write_to_fsm(std::ostream &os, lts_type type, ATermList params)
         s = s.substr(1,s.size()-2);
         os << s << " ";
       } else { // type == lts_mcrl2
-        PrintPart_CXX(os,ATgetFirst(ATLgetFirst(params)),ppDefault);
+        PrintPart_CXX(os,ATgetFirst(params),ppDefault);
         os << "(" << ATgetLength(vals) << ") ";
-        PrintPart_CXX(os,ATgetFirst(ATgetNext(ATLgetFirst(params))),ppDefault);
+        PrintPart_CXX(os,ATgetArgument(ATAgetFirst(params),1),ppDefault);
         os << " ";
       }
 
@@ -394,19 +409,13 @@ static ATermList get_lps_params(ATerm lps)
 
   if ( lps != NULL )
   {
-    params = ATmakeList0();
-
     if ( ATisAppl(lps) && gsIsLinProcSpec((ATermAppl) lps) )
     {
-      ATermList pars = ATLgetArgument(ATAgetArgument((ATermAppl) lps,5),1);
-      for (; !ATisEmpty(pars); pars=ATgetNext(pars))
-      {
-        params = ATinsert(params,(ATerm) ATmakeList2(ATgetFirst(pars),(ATerm) gsGetSort(ATAgetFirst(pars))));
-      }
-      params = ATreverse(params);
+      params = ATLgetArgument(ATAgetArgument((ATermAppl) lps,3),1);
     } else if ( ATisAppl(lps) && !strcmp(ATgetName(ATgetAFun((ATermAppl) lps)),"spec2gen") )
     {
       ATermList pars = ATLgetArgument(ATAgetArgument((ATermAppl) lps,1),1);
+      params = ATmakeList0();
       for (; !ATisEmpty(pars); pars=ATgetNext(pars))
       {
         params = ATinsert(params,(ATerm) ATmakeList2(ATgetArgument(ATAgetFirst(pars),0),ATgetArgument(ATAgetFirst(pars),1)));
@@ -422,18 +431,7 @@ static ATermList get_lps_params(ATerm lps)
 
 static ATermList get_lps_params(lps::linear_process &lps)
 {
-  ATermList params = ATmakeList0();
-
-  data::data_variable_list pars = lps.process_parameters();
-  data::data_variable_list::iterator pb = pars.begin();
-  data::data_variable_list::iterator pe = pars.end();
-  for (; pb != pe; pb++)
-  {
-    ATermAppl p = *pb;
-    params = ATinsert(params,(ATerm) ATmakeList2((ATerm) p,(ATerm) gsGetSort(p)));
-  }
-
-  return ATreverse(params);
+  return lps.process_parameters();
 }
 
 static bool isATermString(ATerm a)

commit a65606ed3a848dfb23d0c87a9b9233530745136b

Changes:
 - fixed bug introduced in r5213 (the lts format changes)

    When operating in SQuADt context the LTS storage format to MIME-type
    conversion was no longer consistent between tools and for some file types
    the wrong file extensions were being used (always .lts instead of .aut or .svc,
    ...).

 - centralised lts-type to MIME-type conversion

    This is still very sub-optimal; any change in the lts::lts_type enumerated
    type may break things again. For a proper fix the lts library must offer
    an interface for iterating the supported storage formats.



git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5324 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/utilities/include/mcrl2/utilities/mcrl2_squadt_interface.h
@@ -131,6 +131,16 @@ namespace mcrl2 {
           }
       };
 
+#ifdef __LIBLTS_H
+      inline tipi::mime_type lts_type_to_mime_type(const mcrl2::lts::lts_type f) {
+        std::string type_strings[] = { "unknown", "mcrl2-lts", "aut", "svc+mcrl", "svc", "fsm", "dot", "bcg", "" };
+  
+        return tipi::mime_type(type_strings[f],
+            ((f == mcrl2::lts::lts_aut || f == mcrl2::lts::lts_fsm || f == mcrl2::lts::lts_dot) ?
+          	 tipi::mime_type::text : tipi::mime_type::application));
+      }
+#endif
+
       /** \brief Used to relay messages generated using core::print */
       inline void relay_message(const ::mcrl2::core::messageType t, const char* data) {
         tipi::report::type report_type;

tools/lps2lts/squadt_interactor.ipp
@@ -137,54 +137,21 @@ class squadt_interactor::storage_configuration {
     }
 
     void update_configuration(boost::shared_ptr< squadt_interactor::storage_configuration >, tipi::configuration& c) {
+      using namespace mcrl2::lts;
+
       /* Add output file to the configuration */
-      std::string outfile;
-      tipi::mime_type mt("application/unknown");
+      std::string     output_name(c.get_output_name("." +
+          lts::extension_for_type(c.get_option_argument< mcrl2::lts::lts_type >(option_lts_type))));
+      tipi::mime_type output_type(mcrl2::utilities::squadt::lts_type_to_mime_type(c.get_option_argument< mcrl2::lts::lts_type >(option_lts_type)));
 
-      if ( c.option_exists(option_lts_type) )
-      {
-        outfile = c.get_output_name("."+mcrl2::lts::lts::extension_for_type(c.get_option_argument< mcrl2::lts::lts_type >(option_lts_type)));
-        switch ( c.get_option_argument< mcrl2::lts::lts_type >(option_lts_type) )
-        {
-          case mcrl2::lts::lts_aut:
-            mt = tipi::mime_type("text/aut");
-            break;
-          case mcrl2::lts::lts_svc:
-            mt = tipi::mime_type("application/svc");
-            break;
-          case mcrl2::lts::lts_mcrl:
-            mt = tipi::mime_type("application/svc+mcrl");
-            break;
-          case mcrl2::lts::lts_mcrl2:
-            mt = tipi::mime_type("application/mcrl2-lts");
-            break;
-#ifdef USE_BCG
-          case mcrl2::lts::lts_bcg:
-            mt = tipi::mime_type("application/bcg");
-            break;
-#endif
-          case mcrl2::lts::lts_fsm:
-            mt = tipi::mime_type("text/fsm");
-            break;
-          case mcrl2::lts::lts_dot:
-            mt = tipi::mime_type("text/dot");
-            break;
-          default:
-            assert(0);
-            throw mcrl2::runtime_error("unsupported LTS format used for output");
-        }
-      } else {
-        outfile = c.get_output_name(".lts");
-      }
+      if (c.output_exists(lts_file_for_output)) {
+        tipi::configuration::object& output_file = c.get_output(lts_file_for_output);
 
-      if (c.output_exists(squadt_interactor::lts_file_for_output)) {
-        tipi::configuration::object& o = c.get_output(lts_file_for_output);
-
-        o.type(mt);
-        o.location(outfile);
+        output_file.type(output_type);
+        output_file.location(output_name);
       }
       else {
-        c.add_output(lts_file_for_output, mt, outfile);
+        c.add_output(lts_file_for_output, output_type, output_name);
       }
 
       c.add_option(option_out_info).set_argument_value< 0 >(cb_out_info.get_status());

tools/ltsconvert/ltsconvert.cpp
@@ -515,29 +515,19 @@ void squadt_interactor::user_interactive_configuration(tipi::configuration& c) {
   /* Wait until the ok button was pressed */
   okay_button.await_change();
 
-  static std::string extensions[6] = {
-    lts::extension_for_type(lts_aut),
-    lts::extension_for_type(lts_mcrl),
-    lts::extension_for_type(lts_mcrl2),
-#ifdef USE_BCG
-    lts::extension_for_type(lts_bcg),
-#endif
-    lts::extension_for_type(lts_fsm),
-    lts::extension_for_type(lts_dot)
-  };
-
   /* Add output file to the configuration */
+  std::string     output_name(c.get_output_name("." +
+                    lts::extension_for_type(format_selector.get_selection())));
+  tipi::mime_type output_type(mcrl2::utilities::squadt::lts_type_to_mime_type(format_selector.get_selection()));
+
   if (c.output_exists(lts_file_for_output)) {
-    std::string  extension(extensions[format_selector.get_selection()]);
     tipi::configuration::object& output_file = c.get_output(lts_file_for_output);
 
-    output_file.type(tipi::mime_type(extension));
-    output_file.location(c.get_output_name("." + extension));
+    output_file.type(output_type);
+    output_file.location(output_name);
   }
   else {
-    std::string  extension(extensions[format_selector.get_selection()]);
-
-    c.add_output(lts_file_for_output, tipi::mime_type(extension), c.get_output_name("." + extension));
+    c.add_output(lts_file_for_output, output_type, output_name);
   }
 
   /* Add lps file when output is FSM format or when the output is mCRL2 and the input is Aldebaran or mCRL */