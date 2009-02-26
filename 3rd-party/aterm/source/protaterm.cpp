e9707d20190b95d4acbab8e402daa576aa291503
Muck van Weerdenburg <m.j.van.weerdenburg@tue.nl>
2009-02-26 03:15:23 +0000

    Fixed bug introduced in r5699; I mistakenly removed the registration of a function that is needed to protect ATerms in atermpp containers.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5701 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- 3rd-party/aterm/source/protaterm.cpp
+++ 3rd-party/aterm/source/protaterm.cpp
@@ -8,9 +8,17 @@ IProtectedATerm::~IProtectedATerm()
 }
 
 typedef multiset<IProtectedATerm *> pa_container;
-static pa_container p_aterms;
 
-void AT_protectProtectedATerms()
+static void AT_protectProtectedATerms();
+
+static pa_container initialise_p_aterms()
+{
+  ATaddProtectFunction(AT_protectProtectedATerms);
+  return pa_container();
+}
+static pa_container p_aterms = initialise_p_aterms();
+
+static void AT_protectProtectedATerms()
 {
   for (pa_container::iterator i=p_aterms.begin(); i!=p_aterms.end(); i++)
   {
