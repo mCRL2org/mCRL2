mCRL2org/mCRL2

commit e9707d20190b95d4acbab8e402daa576aa291503

Fixed bug introduced in r5699; I mistakenly removed the registration of a function that is needed to protect ATerms in atermpp containers.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5701 21252fe5-60e8-0310-8ff0-c1ae1001594e

3rd-party/aterm/source/protaterm.cpp
@@ -8,9 +8,17 @@ IProtectedATerm::~IProtectedATerm()
 }
 
 typedef multiset<IProtectedATerm *> pa_container;
-static pa_container p_aterms;
 
-void AT_protectProtectedATerms()
+static void AT_protectProtectedATerms();
+
+static pa_container initialise_p_aterms()
+{
+  ATaddProtectFunction(AT_protectProtectedATerms);
+  return pa_container();
+}
+static pa_container p_aterms = initialise_p_aterms();
+
+static void AT_protectProtectedATerms()
 {
   for (pa_container::iterator i=p_aterms.begin(); i!=p_aterms.end(); i++)
   {

commit 9996a5685b09bde90b701b3612b3356bfc79ac7a

Fixed problem with static initialisation (#578) introduced in r5701.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5738 21252fe5-60e8-0310-8ff0-c1ae1001594e

3rd-party/aterm/source/protaterm.cpp
@@ -16,11 +16,15 @@ static pa_container initialise_p_aterms()
   ATaddProtectFunction(AT_protectProtectedATerms);
   return pa_container();
 }
-static pa_container p_aterms = initialise_p_aterms();
+static pa_container &p_aterms()
+{
+  static pa_container _p_aterms = initialise_p_aterms();
+  return _p_aterms;
+}
 
 static void AT_protectProtectedATerms()
 {
-  for (pa_container::iterator i=p_aterms.begin(); i!=p_aterms.end(); i++)
+  for (pa_container::iterator i=p_aterms().begin(); i!=p_aterms().end(); i++)
   {
     (*i)->ATprotectTerms();
   }
@@ -28,10 +32,10 @@ static void AT_protectProtectedATerms()
 
 void ATprotectProtectedATerm(IProtectedATerm *i)
 {
-  p_aterms.insert(i);
+  p_aterms().insert(i);
 }
 
 void ATunprotectProtectedATerm(IProtectedATerm *i)
 {
-  p_aterms.erase(p_aterms.find(i));
+  p_aterms().erase(p_aterms().find(i));
 }