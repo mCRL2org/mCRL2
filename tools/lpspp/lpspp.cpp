6a074cf89617c5dbbcb2183212059720f62ecbb5
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2009-04-06 12:13:25 +0000

    Changes:  - fix for pretty printing (debug mode)
        Would have worked out-of-the-box if the lps framework had been used.
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/new-data-lib@5918 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/lpspp/lpspp.cpp
+++ tools/lpspp/lpspp.cpp
@@ -130,7 +130,7 @@ void print_specification_file_name(t_tool_options const& tool_options)
   }
 
   new_data::data_specification data_spec(ATAgetArgument(spec, 0));
-  ATsetArgument(spec, atermpp::aterm((new_data::detail::data_specification_to_aterm_data_spec(data_spec))), 0);
+  spec = ATsetArgument(spec, atermpp::aterm((new_data::detail::data_specification_to_aterm_data_spec(data_spec))), 0);
 
   //open output file for writing or set to stdout
   FILE *output_stream    = NULL;
