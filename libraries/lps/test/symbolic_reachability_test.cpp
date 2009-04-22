d9b58b0122a23d6dd0b085d498dc59a51294b0c4
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2009-04-22 19:54:53 +0000

    Changes:  - added symbolic reachability test
        This is the analysis part used by the ltsmin toolset adapter for mcrl2.
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6057 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lps/test/symbolic_reachability_test.cpp
@@ -0,0 +1,254 @@
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <set>
+#include <vector>
+#include <stack>
+
+#include <boost/test/minimal.hpp>
+
+#include "mcrl2/lps/nextstate.h"
+#include "mcrl2/lps/specification.h"
+#include "mcrl2/lps/mcrl22lps.h"
+#include "mcrl2/atermpp/set.h"
+
+const std::string case_no_influenced_parameters(
+  "act a;\n\n"
+  "proc X(i: Nat) = a.X(i);\n\n"
+  "init X(0);\n");
+
+const std::string case_influenced_condition(
+  "act a;\n\n"
+  "proc X(i: Nat) = (i < 4) -> a.X(i) <> delta;\n\n"
+  "init X(0);\n");
+
+const std::string case_influenced_next(
+  "act a;\n\n"
+  "proc X(i: Nat, j : Nat) = a.X(j, i);\n\n"
+  "init X(0, 0);\n");
+
+const std::string case_influenced_action(
+  "act a : Nat;\n\n"
+  "proc X(i: Nat) = a(i + 1).X(i);\n\n"
+  "init X(0);\n");
+
+const std::string case_influenced_time(
+  "act a;\n\n"
+  "proc X(i: Nat) = a@(i + 1).X(i);\n\n"
+  "init X(0);\n");
+
+const std::string case_two_parameters(
+  "act a : Nat;\n\n"
+  "proc X(i: Nat, j : Nat) = (j < 4) -> a(i).X(i, j + 1) <> delta;\n\n"
+  "init X(0, 1);\n");
+
+const std::string case_summands(
+  "act a: Nat;\n\n"
+  "proc X(i: Nat, j : Nat) = a(j + 1).X(i, j) + a(i + 1).X(i, j);\n\n"
+  "init X(0, 1);\n");
+
+const std::string case_last(
+  "act a: Nat;\n\n"
+  "sort D = struct d1 | d2;\n"
+  "proc X(id : Nat, n : Nat, dd:D) = sum d:D. a(id).X(id,(n + 1) mod 10, d);\n\n"
+  "init allow({a},X(1,0,d1) || X(2,0,d1));\n");
+
+class group_information {
+
+  private:
+
+    mcrl2::lps::specification const&     m_model;
+
+    std::vector< std::vector< size_t > > m_group_indices;
+
+  private:
+
+    void gather(mcrl2::lps::specification const& l);
+
+  public:
+
+    /**
+     * \brief constructor from an mCRL2 lps
+     **/
+    group_information(mcrl2::lps::specification const& l) : m_model(l) {
+      gather(l);
+    }
+
+    /**
+     * \brief The number of groups (summands in the LPS)
+     * \return lps::specification(l).summands().size()
+     **/
+    inline size_t number_of_groups() const {
+      return m_group_indices.size();
+    }
+
+    inline size_t number_of_parameters() const {
+      return m_model.process().process_parameters().size();
+    }
+
+    /**
+     * \brief Indices of process parameters that influence event or next state of a summand
+     * \param[in] index the selected summand
+     * \returns reference to a vector of indices of parameters
+     **/
+    inline std::vector< size_t > const& operator[](size_t index) const {
+      return m_group_indices[index];
+    }
+};
+
+void group_information::gather(mcrl2::lps::specification const& l) {
+  using namespace mcrl2;
+
+  using new_data::find_all_variables;
+  using new_data::variable;
+
+  struct local {
+    static void add_used_variables(std::set< variable >& r, std::set< variable > const& c) {
+      r.insert(c.begin(), c.end());
+    }
+  };
+
+  lps::linear_process specification(l.process());
+
+  // the set with process parameters
+  std::set< variable > parameters = find_all_variables(specification.process_parameters());
+
+  // the list of summands
+  std::vector< lps::summand > summands(specification.summands().begin(), specification.summands().end());
+
+  m_group_indices.resize(summands.size());
+
+  for (std::vector< lps::summand >::const_iterator i = summands.begin(); i != summands.end(); ++i) {
+    std::set< variable > used_variables;
+
+    local::add_used_variables(used_variables, find_all_variables(i->condition()));
+    local::add_used_variables(used_variables, find_all_variables(i->actions()));
+
+    if (i->has_time()) {
+      local::add_used_variables(used_variables, find_all_variables(i->time()));
+    }
+
+    new_data::assignment_list assignments(i->assignments());
+
+    for (new_data::assignment_list::const_iterator j = assignments.begin(); j != assignments.end(); ++j) {
+      if(j->lhs() != j->rhs()) {
+        local::add_used_variables(used_variables, find_all_variables(j->lhs()));
+        local::add_used_variables(used_variables, find_all_variables(j->rhs()));
+      }
+    }
+
+    // process parameters used in condition or action of summand
+    std::set< variable > used_parameters;
+
+    std::set_intersection(used_variables.begin(), used_variables.end(),
+                          parameters.begin(), parameters.end(), std::inserter(used_parameters, used_parameters.begin()));
+
+    std::vector< variable > parameters_list(specification.process_parameters().begin(), specification.process_parameters().end());
+
+    for (std::vector< variable >::const_iterator j = parameters_list.begin(); j != parameters_list.end(); ++j) {
+      if (used_parameters.find(*j) != used_parameters.end()) {
+        m_group_indices[i - summands.begin()].push_back(j - parameters_list.begin());
+      }
+    }
+  }
+}
+
+void check_info(mcrl2::lps::specification const& model) {
+  group_information info(model);
+
+  BOOST_CHECK(info.number_of_groups() == model.process().summands().size());
+
+#ifdef SHOW_INFO
+  for (size_t i = 0; i < info.number_of_groups(); ++i) {
+    std::vector< size_t > const& group_info(info[i]);
+
+    std::cerr << "group " << i << " : {";
+
+    if (!group_info.empty()) {
+      std::cerr << group_info[0];
+    }
+
+    for (std::vector< size_t >::const_iterator j = ++group_info.begin(); j < group_info.end(); ++j) {
+
+      std::cerr << "," << *j;
+    }
+
+    std::cerr << "}" << std::endl;
+  }
+#endif
+}
+
+mcrl2::lps::specification convert(mcrl2::lps::specification const& l) {
+  mcrl2::lps::linear_process lp(l.process());
+
+  mcrl2::lps::summand_list summands;
+
+  for (mcrl2::lps::non_delta_summand_list::iterator i = lp.non_delta_summands().begin(); i != lp.non_delta_summands().end(); ++i) {
+    summands = push_front(summands, *i);
+  }
+
+  summands = reverse(summands);
+
+  return mcrl2::lps::specification(l.data(), l.action_labels(),
+            mcrl2::lps::linear_process(lp.free_variables(), lp.process_parameters(), summands),
+               l.initial_process());
+}
+
+int test_main(int argc, char** argv) {
+  MCRL2_ATERMPP_INIT_DEBUG(argc, argv);
+
+  using namespace mcrl2;
+  using namespace mcrl2::lps;
+
+  check_info(mcrl22lps(case_no_influenced_parameters));
+  check_info(mcrl22lps(case_influenced_condition));
+  check_info(mcrl22lps(case_influenced_action));
+  check_info(mcrl22lps(case_influenced_time));
+  check_info(mcrl22lps(case_influenced_next));
+  check_info(mcrl22lps(case_two_parameters));
+  check_info(mcrl22lps(case_summands));
+  check_info(mcrl22lps(case_last));
+
+  lps::specification model(mcrl22lps(case_no_influenced_parameters));
+
+  if (1 < argc) {
+    model.load(argv[1]);
+
+    model = convert(model);
+
+    // Note the second argument that specifies that don't care variables are not treated specially
+    NextState* explorer = createNextState(model, false);
+
+    std::stack< ATerm >     stack;
+    atermpp::set< ATerm >   known;
+
+    stack.push(explorer->getInitialState());
+    known.insert(stack.top());
+
+    while (!stack.empty()) {
+      ATerm     current(stack.top());
+
+      stack.pop();
+
+      for (size_t i = 0; i < model.process().summands().size(); ++i) {
+        ATerm     state;
+        ATermAppl transition;
+
+        std::auto_ptr< NextStateGenerator > generator(explorer->getNextStates(current, i));
+
+        while (generator->next(&transition, &state)) {
+          if (known.find(state) == known.end()) {
+            ATwarning("%t\n", state);
+
+            known.insert(state);
+            stack.push(state);
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
