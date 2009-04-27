ce73f4b9a02466a31c7b6ad200b4f9c92b3ea5a7
Jeroen Keiren <j.j.a.keiren@tue.nl>
2009-04-27 14:03:06 +0000

    - Fix compilation of bes.h, for a BES we only need to check whether the initial   state is defined in the BES, as there are no sorts.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6100 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/bes.h
+++ libraries/pbes/include/mcrl2/pbes/bes.h
@@ -641,7 +641,7 @@ namespace bes {
       {
         atermpp::set<boolean_variable> bnd = binding_variables();
         atermpp::set<boolean_variable> occ = occurring_variables();
-        return std::includes(bnd.begin(), bnd.end(), occ.begin(), occ.end()) && is_declared_in(bnd.begin(), bnd.end(), initial_state());
+        return std::includes(bnd.begin(), bnd.end(), occ.begin(), occ.end()) && bnd.find(initial_state()) != bnd.end();
       }
 
       /// \brief Applies a low level substitution function to this term.
