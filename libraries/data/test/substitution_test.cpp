e57eaa46a0c9217db3b9d67952ed294d9593b2a5
Jeroen Keiren <j.j.a.keiren@tue.nl>
2009-04-27 08:46:51 +0000

    - Add test for assignment_list substitution.   Let g denote the assignment list [x:=y, y:=z].   Do we expect g(x) to be y or z (I'd say z, but currently the result is y).
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6094 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/data/test/substitution_test.cpp
+++ libraries/data/test/substitution_test.cpp
@@ -11,6 +11,8 @@
 
 #include <boost/test/minimal.hpp>
 
+#include "mcrl2/data/assignment.h"
+#include "mcrl2/data/assignment_list_substitution.h"
 #include "mcrl2/data/lambda.h"
 #include "mcrl2/data/substitution.h"
 #include "mcrl2/data/mutable_substitution_adapter.h"
@@ -68,6 +70,34 @@ void test1()
   BOOST_CHECK(s(application(lambda(y,y),x) + y) != application(lambda(y,y), x) + c);
 }
 
+void test_assignment_list_substitution()
+{
+  variable x("x", sort_nat::nat());
+  variable y("y", sort_nat::nat());
+  variable z("z", sort_nat::nat());
+  variable u("u", sort_nat::nat());
+
+  assignment xy(x,y);
+  assignment uz(u,z);
+  assignment_list l(make_list(xy, uz));
+
+  assignment_list_substitution f(l);
+
+  BOOST_CHECK(f(x) == y);
+  BOOST_CHECK(f(y) == y);
+  BOOST_CHECK(f(z) == z);
+  BOOST_CHECK(f(u) == z);
+
+  assignment yz(y,z);
+  l = make_list(xy, uz, yz);
+  assignment_list_substitution g(l);
+
+  BOOST_CHECK(g(x) == z);
+  BOOST_CHECK(g(y) == z);
+  BOOST_CHECK(g(z) == z);
+  BOOST_CHECK(g(u) == z);
+}
+
 void test_mutable_substitution_adapter()
 {
   mutable_substitution<variable, data_expression> f;
@@ -110,6 +140,9 @@ int test_main(int a, char**aa)
   MCRL2_ATERMPP_INIT(a, aa);
 
   test1();
+  core::garbage_collect();
+  
+  test_assignment_list_substitution();
   core::garbage_collect();
 
   test_mutable_substitution_adapter();
