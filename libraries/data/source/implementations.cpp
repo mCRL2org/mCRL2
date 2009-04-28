7507994a9333ad360a312ead64d484d818b17515
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2009-04-28 16:33:22 +0000

    Changes:  - build performance experiment
        less .cpp files: lowering header-file inclusion overhead
    
        Incidentally, this also slightly lowers the target count and thereby the
        Boost Build v2 initialisation overhead.
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6111 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/data/source/implementations.cpp
@@ -0,0 +1,36 @@
+#include "mcrl2/core/messaging.h"
+#include "data_implementation_concrete.cpp"
+#include "data_reconstruct.cpp"
+#include "data_specification.cpp"
+#include "standard.cpp"
+#include "detail/enumerate/enum.cpp"
+#include "detail/enumerate/enum_standard.cpp"
+
+#undef ATisList
+#undef ATisAppl
+#undef ATisInt
+
+#undef ATAgetFirst
+#undef ATLgetFirst
+#undef ATAgetArgument
+#undef ATLgetArgument
+
+#undef MAX_VARS_INIT
+#undef MAX_VARS_FACTOR
+
+#undef fs_bottom
+#undef fs_top
+
+#undef fs_filled
+#undef ss_filled
+
+#include "detail/prover/bdd2dot.cpp"
+#include "detail/prover/bdd_path_eliminator.cpp"
+#include "detail/prover/bdd_prover.cpp"
+#include "detail/prover/bdd_simplifier.cpp"
+#include "detail/prover/formula_checker.cpp"
+#include "detail/prover/induction.cpp"
+#include "detail/prover/info.cpp"
+#include "detail/prover/manipulator.cpp"
+#include "detail/prover/prover.cpp"
+#include "detail/prover/smt_lib_solver.cpp"
