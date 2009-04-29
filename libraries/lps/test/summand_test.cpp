1454dcbba3e54bf89bee0c4e9cd91cd0b17e4761
Jeroen Keiren <j.j.a.keiren@tue.nl>
2009-04-29 06:59:33 +0000

    - Add very basic test for lps summands (forgot to commit this one with the other   tests yesterday)
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6115 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lps/test/summand_test.cpp
@@ -0,0 +1,56 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file summand_test.cpp
+/// \brief Simple test for summands
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <iterator>
+#include <boost/test/minimal.hpp>
+#include "mcrl2/core/garbage_collection.h"
+#include "mcrl2/core/detail/print_utility.h"
+#include "mcrl2/data/bool.h"
+#include "mcrl2/data/pos.h"
+#include "mcrl2/lps/summand.h"
+
+using namespace mcrl2;
+using namespace mcrl2::data;
+using namespace mcrl2::lps;
+
+void test_assignments()
+{
+  variable x("x", sort_pos::pos());
+  variable y("y", sort_pos::pos());
+  assignment xy(x,y);
+  assignment_list l(make_list(xy));
+  
+  multi_action a(action(action_label(core::identifier_string("a"), sort_expression_list()), data_expression_list()));
+    
+  summand s(variable_list(), data_expression(sort_bool_::true_()), a, l);
+
+  multi_action s_a = s.multi_action();
+  assignment_list s_l = s.assignments();
+
+  BOOST_CHECK(s.multi_action() == a);
+  BOOST_CHECK(s_a == a);
+  BOOST_CHECK(s.assignments() == l);
+  BOOST_CHECK(s_l == l);
+}
+
+int test_main(int argc, char* argv[])
+{
+  MCRL2_ATERMPP_INIT(argc, argv)
+
+  test_assignments();
+  core::garbage_collect();
+
+  return 0;
+}
+
