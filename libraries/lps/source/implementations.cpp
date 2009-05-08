7e5eb04fc073bfa4a6fbd9a867fd2554e2985e95
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2009-05-08 23:39:30 +0000

    Should have been part of previous changeset.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6201 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lps/source/implementations.cpp
@@ -0,0 +1,12 @@
+#include "boost.hpp" // precompiled headers
+
+#include "sumelm.cpp"
+#include "binary.cpp"
+#include "data_elimination.cpp"
+#include "invariant_checker.cpp"
+#include "nextstate_standard.cpp"
+#include "untime.cpp"
+#include "confluence_checker.cpp"
+#include "disjointness_checker.cpp"
+#include "invariant_eliminator.cpp"
+#include "suminst.cpp"
