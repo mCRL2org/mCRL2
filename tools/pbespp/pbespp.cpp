c88fae8e703fe4711c5e8447eeed4216fa5983a1
Aad Mathijssen <a.h.j.mathijssen@tue.nl>
2009-05-09 09:02:48 +0000

    - adapted pbespp to the input_output_tool class, saving 16 loc
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6203 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/pbespp/pbespp.cpp
+++ tools/pbespp/pbespp.cpp
@@ -25,10 +25,10 @@
 #include "mcrl2/core/print.h"
 #include "mcrl2/core/messaging.h"
 #include "mcrl2/core/aterm_ext.h"
-#include "mcrl2/utilities/command_line_interface.h"
-#include "mcrl2/utilities/command_line_messaging.h"
+#include "mcrl2/utilities/input_output_tool.h"
 #include "mcrl2/data/data_specification.h"
 
+using namespace mcrl2::utilities::tools;
 using namespace mcrl2::utilities;
 using namespace mcrl2::core;
 
@@ -39,30 +39,42 @@ int main(int argc, char *argv[]);
 //  argc represents the number of arguments
 //  argv represents the arguments
 
-struct t_tool_options {
-  std::string  specification_file_name;
-  std::string  output_file_name;
-  t_pp_format  format;
+static std::string pp_format_to_string(t_pp_format pp_format);
 
-  bool parse_command_line(int ac, char** av) {
-    interface_description clinterface(av[0], NAME, AUTHOR,
-      "pretty print a PBES",
-      "[OPTION]... [INFILE [OUTFILE]]\n",
-      "Print the PBES in INFILE to OUTFILE in a human readable format. If OUTFILE "
-      "is not present, stdout is used. If INFILE is not present, stdin is used.");
+class pbespp_tool: public input_output_tool
+{
+  public:
+    pbespp_tool()
+      : input_output_tool(NAME, AUTHOR,
+          "pretty print a PBES",
+          "Print the PBES in INFILE to OUTFILE in a human readable format. If OUTFILE "
+          "is not present, stdout is used. If INFILE is not present, stdin is used."
+        ),
+        format(ppDefault)
+    {}
 
-    clinterface.
-      add_option("format", make_mandatory_argument("FORMAT"),
+    bool run()
+    {
+      print_specification();
+      return true;
+    }
+
+  protected:
+    t_pp_format  format;
+
+    void add_options(interface_description& desc)
+    {
+      input_output_tool::add_options(desc);
+      desc.add_option("format", make_mandatory_argument("FORMAT"),
         "print the PBES in the specified FORMAT:\n"
         "  'internal' for a textual ATerm representation of the internal format,\n"
-        "  'default' for an PBES specification (default), or\n"
+        "  'default' for a PBES specification (default), or\n"
         "  'debug' for 'default' with the exceptions that data expressions are printed in prefix notation using identifiers from the internal format, and each data equation is put in a separate data equation section", 'f');
+    }
 
-    command_line_parser parser(clinterface, ac, av);
-
-    if (parser.continue_execution()) {
-      format = ppDefault;
-
+    void parse_options(const command_line_parser& parser)
+    {
+      input_output_tool::parse_options(parser);
       if (parser.options.count("format")) {
         std::string str_format(parser.option_argument("format"));
         if (str_format == "internal") {
@@ -73,88 +85,52 @@ struct t_tool_options {
           parser.error("option -f/--format has illegal argument '" + str_format + "'");
         }
       }
-
-      if (2 < parser.arguments.size()) {
-        parser.error("too many file arguments");
-      }
-      if (0 < parser.arguments.size()) {
-        specification_file_name = parser.arguments[0];
-      }
-      if (1 < parser.arguments.size()) {
-        output_file_name = parser.arguments[1];
-      }
     }
 
-    return parser.continue_execution();
-  }
+  private:
+    void print_specification()
+    {
+      std::string str_in  = (input_filename().empty())?"stdin":("'" + input_filename() + "'");
+      std::string str_out = (output_filename().empty())?"stdout":("'" + output_filename() + "'");
+      ATermAppl spec = (ATermAppl) mcrl2::core::detail::load_aterm(input_filename());
+    
+      mcrl2::data::data_specification data_spec(ATAgetArgument(spec, 0));
+    
+      spec = ATsetArgument(spec, atermpp::aterm((mcrl2::data::detail::data_specification_to_aterm_data_spec(
+        (format != ppDebug) ? mcrl2::data::remove_all_system_defined(data_spec) : data_spec))), 0);
+    
+      if (!mcrl2::core::detail::gsIsPBES(spec)) {
+        throw mcrl2::runtime_error(str_in + " does not contain an PBES");
+      }
+      //open output file for writing or set to stdout
+      FILE *output_stream    = NULL;
+      if (output_filename().empty()) {
+        output_stream = stdout;
+        gsDebugMsg("output to stdout.\n");
+      } else {
+        output_stream = fopen(output_filename().c_str(), "w");
+        if (output_stream == NULL) {
+          std::string err_msg(strerror(errno));
+          if (err_msg.length() > 0 && err_msg[err_msg.length()-1] == '\n') {
+            err_msg.replace(err_msg.length()-1, 1, "");
+          }
+          throw mcrl2::runtime_error("could not open output file " + str_out + " for writing (" + err_msg + ")");
+        }
+        gsDebugMsg("output file %s is opened for writing.\n", str_out.c_str());
+      }
+      assert(output_stream != NULL);
+      //print spec to output_stream
+      gsVerboseMsg("printing PBES from %s to %s in the %s format\n",
+        str_in.c_str(), str_out.c_str(), pp_format_to_string(format).c_str());
+      //pretty print spec to output_stream
+      PrintPart_C(output_stream, (ATerm) spec, format);
+      if (output_stream != stdout) {
+        fclose(output_stream);
+      }
+    }
 };
 
-static void print_specification_file_name(t_tool_options const& tool_options);
-
-static std::string pp_format_to_string(t_pp_format pp_format);
-
 //implementation
-
-int main(int argc, char* argv[]) {
-  MCRL2_ATERM_INIT(argc, argv)
-
-  try {
-    t_tool_options options;
-
-    if (options.parse_command_line(argc, argv)) {
-      //print specification
-      print_specification_file_name(options);
-    }
-  }
-  catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
-}
-
-void print_specification_file_name(t_tool_options const& tool_options)
-{
-  std::string str_in  = (tool_options.specification_file_name.empty())?"stdin":("'" + tool_options.specification_file_name + "'");
-  std::string str_out = (tool_options.output_file_name.empty())?"stdout":("'" + tool_options.output_file_name + "'");
-  ATermAppl spec = (ATermAppl) mcrl2::core::detail::load_aterm(tool_options.specification_file_name);
-
-  mcrl2::data::data_specification data_spec(ATAgetArgument(spec, 0));
-
-  spec = ATsetArgument(spec, atermpp::aterm((mcrl2::data::detail::data_specification_to_aterm_data_spec(
-    (tool_options.format != ppDebug) ?
-            mcrl2::data::remove_all_system_defined(data_spec) : data_spec))), 0);
-
-  if (!mcrl2::core::detail::gsIsPBES(spec)) {
-    throw mcrl2::runtime_error(str_in + " does not contain an PBES");
-  }
-  //open output file for writing or set to stdout
-  FILE *output_stream    = NULL;
-  if (tool_options.output_file_name.empty()) {
-    output_stream = stdout;
-    gsDebugMsg("output to stdout.\n");
-  } else {
-    output_stream = fopen(tool_options.output_file_name.c_str(), "w");
-    if (output_stream == NULL) {
-      std::string err_msg(strerror(errno));
-      if (err_msg.length() > 0 && err_msg[err_msg.length()-1] == '\n') {
-        err_msg.replace(err_msg.length()-1, 1, "");
-      }
-      throw mcrl2::runtime_error("could not open output file " + str_out + " for writing (" + err_msg + ")");
-    }
-    gsDebugMsg("output file %s is opened for writing.\n", str_out.c_str());
-  }
-  assert(output_stream != NULL);
-  //print spec to output_stream
-  gsVerboseMsg("printing PBES from %s to %s in the %s format\n",
-    str_in.c_str(), str_out.c_str(), pp_format_to_string(tool_options.format).c_str());
-  //pretty print spec to output_stream
-  PrintPart_C(output_stream, (ATerm) spec, tool_options.format);
-  if (output_stream != stdout) {
-    fclose(output_stream);
-  }
-}
 
 std::string pp_format_to_string(t_pp_format pp_format)
 {
@@ -168,3 +144,11 @@ std::string pp_format_to_string(t_pp_format pp_format)
     throw mcrl2::runtime_error("unknown pretty print format");
   }
 }
+
+int main(int argc, char* argv[])
+{
+  MCRL2_ATERM_INIT(argc, argv)
+  pbespp_tool tool;
+  return tool.execute(argc, argv);
+}
+
