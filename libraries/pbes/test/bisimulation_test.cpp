2df7d95259fb4f6441aeed3efef2003d0e64b11c
Wieger Wesselink <j.w.wesselink@tue.nl>
2009-07-03 07:43:39 +0000

    - Removed debug statements.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6681 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/test/bisimulation_test.cpp
+++ libraries/pbes/test/bisimulation_test.cpp
@@ -65,10 +65,6 @@ void test_bisimulation(const std::string& lps_spec)
 
   BOOST_CHECK(bb.is_well_typed());
   BOOST_CHECK(sb.is_well_typed());
-if (!wb.is_well_typed())
-{
-  wb.save("wb.pbes", true, true);
-}
   BOOST_CHECK(wb.is_well_typed());
   BOOST_CHECK(sbe.is_well_typed());
 }
