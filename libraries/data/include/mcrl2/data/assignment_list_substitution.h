39e53bd439c1ec34fedf365248cff9a2b572a604
Jeroen van der Wulp <j.v.d.wulp@tue.nl>
2009-07-09 11:06:20 +0000

    Changes:  - adapted to use new substitution functionality
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6721 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/data/include/mcrl2/data/assignment_list_substitution.h
+++ libraries/data/include/mcrl2/data/assignment_list_substitution.h
@@ -13,8 +13,9 @@
 #define MCRL2_NEWDATA_DATA_ASSIGNMENT_H
 
 #include <utility>
-#include "mcrl2/core/substitution_function.h"
+
 #include "mcrl2/data/variable.h"
+#include "mcrl2/data/substitution.h"
 #include "mcrl2/data/assignment.h"
 #include "mcrl2/data/data_expression.h"
 
@@ -27,78 +28,43 @@ namespace data {
 /// \brief Sequence of data assignments.
 // A linear search is done in the list of assignments.
 // Note that a data_assigment_list doesn't allow for an efficient implementation.
-struct assignment_list_substitution: public core::substitution_function<variable, data_expression>
+class assignment_list_substitution: public substitution< assignment_list_substitution >
 {
-  /// \brief An assignment list.
-  const assignment_list& m_assignments;
-
-  /// \cond INTERNAL_DOCS
-  struct compare_assignment_lhs
-  {
-    variable m_variable;
-
-    compare_assignment_lhs(const variable& variable)
-      : m_variable(variable)
-    {}
-
-    /// \brief Function call operator
-    /// \param a An assignment to a data variable
-    /// \return The function result
-    bool operator()(const assignment& a) const
-    {
-      return m_variable == a.lhs();
-    }
-  };
-
-  struct assignment_list_substitution_helper
-  {
-    const assignment_list& l;
-
-    assignment_list_substitution_helper(const assignment_list& l_)
-      : l(l_)
-    {}
-
-    /// \brief Function call operator
-    /// \param t A term
-    /// \return The function result
-    std::pair<atermpp::aterm_appl, bool> operator()(atermpp::aterm_appl t) const
-    {
-      if (!data_expression(t).is_variable())
-      {
-        return std::pair<atermpp::aterm_appl, bool>(t, true); // continue the recursion
-      }
-      assignment_list::const_iterator i = std::find_if(l.begin(), l.end(), compare_assignment_lhs(t));
-      if (i == l.end())
-      {
-        return std::pair<atermpp::aterm_appl, bool>(t, false); // don't continue the recursion
-      }
-      else
-      {
-        return std::pair<atermpp::aterm_appl, bool>(i->rhs(), false); // don't continue the recursion
-      }
-    }
-  };
-  /// \endcond
-
-  /// \brief Constructor.
-  /// \param assignments A sequence of assignments to data variables
-  assignment_list_substitution(const assignment_list& assignments)
-    : m_assignments(assignments)
-  {}
-
-  /// \brief Applies the assignments to the term t and returns the result.
-  /// \param t A term
-  /// \return The application of the assignments to the term.
-  data_expression operator()(data_expression t) const
-  {
-    return partial_replace(t, assignment_list_substitution_helper(m_assignments));
-  }
+  friend class substitution< assignment_list_substitution, variable_type, expression_type >;
 
   private:
+
+    /// \brief An assignment list.
+    const assignment_list& m_assignments;
+
+    /// \brief Applies the assignments to a variable v and returns the result.
+    /// \param v A term
+    /// \return The application of the assignments to the term.
+    data_expression apply(variable const& v) const
+    {
+      for (assignment_list::const_iterator i = m_assignments.begin(); i != m_assignments.end(); ++i)
+      {
+        if (i->lhs() == v)
+        {
+          return i->rhs();
+        }
+      }
+
+      return v;
+    }
+
     assignment_list_substitution& operator=(const assignment_list_substitution&)
     {
       return *this;
     }
+
+  public:
+
+    /// \brief Constructor.
+    /// \param assignments A sequence of assignments to data variables
+    assignment_list_substitution(const assignment_list& assignments)
+      : m_assignments(assignments)
+    {}
 };
 
 } // namespace data
