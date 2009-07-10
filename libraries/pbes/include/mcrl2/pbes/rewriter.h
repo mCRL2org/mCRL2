2b970ac530960ec9345ee77d1a99346f573f1033
Wieger Wesselink <j.w.wesselink@tue.nl>
2009-07-10 15:11:13 +0000

    - Added debug statements to enumerate_quantifiers_rewriter (requested by Jan Friso).
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6731 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/rewriter.h
+++ libraries/pbes/include/mcrl2/pbes/rewriter.h
@@ -215,7 +215,16 @@ namespace pbes_system {
         substitution_function sigma;
         detail::enumerate_quantifiers_builder<Term, DataRewriter, DataEnumerator, substitution_function> r(m_rewriter, m_enumerator, m_enumerate_infinite_sorts);
         simplifying_quantifier_rewriter<Term, DataRewriter> simplify(m_rewriter);
-        term_type result = r(simplify(x), sigma);
+        term_type y = simplify(x);
+if (core::gsDebug)
+{
+  std::cerr << "<simplify-step>" << core::pp(x) << " -> " << core::pp(y) << std::endl;
+}
+        term_type result = r(y, sigma);
+if (core::gsDebug)
+{
+  std::cerr << "<eliminate-quantifier-step>" << core::pp(y) << " -> " << core::pp(result) << std::endl;
+}
 #ifdef MCRL2_ENUMERATE_QUANTIFIERS_REWRITER_DEBUG
 std::cerr << "<enumerate-quantifiers>" << core::pp(x) << " -> " << core::pp(result) << std::endl;
 #endif
@@ -234,7 +243,16 @@ std::cerr << "<enumerate-quantifiers>" << core::pp(x) << " -> " << core::pp(resu
 
         detail::enumerate_quantifiers_builder<Term, DataRewriter, DataEnumerator, substitution_function> r(m_rewriter, m_enumerator, m_enumerate_infinite_sorts);
         simplifying_quantifier_rewriter<Term, DataRewriter> simplify(m_rewriter);
-        term_type result = r(simplify(x), substitution_function(sigma));
+        term_type y = simplify(x, sigma);
+if (core::gsDebug)
+{
+  std::cerr << "<simplify-step>" << core::pp(x) << " -> " << core::pp(y) << std::endl;
+}
+        term_type result = r(y, substitution_function(sigma));
+if (core::gsDebug)
+{
+  std::cerr << "<eliminate-quantifier-step>" << core::pp(y) << " -> " << core::pp(result) << std::endl;
+}
 #ifdef MCRL2_ENUMERATE_QUANTIFIERS_REWRITER_DEBUG
 std::cerr << "<enumerate-quantifiers>" << core::pp(x) << " -> " << core::pp(result) << data::to_string(sigma) << std::endl;
 #endif
