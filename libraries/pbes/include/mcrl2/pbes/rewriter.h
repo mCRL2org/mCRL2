d32ceaa3d334a6313e814fb2e7691f021b977161
Wieger Wesselink <j.w.wesselink@tue.nl>
2009-07-17 12:00:38 +0000

    - Switched off the simplify quantifier preprocessing step, since it causes errors under certain circumstances. It has to be investigated why. Using the flag MCRL2_SIMPLIFY_QUANTIFIERS the simplify quantifier preprocessing step can be added again.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6799 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/rewriter.h
+++ libraries/pbes/include/mcrl2/pbes/rewriter.h
@@ -213,6 +213,7 @@ namespace pbes_system {
 
         substitution_function sigma;
         detail::enumerate_quantifiers_builder<Term, DataRewriter, DataEnumerator, substitution_function> r(m_rewriter, m_enumerator, m_enumerate_infinite_sorts);
+#ifdef MCRL2_SIMPLIFY_QUANTIFIERS
         simplifying_quantifier_rewriter<Term, DataRewriter> simplify(m_rewriter);
         term_type y = simplify(x);
 if (core::gsDebug)
@@ -228,6 +229,9 @@ if (core::gsDebug)
 std::cerr << "<enumerate-quantifiers>" << core::pp(x) << " -> " << core::pp(result) << std::endl;
 #endif
         return result;
+#else
+        return r(x, sigma);
+#endif
       }
 
       /// \brief Rewrites a pbes expression.
