a30bb22ca471a68879032d0d47c6a52f3702daaa
Jeroen Keiren <j.j.a.keiren@tue.nl>
2009-07-22 09:48:44 +0000

    - Add test that shows that is_negate_application(i-5) returns true, and   similarly is_minus_application(-i) returns true. This shows that the is_*_application functions in the generated code need to be made more strict. More specificically, the allowed signatures of the function symbols need to be taken into account. This basically is the problem described by Jan Friso in one of his previous commits
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6824 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/data/test/int_test.cpp
@@ -0,0 +1,43 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file data_expression_test.cpp
+/// \brief Basic regression test for data expressions.
+
+#include <iostream>
+#include <boost/range/iterator_range.hpp>
+#include <boost/test/minimal.hpp>
+
+#include "mcrl2/data/standard_utility.h"
+
+#include "mcrl2/core/garbage_collection.h"
+
+using namespace mcrl2;
+using namespace mcrl2::data;
+
+void recogniser_test()
+{
+  variable i("i", sort_int::int_());
+  application minus(sort_int::minus(i, sort_int::int_("5")));
+  application negate(sort_int::negate(i));
+
+  BOOST_CHECK(sort_int::is_minus_application(minus));
+  BOOST_CHECK(!sort_int::is_negate_application(minus));
+  BOOST_CHECK(sort_int::is_negate_application(negate));
+  BOOST_CHECK(!sort_int::is_minus_application(negate));
+}
+
+int test_main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv);
+
+  recogniser_test();
+  core::garbage_collect();
+
+  return EXIT_SUCCESS;
+}
