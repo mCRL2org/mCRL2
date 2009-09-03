21af5a68c89c502be4016cdbb69e13ecab2487a0
Wieger Wesselink <j.w.wesselink@tue.nl>
2009-09-03 15:24:36 +0000

    - Made preparations for new finite pbes2bes algorithm (commented out using flag PBES2BES_FINITE_ALGORITHM)
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7000 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/pbes/include/mcrl2/pbes/detail/pbes2bes_finite_builder.h
@@ -0,0 +1,135 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/pbes/detail/pbes2bes_finite_builder.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_PBES_DETAIL_PBES2BES_FINITE_BUILDER_H
+#define MCRL2_PBES_DETAIL_PBES2BES_FINITE_BUILDER_H
+
+#include "mcrl2/data/classic_enumerator.h"
+#include "mcrl2/data/replace.h"
+#include "mcrl2/pbes/pbes_expression.h"
+#include "mcrl2/pbes/pbes_expression_builder.h"
+
+namespace mcrl2 {
+
+namespace pbes_system {
+
+namespace detail {
+
+/// Visitor that applies a propositional variable substitution to a pbes expression.
+template <typename DataRewriter, typename RenameFunction, typename Substitution>
+struct pbes2bes_finite_builder: public pbes_expression_builder<pbes_expression, Substitution>
+{
+  typedef core::term_traits<pbes_expression> tr;
+  
+  const DataRewriter& m_rewriter;
+  RenameFunction& m_rename;
+  const data::data_specification& m_dataspec;
+  std::map<core::identifier_string, std::vector<data::variable> >& m_finite_variables;
+  
+  pbes2bes_finite_builder(const DataRewriter& r, RenameFunction& rho, const data::data_specification& dataspec, std::map<core::identifier_string, std::vector<data::variable> >& finite_variables)
+    : m_rewriter(r_), m_rename(rho_), m_dataspec(dataspec), m_finite_variables(finite_variables)
+  {}
+
+  /// \brief Computes the subset with variables of finite sort and infinite.
+  // TODO: this should be done more efficiently, by avoiding aterm lists
+  /// \param e A sequence of data expressions
+  /// \param data A data specification
+  /// \param finite A sequence of data expressions
+  /// \param infinite A sequence of data expressions
+  void split_expressions(const data::data_expression_list& e,
+                         const data::data_specification& data,
+                         std::vector<data::data_expression>& finite,
+                         std::vector<data::data_expression>& infinite
+                        )
+  {
+    for (data::data_expression_list::iterator i = e.begin(); i != e.end(); ++i)
+    {
+      if (data.is_certainly_finite(i->sort()))
+      {
+        finite.push_back(*i);
+      }
+      else
+      {
+        infinite.push_back(*i);
+      }
+    }
+  }
+
+  data::data_expression make_condition(const VariableContainer& variables, const ExpressionContainer& expressions) const
+  {
+    using namespace data::sort_bool;
+    data::data_expression result = true_();
+    assert(variables.size() == expressions.size());
+    typename VariableContainer::const_iterator vi = variables.begin();
+    typename ExpressionContainer::const_iterator ei = expressions.begin();
+    for (; vi != variables.end(); ++vi, ++ei)
+    {
+      result = and_(result, equals(*vi, *ei));
+    }
+    return result;
+  }
+
+  template <typename ExpressionContainer, typename Substitution>
+  data::data_expression_list rewrite(const ExpressionContainer& e, const Substitution& sigma) const
+  {
+    std::vector<data::data_expression> result = data::convert<std::vector<data::data_expression> >(e);
+    for (std::vector<data::data_expression>::iterator i = e.begin(); i != e.end(); ++i)
+    {
+      *i = m_rewriter(*i, sigma);
+    }
+    return data::convert<data::data_expression_list>(result);
+  }
+
+  /// \brief Visit data_expression node
+  /// \param e A PBES expression
+  /// \param d A data expression
+  /// \return The result of visiting the node
+  pbes_expression visit_data_expression(const pbes_expression& e, const data::data_expression& d, Substitution& sigma)
+  {
+    return data::replace_free_variables(d, sigma);
+  }
+
+  /// \brief Visit propositional_variable node
+  /// \param x A term
+  /// \return The result of visiting the node
+  pbes_expression visit_propositional_variable(const pbes_expression& x, const propositional_variable_instantiation& v, Substitution& sigma)
+  {
+    std::vector<data::data_expression> finite_expressions;
+    std::vector<data::data_expression> infinite_expressions;
+    split_expressions(v.parameters(), m_dataspec, finite_expressions, infinite_expressions);
+
+    const std::vector<data::variable>& finite_variables = m_finite_variables[v.name()];
+    data::data_expression condition = make_condition(finite_variables, finite_expressions);
+std::cerr << "<condition>" << core::pp(condition) << std::endl;
+
+    atermpp::vector<pbes_expression> result;
+    for (classic_enumerator<> i(m_dataspec, finite_variables, m_rewriter, condition); i != classic_enumerator<>(); ++i)
+    {
+      data::data_expression c = (*i)(condition);
+std::cerr << "<c>" << core::pp(c) << std::endl;
+      data::data_expression_list e_finite = rewrite(finite_expressions, *i);
+      core::identifier_string Y = m_rename(v.name(), e_finite);
+std::cerr << "<Y>" << core::pp(Y) << std::endl;
+      data::data_expression_list e_infinite = rewrite(infinite_expressions, *i);
+      result.insert(tr::and_(c, propositional_variable_instantiation(Y, e_infinite)));
+    }
+    
+    return tr::join_or(result.begin(), result.end());
+  }
+};
+
+} // namespace detail
+
+} // namespace pbes_system
+
+} // namespace mcrl2
+
+#endif // MCRL2_PBES_DETAIL_PBES2BES_FINITE_BUILDER_H
--- libraries/pbes/include/mcrl2/pbes/pbes2bes_algorithm.h
+++ libraries/pbes/include/mcrl2/pbes/pbes2bes_algorithm.h
@@ -11,10 +11,15 @@
 
 #include <set>
 #include <iostream>
+#include <sstream>
 #include "mcrl2/atermpp/set.h"
 #include "mcrl2/pbes/pbes.h"
 #include "mcrl2/pbes/pbes_expression_with_propositional_variables.h"
 #include "mcrl2/pbes/detail/pbes2bes_rewriter.h"
+
+#ifdef PBES2BES_FINITE_ALGORITHM
+#include "mcrl2/pbes/detail/pbes2bes_finite_builder.h"
+#endif
 
 #ifndef MCRL2_PBES_PBES2BES_ALGORITHM_H
 #define MCRL2_PBES_PBES2BES_ALGORITHM_H
@@ -173,6 +178,113 @@ namespace pbes_system {
         return R;
       }
   };
+
+#ifdef PBES2BES_FINITE_ALGORITHM
+  /// \brief Algorithm class for the finite pbes2bes algorithm.
+  class pbes2bes_finite_algorithm
+  {
+    public:
+      struct rename
+      {
+        /// \brief Returns a name.
+        /// Based on implementation made by Alexander van Dam.
+        template <typename VariableContainer>
+        core::identifier_string operator()(core::identifier_string name, const VariableContainer& variables)
+        {
+          std::ostringstream out;
+          out << std::string(name);
+          for (typename Container::const_iterator i = variables.begin(); i != variables.end(); i++)
+          {
+            out << "@" << core::pp(*i);
+          }
+          return core::identifier_string(out.str());
+        }
+      };
+
+    protected:
+      /// \brief Data specification
+      data::data_specification m_dataspec;
+
+      /// \brief Stores the finite variables for each proposition variable declaration.
+      std::map<core::identifier_string, std::vector<data::variable> >& m_finite_variables;
+
+      /// \brief Data structure for storing the result. E[i] corresponds to the equations
+      /// generated from the i-th PBES equation.
+      std::vector<atermpp::vector<pbes_equation> > E;
+
+      /// \brief Returns the finite parameters of equation eq
+      std::vector<data::variable> finite_variables(const pbes_equation& eq, const data::data_specification& dataspec) const
+      {
+        std::vector<data::variable> result;
+        data::variable_list v = eq.variable().parameters();
+        for (data::variable_list::iterator i = v.begin(); i != v.end(); ++i)
+        {
+          if (dataspec.is_certainly_finite(i->sort()))
+          {
+            result.push_back(*i);
+          }
+        }
+        return result;
+      }
+
+    public:
+
+      /// \brief Constructor.
+      /// \param print_equations If true, the generated equations are printed
+      /// \param print_rewriter_output If true, invocations of the rewriter are printed
+      pbes2bes_finite_algorithm(bool print_equations = false, bool print_rewriter_output = false)
+      {}
+
+      /// \brief Runs the algorithm. The result is obtained by calling the function \p get_result.
+      /// \param p A PBES
+      /// \param rewriter_strategy A strategy for the data rewriter
+      void run(pbes<>& p, data::rewriter::strategy rewriter_strategy = data::rewriter::jitty)
+      {
+        p.instantiate_global_variables();
+
+        for (atermpp::vector<pbes_equation>::const_iterator i = p.equations().begin(); i != p.equations().end(); ++i)
+        {
+          m_finite_variables[i->variable().name()] = finite_variables(*i, p.data());
+        }
+
+        /// \brief The rewriter.
+        data::rewriter R(p.data(), rewriter_strategy);
+
+        pbes2bes_finite_builder visitor(R, rename(), p.data(), m_finite_variables);
+        for (atermpp::vector<pbes_equation>::const_iterator i = p.equations().begin(); i != p.equations().end(); ++i)
+        {
+          visitor.visit(
+        }
+      }
+
+      /// \brief Returns the computed bes in pbes format
+      /// \return The computed bes in pbes format
+      pbes<> get_result()
+      {
+        pbes<> result;
+        for (std::vector<atermpp::vector<pbes_equation> >::iterator i =  E.begin(); i != E.end(); ++i)
+        {
+          result.equations().insert(result.equations().end(), i->begin(), i->end());
+        }
+        result.initial_state() = init;
+        return result;
+      }
+
+      /// \brief Returns the flag for printing the generated bes equations
+      /// \return The flag for printing the generated bes equations
+      bool& print_equations()
+      {
+        return m_print_equations;
+      }
+
+      /// \brief Returns the flag for printing rewriter invocations
+      /// \return The flag for printing rewriter invocations
+      pbes2bes_rewriter& rewriter()
+      {
+        return R;
+      }
+  };
+#endif // PBES2BES_FINITE_ALGORITHM
 
 } // namespace pbes_system
 
