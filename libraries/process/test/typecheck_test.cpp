72630d4f8d7b517a191134c1525a12116bd93fb1
Wieger Wesselink <j.w.wesselink@tue.nl>
2009-10-16 09:38:37 +0000

    - Added test case for the type checker.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7092 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/process/test/typecheck_test.cpp
@@ -0,0 +1,45 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file typecheck_test.cpp
+/// \brief Add your file description here.
+
+#include <iostream>
+#include <boost/test/minimal.hpp>
+#include <mcrl2/lps/parse.h>
+#include <mcrl2/lps/parelm.h>
+#include <mcrl2/lps/specification.h>
+#include "mcrl2/core/garbage_collection.h"
+#include "mcrl2/atermpp/aterm_init.h"
+
+using namespace mcrl2;
+
+// Example provided by Tim Willemse, 7 Oct 2009
+const std::string case_1 =
+  "map place : List(Nat) -> List(Nat); \n"
+  "                                    \n"
+  "var l : List(Nat);                  \n"
+  "                                    \n"
+  "eqn place (l) = head(l) ++ tail(l); \n"
+  "                                    \n"
+  "init delta;                         \n"
+  ;
+
+void test_typechecker()
+{
+  process::parse_process_specification(case_1);
+}
+
+int test_main(int argc, char* argv[])
+{
+  MCRL2_ATERMPP_INIT(argc, argv)
+
+  test_typechecker();
+
+  return 0;
+}
