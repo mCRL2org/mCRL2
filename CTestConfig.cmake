acc4fe29d1c28fe3f95e7fdab4562c93ae0a8a2b
Frank Stappers <f.p.m.stappers@tue.nl>
2009-12-17 21:22:59 +0000

    Add CMakefiles config files
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7227 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ CTestConfig.cmake
@@ -0,0 +1,36 @@
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "mCRL2")
+
+if (SUBMIT_LOCAL)
+  set(CTEST_DROP_METHOD "scp")
+  if (WIN32)
+    # Cygwin scp won't take : later in path (and interprets drive as host like
+    # unix scp would) so we use cp, which is ok with drive-letter paths.
+    # Note that CTEST_SCP_COMMAND must be a single executable so we can't
+    # use "cmake -E copy".
+    find_program(CTEST_SCP_COMMAND cp DOC "copy command for local copy of results")
+  else (WIN32)
+    find_program(CTEST_SCP_COMMAND scp DOC "scp command for local copy of results")
+  endif (WIN32)
+  set(CTEST_TRIGGER_SITE "")
+  set(CTEST_DROP_SITE_USER "")
+  # CTest does "scp file ${CTEST_DROP_SITE}:${CTEST_DROP_LOCATION}" so for
+  # local copy w/o needing sshd on localhost we arrange to have : in the
+  # absolute filepath (when absolute, scp interprets as local even if : later)
+  if (NOT EXISTS "${CTEST_DROP_SITE}:${CTEST_DROP_LOCATION}")
+    message(FATAL_ERROR 
+      "must set ${CTEST_DROP_SITE}:${CTEST_DROP_LOCATION} to an existing directory")
+  endif (NOT EXISTS "${CTEST_DROP_SITE}:${CTEST_DROP_LOCATION}")
+else (SUBMIT_LOCAL)
+  set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
+  
+  set(CTEST_DROP_METHOD "http")
+  set(CTEST_DROP_SITE "dyn295.win.tue.nl")
+  set(CTEST_DROP_LOCATION "/~fstapper/cdash/submit.php?project=mCRL2")
+  set(CTEST_DROP_SITE_CDASH TRUE)
+endif(SUBMIT_LOCAL)
