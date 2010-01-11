9751eac4e0341e5c45fe05d09395f83129133246
Jeroen Keiren <j.j.a.keiren@tue.nl>
2010-01-11 20:07:26 +0000

    - Add some more type check tests to find out how the current type checker   behaves (based on discussion with Michel Reniers)
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7293 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/data/test/typecheck_test.cpp
+++ libraries/data/test/typecheck_test.cpp
@@ -23,12 +23,7 @@
 
 using namespace mcrl2;
 
-template <typename VariableIterator>
-void test_data_expression(const std::string &de_in,
-                          const VariableIterator begin,
-                          const VariableIterator end,
-                          bool expect_success = true,
-                          bool test_type_checker = true)
+data::data_expression parse_data_expression(const std::string& de_in)
 {
   std::istringstream de_in_stream(de_in);
   ATermAppl de_aterm = core::parse_data_expr(de_in_stream);
@@ -38,15 +33,38 @@ void test_data_expression(const std::string &de_in,
   //std::cerr << "The following data expressions should be the same:" << std::endl << "  " << de_in  << std::endl << "  " << de_out << std::endl;
   BOOST_CHECK_EQUAL(de_in, de_out);
 
-  if (test_type_checker) {
-    data::data_expression x(de_aterm);
+  return data::data_expression(de_aterm);
+}
 
+data::data_specification parse_data_specification(const std::string& ds_in)
+{
+  std::istringstream ds_in_stream(ds_in);
+  ATermAppl ds_aterm = core::parse_data_spec(ds_in_stream);
+  BOOST_REQUIRE(ds_aterm != NULL);
+
+  std::string ds_out = core::PrintPart_CXX((ATerm) ds_aterm);
+  //std::cerr << "The following data specifications should be the same:" << std::endl << ds_in << std::endl << "and" << std::endl << ds_out << std::endl;
+  BOOST_CHECK_EQUAL(ds_in, ds_out);
+
+  return data::data_specification(ds_aterm);
+}
+
+template <typename VariableIterator>
+void test_data_expression(const std::string &de_in,
+                          const VariableIterator begin,
+                          const VariableIterator end,
+                          bool expect_success = true,
+                          bool test_type_checker = true)
+{
+  data::data_expression x(parse_data_expression(de_in));
+
+  if (test_type_checker) {
     if(expect_success)
     {
       data::type_check(x, begin, end);
-      de_aterm = x;
+      atermpp::aterm de_aterm = x;
 
-      de_out = core::PrintPart_CXX((ATerm) de_aterm);
+      std::string de_out = core::PrintPart_CXX((ATerm) de_aterm);
       //std::cerr << "The following data expressions should be the same:" << std::endl << "  " << de_in  << std::endl << "  " << de_out << std::endl;
       BOOST_CHECK_EQUAL(de_in, de_out);
     }
@@ -58,7 +76,9 @@ void test_data_expression(const std::string &de_in,
   core::garbage_collect();
 }
 
-void test_data_expression(const std::string &de_in, bool expect_success = true, bool test_type_checker = true)
+void test_data_expression(const std::string &de_in,
+                          bool expect_success = true,
+                          bool test_type_checker = true)
 {
   data::variable_vector v;
   test_data_expression(de_in, v.begin(), v.end(), expect_success, test_type_checker);
@@ -100,26 +120,87 @@ BOOST_AUTO_TEST_CASE(test_function_updates)
   */
 }
 
-void test_data_specification(const std::string &ds_in, bool expect_success = true, bool test_type_checker = true)
+BOOST_AUTO_TEST_CASE(test_inline_struct_recogniser)
 {
-  std::istringstream ds_in_stream(ds_in);
-  ATermAppl ds_aterm = core::parse_data_spec(ds_in_stream);
-  BOOST_REQUIRE(ds_aterm != NULL);
+  test_data_expression("lambda x: struct t?is_t. x == t", false);
+}
 
-  std::string ds_out = core::PrintPart_CXX((ATerm) ds_aterm);
-  //std::cerr << "The following data specifications should be the same:" << std::endl << ds_in << std::endl << "and" << std::endl << ds_out << std::endl;
-  BOOST_CHECK_EQUAL(ds_in, ds_out);
+BOOST_AUTO_TEST_CASE(test_inline_struct)
+{
+  test_data_expression("lambda x: struct t. x == t", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_inline_structs_compare)
+{
+  test_data_expression("lambda x,y: struct t. x == y");
+}
+
+BOOST_AUTO_TEST_CASE(test_inline_structs_compare_recogniser)
+{
+  test_data_expression("lambda x: struct t?is_t, y: struct t. x == y", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_lambda_aliasing)
+{
+  test_data_expression("lambda f: Nat. lambda f: Nat -> Bool. f(f)", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_where)
+{
+  test_data_expression("x + y whr x = 3, y = 10 end");
+}
+
+BOOST_AUTO_TEST_CASE(test_where_var_one_occurs_in_two)
+{
+  test_data_expression("x + y whr x = 3, y = x + 10 end", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_where_var_one_and_two_occur_in_two)
+{
+  test_data_expression("x + y whr x = 3, y = x + y + 10 end", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_where_var_two_occurs_in_one)
+{
+  test_data_expression("x + y whr x = y + 10, y = 3 end", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_where_var_one_occurs_in_two_and_vice_versa)
+{
+  test_data_expression("x + y whr x = y + 10, y = x + 3 end", false);
+}
+
+BOOST_AUTO_TEST_CASE(test_upcast_pos2nat)
+{
+  data::variable_vector v;
+  v.push_back(data::variable("x", data::sort_pos::pos()));
+  v.push_back(data::variable("y", data::sort_nat::nat()));
+  test_data_expression("x + y", v.begin(), v.end());
+  test_data_expression("x == y", v.begin(), v.end());
+}
+
+BOOST_AUTO_TEST_CASE(test_upcast_list_pos2nat)
+{
+  data::variable_vector v;
+  v.push_back(data::variable("x", data::sort_list::list(data::sort_pos::pos())));
+  v.push_back(data::variable("y", data::sort_list::list(data::sort_nat::nat())));
+  test_data_expression("x ++ y", v.begin(), v.end(), false);
+  test_data_expression("x == y", v.begin(), v.end(), false);
+}
+
+void test_data_specification(const std::string &ds_in,
+                             bool expect_success = true,
+                             bool test_type_checker = true)
+{
+  data::data_specification ds(parse_data_specification(ds_in));
 
   if (test_type_checker) {
-    //ds_aterm = core::type_check_data_spec(ds_aterm);
-    //BOOST_REQUIRE(ds_aterm != NULL);
-    data::data_specification ds(ds_aterm);
 
     if (expect_success) {
       data::type_check(ds);
 
       //ds_out = core::PrintPart_CXX((ATerm) ds_aterm);
-      ds_out = data::pp(ds);
+      std::string ds_out = data::pp(ds);
       //std::cerr << "The following data specifications should be the same:" << std::endl << ds_in  << std::endl << "and" << std::endl << ds_out << std::endl;
       BOOST_CHECK_EQUAL(ds_in, ds_out);
     }
@@ -128,7 +209,7 @@ void test_data_specification(const std::string &ds_in, bool expect_success = tru
       BOOST_CHECK_THROW(data::type_check(ds), mcrl2::runtime_error);
     }
   }
-  //core::garbage_collect();
+  core::garbage_collect();
 }
 
 BOOST_AUTO_TEST_CASE(test_data_specification_struct_with_projection)
@@ -150,17 +231,6 @@ BOOST_AUTO_TEST_CASE(test_duplicate_sort)
   );
 }
 
-BOOST_AUTO_TEST_CASE(test_data_specification_constructor_different_signature)
-{
-  test_data_specification(
-    "sort S;\n"
-    "     T;\n"
-    "\n"
-    "cons f: S;\n"
-    "     f: S -> T;\n"
-  );
-}
-
 BOOST_AUTO_TEST_CASE(test_data_specification_constructor_same_signature)
 {
   //test data specification involving multiple constructors/functions with
@@ -173,6 +243,123 @@ BOOST_AUTO_TEST_CASE(test_data_specification_constructor_same_signature)
     "     f: T;\n",
     false
   );
+}
+
+BOOST_AUTO_TEST_CASE(test_data_specification_constructor_map_same_signature)
+{
+  //test data specification involving multiple constructors/functions with
+  //different signatures
+  test_data_specification(
+    "sort S;\n"
+    "     T;\n"
+    "\n"
+    "cons f: S;\n"
+    "\n"
+    "map f: T;\n",
+    false
+  );
+}
+
+BOOST_AUTO_TEST_CASE(test_data_specification_constructor_different_signature)
+{
+  test_data_specification(
+    "sort S;\n"
+    "     T;\n"
+    "\n"
+    "cons f: S;\n"
+    "     f: S -> T;\n"
+  );
+}
+
+BOOST_AUTO_TEST_CASE(test_data_specification_nested_struct)
+{
+  test_data_specification(
+    "sort S = struct t(struct e(Nat));\n"
+  );
+}
+
+template <typename VariableIterator>
+void test_data_expression_in_specification_context(const std::string &de_in,
+                          const std::string& ds_in,
+                          const VariableIterator begin,
+                          const VariableIterator end,
+                          bool expect_success = true,
+                          bool test_type_checker = true)
+{
+  data::data_specification ds(parse_data_specification(ds_in));
+
+  if(test_type_checker)
+  {
+    data::type_check(ds);
+
+    std::string ds_out = data::pp(ds);
+    BOOST_CHECK_EQUAL(ds_in, ds_out);
+  }
+
+  data::data_expression de(parse_data_expression(de_in));
+
+  if(test_type_checker)
+  {
+    if(expect_success)
+    {
+      data::type_check(de, begin, end, ds);
+      atermpp::aterm de_aterm = de;
+
+      std::string de_out = core::PrintPart_CXX((ATerm) de_aterm);
+
+      BOOST_CHECK_EQUAL(de_in, de_out);
+    }
+    else
+    {
+      BOOST_CHECK_THROW(data::type_check(de, begin, end, ds), mcrl2::runtime_error);
+    }
+  }
+}
+
+void test_data_expression_in_specification_context(const std::string &de_in,
+                          const std::string& ds_in,
+                          bool expect_success = true,
+                          bool test_type_checker = true)
+{
+  data::variable_vector v;
+  test_data_expression_in_specification_context(de_in, ds_in, v.begin(), v.end(),expect_success,test_type_checker);
+}
+
+BOOST_AUTO_TEST_CASE(test_data_expressions_different_signature)
+{
+  test_data_expression_in_specification_context(
+    "f(f)",
+    "sort S;\n"
+    "     T;\n"
+    "\n"
+    "cons f: S;\n"
+    "     f: S -> T;\n"
+  );
+}
+
+BOOST_AUTO_TEST_CASE(test_data_expressions_struct)
+{
+  data::variable_vector v;
+  data::basic_sort s("S");
+  v.push_back(data::variable("x", s));
+
+  test_data_expression_in_specification_context(
+    "x == t(e(3))",
+    "sort S = struct t(struct e(Nat));\n",
+    v.begin(), v.end()
+  );
+}
+
+BOOST_AUTO_TEST_CASE(test_lambda_variable_aliasing)
+{
+  data::variable_vector v;
+  data::function_sort s(data::basic_sort("S"), data::basic_sort("T"));
+  v.push_back(data::variable("x", s));
+  test_data_expression_in_specification_context(
+    "lambda x: S. x(x)",
+    "sort S;\n",
+    v.begin(), v.end(),
+    false);
 }
 
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
