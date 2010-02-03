00c7c132eb628670c706f63751e3047c659cca30
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-02-03 20:17:43 +0000

    - Added binding aware traverser for state formulas. - Added find functions for state formulas (untested).
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7368 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lps/include/mcrl2/modal_formula/detail/binding_aware_traverser.h
@@ -0,0 +1,130 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/modal_formula/detail/binding_aware_traverser.h
+/// \brief add your file description here.
+
+#include "mcrl2/modal_formula/detail/traverser.h"
+#include "mcrl2/data/detail/binding_aware_traverser.h"
+
+#ifndef MCRL2_MODAL_FORMULA_DETAIL_BINDING_AWARE_TRAVERSER_H
+#define MCRL2_MODAL_FORMULA_DETAIL_BINDING_AWARE_TRAVERSER_H
+
+namespace mcrl2 {
+
+namespace state_formulas {
+
+namespace detail {
+
+  // Adds state formula traversal functions to the data binding aware traverser.
+  template <typename Derived>
+  struct binding_aware_traverser_base: public data::detail::binding_aware_traverser<Derived>
+  {
+    typedef data::detail::binding_aware_traverser<Derived> super;
+    using super::operator();
+    using super::increase_bind_count;
+    using super::decrease_bind_count;
+
+// Include file with traverse member functions. This is to avoid duplication.
+#include "mcrl2/modal_formula/detail/traverser.inc.h"
+  };
+
+  /// \brief Binding aware traverser.
+  template <typename Derived>
+  struct binding_aware_traverser: public binding_aware_traverser_base<Derived>
+  {
+    typedef binding_aware_traverser_base<Derived> super;
+
+    using super::operator();
+    using super::increase_bind_count;
+    using super::decrease_bind_count;
+
+    /// \brief Returns the left hand sides of the assignments.
+    data::variable_list left_hand_sides(const data::assignment_list& a)
+    {
+      std::vector<data::variable> v;
+      for (data::assignment_list::const_iterator i = a.begin(); i != a.end(); ++i)
+      {
+        v.push_back(i->lhs());
+      }
+      return data::convert<data::variable_list>(v);
+    }
+
+    /// \brief Traverses a forall quantifier
+    /// \param x A forall quantifier
+    void operator()(const forall& x)
+    {
+      increase_bind_count(x.variables());
+      super::operator()(x);
+      decrease_bind_count(x.variables());
+    }
+
+    /// \brief Traverses a forall quantifier
+    /// \param x An exists quantifier
+    void operator()(const exists& x)
+    {
+      increase_bind_count(x.variables());
+      super::operator()(x);
+      decrease_bind_count(x.variables());
+    }
+
+    /// \brief Traverses a mu expression
+    /// \param x A mu expression
+    void operator()(const mu& x)
+    {
+    	data::variable_list v = left_hand_sides(x.assignments());
+      increase_bind_count(v);
+      super::operator()(x);
+      decrease_bind_count(v);
+    }
+
+    /// \brief Traverses a nu expression
+    /// \param x A nu expression
+    void operator()(const nu& x)
+    {
+    	data::variable_list v = left_hand_sides(x.assignments());
+      increase_bind_count(v);
+      super::operator()(x);
+      decrease_bind_count(v);
+    }
+  };
+
+  // Adds state formula traversal functions to the data selective binding aware traverser.
+  template <typename Derived, typename AdaptablePredicate>
+  class selective_binding_aware_traverser_base: public data::detail::selective_binding_aware_traverser<Derived, AdaptablePredicate>
+  {
+    typedef data::detail::selective_binding_aware_traverser<Derived, AdaptablePredicate> super;
+    using super::operator();
+
+// Include file with traverse member functions. This is to avoid duplication.
+#include "mcrl2/modal_formula/detail/traverser.inc.h"
+  };
+
+  template <typename Derived, typename AdaptablePredicate>
+  class selective_binding_aware_traverser: public selective_binding_aware_traverser_base<Derived, AdaptablePredicate>
+  {
+    typedef selective_binding_aware_traverser_base<Derived, AdaptablePredicate> super;
+    using super::operator();
+    using super::increase_bind_count;
+    using super::decrease_bind_count;
+
+    public:
+      selective_binding_aware_traverser()
+      { }
+
+      selective_binding_aware_traverser(AdaptablePredicate predicate) : super(predicate)
+      { }
+  };
+
+} // namespace detail
+
+} // namespace state_formulas
+
+} // namespace mcrl2
+
+#endif // MCRL2_MODAL_FORMULA_DETAIL_BINDING_AWARE_TRAVERSER_H
---
+++ libraries/lps/include/mcrl2/modal_formula/find.h
@@ -0,0 +1,125 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/modal_formula/find.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_MODAL_FORMULA_FIND_H
+#define MCRL2_MODAL_FORMULA_FIND_H
+
+#include "mcrl2/data/variable.h"                         
+#include "mcrl2/data/detail/find.h"                      
+#include "mcrl2/modal_formula/detail/traverser.h"              
+#include "mcrl2/modal_formula/detail/sort_traverser.h"         
+#include "mcrl2/modal_formula/detail/binding_aware_traverser.h"
+
+// TODO: remove this code and use find_free_variables instead
+#include "mcrl2/modal_formula/detail/free_variable_visitor.h"
+
+namespace mcrl2 {
+
+namespace state_formulas {
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \param[in,out] o an output iterator to which all data variables occurring in t
+  ///             are added.
+  /// \return All data variables that occur in the term t
+  template <typename Container, typename OutputIterator>
+  void find_variables(Container const& container, OutputIterator o)
+  {
+    data::detail::make_find_helper<data::variable, state_formulas::detail::traverser, OutputIterator>(o)(container);
+  }
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \return All data variables that occur in the term t
+  template <typename Container>
+  std::set<data::variable> find_variables(Container const& container)
+  {
+    std::set<data::variable> result;
+    state_formulas::find_variables(container, std::inserter(result, result.end()));
+    return result;
+  }
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \param[in,out] o an output iterator to which all data variables occurring in t
+  ///             are added.
+  /// \return All data variables that occur in the term t
+  template <typename Container, typename OutputIterator>
+  void find_free_variables(Container const& container, OutputIterator o,
+  		           typename data::detail::disable_if_container<OutputIterator>::type* = 0)
+  {
+    data::detail::make_free_variable_find_helper<state_formulas::detail::binding_aware_traverser>(o)(container);
+  }
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \param[in,out] o an output iterator to which all data variables occurring in t
+  ///             are added.
+  /// \param[in] bound a set of variables that should be considered as bound
+  /// \return All data variables that occur in the term t
+  /// TODO prevent copy of Sequence
+  template <typename Container, typename OutputIterator, typename Sequence>
+  void find_free_variables(Container const& container, OutputIterator o, Sequence const& bound)
+  {
+   // data::detail::make_free_variable_find_helper<state_formulas::detail::binding_aware_traverser>(bound, o)(container);
+  }
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \return All data variables that occur in the term t
+  template <typename Container>
+  std::set<data::variable> find_free_variables(Container const& container)
+  {
+    std::set<data::variable> result;
+    state_formulas::find_free_variables(container, std::inserter(result, result.end()));
+    return result;
+  }
+
+  /// \brief Returns all data variables that occur in a range of expressions
+  /// \param[in] container a container with expressions
+  /// \param[in] bound a set of variables that should be considered as bound
+  /// \return All data variables that occur in the term t
+  /// TODO prevent copy of Sequence
+  template <typename Container, typename Sequence>
+  std::set<data::variable> find_free_variables(Container const& container, Sequence const& bound,
+                                          typename data::detail::enable_if_container<Sequence, data::variable>::type* = 0)
+  {
+    std::set<data::variable> result;
+    state_formulas::find_free_variables(container, std::inserter(result, result.end()), bound);
+    return result;
+  }
+
+  /// \brief Returns all sort expressions that occur in the term t
+  /// \param[in] container an expression or container of expressions
+  /// \param[in] o an output iterator
+  /// \return All sort expressions that occur in the term t
+  template <typename Container, typename OutputIterator>
+  void find_sort_expressions(Container const& container, OutputIterator o)
+  {
+    data::detail::make_find_helper<data::sort_expression, state_formulas::detail::sort_traverser>(o)(container);
+  }
+
+  /// \brief Returns all sort expressions that occur in the term t
+  /// \param[in] container an expression or container of expressions
+  /// \return All sort expressions that occur in the term t
+  template <typename Container>
+  std::set<data::sort_expression> find_sort_expressions(Container const& container)
+  {
+    std::set<data::sort_expression> result;
+    state_formulas::find_sort_expressions(container, std::inserter(result, result.end()));
+    return result;
+  }
+
+} // namespace state_formulas
+
+} // namespace mcrl2
+
+#endif // MCRL2_MODAL_FORMULA_FIND_H
