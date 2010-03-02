bf17e5500e7ba4568f02522b47f832db847407d9
Jeroen Keiren <j.j.a.keiren@tue.nl>
2010-03-02 13:59:38 +0000

    - Remove superfluous include
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7481 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/include/mcrl2/core/detail/print_implementation.h
+++ libraries/core/include/mcrl2/core/detail/print_implementation.h
@@ -30,7 +30,6 @@
 #include "mcrl2/core/messaging.h"
 #include "mcrl2/core/aterm_ext.h"
 #include "mcrl2/core/detail/struct_core.h"
-#include "mcrl2/data/data_specification.h"
 #include "mcrl2/data/bool.h"
 #include "mcrl2/data/pos.h"
 #include "mcrl2/data/nat.h"
