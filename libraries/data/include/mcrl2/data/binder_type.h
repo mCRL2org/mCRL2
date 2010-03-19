2bdb91e59d4c3efb6a9c72d387b7ffcced9d9507
Jeroen Keiren <j.j.a.keiren@tue.nl>
2010-03-19 14:26:40 +0000

    - Add missing file
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7527 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/data/include/mcrl2/data/binder_type.h
@@ -0,0 +1,150 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/data/binder_type.h
+/// \brief The class binder_type.
+
+#ifndef MCRL2_DATA_BINDER_TYPE_H
+#define MCRL2_DATA_BINDER_TYPE_H
+
+#include "mcrl2/atermpp/aterm_appl.h"
+#include "mcrl2/core/detail/constructors.h"
+#include "mcrl2/core/detail/soundness_checks.h"
+
+namespace mcrl2 {
+
+  namespace data {
+
+    class binder_type: public atermpp::aterm_appl
+    {
+      public:
+        binder_type()
+         : atermpp::aterm_appl(core::detail::constructBindingOperator())
+        {}
+
+        binder_type(atermpp::aterm_appl term)
+         : atermpp::aterm_appl(term)
+        {
+          assert(core::detail::check_rule_BindingOperator(term));
+        }
+    };
+
+//--- start generated classes ---//
+/// \brief Binder for set or bag comprehension
+class set_or_bag_comprehension_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    set_or_bag_comprehension_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_SetBagComp(m_term));
+    }
+
+    /// \brief Constructor.
+    set_or_bag_comprehension_binder()
+      : binder_type(core::detail::gsMakeSetBagComp())
+    {}
+};
+
+/// \brief Binder for set comprehension
+class set_comprehension_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    set_comprehension_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_SetComp(m_term));
+    }
+
+    /// \brief Constructor.
+    set_comprehension_binder()
+      : binder_type(core::detail::gsMakeSetComp())
+    {}
+};
+
+/// \brief Binder for bag comprehension
+class bag_comprehension_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    bag_comprehension_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_BagComp(m_term));
+    }
+
+    /// \brief Constructor.
+    bag_comprehension_binder()
+      : binder_type(core::detail::gsMakeBagComp())
+    {}
+};
+
+/// \brief Binder for universal quantification
+class forall_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    forall_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_Forall(m_term));
+    }
+
+    /// \brief Constructor.
+    forall_binder()
+      : binder_type(core::detail::gsMakeForall())
+    {}
+};
+
+/// \brief Binder for existential quantification
+class exists_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    exists_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_Exists(m_term));
+    }
+
+    /// \brief Constructor.
+    exists_binder()
+      : binder_type(core::detail::gsMakeExists())
+    {}
+};
+
+/// \brief Binder for lambda abstraction
+class lambda_binder: public binder_type
+{
+  public:
+    /// \brief Constructor.
+    /// \param term A term
+    lambda_binder(atermpp::aterm_appl term)
+      : binder_type(term)
+    {
+      assert(core::detail::check_term_Lambda(m_term));
+    }
+
+    /// \brief Constructor.
+    lambda_binder()
+      : binder_type(core::detail::gsMakeLambda())
+    {}
+};
+//--- end generated classes ---//
+
+  } // namespace data
+
+} // namespace mcrl2
+#endif // MCRL2_DATA_BINDER_TYPE_H
