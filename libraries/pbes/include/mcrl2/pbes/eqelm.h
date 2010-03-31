90a6b75ef1488f7a02f85efc4f4cab6702b10bd0
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-03-31 20:47:29 +0000

    - Added missing include.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7580 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/eqelm.h
+++ libraries/pbes/include/mcrl2/pbes/eqelm.h
@@ -25,6 +25,7 @@
 #include "mcrl2/data/substitution.h"
 #include "mcrl2/pbes/pbes.h"
 #include "mcrl2/pbes/find.h"
+#include "mcrl2/pbes/rewriter.h"
 #include "mcrl2/pbes/substitute.h"
 #include "mcrl2/pbes/remove_parameters.h"
 #include "mcrl2/core/detail/print_utility.h"
