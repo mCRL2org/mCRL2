7a736cb34537ecff0484d9dc7da25fbdd50c0574
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-04-09 09:23:46 +0000

    - Added squadt interface to tbf2lps.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7604 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/tbf2lps/tbf2lps.cpp
+++ tools/tbf2lps/tbf2lps.cpp
@@ -23,17 +23,20 @@
 #include "mcrl2/core/messaging.h"
 #include "mcrl2/core/aterm_ext.h"
 #include "mcrl2/utilities/input_output_tool.h"
+#include "mcrl2/utilities/squadt_tool.h"
 #include "mcrl2/exception.h"
 
 using namespace mcrl2::utilities;
 using namespace mcrl2::utilities::tools;
 using namespace mcrl2::core;
 
-class tbf2lps_tool: public input_output_tool
+class tbf2lps_tool: public squadt_tool<input_output_tool>
 {
+  typedef squadt_tool<input_output_tool> super;
+
   public:
     tbf2lps_tool()
-      : input_output_tool(NAME, AUTHOR,
+      : super(NAME, AUTHOR,
           "convert a muCRL LPE to an mCRL2 LPS",
           "Read mCRL LPS from INFILE, convert it to a mCRL2 LPS and save the result to "
           "OUTFILE. If OUTFILE is not present, stdout is used. If INFILE is not present, "
@@ -46,7 +49,7 @@ class tbf2lps_tool: public input_output_tool
           "  mapping and: Bool # Bool -> Bool is replaced by ||, and\n"
           "  mapping eq: S # S -> Bool is replaced by == for each sort S"
         ),
-        convert_funcs(true)
+        m_not_convert_mappings(true)
     {}
 
     bool run()
@@ -89,7 +92,7 @@ class tbf2lps_tool: public input_output_tool
       ATprotectAppl(&mcrl_spec);
       assert(is_mCRL_spec(mcrl_spec));
 
-      ATermAppl spec = translate(mcrl_spec,true,convert_funcs);
+      ATermAppl spec = translate(mcrl_spec,true,m_not_convert_mappings);
       ATprotectAppl(&spec);
 
       if (output_filename().empty()) {
@@ -113,20 +116,108 @@ class tbf2lps_tool: public input_output_tool
       return true;
     }
 
+//Squadt connectivity
+#ifdef ENABLE_SQUADT_CONNECTIVITY
   protected:
-    bool convert_funcs;
+
+# define option_not_convert_mappings "not_convert_mappings"
+
+    /** \brief configures tool capabilities */
+    void set_capabilities(tipi::tool::capabilities& capabilities) const
+    {
+      // The tool has only one main input combination
+      capabilities.add_input_configuration("main-input",
+                 tipi::mime_type("tbf", tipi::mime_type::application), tipi::tool::category::transformation);
+    }
+
+    /** \brief queries the user via SQuADT if needed to obtain configuration information */
+    void user_interactive_configuration(tipi::configuration& configuration)
+    {
+      using namespace tipi;
+      using namespace tipi::layout;
+      using namespace tipi::layout::elements;
+
+      // Let squadt_tool update configuration and add output file configuration
+      synchronise_with_configuration(configuration);
+
+      /* Set defaults where the supplied configuration does not have values */
+      if (!configuration.option_exists(option_not_convert_mappings)) {
+        configuration.add_option(option_not_convert_mappings).
+           set_argument_value< 0 >(true, false);
+      }
+      if (!configuration.output_exists("main-output")) {
+        configuration.add_output("main-output",
+           tipi::mime_type("tbf", tipi::mime_type::application), configuration.get_output_name(".lps"));
+      }
+
+      /* Create display */
+      tipi::tool_display d;
+
+      layout::vertical_box& m = d.create< vertical_box >();
+
+      /* Prepare user interaction */
+      checkbox& convert_mappings_checkbox = d.create< checkbox >().set_status(configuration.get_option_argument< bool >(option_not_convert_mappings));
+      m.append(d.create< label >().set_text(" ")).
+        append(convert_mappings_checkbox.set_label("Do not apply conversion of mappings and equations"), layout::left);
+
+      button& okay_button = d.create< button >().set_label("OK");
+
+      m.append(d.create< label >().set_text(" ")).
+        append(okay_button, layout::right);
+
+      send_display_layout(d.manager(m));
+
+      okay_button.await_change();
+
+      // let squadt_tool update configuration for input/output files
+      update_configuration(configuration);
+
+      /* Update configuration */
+      configuration.get_option(option_not_convert_mappings).
+         set_argument_value< 0 >(convert_mappings_checkbox.get_status());
+    }
+
+    /** \brief check an existing configuration object to see if it is usable */
+    bool check_configuration(tipi::configuration const& configuration) const
+    {
+      return configuration.input_exists("main-input") ||
+             configuration.input_exists("main-output");
+    }
+
+    /** \brief performs the task specified by a configuration */
+    bool perform_task(tipi::configuration& configuration)
+    {
+      using namespace tipi;
+      using namespace tipi::layout;
+      using namespace tipi::layout::elements;
+
+      // Let squadt_tool update configuration and add output file configuration
+      synchronise_with_configuration(configuration);
+   
+      m_not_convert_mappings = configuration.option_exists(option_not_convert_mappings);
+
+      /* Create display */
+      tipi::tool_display d;
+
+      //Perform instantiation
+      return run();
+    }
+#endif //ENABLE_SQUADT_CONNECTIVITY
+
+  protected:
+    bool m_not_convert_mappings;
 
     void add_options(interface_description& desc)
     {
       input_output_tool::add_options(desc);
       desc.add_option("no-conv-map",
-        "do not apply the conversion of mappings and and eq", 'n');
+        "do not apply conversion of mappings and equations", 'n');
     }
 
     void parse_options(const command_line_parser& parser)
     {
       input_output_tool::parse_options(parser);
-      convert_funcs = parser.options.count("no-conv-map") == 0;
+      m_not_convert_mappings = parser.options.count("no-conv-map") == 0;
     }
 };
 
