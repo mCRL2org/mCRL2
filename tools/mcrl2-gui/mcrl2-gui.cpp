83d44892aaa2bb8961f2f2a83e18b4c206938e0b
Frank Stappers <f.p.m.stappers@tue.nl>
2010-06-29 14:50:32 +0000

    * Provided mcrl2-gui with CLI interface * Enabled man-page generation for mcrl2-gui
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7865 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/mcrl2-gui/mcrl2-gui.cpp
+++ tools/mcrl2-gui/mcrl2-gui.cpp
@@ -9,34 +9,73 @@
 #include "initialization.h"
 #include "gui/mainframe.h"
 
-// TODO: Build-in test to detect whether a tool actually exists from tool catalog
+#include "mcrl2/utilities/tool.h"
+#include "mcrl2/utilities/wx_tool.h"
 
+#define NAME   "mcrl2-gui"
+#define AUTHOR "Frank Stappers"
 
-// ----------------------------------------------------------------------------
-// the application class
-// ----------------------------------------------------------------------------
+using namespace mcrl2::core;
+using namespace mcrl2::utilities::tools;
 
-class mCRL2_gui: public wxApp {
-public:
-	virtual bool OnInit();
+class mCRL2_gui: public mcrl2::utilities::wx::tool< mCRL2_gui, tool >
+{
+  friend class mcrl2::utilities::wx::tool< mCRL2_gui, tool >;
 
-	Initialization* m_initialization_result;
+  private:
+
+  Initialization* m_initialization_result;
+
+    std::vector< std::string > developers() {
+
+      return std::vector< std::string >(1, "Frank Stappers");
+    }
+
+    std::vector< std::string > documenters() {
+      return std::vector< std::string >(1, "Frank Stappers");
+    }
+
+  public:
+    mCRL2_gui() : mcrl2::utilities::wx::tool< mCRL2_gui, tool >("mcrl2-gui",
+                  "graphical front-end for mCRL2 tools",
+                  "A graphical front-end for mCRL2 tools.",
+                  "A graphical front-end for mCRL2 tools",
+                  developers(),
+                  "",
+                  documenters()) {
+    }
+
+  bool run()
+  {
+  		m_initialization_result = new Initialization();
+
+  		vector< Tool > tool_catalog = m_initialization_result->m_tool_catalog;
+  		std::multimap<std::string,std::string> extention_tool_mapping = m_initialization_result->m_extention_tool_mapping;
+
+  		// Create the main application window
+  		MainFrame *frame = new MainFrame(wxT("mCRL2-gui"), wxDefaultPosition,
+  				wxSize(800, 600), tool_catalog, extention_tool_mapping
+  		);
+  		frame->Show(true);
+  		SetTopWindow(frame);
+
+  		return true;
+  }
 };
-IMPLEMENT_APP(mCRL2_gui)
 
-// `Main program' equivalent: the program execution "starts" here
-bool mCRL2_gui::OnInit() {
-	m_initialization_result = new Initialization();
-
-	vector< Tool > tool_catalog = m_initialization_result->m_tool_catalog;
-	std::multimap<std::string,std::string> extention_tool_mapping = m_initialization_result->m_extention_tool_mapping;
-
-	// Create the main application window
-	MainFrame *frame = new MainFrame(wxT("mCRL2-gui"), wxDefaultPosition,
-			wxSize(800, 600), tool_catalog, extention_tool_mapping
-	);
-	frame->Show(true);
-	SetTopWindow(frame);
-
-	return true;
+#ifdef __WINDOWS__
+extern "C" int WINAPI WinMain(HINSTANCE hInstance,
+                                  HINSTANCE hPrevInstance,
+                                  wxCmdLineArgType lpCmdLine,
+                                  int nCmdShow) {
+  return wxEntry(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 }
+#endif
+
+int main(int argc, char** argv)
+{
+  return wxEntry(argc, argv);
+}
+
+IMPLEMENT_APP_NO_MAIN(mCRL2_gui)
+
