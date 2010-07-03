7960f5a52a807e3e4d7892097b1ffc1fa5e004c5
Jan Friso Groote <j.f.groote@tue.nl>
2010-07-03 19:34:50 +0000

    Cleaned up the code of bithashtable a little bit more. Made some class variables local, and replaced the type boost::uint64_t by vector < bool > size_type;
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7901 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lts/include/mcrl2/lts/bithashtable.h
+++ libraries/lts/include/mcrl2/lts/bithashtable.h
@@ -16,13 +16,33 @@
 #include <vector>
 
 #include <aterm2.h>
-#include "boost/cstdint.hpp"
 #include "mcrl2/exception.h"
 
 namespace mcrl2
 {
   namespace lts
   {
+
+    class bit_hash_table
+    {
+      private:
+        typedef std::vector < bool>::size_type size_t;
+
+        std::vector < bool > m_bit_hash_table;
+
+      public:
+        bit_hash_table() 
+        {}; 
+
+        bit_hash_table(const size_t size) :
+          m_bit_hash_table(size,false)
+        {};
+
+        ~bit_hash_table()
+        {}
+
+      private:
+
 
     // 96 bit mix function of Robert Jenkins
     #define mix(a,b,c) \
@@ -37,39 +57,11 @@ namespace mcrl2
         c -= a; c -= b; c ^= (b>>15); \
       }
 
-
-    class bit_hash_table
-    {
-      private:
-        unsigned long sh_a,sh_b,sh_c;
-        unsigned int sh_i;
-        std::vector < bool > m_bit_hash_table;
-
-      public:
-        bit_hash_table() :
-          m_bit_hash_table(0,false)
-        { 
-        }; 
-
-        bit_hash_table(boost::uint64_t const size) :
-          m_bit_hash_table(size,false)
-        { 
-        };
-
-        ~bit_hash_table()
-        {
-        }
-
-      private:
-        void calc_hash_init()
-        {
-          sh_a = 0x9e3779b9;
-          sh_b = 0x65e3083a;
-          sh_c = 0xa45f7582;
-          sh_i = 0;
-        }
-
-        void calc_hash_add(unsigned long n)
+        void calc_hash_add(const size_t n,
+                           size_t &sh_a,
+                           size_t &sh_b,
+                           size_t &sh_c,
+                           unsigned int &sh_i)
         {
           switch ( sh_i )
           {
@@ -89,71 +81,83 @@ namespace mcrl2
           }
         }
 
-        boost::uint64_t calc_hash_finish()
-        {
-          while ( sh_i != 0 )
-          {
-            calc_hash_add(0x76a34e87);
-          }
-          return (((boost::uint64_t) (sh_a & 0xffff0000)) << 24) |
-                 (((boost::uint64_t) (sh_b & 0xffff0000)) << 16) |
-                 (((boost::uint64_t) (sh_c & 0xffff0000))     ) |
-                 ((sh_a & 0x0000ffff)^(sh_b & 0x0000ffff)^(sh_c & 0x0000ffff));
-        }
-
-        void calc_hash_aterm(ATerm t)
+        void calc_hash_aterm(ATerm t,
+                             size_t &sh_a,
+                             size_t &sh_b,
+                             size_t &sh_c,
+                             unsigned int &sh_i)
         {
           switch ( ATgetType(t) )
           {
             case AT_APPL:
-              calc_hash_add(0x13ad3780);
+              calc_hash_add(0x13ad3780,sh_a,sh_b,sh_c,sh_i);
               {
                 unsigned int len = ATgetArity(ATgetAFun((ATermAppl) t));
                 for (unsigned int i=0; i<len; i++)
                 {
-                  calc_hash_aterm(ATgetArgument((ATermAppl) t, i));
+                  calc_hash_aterm(ATgetArgument((ATermAppl) t, i),sh_a,sh_b,sh_c,sh_i);
                 }
               }
               break;
             case AT_LIST:
-              calc_hash_add(0x7eb9cdba);
+              calc_hash_add(0x7eb9cdba,sh_a,sh_b,sh_c,sh_i);
               for (ATermList l=(ATermList) t; !ATisEmpty(l); l=ATgetNext(l))
               {
-                calc_hash_aterm(ATgetFirst(l));
+                calc_hash_aterm(ATgetFirst(l),sh_a,sh_b,sh_c,sh_i);
               }
               break;
             case AT_INT:
-              calc_hash_add(ATgetInt((ATermInt) t));
+              calc_hash_add(ATgetInt((ATermInt) t),sh_a,sh_b,sh_c,sh_i);
               break;
             default:
-              calc_hash_add(0xaa143f06);
+              calc_hash_add(0xaa143f06,sh_a,sh_b,sh_c,sh_i);
               break;
           }
         }
 
-        boost::uint64_t calc_hash(ATerm state)
-        { assert(m_bit_hash_table.size()>0);
-          calc_hash_init();
-          calc_hash_aterm(state);
-          return calc_hash_finish() % m_bit_hash_table.size();
+        size_t calc_hash_finish(size_t &sh_a,
+                                size_t &sh_b,
+                                size_t &sh_c,
+                                unsigned int &sh_i)
+        {
+          while ( sh_i != 0 )
+          {
+            calc_hash_add(0x76a34e87,sh_a,sh_b,sh_c,sh_i);
+          }
+          return (((size_t) (sh_a & 0xffff0000)) << 24) |
+                 (((size_t) (sh_b & 0xffff0000)) << 16) |
+                 (((size_t) (sh_c & 0xffff0000))     ) |
+                 ((sh_a & 0x0000ffff)^(sh_b & 0x0000ffff)^(sh_c & 0x0000ffff));
+        }
+
+        size_t calc_hash(ATerm state)
+        { 
+          assert(m_bit_hash_table.size()>0);
+          size_t sh_a = 0x9e3779b9;
+          size_t sh_b = 0x65e3083a;
+          size_t sh_c = 0xa45f7582;
+          unsigned int sh_i = 0;
+
+          calc_hash_aterm(state,sh_a,sh_b,sh_c,sh_i);
+          return calc_hash_finish(sh_a,sh_b,sh_c,sh_i) % m_bit_hash_table.size();
         }
 
       public:
         void remove_state_from_bithash(const ATerm state)
         {
-          boost::uint64_t i = calc_hash(state);
+          size_t i = calc_hash(state);
           m_bit_hash_table[i] = false;
         }
 
-        boost::uint64_t add_state(ATerm state, bool &is_new)
+        size_t add_state(ATerm state, bool &is_new)
         {
-          boost::uint64_t i = calc_hash(state);
+          size_t i = calc_hash(state);
           is_new = !m_bit_hash_table[i];
           m_bit_hash_table[i] = true;
           return i;
         }
     
-        boost::uint64_t state_index(ATerm state)
+        size_t state_index(ATerm state)
         {
           assert(m_bit_hash_table[calc_hash(state)]);
           return calc_hash(state);
