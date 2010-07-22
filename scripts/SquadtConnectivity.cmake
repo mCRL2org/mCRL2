b1c4aed8baa2f14f0dfbd9a1a7fd51f5694ac990
Jeroen Keiren <j.j.a.keiren@tue.nl>
2010-07-22 09:14:05 +0000

    - Remove squadt connectivity option from CMake build as discussed last week
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@7975 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/SquadtConnectivity.cmake
+++ scripts/SquadtConnectivity.cmake
@@ -8,10 +8,8 @@
 
 # This script adds squadt connectivity for a tool when required.
 
-if(MCRL2_SQUADT_CONNECTIVITY)
-  add_definitions("-DENABLE_SQUADT_CONNECTIVITY")
-  include_directories(${CMAKE_SOURCE_DIR}/tools/squadt/libraries/tipi/include)
-  target_link_libraries(${PROJECT_NAME}
-    mcrl2_utilities_squadt
-  )
-endif(MCRL2_SQUADT_CONNECTIVITY)
+add_definitions("-DENABLE_SQUADT_CONNECTIVITY")
+include_directories(${CMAKE_SOURCE_DIR}/tools/squadt/libraries/tipi/include)
+target_link_libraries(${PROJECT_NAME}
+  mcrl2_utilities_squadt
+)
