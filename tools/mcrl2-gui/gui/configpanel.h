aa828b7debaf9afccf66a67b2ef9bc5517218e9f
Frank Stappers <f.p.m.stappers@tue.nl>
2010-08-27 09:26:32 +0000

    * mcrl2-gui: Fixed problem with filenames that contain spaces. Note: Only tested with Unix.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8061 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/mcrl2-gui/gui/configpanel.h
+++ tools/mcrl2-gui/gui/configpanel.h
@@ -297,6 +297,8 @@ public:
     UpdateToolTipStatus(STATUS_RUNNING);
 
     wxString cmd = wxString(m_tool.m_location.c_str(), wxConvUTF8);
+    wxArrayString run_argv;
+    run_argv.Add( cmd );
 
     wxString run = cmd;
     for (vector<wxRadioBox*>::iterator i = m_radiobox_ptrs.begin(); i
@@ -323,11 +325,13 @@ public:
         run = run + wxT(" --") + (*i)->GetLabel() + wxT("=") +(*i)->GetPath();
     }
 
-
     wxString input_file = wxString(m_fileIO.input_file.c_str(), wxConvUTF8);
 
     wxString output_file = wxString(m_fileIO.output_file.c_str(),
         wxConvUTF8);
+
+    input_file.Replace( wxT(" "),wxT("\\ "));
+    output_file.Replace( wxT(" "),wxT("\\ "));
 
     run = run + wxT(" ") + input_file + wxT(" ") + output_file;
 
