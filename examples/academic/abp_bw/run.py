e999e567625ec2fc3cf4c1e7e77b1b2c5f636b44
Jeroen Keiren <j.j.a.keiren@tue.nl>
2010-09-13 19:58:04 +0000

    - Confirmed abp examples also for abp_bw.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8118 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ examples/academic/abp_bw/run.py
@@ -0,0 +1,25 @@
+import os
+
+os.system('mcrl22lps -v abp_bw.mcrl2 abp_bw.lps')
+os.system('lps2pbes -v -f nodeadlock.mcf abp_bw.lps abp_bw.nodeadlock.pbes')
+os.system('pbes2bool -v abp_bw.nodeadlock.pbes')
+
+os.system('lps2pbes -v -f infinitely_often_enabled_then_infinitely_often_taken.mcf abp_bw.lps abp_bw.infinitely_often_enabled_then_infinitely_often_taken.pbes')
+os.system('pbes2bool -v abp_bw.infinitely_often_enabled_then_infinitely_often_taken.pbes')
+
+os.system('lps2pbes -v -f infinitely_often_lost.mcf abp_bw.lps abp_bw.infinitely_often_lost.pbes')
+os.system('pbes2bool -v abp_bw.infinitely_often_lost.pbes')
+
+os.system('lps2pbes -v -f infinitely_often_receive_d1.mcf abp_bw.lps abp_bw.infinitely_often_receive_d1.pbes')
+os.system('pbes2bool -v abp_bw.infinitely_often_receive_d1.pbes')
+
+os.system('lps2pbes -v -f infinitely_often_receive_for_all_d.mcf abp_bw.lps abp_bw.infinitely_often_receive_for_all_d.pbes')
+os.system('pbes2bool -v abp_bw.infinitely_often_receive_for_all_d.pbes')
+
+os.system('lps2pbes -v -f read_then_eventually_send.mcf abp_bw.lps abp_bw.read_then_eventually_send.pbes')
+os.system('pbes2bool -v abp_bw.read_then_eventually_send.pbes')
+
+os.system('lps2pbes -v -f read_then_eventually_send_if_fair.mcf abp_bw.lps abp_bw.read_then_eventually_send_if_fair.pbes')
+os.system('pbes2bool -v abp_bw.read_then_eventually_send_if_fair.pbes')
+
+
