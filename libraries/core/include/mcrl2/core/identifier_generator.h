609d6ef9ac2e4313654de4dbed4f4e71d32bf103
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-09-21 08:03:30 +0000

    - Added a new identifier generator class that uses a map internally. For each prefix a number is stored. For example if f is an identifier generator with map {"a" -> 6} then f("a") will return the identifier "a7", and the map is updated to {"a" -> 7}. This identifier generator is expected to be more efficient than data::number_postfix_generator. Performance can be increased by using spirit::qi and spirit::karma instead of stringstream based conversions of numbers.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8154 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/core/include/mcrl2/core/identifier_generator.h
@@ -0,0 +1,128 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/core/identifier_generator.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_CORE_IDENTIFIER_GENERATOR_H
+#define MCRL2_CORE_IDENTIFIER_GENERATOR_H
+
+#include <cassert>
+#include <cctype>
+#include <map>
+#include <string>
+#include <sstream>
+#include <boost/lexical_cast.hpp>
+#include "mcrl2/core/find.h"
+#include "mcrl2/atermpp/container_utility.h"
+
+namespace mcrl2 {
+
+namespace core {
+
+  /// \brief Identifier generator that generates names with a postfix consisting of a number.
+  class number_postfix_generator
+  {
+    protected:
+      /// \brief A map that maintains the highest index for each prefix.
+      std::map<std::string, int> m_index;
+     
+      /// \brief The default hint.
+      std::string m_hint;
+
+
+    public:
+
+      /// \brief Constructor.
+      number_postfix_generator(std::string hint = "FRESH_VAR")
+        : m_hint(hint)
+      {}
+
+      /// \brief Adds the strings in the range [first, last) to the context.
+      /// \param id A string
+      void add_to_context(const std::string& id)
+      {
+        std::string::size_type i = id.find_last_not_of("0123456789");
+        if (i == std::string::npos)
+        {
+          m_index[id] = -1;
+        }
+        else
+        {
+          std::string s = id.substr(0, i + 1);
+          std::string num = id.substr(i + 1);
+          int index = boost::lexical_cast<int>(num);
+          m_index[s] = index;
+        }
+      }
+  
+      /// \brief Adds the strings in the range [first, last) to the context.
+      /// \param first
+      /// \param last
+      /// [first, last) is a sequence of strings that is used as context.
+      template <typename Iter>
+      void add_to_context(Iter first, Iter last)
+      {
+        for (Iter i = first; i != last; ++i)
+        {
+          add_to_context(*i);
+        }
+      }
+  
+      /// \brief Constructor.
+      /// \param first
+      /// \param last
+      /// [first, last) is a sequence of strings that is used as context.
+      template <typename Iter>
+      number_postfix_generator(Iter first, Iter last, std::string hint = "FRESH_VAR")
+        : m_hint(hint)
+      {
+        add_to_context(first, last);
+      }
+  
+      /// \brief Generates a fresh identifier that doesn't appear in the context.
+      /// \return A fresh identifier.
+      std::string operator()(std::string hint)
+      {
+        // make sure there are no digits at the end of hint
+		    if (std::isdigit(hint.back()))
+        {
+          std::string::size_type i = hint.find_last_not_of("0123456789");
+          hint = hint.substr(0, i + 1);
+        }
+
+        std::ostringstream out;
+        out << hint << ++m_index[hint];
+        return out.str();
+      }
+
+      /// \brief Generates a fresh identifier that doesn't appear in the context.
+      /// \return A fresh identifier.
+      std::string operator()()
+      {
+        return (*this)(m_hint);
+      }
+      
+      /// \brief Returns the default hint.
+      const std::string& hint() const
+      {
+        return m_hint;
+      }
+      
+      /// \brief Returns the default hint.
+      std::string& hint()
+      {
+        return m_hint;
+      }
+  };
+
+} // namespace core
+
+} // namespace mcrl2
+
+#endif // MCRL2_CORE_IDENTIFIER_GENERATOR_H
---
+++ libraries/core/test/identifier_generator_test.cpp
@@ -0,0 +1,61 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file identifier_generator_test.cpp
+/// \brief Test for identifier generators.
+
+#include <iostream>
+#include <boost/test/minimal.hpp>
+
+#include "mcrl2/core/identifier_generator.h"
+#include "mcrl2/core/garbage_collection.h"
+#include "mcrl2/atermpp/aterm_init.h"
+
+using namespace mcrl2;
+using namespace mcrl2::core;
+
+void identifier_generator_test()
+{
+  number_postfix_generator generator;
+  generator.add_to_context("c6");
+  std::string s;
+
+  s = generator("c");
+  BOOST_CHECK(s == "c7");
+
+  std::vector<std::string> v;
+  v.push_back("a1");
+  v.push_back("c012");
+  generator.add_to_context(v.begin(), v.end());
+
+  s = generator("c");
+  BOOST_CHECK(s == "c13");
+
+  s = generator("a");
+  BOOST_CHECK(s == "a2");
+
+  s = generator("a");
+  BOOST_CHECK(s == "a3");
+
+  s = generator("a2");
+  BOOST_CHECK(s == "a4");
+
+  s = generator();
+  BOOST_CHECK(s == "FRESH_VAR1");
+
+  core::garbage_collect();
+}
+
+int test_main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv);
+
+  identifier_generator_test();
+
+  return EXIT_SUCCESS;
+}
