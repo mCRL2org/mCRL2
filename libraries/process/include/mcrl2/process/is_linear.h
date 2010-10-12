66a4377eb87b5a6fdb211573658cd9314accd092
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-10-12 08:11:12 +0000

    - Added missing file.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8246 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/process/include/mcrl2/process/is_linear.h
@@ -0,0 +1,57 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/process/is_linear.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_PROCESS_IS_LINEAR_H
+#define MCRL2_PROCESS_IS_LINEAR_H
+
+#include "mcrl2/process/detail/linear_process_expression_visitor.h"
+
+namespace mcrl2 {
+
+namespace process {
+
+  /// \brief Returns true if the process specification is linear.
+  /// \param p A process specification
+  /// \return True if the process specification is linear.
+  inline
+  bool is_linear(const process_specification& p, bool verbose = false)
+  {
+    if (p.equations().size() != 1)
+    {
+      if (verbose)
+      {
+        std::clog << "The number of equations is not equal to 1" << std::endl;
+      }
+      return false;
+    }
+    detail::linear_process_expression_visitor visitor;
+    {
+      if (!visitor.is_linear(*p.equations().begin(), verbose))
+      {
+        return false;
+      }
+      if (!is_process_instance(p.init()) && ! (is_process_instance_assignment(p.init())))
+      {
+        if (verbose)
+        {
+          std::clog << "The initial process " << core::pp(p.init()) << " is not a process instance or a process instance assignment" << std::endl;
+        }
+        return false;
+      }
+    }
+    return true;
+  } 
+
+} // namespace process
+
+} // namespace mcrl2
+
+#endif // MCRL2_PROCESS_IS_LINEAR_H
