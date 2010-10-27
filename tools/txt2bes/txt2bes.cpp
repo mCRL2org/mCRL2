9f2ed0d4a6933762613e7c6871bc052c70e3c857
Wieger Wesselink <j.w.wesselink@tue.nl>
2010-10-27 09:59:35 +0000

    - Added the tool txt2bes, since it is needed for testing. It currently uses txt2pbes, since there is no parser for BESs available.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8301 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ tools/txt2bes/txt2bes.cpp
@@ -0,0 +1,75 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file txt2pbes.cpp
+/// \brief Parse a textual description of a BES.
+
+#include "boost.hpp" // precompiled headers
+
+#define NAME "txt2bes"
+#define AUTHOR "Wieger Wesselink"
+
+//C++
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+//mCRL2 specific
+#include "mcrl2/core/text_utility.h"
+#include "mcrl2/utilities/input_output_tool.h"
+#include "mcrl2/utilities/mcrl2_gui_tool.h"
+#include "mcrl2/pbes/txt2pbes.h"
+#include "mcrl2/pbes/pbes2besconversion.h"
+#include "mcrl2/atermpp/aterm_init.h"
+
+using namespace mcrl2;
+using namespace mcrl2::utilities;
+using namespace mcrl2::utilities::tools;
+
+class txt2pbes_tool: public input_output_tool
+{
+  typedef input_output_tool super;
+
+  public:
+    txt2pbes_tool()
+      : super(NAME, AUTHOR,
+          "parse a textual description of a BES",
+          "Parse the textual description of a BES from INFILE and write it to OUTFILE. "
+          "If INFILE is not present, stdin is used. If OUTFILE is not present, stdout is used."
+        )
+    {}
+
+    bool run()
+    {
+      pbes_system::pbes<> p;
+      if (input_filename().empty()) 
+      { 
+        p = pbes_system::txt2pbes(std::cin);
+      } 
+      else
+      {
+        std::ifstream from(input_filename().c_str());
+        p = pbes_system::txt2pbes(from);
+      }
+      bes::boolean_equation_system<> b = pbes_system::pbes2besconversion(p);
+      b.save(output_filename());
+      return true;
+    }
+};
+
+class txt2pbes_gui_tool: public mcrl2_gui_tool<txt2pbes_tool> {
+public:
+	txt2pbes_gui_tool() {}
+};
+
+
+int main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv)
+  return txt2pbes_gui_tool().execute(argc, argv);
+}
