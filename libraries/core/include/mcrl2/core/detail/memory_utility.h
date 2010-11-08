adb9c2ae8e2727b5635f35c2bae71293532d9416
Jan Friso Groote <j.f.groote@tue.nl>
2010-11-08 08:08:25 +0000

    Small textual adaptions. Added a check to include this file only once.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8349 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/include/mcrl2/core/detail/memory_utility.h
+++ libraries/core/include/mcrl2/core/detail/memory_utility.h
@@ -6,18 +6,32 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-/// \file mcrl2/lps/print.h
-/// \brief add your file description here.
+/// \file mcrl2/core/detail/memory_utility.h
+/// \brief This file contains a workaround that allows to
+/// assign a small array of elements on the stack. For most
+/// systems this can be done by:
+///
+///    type var[n];
+///
+/// for variable n, but for the MSVC compilers, the
+/// special function _alloca must be used.
 
 
 // Declare a local array NAME of type TYPE and SIZE elements (where SIZE
 // is not a constant value). These will be allocated on the stack, and
 // therefore any aterms put in there will be automatically protected.
 
+
+#ifndef MCRL2_CORE_DETAIL_MEMORY_UTILITY_H
+#define MCRL2_CORE_DETAIL_MEMORY_UTILITY_H
+
+
 #ifdef _MSC_VER
 #include "malloc.h"
 #define SYSTEM_SPECIFIC_ALLOCA(NAME,TYPE,SIZE)  TYPE *NAME = (TYPE *) _alloca((SIZE)*sizeof(TYPE))
 #else
 #define SYSTEM_SPECIFIC_ALLOCA(NAME,TYPE,SIZE)  TYPE NAME[SIZE]
+#endif
+
 #endif
 
