mCRL2org/mCRL2

commit 2dc1066368680ada5f70d5d7ea84a7ea5f2a5071

Renamed Refresh to DoRefresh. This will remove the warning about a virtual hidden function
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8377 21252fe5-60e8-0310-8ff0-c1ae1001594e

tools/mcrl2-gui/gui/projectcontrol.h
@@ -158,7 +158,7 @@ public:
 							+ wxT("'."));
 				} else {
 					//Collapse tree
-		            this->Refresh();
+		            this->DoRefresh();
 					//Expand tree to new dir
 					this->ExpandPath(s);
 					//Re-label dir
@@ -179,7 +179,7 @@ public:
 				Delete();
 				break;
 			case ID_REFRESH:
-				Refresh();
+				DoRefresh();
 				break;
 			case ID_EXPAND:
 			  this->ExpandPath(this->GetPath());
@@ -294,7 +294,7 @@ public:
 			CreateNewFile();
 			break;
     case WXK_F5:
-    	Refresh();
+    	DoRefresh();
 			break;
     case WXK_F6:
       CopyFile();
@@ -330,7 +330,7 @@ public:
     }
 	}
 
-	void Refresh(){
+	void DoRefresh(){
 		//std::cout<< "Refresh Tree" << std::	endl;
 		wxString selected_file = this->GetPath();
 		this->ReCreateTree();
@@ -557,7 +557,7 @@ public:
 
       f.Create( wf->GetFullPath() , false);
       //Expand to new created file
-      this->Refresh();
+      this->DoRefresh();
       this->ExpandPath( wf->GetFullPath());
 
     } else {
@@ -569,7 +569,7 @@ public:
       f.Create(wxString::Format( wf->GetFullPath() + wxT("(%i)"), i));
 
       //Expand to new created file
-      this->Refresh();
+      this->DoRefresh();
       this->ExpandPath(wxString::Format( wf->GetFullPath() + wxT("(%i)"), i));
     }
 

commit 2e8d02004078ac9880485860453579dbdc7b77f8

* Fixed Ticket #792 * Fixed Ticket #791 * Removed obsolete declared timer.
These faults where introduced in r8377


git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8397 21252fe5-60e8-0310-8ff0-c1ae1001594e

tools/mcrl2-gui/gui/mainframe.h
@@ -185,7 +185,7 @@ public:
 				m_extention_tool_mapping, m_ExecutedCommandsPanel, this->GetNoteBookToolPanel());
 
 		m_FileBrowserPanel->SetSize(250,-1);
-		m_FileBrowserPanel->Refresh();
+		m_FileBrowserPanel->DoRefresh();
 
 		m_mgr.AddPane(m_FileBrowserPanel, wxLEFT, wxT("File Browser"));
 
@@ -398,7 +398,7 @@ public:
 	;
 
 	void OnRefresh(wxCommandEvent& /*event*/) {
-		m_FileBrowserPanel->Refresh();
+		m_FileBrowserPanel->DoRefresh();
 	}
 	;
 
@@ -487,7 +487,7 @@ public:
         p = (wxStringClientData*) evt.GetClientData();
         if (p)
         {
-          m_FileBrowserPanel->Refresh();
+          m_FileBrowserPanel->DoRefresh();
           m_FileBrowserPanel->ExpandPath(p->GetData());
         }
       }

tools/mcrl2-gui/gui/projectcontrol.h
@@ -77,8 +77,6 @@ public:
 		m_listbox_output = output;
 		m_notebookpanel = notebookpanel;
 		m_extention_tool_mapping = extention_tool_mapping;
-		refresh_dir = new wxTimer(this, ID_TIMER);
-		refresh_dir->Start(500);
 		this->Fit();
 		this->Layout();
 
@@ -606,7 +604,6 @@ public:
 
 	}
 
-	wxTimer *refresh_dir;
 protected:
 	wxWindow *m_parent;
 