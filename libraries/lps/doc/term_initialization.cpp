bc66544dce06fda64d8221373411b40707d13984
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-01-25 13:30:37 +0000

    - Removed two outdated doc examples.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8742 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/doc/term_initialization.cpp
+++
@@ -1,7 +0,0 @@
-//[ term_initialization
-int main(int argc, char **argv)
-{
-  /*<< The ATerm Library must be initialized with a dummy term that is declared at the beginning of the `main` function. There is no known way to circumvent this.>>*/
-  MCRL2_ATERM_INIT(argc, argv)
-}
-//]
