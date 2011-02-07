5e569e69decb6fcf574dbe65900837fc74da2dc0
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-02-07 15:47:28 +0000

    - Added generation of find functions in terms of new traversers.
    - Put traverser workaround for Visual C++ in a separate header file, similar to how it is done for builders.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8817 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/build/generate_traverser_functions.py
+++ libraries/core/build/generate_traverser_functions.py
@@ -7,7 +7,7 @@ import re
 from path import *
 from mcrl2_utility import *
 
-REPLACE_FUNCTION_TEXT = '''template <typename T, typename Substitution>
+SUBSTITUTE_FUNCTION_TEXT = '''template <typename T, typename Substitution>
   void substitute_variables(T& x,
                             Substitution sigma,
                             typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
@@ -64,14 +64,97 @@ REPLACE_FUNCTION_TEXT = '''template <typename T, typename Substitution>
   }
 '''
 
+FIND_VARIABLES_FUNCTION_TEXT = '''  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \param[in,out] o an output iterator to which all variables occurring in x are written.
+  /// \\\\return All variables that occur in the term x
+  template <typename T, typename OutputIterator>
+  void find_variables(const T& x, OutputIterator o)
+  {
+    data::detail::make_find_variables_traverser<NAMESPACE::traverser>(o)(x);
+  }
+
+  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \\\\return All variables that occur in the object x
+  template <typename T>
+  std::set<data::variable> find_variables(const T& x)
+  {
+    std::set<data::variable> result;
+    NAMESPACE::find_variables(x, std::inserter(result, result.end()));
+    return result;
+  }
+
+  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \param[in,out] o an output iterator to which all variables occurring in x are added.
+  /// \\\\return All free variables that occur in the object x
+  template <typename T, typename OutputIterator>
+  void find_free_variables(const T& x, OutputIterator o, typename atermpp::detail::disable_if_container<OutputIterator>::type* = 0)
+  {
+    data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o)(x);
+  }
+
+  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \param[in,out] o an output iterator to which all variables occurring in x are written.
+  /// \param[in] bound a container of variables
+  /// \\\\return All free variables that occur in the object x
+  template <typename T, typename OutputIterator, typename VariableContainer>
+  void find_free_variables(const T& x, OutputIterator o, const VariableContainer& bound)
+  {
+    data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o, bound)(x);
+  }
+
+  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \\\\return All free variables that occur in the object x
+  template <typename T>
+  std::set<data::variable> find_free_variables(const T& x)
+  {
+    std::set<data::variable> result;
+    NAMESPACE::find_free_variables(x, std::inserter(result, result.end()));
+    return result;
+  }
+
+  /// \\\\brief Returns all variables that occur in an object
+  /// \param[in] x an object containing variables
+  /// \param[in] bound a bound a container of variables
+  /// \\\\return All free variables that occur in the object x
+  template <typename T, typename VariableContainer>
+  std::set<data::variable> find_free_variables(const T& x, VariableContainer const& bound, typename atermpp::detail::enable_if_container<VariableContainer, data::variable>::type* = 0)
+  {
+    std::set<data::variable> result;
+    NAMESPACE::find_free_variables(x, std::inserter(result, result.end()), bound);
+    return result;
+  }
+'''
+
 def generate_code(filename, namespace, label, text):
     text = re.sub('NAMESPACE', namespace, text)
-    insert_text_in_file(filename, text, 'generated %s %s code' % (namespace, label))   
+    insert_text_in_file(filename, text, 'generated %s %s code' % (namespace, label))
+    print_labels(namespace, label)   
+
+def print_labels(namespace, label):
+    print '//--- start generated %s %s code ---//' % (namespace, label)
+    print '//--- end generated %s %s code ---//' % (namespace, label)
+
+def generate_substitute_functions():
+    generate_code('../../lps/include/mcrl2/lps/substitute.h'          , 'lps'             , 'replace', SUBSTITUTE_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'action_formulas' , 'replace', SUBSTITUTE_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'regular_formulas', 'replace', SUBSTITUTE_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'state_formulas'  , 'replace', SUBSTITUTE_FUNCTION_TEXT)
+    generate_code('../../pbes/include/mcrl2/pbes/substitute.h'        , 'pbes_system'     , 'replace', SUBSTITUTE_FUNCTION_TEXT)
+    generate_code('../../process/include/mcrl2/process/substitute.h'  , 'process'         , 'replace', SUBSTITUTE_FUNCTION_TEXT)
+
+def generate_find_variable_functions():
+    generate_code('../../lps/include/mcrl2/lps/find.h'          , 'lps'             , 'find', FIND_VARIABLES_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/find.h', 'action_formulas' , 'find', FIND_VARIABLES_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/find.h', 'regular_formulas', 'find', FIND_VARIABLES_FUNCTION_TEXT)
+    generate_code('../../lps/include/mcrl2/modal_formula/find.h', 'state_formulas'  , 'find', FIND_VARIABLES_FUNCTION_TEXT)
+    generate_code('../../pbes/include/mcrl2/pbes/find.h'        , 'pbes_system'     , 'find', FIND_VARIABLES_FUNCTION_TEXT)
+    generate_code('../../process/include/mcrl2/process/find.h'  , 'process'         , 'find', FIND_VARIABLES_FUNCTION_TEXT)
 
 if __name__ == "__main__":
-    generate_code('../../lps/include/mcrl2/lps/substitute.h'          , 'lps'             , 'replace', REPLACE_FUNCTION_TEXT)
-    generate_code('../../process/include/mcrl2/process/substitute.h'  , 'process'         , 'replace', REPLACE_FUNCTION_TEXT)
-    generate_code('../../pbes/include/mcrl2/pbes/substitute.h'        , 'pbes_system'     , 'replace', REPLACE_FUNCTION_TEXT)
-    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'action_formulas' , 'replace', REPLACE_FUNCTION_TEXT)
-    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'regular_formulas', 'replace', REPLACE_FUNCTION_TEXT)
-    generate_code('../../lps/include/mcrl2/modal_formula/substitute.h', 'state_formulas'  , 'replace', REPLACE_FUNCTION_TEXT)
+    #generate_substitute_functions()
+    generate_find_variable_functions()
---
+++ libraries/core/include/mcrl2/core/detail/traverser_msvc.inc.h
@@ -0,0 +1,16 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/core/detail/traverser_msvc.inc.h
+/// \brief add your file description here.
+
+template <typename Container >
+void operator()(Container const& a)
+{
+  super::operator()(a);
+}
