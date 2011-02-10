e369e5b4e7b734a3ddce92c3608591fe5e7d34e7
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-02-10 14:44:38 +0000

    - Adapted the script for generating wiki tool manual pages such that the pages are automatically generated for all tools that have a file named tool_manual.wiki in the top level tool directory. This will have to be combined with a script that imports the generated files into the wiki.
    Note that the content of tool_manual.wiki is inserted into the corresponding tool manual page.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8843 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- build/wiki/print_manual_pages.py
+++ build/wiki/print_manual_pages.py
@@ -14,75 +14,7 @@ import re
 from optparse import OptionParser
 from path import *
 
-TOOLS = '''
-  lps2pbes
-  lpsbisim2pbes
-  lpsconstelm
-  lpsinfo
-  lpsparelm
-  lpsrewr
-  pbes2bes
-  pbesconstelm
-  pbesinfo
-  pbesparelm
-  pbespareqelm
-  pbespgsolve
-  pbesrewr
-  txt2lps
-  txt2pbes
-'''
-
-# Some tools have additional information in the short description that is not present
-# in the command line help.
-extended_short_description = {
-  'lpsconstelm' : '''
-
-If it can be determined that certain parameters of this LPS remain constant throughout any run of the process, all occurrences of these process parameter are replaced by the initial value and the process parameters are removed from the LPS.
-
-If the initial value of a process parameter is a global variable and remains a global variable throughout the run of the process, the process variable is considered constant.
-
-If the initial value of a process parameter is a global variable and is only changed once to a certain value, the process parameter is constant and the specific value is used for substitution.
-''',
-  'lps2pbes' : '''
-
-The concrete syntax of state formulas can be found at 
-<http://www.mcrl2.org/mcrl2/wiki/index.php/Language_reference/mu-calculus_syntax>.
-''',
-  'lpsinfo' : '''
-
-By default, the following information about the LPS is shown: 
-* number of summands
-* number of tau-summands
-* number of free variables
-* number of process parameters
-* number of action labels
-* number of used versus the number of declared actions
-* number of sorts
-''',
-  'lpsparelm' : '''
-
-Removing parameters from the LPS can lead to a reduction when generating a state space of an LPS.
-''',
-  'lpsrewr' : '''
-
-The following data expressions are rewritten: 
-* conditions, action parameters, time expressions and next states of LPS summands
-* process parameters of the initial state
-* conditions and right-hand sides of data equations
-
-Rewriting LPS summands and the initial state is done to simplify these parts of the LPS. Rewriting data equations is done to speed up state space generation. In most cases, this results in a performance gain of at most 5%.
-''',
-  'pbesinfo' : '''
-
-By default, the following information about the PBES is shown: 
-* information if the PBES is closed and well-formed;
-* number of equations, &mu;s and &nu;s.
-''',
-  'txt2pbes' : '''
-
-The textual description should adhere to the following [[Language reference/PBES syntax|BNF syntax description]].
-''',
-}
+#extended_short_description = {
 
 # change the line 'Written by X; Y' into 'Implemented by X, with contributions from Y'
 def process_authors(filename):
@@ -94,10 +26,9 @@ def process_authors(filename):
 def main():
     usage = "usage: %prog [options]"
     parser = OptionParser(usage)
+    parser.add_option("-m", "--mcrl2dir", dest="mcrl2dir", help="the location of the mcrl2 distribution")
     parser.add_option("-t", "--tooldir", dest="tooldir", help="the tools directory")
     parser.add_option("-g", "--generate-pages", action="store_true", help="generate manual pages for selected tools")
-    parser.add_option("-i", "--import-pages", action="store_false", help="import manual pages into mediawiki")
-    parser.add_option("-u", "--user", dest="user", help="the mediawiki user name")
     (options, args) = parser.parse_args()
 
     # create subdirectory output/User_manual if it doesn't exist
@@ -105,9 +36,21 @@ def main():
         os.makedirs('output/User_manual')
 
     if options.generate_pages:
-        # generate tool pages
-        for toolname in TOOLS.split():
-            filename = 'output/User_manual%s%s' % (os.sep, toolname)
+        for dir in (path(options.mcrl2dir) / 'tools').dirs():
+            toolname = dir.basename()
+
+            # skip .svn directory
+            if toolname.startswith('.'):
+                continue
+
+            tool_manual_file = dir / 'tool_manual.wiki'           
+
+            # skip directories that do not have the file tool_manual.wiki
+            if not tool_manual_file.exists():
+                continue          
+
+            filename = path('output') / 'User_manual' / toolname
+            
             if options.tooldir == None:
                 tool = toolname
             else:
@@ -115,22 +58,12 @@ def main():
             print tool
             os.system('%s --generate-wiki-page > %s' % (tool, filename))
             process_authors(filename)
-        
-        # update tool pages with custom additions
-        for toolname in TOOLS.split():
-            filename = 'output/User_manual%s%s' % (os.sep, toolname)
-            manual = path(filename).text()
-            if extended_short_description.has_key(toolname):
-                manual = re.compile(r'(== Short Description ==.*)(== Options ==)', re.S).sub(r'\1' + extended_short_description[tool] + r'\2', manual)
-                path(filename).write_text(manual)
 
-        # import changes into mediawiki
-        if options.import_pages:
-            os.chdir('output')
-            for file in path('.').walkfiles():
-                file = re.sub(r'^\.\/', '', file)
-                cmd = 'php /var/www/win/mcrl2/wiki/maintenance/importTextFile.php --user %s --comment update --title %s %s' % (options.user, file, file)
-                os.system(cmd)
+            text = filename.text()
+            mtext = tool_manual_file.text()
+            if len(mtext.strip()) > 0:
+                text = re.compile(r'(== Short Description ==.*)(== Options ==)', re.S).sub(r'\1' + mtext + r'\2', text)
+            filename.write_text(text)
 
 if __name__ == '__main__':
     main()
