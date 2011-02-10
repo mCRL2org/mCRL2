20b4cddedf72cc893da436f1122448f42011cb4c
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-02-10 16:11:55 +0000

    - - Applied a number of changes to the indentation of generated code, such that it conforms to the AStyle layout.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8846 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/build/generate_traverser_functions.py
+++ libraries/core/build/generate_traverser_functions.py
@@ -8,189 +8,189 @@ from path import *
 from mcrl2_utility import *
 
 SUBSTITUTE_FUNCTION_TEXT = '''template <typename T, typename Substitution>
-  void substitute_variables(T& x,
+void substitute_variables(T& x,
+                          Substitution sigma,
+                          typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                         )
+{
+  core::make_update_apply_builder<NAMESPACE::data_expression_builder>(sigma)(x);
+}
+
+template <typename T, typename Substitution>
+T substitute_variables(const T& x,
+                       Substitution sigma,
+                       typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                      )
+{   
+  return core::make_update_apply_builder<NAMESPACE::data_expression_builder>(sigma)(x);
+}
+
+template <typename T, typename Substitution>
+void substitute_free_variables(T& x,
+                               Substitution sigma,
+                               typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                              )
+{
+  data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x);
+}
+
+template <typename T, typename Substitution>
+T substitute_free_variables(const T& x,
                             Substitution sigma,
-                            typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                            typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
                            )
-  {
-    core::make_update_apply_builder<NAMESPACE::data_expression_builder>(sigma)(x);
-  }
+{
+  return data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x);
+}
 
-  template <typename T, typename Substitution>
-  T substitute_variables(const T& x,
-                         Substitution sigma,
-                         typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
-                        )
-  {   
-    return core::make_update_apply_builder<NAMESPACE::data_expression_builder>(sigma)(x);
-  }
+template <typename T, typename Substitution, typename VariableContainer>
+void substitute_free_variables(T& x,
+                               Substitution sigma,
+                               const VariableContainer& bound_variables,
+                               typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                              )
+{
+  data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x, bound_variables);
+}
 
-  template <typename T, typename Substitution>
-  void substitute_free_variables(T& x,
-                                 Substitution sigma,
-                                 typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
-                                )
-  {
-    data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x);
-  }
-
-  template <typename T, typename Substitution>
-  T substitute_free_variables(const T& x,
-                              Substitution sigma,
-                              typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
-                             )
-  {
-    return data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x);
-  }
-
-  template <typename T, typename Substitution, typename VariableContainer>
-  void substitute_free_variables(T& x,
-                                 Substitution sigma,
-                                 const VariableContainer& bound_variables,
-                                 typename boost::disable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
-                                )
-  {
-    data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x, bound_variables);
-  }
-
-  template <typename T, typename Substitution, typename VariableContainer>
-  T substitute_free_variables(const T& x,
-                              Substitution sigma,
-                              const VariableContainer& bound_variables,
-                              typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
-                             )
-  {
-    return data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x, bound_variables);
-  }
+template <typename T, typename Substitution, typename VariableContainer>
+T substitute_free_variables(const T& x,
+                            Substitution sigma,
+                            const VariableContainer& bound_variables,
+                            typename boost::enable_if<typename boost::is_base_of<atermpp::aterm_base, T>::type>::type* = 0
+                           )
+{
+  return data::detail::make_substitute_free_variables_builder<NAMESPACE::data_expression_builder, NAMESPACE::add_data_variable_binding>(sigma)(x, bound_variables);
+}
 '''
 
-FIND_VARIABLES_FUNCTION_TEXT = '''  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \param[in,out] o an output iterator to which all variables occurring in x are written.
-  /// \\\\return All variables that occur in the term x
-  template <typename T, typename OutputIterator>
-  void find_variables(const T& x, OutputIterator o)
-  {
-    data::detail::make_find_variables_traverser<NAMESPACE::variable_traverser>(o)(x);
-  }
+FIND_VARIABLES_FUNCTION_TEXT = '''/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \param[in,out] o an output iterator to which all variables occurring in x are written.
+/// \\\\return All variables that occur in the term x
+template <typename T, typename OutputIterator>
+void find_variables(const T& x, OutputIterator o)
+{
+  data::detail::make_find_variables_traverser<NAMESPACE::variable_traverser>(o)(x);
+}
 
-  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \\\\return All variables that occur in the object x
-  template <typename T>
-  std::set<data::variable> find_variables(const T& x)
-  {
-    std::set<data::variable> result;
-    NAMESPACE::find_variables(x, std::inserter(result, result.end()));
-    return result;
-  }
+/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \\\\return All variables that occur in the object x
+template <typename T>
+std::set<data::variable> find_variables(const T& x)
+{
+  std::set<data::variable> result;
+  NAMESPACE::find_variables(x, std::inserter(result, result.end()));
+  return result;
+}
 
-  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \param[in,out] o an output iterator to which all variables occurring in x are added.
-  /// \\\\return All free variables that occur in the object x
-  template <typename T, typename OutputIterator>
-  void find_free_variables(const T& x, OutputIterator o)
-  {
-    data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o)(x);
-  }
+/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \param[in,out] o an output iterator to which all variables occurring in x are added.
+/// \\\\return All free variables that occur in the object x
+template <typename T, typename OutputIterator>
+void find_free_variables(const T& x, OutputIterator o)
+{
+  data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o)(x);
+}
 
-  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \param[in,out] o an output iterator to which all variables occurring in x are written.
-  /// \param[in] bound a container of variables
-  /// \\\\return All free variables that occur in the object x
-  template <typename T, typename OutputIterator, typename VariableContainer>
-  void find_free_variables_with_bound(const T& x, OutputIterator o, const VariableContainer& bound)
-  {
-    data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o, bound)(x);
-  }
+/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \param[in,out] o an output iterator to which all variables occurring in x are written.
+/// \param[in] bound a container of variables
+/// \\\\return All free variables that occur in the object x
+template <typename T, typename OutputIterator, typename VariableContainer>
+void find_free_variables_with_bound(const T& x, OutputIterator o, const VariableContainer& bound)
+{
+  data::detail::make_find_free_variables_traverser<NAMESPACE::variable_traverser, NAMESPACE::add_data_variable_binding>(o, bound)(x);
+}
 
-  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \\\\return All free variables that occur in the object x
-  template <typename T>
-  std::set<data::variable> find_free_variables(const T& x)
-  {
-    std::set<data::variable> result;
-    NAMESPACE::find_free_variables(x, std::inserter(result, result.end()));
-    return result;
-  }
+/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \\\\return All free variables that occur in the object x
+template <typename T>
+std::set<data::variable> find_free_variables(const T& x)
+{
+  std::set<data::variable> result;
+  NAMESPACE::find_free_variables(x, std::inserter(result, result.end()));
+  return result;
+}
 
-  /// \\\\brief Returns all variables that occur in an object
-  /// \param[in] x an object containing variables
-  /// \param[in] bound a bound a container of variables
-  /// \\\\return All free variables that occur in the object x
-  template <typename T, typename VariableContainer>
-  std::set<data::variable> find_free_variables_with_bound(const T& x, VariableContainer const& bound)
-  {
-    std::set<data::variable> result;
-    NAMESPACE::find_free_variables_with_bound(x, std::inserter(result, result.end()), bound);
-    return result;
-  }
+/// \\\\brief Returns all variables that occur in an object
+/// \param[in] x an object containing variables
+/// \param[in] bound a bound a container of variables
+/// \\\\return All free variables that occur in the object x
+template <typename T, typename VariableContainer>
+std::set<data::variable> find_free_variables_with_bound(const T& x, VariableContainer const& bound)
+{
+  std::set<data::variable> result;
+  NAMESPACE::find_free_variables_with_bound(x, std::inserter(result, result.end()), bound);
+  return result;
+}
 
-  /// \\\\brief Returns all identifiers that occur in an object
-  /// \param[in] x an object containing identifiers
-  /// \param[in,out] o an output iterator to which all identifiers occurring in x are written.
-  /// \\\\return All identifiers that occur in the term x
-  template <typename T, typename OutputIterator>
-  void find_identifiers(const T& x, OutputIterator o)
-  {
-    data::detail::make_find_identifiers_traverser<NAMESPACE::identifier_string_traverser>(o)(x);
-  }
-  
-  /// \\\\brief Returns all identifiers that occur in an object
-  /// \param[in] x an object containing identifiers
-  /// \\\\return All identifiers that occur in the object x
-  template <typename T>
-  std::set<core::identifier_string> find_identifiers(const T& x)
-  {
-    std::set<core::identifier_string> result;
-    NAMESPACE::find_identifiers(x, std::inserter(result, result.end()));
-    return result;
-  }
+/// \\\\brief Returns all identifiers that occur in an object
+/// \param[in] x an object containing identifiers
+/// \param[in,out] o an output iterator to which all identifiers occurring in x are written.
+/// \\\\return All identifiers that occur in the term x
+template <typename T, typename OutputIterator>
+void find_identifiers(const T& x, OutputIterator o)
+{
+  data::detail::make_find_identifiers_traverser<NAMESPACE::identifier_string_traverser>(o)(x);
+}
 
-  /// \\\\brief Returns all sort expressions that occur in an object
-  /// \param[in] x an object containing sort expressions
-  /// \param[in,out] o an output iterator to which all sort expressions occurring in x are written.
-  /// \\\\return All sort expressions that occur in the term x
-  template <typename T, typename OutputIterator>
-  void find_sort_expressions(const T& x, OutputIterator o)
-  {
-    data::detail::make_find_sort_expressions_traverser<NAMESPACE::sort_expression_traverser>(o)(x);
-  }
-  
-  /// \\\\brief Returns all sort expressions that occur in an object
-  /// \param[in] x an object containing sort expressions
-  /// \\\\return All sort expressions that occur in the object x
-  template <typename T>
-  std::set<data::sort_expression> find_sort_expressions(const T& x)
-  {
-    std::set<data::sort_expression> result;
-    NAMESPACE::find_sort_expressions(x, std::inserter(result, result.end()));
-    return result;
-  }
+/// \\\\brief Returns all identifiers that occur in an object
+/// \param[in] x an object containing identifiers
+/// \\\\return All identifiers that occur in the object x
+template <typename T>
+std::set<core::identifier_string> find_identifiers(const T& x)
+{
+  std::set<core::identifier_string> result;
+  NAMESPACE::find_identifiers(x, std::inserter(result, result.end()));
+  return result;
+}
 
-  /// \\\\brief Returns all function symbols that occur in an object
-  /// \param[in] x an object containing function symbols
-  /// \param[in,out] o an output iterator to which all function symbols occurring in x are written.
-  /// \\\\return All function symbols that occur in the term x
-  template <typename T, typename OutputIterator>
-  void find_function_symbols(const T& x, OutputIterator o)
-  {
-    data::detail::make_find_function_symbols_traverser<NAMESPACE::data_expression_traverser>(o)(x);
-  }
-  
-  /// \\\\brief Returns all function symbols that occur in an object
-  /// \param[in] x an object containing function symbols
-  /// \\\\return All function symbols that occur in the object x
-  template <typename T>
-  std::set<data::function_symbol> find_function_symbols(const T& x)
-  {
-    std::set<data::function_symbol> result;
-    NAMESPACE::find_function_symbols(x, std::inserter(result, result.end()));
-    return result;
-  }
+/// \\\\brief Returns all sort expressions that occur in an object
+/// \param[in] x an object containing sort expressions
+/// \param[in,out] o an output iterator to which all sort expressions occurring in x are written.
+/// \\\\return All sort expressions that occur in the term x
+template <typename T, typename OutputIterator>
+void find_sort_expressions(const T& x, OutputIterator o)
+{
+  data::detail::make_find_sort_expressions_traverser<NAMESPACE::sort_expression_traverser>(o)(x);
+}
+
+/// \\\\brief Returns all sort expressions that occur in an object
+/// \param[in] x an object containing sort expressions
+/// \\\\return All sort expressions that occur in the object x
+template <typename T>
+std::set<data::sort_expression> find_sort_expressions(const T& x)
+{
+  std::set<data::sort_expression> result;
+  NAMESPACE::find_sort_expressions(x, std::inserter(result, result.end()));
+  return result;
+}
+
+/// \\\\brief Returns all function symbols that occur in an object
+/// \param[in] x an object containing function symbols
+/// \param[in,out] o an output iterator to which all function symbols occurring in x are written.
+/// \\\\return All function symbols that occur in the term x
+template <typename T, typename OutputIterator>
+void find_function_symbols(const T& x, OutputIterator o)
+{
+  data::detail::make_find_function_symbols_traverser<NAMESPACE::data_expression_traverser>(o)(x);
+}
+
+/// \\\\brief Returns all function symbols that occur in an object
+/// \param[in] x an object containing function symbols
+/// \\\\return All function symbols that occur in the object x
+template <typename T>
+std::set<data::function_symbol> find_function_symbols(const T& x)
+{
+  std::set<data::function_symbol> result;
+  NAMESPACE::find_function_symbols(x, std::inserter(result, result.end()));
+  return result;
+}
 '''
 
 def generate_code(filename, namespace, label, text):
