400111c2466acbf5d7de3d855ee4f9db4002292b
Frank Stappers <f.p.m.stappers@tue.nl>
2011-02-17 15:55:04 +0000

    Added remaining random tests
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8895 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/GenerateRandomTests.cmake
+++ scripts/GenerateRandomTests.cmake
@@ -1,2 +1,10 @@
-add_test(test_pbes2bes_finite_random 
-				${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesinst_finite.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_bes_solvers_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_bes_solvers.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_parser_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_parser.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbesabstract_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesabstract.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbesconstelm_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesconstelm.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbesinst_finite_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesinst_finite.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbesinst_lazy_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesinst_lazy.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbesparelm_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbesparelm.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbespareqelm_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbespareqelm.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pbes_solvers_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pbes_solvers.py -t ${CMAKE_BINARY_DIR}/tools)
+add_test(test_pfnf_rewriter_random ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/libraries/core/build/test_pfnf_rewriter.py -t ${CMAKE_BINARY_DIR}/tools)
