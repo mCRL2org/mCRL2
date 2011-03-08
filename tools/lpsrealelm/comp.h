4926d0afa47a4cffb5d707fafd2d519c10138bbc
Jan Friso Groote <j.f.groote@tue.nl>
2011-03-08 20:12:18 +0000

    lpsrealelm did not survive testing under intensive garbage collection strain. This has now been repaired.
    Proposed change rejected by Jeroen K. I implemented the
    suggestion by Jeroen.
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@8979 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/lpsrealelm/comp.h
+++ tools/lpsrealelm/comp.h
@@ -13,24 +13,23 @@
 #define MCRL2_LPSRTA_COMP_H
 
 #include <iostream>
+#include "mcrl2/data/detail/construction_utility.h"
 #include "mcrl2/data/data_equation.h"
 #include "mcrl2/data/data_specification.h"
 #include "mcrl2/data/structured_sort.h"
 
-// using namespace atermpp;
-using namespace mcrl2;
-// using namespace mcrl2::core;
-// using namespace mcrl2::core::detail;
-using namespace mcrl2::data;
-// using namespace mcrl2::data::data_expr;
-using namespace mcrl2::data::detail;
 
 /// \brief A local class defining a data type for use in lpsrealelm.
 /// \details The defined data type is
 ///          sort Comp = struct smaller?is_smaller | equal?is_equal | larger?is_larger;
 
-class comp_struct:public structured_sort
+using namespace mcrl2;
+using namespace mcrl2::data;
+using namespace mcrl2::data::detail;
+
+class comp_struct:public mcrl2::data::structured_sort
 {
+
   private:
     function_symbol f_smaller;
     function_symbol f_equal;
@@ -41,36 +40,36 @@ class comp_struct:public structured_sort
 
     static structured_sort_constructor& c_smaller()
     {
-      static structured_sort_constructor c_smaller("smaller","is_smaller");
+      static structured_sort_constructor c_smaller =
+                    initialise_static_expression(c_smaller,structured_sort_constructor("smaller", "is_smaller"));
       return c_smaller;
     }
 
     static structured_sort_constructor& c_equal()
     {
-      static structured_sort_constructor c_equal("equal","is_equal");
+      static structured_sort_constructor c_equal =
+                    initialise_static_expression(c_equal,structured_sort_constructor("equal", "is_equal"));
       return c_equal;
     }
 
     static structured_sort_constructor& c_larger()
     {
-      static structured_sort_constructor c_larger("larger","is_larger");
+      static structured_sort_constructor c_larger =
+                    initialise_static_expression(c_larger,structured_sort_constructor("larger", "is_larger"));
       return c_larger;
     }
 
-    static basic_sort& comp_sort()
+    static basic_sort& comp_sort() 
     {
-      static basic_sort comp_sort("Comp");
+      static basic_sort comp_sort = initialise_static_expression(comp_sort,basic_sort("Comp"));
       return comp_sort;
     }
+
 
   public:
     comp_struct():
       structured_sort(make_vector(c_smaller(),c_equal(),c_larger()))
     {
-      c_smaller().protect();
-      c_equal().protect();
-      c_larger().protect();
-      comp_sort().protect();
       f_smaller=c_smaller().constructor_function(sort());
       f_equal=c_equal().constructor_function(sort());
       f_larger=c_larger().constructor_function(sort());
@@ -78,6 +77,14 @@ class comp_struct:public structured_sort
       f_is_equal=c_equal().recogniser_function(sort());
       f_is_larger=c_larger().recogniser_function(sort());
       // f_is_larger=c_larger().recogniser_function(comp_sort());
+    }
+
+    ~comp_struct()
+    { 
+      c_smaller().unprotect();
+      c_equal().unprotect();
+      c_larger().unprotect();
+      comp_sort().unprotect();
     }
 
     sort_expression sort() const
