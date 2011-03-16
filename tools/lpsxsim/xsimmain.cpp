336ef671edc54b360c0751b488d37150c8f37302
Frank Stappers <f.p.m.stappers@tue.nl>
2011-03-16 15:06:35 +0000

    * Changed tool-tip formatting, replaced tabs by double spacing * Explicit setting the focus to first item in transition listview. Resolves issue in Windows for pressing the down key twice in order to change the selected transition.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9024 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/lpsxsim/xsimmain.cpp
+++ tools/lpsxsim/xsimmain.cpp
@@ -429,6 +429,8 @@ void XSimMain::StateChanged(ATermAppl /*Transition*/, ATerm State, ATermList Nex
   {
     SetCurrentState(State);
     UpdateTransitions(NextStates);
+
+	transview->Focus(0);
     // if ( simulator->ErrorOccurred() )
   }
   catch (mcrl2::runtime_error e)
