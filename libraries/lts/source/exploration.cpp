05927ecb3c93c5dd9f6d7958720243350b067d81
Jan Friso Groote <j.f.groote@tue.nl>
2011-03-19 14:38:55 +0000

    Forgot one file, and reverted another.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9039 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lts/source/exploration.cpp
+++ libraries/lts/source/exploration.cpp
@@ -1332,7 +1332,7 @@ bool lps2lts_algorithm::generate_lts()
         NextStateGenerator* nsgen = nsgens[nsgens_num-1];
         state = nsgen->get_state();
         check_divergence(state);
-        multi_action Transition;
+        ATermAppl Transition;
         ATerm NewState;
         bool new_state = false;
         bool add_new_states = (current_state < lgopts->max_states);
