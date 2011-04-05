c55810ad1ebb01f0df1bc124902c880c5a229de5
Jeroen Keiren <j.j.a.keiren@tue.nl>
2011-04-05 09:19:55 +0000

    Add small test for memory_utility.h. This mainly serves as a purpose to check whether system_specific_alloc_a can be removed.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9114 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/core/test/memory_utility_test.cpp
@@ -0,0 +1,50 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file logger_test.cpp
+
+#define BOOST_AUTO_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/included/unit_test_framework.hpp>
+
+#include "mcrl2/core/detail/memory_utility.h"
+
+size_t args()
+{
+  return 10;
+}
+
+BOOST_AUTO_TEST_CASE(test_alloca)
+{
+  size_t nr_args = args();
+  MCRL2_SYSTEM_SPECIFIC_ALLOCA(array,size_t,nr_args);
+  for (size_t i = 0; i < nr_args; ++i)
+  {
+    array[i] = 10*i;
+  }
+
+  for (size_t i = 0; i < nr_args; ++i)
+  {
+    BOOST_CHECK(array[i] == 10*i);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_varsize_array)
+{
+  size_t nr_args = args();
+  size_t array[nr_args];
+  for (size_t i = 0; i < nr_args; ++i)
+  {
+    array[i] = 10*i;
+  }
+
+  for (size_t i = 0; i < nr_args; ++i)
+  {
+    BOOST_CHECK(array[i] == 10*i);
+  }
+}
