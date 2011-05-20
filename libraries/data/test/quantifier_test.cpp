6b62bec5858e6a99c5da191ae43e58318ecf937b
Frank Stappers <f.p.m.stappers@tue.nl>
2011-05-20 12:34:27 +0000

    Added regression test for exists/forall quantifiers
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9267 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/data/test/quantifier_test.cpp
@@ -0,0 +1,82 @@
+// Author(s): Frank Stappers
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file list_test.cpp
+/// \brief Basic regression test for quantifier expressions.
+
+#include <boost/range/iterator_range.hpp>
+#include <boost/test/minimal.hpp>
+
+#include "mcrl2/data/list.h"
+#include "mcrl2/data/parse.h"
+#include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/set.h"
+#include "mcrl2/data/fset.h"
+#include "mcrl2/data/standard.h"
+
+#include "mcrl2/core/garbage_collection.h"
+#include "mcrl2/atermpp/aterm_init.h"
+
+using namespace mcrl2;
+using namespace mcrl2::data;
+using namespace mcrl2::data::sort_list;
+
+void quantifier_expression_test()
+{
+  data_specification specification;
+  specification = parse_data_specification(
+                                       "sort S = struct s1?is_s1 | s2?is_s2;"
+                                       "sort T = struct t;"
+                                       "sort L = List(T);");
+
+  specification.add_context_sort(sort_nat::nat());
+  specification.add_context_sort(sort_set::set_(sort_nat::nat()));
+
+  rewriter r(specification);
+
+  /* Test 1*/
+  data_expression t1d1 = parse_data_expression("exists x: Bool. x == false");
+  data_expression t1d2 = parse_data_expression("true");
+  BOOST_CHECK(r(t1d1) == r(t1d2));
+
+  /* Test 2*/
+  data_expression t2d1 = parse_data_expression("exists x: Nat. (  x in {1,2,25,600} && 25 == x )", specification);
+  data_expression t2d2 = parse_data_expression("true");
+  BOOST_CHECK(r(t2d1) == r(t2d2));
+
+  /* Test 3*/
+  data_expression t3d1 = parse_data_expression("forall x: Nat. exists y: Nat. y == x");
+  data_expression t3d2 = parse_data_expression("true");
+  BOOST_CHECK(r(t3d1) == r(t3d2));
+
+  /* Test 4*/
+  data_expression t4d1 = parse_data_expression("exists s: S.( is_s1(s) && is_s2(s) )", specification);
+  data_expression t4d2 = parse_data_expression("false");
+  BOOST_CHECK(r(t4d1) == r(t4d2));
+
+  /* Test 5*/
+  data_expression t5d1 = parse_data_expression("exists s: S.( s == s2 && is_s2(s) )", specification);
+  data_expression t5d2 = parse_data_expression("true");
+  BOOST_CHECK(r(t5d1) == r(t5d2));
+
+  /* Test 6*/
+  data_expression t6d1 = parse_data_expression("forall y: T. y in [t]", specification);
+  data_expression t6d2 = parse_data_expression("true");
+  BOOST_CHECK(r(t6d1) == r(t6d2));
+
+}
+
+int test_main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv);
+
+  quantifier_expression_test();
+  core::garbage_collect();
+
+  return EXIT_SUCCESS;
+}
