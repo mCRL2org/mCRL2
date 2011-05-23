a8790d8999da50acb3636c3cfddd168151bce2c8
Frank Stappers <f.p.m.stappers@tue.nl>
2011-05-23 15:01:02 +0000

    * Fixed propagating the correct file extension in mCRL2-gui. * Removed lps2torx from mCRL2-gui's tool catalog. * Ran make parsers
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9277 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/chi2mcrl2/chilexer.cpp
+++ tools/chi2mcrl2/chilexer.cpp
@@ -58,6 +58,7 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
+#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -88,16 +89,14 @@ typedef unsigned int flex_uint32_t;
 #define UINT32_MAX             (4294967295U)
 #endif
 
-#endif /* ! C99 */
-
 #endif /* ! FLEXINT_H */
 
 /* begin standard C++ headers. */
 #include <iostream> 
 #include <errno.h>
 #include <cstdlib>
-#include <cstdio>
 #include <cstring>
+#include <cstdio>
 /* end standard C++ headers. */
 
 #ifdef __cplusplus
@@ -154,15 +153,7 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k.
- * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
- * Ditto for the __ia64__ case accordingly.
- */
-#define YY_BUF_SIZE 32768
-#else
 #define YY_BUF_SIZE 16384
-#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -604,7 +595,7 @@ int chiyyFlexLexer::yylex(void) { return 1; }
 chi_lexer *a_chi_lexer = NULL;       /* lexer object, used by parse_streams */
 
 
-#line 608 "chilexer.cpp"
+#line 599 "chilexer.cpp"
 
 #define INITIAL 0
 
@@ -634,12 +625,7 @@ static int yy_flex_strlen (yyconst char * );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
-#ifdef __ia64__
-/* On IA-64, the buffer size is 16k, not 8k */
-#define YY_READ_BUF_SIZE 16384
-#else
 #define YY_READ_BUF_SIZE 8192
-#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -711,7 +697,7 @@ YY_DECL
     
 #line 94 "chilexer.ll"
 
-#line 715 "chilexer.cpp"
+#line 701 "chilexer.cpp"
 
 	if ( !(yy_init) )
 		{
@@ -1250,7 +1236,7 @@ YY_RULE_SETUP
 #line 199 "chilexer.ll"
 ECHO;
 	YY_BREAK
-#line 1254 "chilexer.cpp"
+#line 1240 "chilexer.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
