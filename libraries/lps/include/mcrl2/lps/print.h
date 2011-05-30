a372a7999cd1b2a8cc4bf9288c72210c17be6708
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-05-30 15:27:25 +0000

    - Added pretty printing of LPSs.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9309 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/include/mcrl2/lps/print.h
+++ libraries/lps/include/mcrl2/lps/print.h
@@ -34,16 +34,121 @@ struct printer: public lps::add_traverser_sort_expressions<data::detail::printer
   using super::leave;
   using super::operator();
   using core::detail::printer<Derived>::print_sorts;
+  using super::print_function_declarations;
 
   Derived& derived()
   {
     return static_cast<Derived&>(*this);
   }
 
+  template <typename Container>
+  void print_list(const Container& container,
+                  const std::string& opener = "(",
+                  const std::string& closer = ")",
+                  const std::string& separator = ", "
+                 )
+  {
+    if (container.empty())
+    {
+      return;
+    }
+    derived().print(opener);
+    for (typename Container::const_iterator i = container.begin(); i != container.end(); ++i)
+    {
+      if (i != container.begin())
+      {
+        derived().print(separator);
+      }
+      derived()(*i);
+    }
+    derived().print(closer);
+  }
+
+  template <typename Container>
+  void print_assignments(const Container& container,
+                         const std::string& opener = "",
+                         const std::string& closer = "",
+                         const std::string& separator = ", ",
+                         bool print_lhs = true,
+                         const std::string& assignment_symbol = " = "
+                        )
+  {
+    if (container.empty())
+    {
+      return;
+    }
+    derived().print(opener);
+    for (typename Container::const_iterator i = container.begin(); i != container.end(); ++i)
+    {
+      if (i != container.begin())
+      {
+        derived().print(separator);
+      }
+      if (print_lhs)
+      {
+        derived()(i->lhs());
+        derived().print(assignment_symbol);
+      }
+      derived()(i->rhs());
+    }
+    derived().print(closer);
+  }
+
+  // Container contains elements of type T such that t.sort() is a sort_expression.
+  template <typename Container>
+  void print_action_declarations(const Container& container,
+                                 const std::string& opener = "(",
+                                 const std::string& closer = ")",
+                                 const std::string& separator = ", "
+                                )
+  {
+    typedef typename Container::value_type T;
+    
+    // print nothing if the container is empty
+    if (container.empty())
+    {
+      return;
+    }
+
+    // sort_map[s] will contain all elements t of container with t.sorts() == s.
+    std::map<data::sort_expression_list, std::vector<T> > sort_map;
+
+    // sort_lists will contain all sort expression lists s that appear as a key in sort_map,
+    // in the order they are encountered in container
+    std::vector<data::sort_expression_list> sort_lists;
+
+    for (typename Container::const_iterator i = container.begin(); i != container.end(); ++i)
+    {
+      if (sort_map.find(i->sorts()) == sort_map.end())
+      {
+        sort_lists.push_back(i->sorts());
+      }
+      sort_map[i->sorts()].push_back(*i);
+    }
+
+    // do the actual printing
+    derived().print(opener);   
+    for (std::vector<data::sort_expression_list>::iterator i = sort_lists.begin(); i != sort_lists.end(); ++i)
+    {
+      if (i != sort_lists.begin())
+      {
+        derived().print(separator);
+      }
+      const std::vector<T>& v = sort_map[*i];
+      print_list(v, "", "", ",");
+      if (!i->empty())
+      {
+        derived().print(": ");
+        print_list(*i, "", "", " # ");
+      }
+    }
+    derived().print(closer);
+  }
+
   void operator()(const lps::action_label& x)
   {
     derived().enter(x);
-    derived()(x.sorts());
+    derived()(x.name());
     derived().leave(x);
   }
 
@@ -51,7 +156,7 @@ struct printer: public lps::add_traverser_sort_expressions<data::detail::printer
   {
     derived().enter(x);
     derived()(x.label());
-    derived()(x.arguments());
+    print_list(x.arguments(), "(", ")", ", ");
     derived().leave(x);
   }
 
@@ -88,26 +193,56 @@ struct printer: public lps::add_traverser_sort_expressions<data::detail::printer
   void operator()(const lps::action_summand& x)
   {
     derived().enter(x);
-    derived()(x.summation_variables());
-    derived()(x.condition());
+    
+    // print summation variables
+    print_sorts() = true;
+    print_list(x.summation_variables(), "sum ", ".\n         ", ", ");
+    print_sorts() = false;
+
+    // print condition
+    if (!data::sort_bool::is_true_function_symbol(x.condition()))
+    {
+      derived()(x.condition());
+      derived().print(" ->\n         ");
+    }
     derived()(x.multi_action());
-    derived()(x.assignments());
+    derived().print(" .\n         ");
+    derived().print("P");
+    print_assignments(x.assignments(), "P(", ")", ", ");
     derived().leave(x);
   }
 
   void operator()(const lps::process_initializer& x)
   {
     derived().enter(x);
-    derived()(x.assignments());
+    derived().print("init P");
+    print_assignments(x.assignments(), "(", ")", ", ", false);
+    derived().print(";");
     derived().leave(x);
   }
 
   void operator()(const lps::linear_process& x)
   {
     derived().enter(x);
-    derived()(x.process_parameters());
-    derived()(x.deadlock_summands());
-    derived()(x.action_summands());
+    derived().print("proc P");
+    print_sorts() = true;
+    print_list(x.process_parameters(), "(", ")", ", ");
+    print_sorts() = false;
+    derived().print(" =\n       ");
+
+    // print action summands
+    std::string opener = "";
+    std::string closer = "";
+    std::string separator = "\n     + ";
+    print_list(x.action_summands(), opener, closer, separator);
+
+    // print deadlock summands
+    if (!x.action_summands().empty())
+    {
+      opener = separator;
+    }
+    print_list(x.deadlock_summands(), opener, closer, separator);
+
     derived().leave(x);
   }
 
@@ -115,8 +250,10 @@ struct printer: public lps::add_traverser_sort_expressions<data::detail::printer
   {
     derived().enter(x);
     derived()(x.data());
-    derived()(x.action_labels());
+    print_action_declarations(x.action_labels(), "act  ",";\n\n", ";\n     ");
+    print_function_declarations(x.global_variables(), "glob ", ";\n\n", ";\n     ");
     derived()(x.process());
+    derived().print(";\n\n");
     derived()(x.initial_process());
     derived().leave(x);
   }
