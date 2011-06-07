b5fb818aca6e84d27efa5a2dd4e7dae447b78712
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-06-07 10:43:32 +0000

    - Fixed the Visual C++ compilation, by moving the now_time function of the logger to a source file. The problem was the include of <windows.h> in logger.h, that conflicted with the parser. In general the include of <windows.h> should be avoided, since it contains many ugly defines.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9332 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/utilities/include/mcrl2/utilities/logger.h
+++ libraries/utilities/include/mcrl2/utilities/logger.h
@@ -459,44 +459,4 @@ if (level > MCRL2_MAX_LOG_LEVEL) ; \
 else if (level > mcrl2_logger::get_reporting_level()) ; \
 else mcrl2_logger().get(level)
 
-// Implementation of now_time; platform specific.
-// used to print timestamps in front of debug messages.
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
-
-#include <windows.h>
-
-inline std::string now_time()
-{
-    const int MAX_LEN = 200;
-    char buffer[MAX_LEN];
-    if (GetTimeFormatA(LOCALE_USER_DEFAULT, 0, 0,
-            "HH':'mm':'ss", buffer, MAX_LEN) == 0)
-        return "Error in NowTime()";
-
-    char result[100] = {0};
-    static DWORD first = GetTickCount();
-    std::sprintf(result, "%s.%03ld", buffer, (long)(GetTickCount() - first) % 1000);
-    return result;
-}
-
-#else
-
-#include <sys/time.h>
-
-inline std::string now_time()
-{
-    char buffer[11];
-    time_t t;
-
-    time(&t);
-    tm r;
-    strftime(buffer, sizeof(buffer), "%X", localtime_r(&t, &r));
-    struct timeval tv;
-    gettimeofday(&tv, 0);
-    char result[100] = {0};
-    std::sprintf(result, "%s.%03ld", buffer, (long)tv.tv_usec / 1000);
-    return result;
-}
-#endif //WIN32
-
 #endif /* MCRL_UTILITIES_LOGGER_H */
