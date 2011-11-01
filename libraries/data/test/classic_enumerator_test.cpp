b982436e18cd81f8e2a04bc95bc4b710bfa4ffca
Wieger Wesselink <j.w.wesselink@tue.nl>
2011-11-01 09:27:35 +0000

    - Added a test for generating values of a sort using the classic enumerator.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@9854 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/data/test/classic_enumerator_test.cpp
+++ libraries/data/test/classic_enumerator_test.cpp
@@ -15,6 +15,7 @@
 #include "mcrl2/atermpp/aterm_init.h"
 #include "mcrl2/core/print.h"
 #include "mcrl2/core/garbage_collection.h"
+#include "mcrl2/core/detail/print_utility.h"
 #include "mcrl2/data/data_expression.h"
 #include "mcrl2/data/parse.h"
 #include "mcrl2/data/replace.h"
@@ -214,6 +215,90 @@ void check_concepts()
   BOOST_CONCEPT_ASSERT((classic_enumerator< >::iterator ));
 }
 
+inline
+data::data_expression_vector generate_values(const data::data_specification& dataspec, const data::sort_expression& s, std::size_t max_size = 1000)
+{
+  std::size_t max_internal_variables = 10000;
+  data::data_expression_vector result;
+
+  data::rewriter rewr(dataspec);
+  data::classic_enumerator<data::rewriter> enumerator(dataspec, rewr);
+  data::variable x("x", s);
+  data::variable_vector v;
+  v.push_back(x);
+  for (data::classic_enumerator<data::rewriter>::iterator i = enumerator.begin(v, data::sort_bool::true_(), max_internal_variables); i != enumerator.end() ; ++i)
+  {
+    result.push_back((*i)(x));
+    if (result.size() >= max_size)
+    {
+      break;
+    }
+  }
+  return result;
+}
+
+bool no_duplicates(const data::data_expression_vector& v)
+{
+  std::set<data::data_expression> s;
+  s.insert(v.begin(), v.end());
+  return s.size() == v.size();
+}
+
+void generate_values_test()
+{
+  std::string DATASPEC =
+    "sort Identifiers = struct t1 | t2 | t3 | t4 | t5 | p1 | p2 | s1 | r1 | r2 | r3 | IL;                                                                                  \n"
+    "     signal_Messages = struct ic_set_proceed_signal | ic_set_stop_signal;                                                                                             \n"
+    "     track_Messages = struct dv_free_track | dv_occupied_track;                                                                                                       \n"
+    "     point_Messages = struct ic_move_right_point | ic_move_left_point | dv_at_right_point | dv_at_left_point;                                                         \n"
+    "     route_Messages = struct ic_cancel_route_route | ic_set_route_route;                                                                                              \n"
+    "     Interlocking_Messages = struct c_set_stop | c_move_left | c_set_route | c_IL_off | c_cancel_route | tc_set_proceed | c_IL_on | c_move_right;                     \n"
+    "     environment_Messages = struct i_signal_status_proceed_signal_environment | i_signal_status_stop_signal_environment | i_track_status_occupied_track_environment | \n"
+    "       i_track_status_free_track_environment | i_point_at_right_point_environment | i_point_locked_point_environment | i_point_ok_point_environment |                 \n"
+    "       i_point_broken_point_environment | i_point_at_left_point_environment | i_route_cancelled_route_environment | i_route_established_route_environment;            \n"
+    "     rail_yard_Messages = struct sv_set_proceed_signal_railyard | sv_set_stop_signal_railyard | sv_move_right_point_railyard | sv_move_left_point_railyard;           \n"
+    "     Enum3 = struct e2_3 | e1_3 | e0_3;                                                                                                                               \n"
+    "     track__ready_States = struct track__ready_free_substate | track__ready_occupied_substate | track__ready_startup_substate | track__ready_nop;                     \n"
+    "     Enum15 = struct e14_15 | e13_15 | e12_15 | e11_15 | e10_15 | e9_15 | e8_15 | e7_15 | e6_15 | e5_15 | e4_15 | e3_15 | e2_15 | e1_15 | e0_15;                      \n"
+    "     point_States = struct point__broken_substate | point__startup_substate | point__working_substate;                                                                \n"
+    "     point__working_States = struct point__working_right_substate | point__working_left_substate | point__working_moving_substate | point__working_nop;               \n"
+    "     point__working_moving_States = struct point__working_moving_left_substate | point__working_moving_right_substate | point__working_moving_nop;                    \n"
+    "     Enum7 = struct e6_7 | e5_7 | e4_7 | e3_7 | e2_7 | e1_7 | e0_7;                                                                                                   \n"
+    "     signal_States = struct signal__stop_substate | signal__proceed_substate;                                                                                         \n"
+    "     Enum10 = struct e9_10 | e8_10 | e7_10 | e6_10 | e5_10 | e4_10 | e3_10 | e2_10 | e1_10 | e0_10;                                                                   \n"
+    "     route_States = struct route__idle_substate | route__established_substate | route__setting_up_substate;                                                           \n"
+    "     route__established_States = struct route__established_in_use_substate | route__established_active_substate | route__established_nop;                             \n"
+    "     Enum11 = struct e10_11 | e9_11 | e8_11 | e7_11 | e6_11 | e5_11 | e4_11 | e3_11 | e2_11 | e1_11 | e0_11;                                                          \n"
+    "     Enum13 = struct e12_13 | e11_13 | e10_13 | e9_13 | e8_13 | e7_13 | e6_13 | e5_13 | e4_13 | e3_13 | e2_13 | e1_13 | e0_13;                                        \n"
+    "     HAL_device_States = struct HAL_device__normal_substate;                                                                                                          \n"
+    "     track_States = struct track__ready_substate;                                                                                                                     \n"
+    ;
+  data::data_specification dataspec = data::parse_data_specification(DATASPEC);
+
+  const data::sort_expression_vector& sorts = dataspec.user_defined_sorts();
+  for (data::sort_expression_vector::const_iterator i = sorts.begin(); i != sorts.end(); ++i)
+  {
+    data::sort_expression s = *i;
+    std::clog << "--- sort " << data::pp(s) << std::endl;
+    data::data_expression_vector v = generate_values(dataspec, s, 10);
+    std::clog << " possible values: " << core::detail::print_set(v, data::stream_printer()) << std::endl;
+    BOOST_CHECK(v.size() <= 10);
+    BOOST_CHECK(no_duplicates(v));
+  }
+
+  const data::alias_vector& aliases = dataspec.user_defined_aliases();
+  for (data::alias_vector::const_iterator i = aliases.begin(); i != aliases.end(); ++i)
+  {
+    data::alias a = *i;
+    data::sort_expression s = i->reference();
+    std::clog << "--- sort " << data::pp(s) << std::endl;
+    data::data_expression_vector v = generate_values(dataspec, s, 10);
+    std::clog << " possible values: " << core::detail::print_set(v, data::stream_printer()) << std::endl;
+    BOOST_CHECK(v.size() <= 10);
+    BOOST_CHECK(no_duplicates(v));
+  }
+}
+
 int test_main(int argc, char** argv)
 {
   MCRL2_ATERMPP_INIT_DEBUG(argc, argv);
@@ -231,6 +316,8 @@ int test_main(int argc, char** argv)
   mutually_recursive_test();
   core::garbage_collect();
   equality_substitution_test();
+
+  generate_values_test();
 
   return EXIT_SUCCESS;
 }
