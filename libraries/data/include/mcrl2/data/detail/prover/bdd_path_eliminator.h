mCRL2org/mCRL2

commit 6d3de66e2f804bd09b0a7a11360f4eb1446c656b

Move lpsbinary, lpsinvelm, lpssumelm, lpssuminst and lpsuntime to tools.h/tools.cpp
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10038 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/data/include/mcrl2/data/detail/prover/bdd_path_eliminator.h
@@ -21,6 +21,7 @@
 #include "mcrl2/aterm/aterm_ext.h"
 #include "mcrl2/utilities/logger.h"
 #include "mcrl2/core/detail/struct_core.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/prover/bdd_simplifier.h"
 #include "mcrl2/data/detail/prover/smt_lib_solver.h"
 #include "mcrl2/data/detail/prover/bdd_manipulator.h"
@@ -32,52 +33,6 @@ namespace data
 {
 namespace detail
 {
-
-/// \brief The enumaration type SMT_Solver_Type enumerates all available SMT solvers.
-enum SMT_Solver_Type
-{
-  solver_type_cvc,
-};
-
-/// \brief standard conversion from stream to solver type
-inline
-std::istream& operator>>(std::istream& is, mcrl2::data::detail::SMT_Solver_Type& s)
-{
-  char solver_type[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-  using namespace mcrl2::data::detail;
-
-  /// no standard conversion available function, so implement on-the-spot
-  is.readsome(solver_type, 10);
-
-  s = solver_type_cvc;
-  if (strncmp(solver_type, "cvc", 3) == 0)
-  {
-    if (solver_type[3] != '\0')
-    {
-      is.setstate(std::ios_base::failbit);
-    }
-  }
-  else
-  {
-    is.setstate(std::ios_base::failbit);
-  }
-
-  return is;
-}
-
-/// \brief standard conversion from solvert type to stream
-inline std::ostream& operator<<(std::ostream& os, mcrl2::data::detail::SMT_Solver_Type s)
-{
-  static char const* solvers[] =
-  {
-    "cvc",
-  };
-
-  os << solvers[s];
-
-  return os;
-}
 
 /** \brief Base class for eliminating inconsistent paths from BDDs.
  *
@@ -120,8 +75,8 @@ class BDD_Path_Eliminator: public BDD_Simplifier
     /// \param a_guard A guard or a negated guard.
     /// \param a_minimal A boolean value indicating whether or not minimal sets of possibly inconsistent guards are constructed.
     data_expression_list create_condition(
-               data_expression_list a_path, 
-               const data_expression a_guard, 
+               data_expression_list a_path,
+               const data_expression a_guard,
                bool a_minimal)
     {
       if (!a_minimal)
@@ -228,7 +183,7 @@ class BDD_Path_Eliminator: public BDD_Simplifier
 
     /// \brief Constructor that initializes the field BDD_Path_Eliminator::f_smt_solver.
     /// \param a_solver_type A value of an enumerated type, representing an SMT solver.
-    BDD_Path_Eliminator(SMT_Solver_Type a_solver_type)
+    BDD_Path_Eliminator(smt_solver_type a_solver_type)
     {
 #if !(defined(_MSC_VER) || defined(__MINGW32__) || defined(__CYGWIN__))
       if (a_solver_type == solver_type_cvc)

libraries/data/include/mcrl2/data/detail/bdd_prover.h
@@ -15,6 +15,7 @@
 #include "mcrl2/aterm/aterm2.h"
 #include "mcrl2/data/data_specification.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/prover.h"
 #include "mcrl2/data/detail/prover/bdd_simplifier.h"
 #include "mcrl2/data/detail/prover/bdd_path_eliminator.h"
@@ -440,7 +441,7 @@ class BDD_Prover: public Prover
       mcrl2::data::rewriter::strategy a_rewrite_strategy = mcrl2::data::rewriter::jitty,
       int a_time_limit = 0,
       bool a_path_eliminator = false,
-      SMT_Solver_Type a_solver_type = solver_type_cvc,
+      smt_solver_type a_solver_type = solver_type_cvc,
       bool a_apply_induction = false)
       : Prover(data_spec, equations_selector, a_rewrite_strategy, a_time_limit),
         f_data_spec(data_spec),

libraries/data/include/mcrl2/data/detail/prover/formula_checker.h
@@ -16,6 +16,7 @@
 #include "mcrl2/aterm/aterm_ext.h"
 #include "mcrl2/utilities/logger.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/bdd_prover.h"
 #include "mcrl2/data/detail/prover/bdd2dot.h"
 
@@ -144,7 +145,7 @@ class Formula_Checker
       mcrl2::data::rewriter::strategy a_rewrite_strategy = mcrl2::data::rewriter::jitty,
       int a_time_limit = 0,
       bool a_path_eliminator = false,
-      mcrl2::data::detail::SMT_Solver_Type a_solver_type = mcrl2::data::detail::solver_type_cvc,
+      mcrl2::data::detail::smt_solver_type a_solver_type = mcrl2::data::detail::solver_type_cvc,
       bool a_apply_induction = false,
       bool a_counter_example = false,
       bool a_witness = false,

libraries/lps/include/mcrl2/lps/confluence_checker.h
@@ -18,6 +18,7 @@
 #include <string>
 #include "mcrl2/aterm/aterm2.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/bdd_prover.h"
 #include "mcrl2/lps/disjointness_checker.h"
 #include "mcrl2/lps/invariant_checker.h"
@@ -92,7 +93,7 @@
     Confluent tau-summands will be marked by renaming their tau-actions to ctau. The constructor
     Confluence_Checker::Confluence_Checker initializes the BDD based prover with the parameters a_rewrite_strategy,
     a_time_limit, a_path_eliminator, a_solver_type, a_apply_induction and a_lps. The parameter a_rewrite_strategy
-    specifies which rewrite strategy is used by the prover's rewriter. It can be set to either 
+    specifies which rewrite strategy is used by the prover's rewriter. It can be set to either
     GS_REWR_JITTY or GS_REWR_JITTYC. The parameter a_time_limit specifies the maximum amount of time in
     seconds to be spent by the prover on proving a single expression. If a_time_limit is set to 0, no time limit will be
     enforced. The parameter a_path_eliminator specifies whether or not path elimination is applied. When path
@@ -242,7 +243,7 @@ class Confluence_Checker
       mcrl2::data::rewriter::strategy a_rewrite_strategy = mcrl2::data::rewriter::jitty,
       int a_time_limit = 0,
       bool a_path_eliminator = false,
-      mcrl2::data::detail::SMT_Solver_Type a_solver_type = mcrl2::data::detail::solver_type_cvc,
+      mcrl2::data::detail::smt_solver_type a_solver_type = mcrl2::data::detail::solver_type_cvc,
       bool a_apply_induction = false,
       bool a_no_marking = false,
       bool a_check_all = false,

libraries/lps/include/mcrl2/lps/invariant_checker.h
@@ -17,6 +17,7 @@
 #include <string>
 #include "mcrl2/aterm/aterm2.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/bdd_prover.h"
 #include "mcrl2/data/detail/prover/bdd2dot.h"
 #include "mcrl2/data/substitutions.h"
@@ -97,7 +98,7 @@ class Invariant_Checker
       mcrl2::data::rewriter::strategy a_rewrite_strategy = mcrl2::data::rewriter::jitty,
       int a_time_limit = 0,
       bool a_path_eliminator = false,
-      mcrl2::data::detail::SMT_Solver_Type a_solver_type = mcrl2::data::detail::solver_type_cvc,
+      mcrl2::data::detail::smt_solver_type a_solver_type = mcrl2::data::detail::solver_type_cvc,
       bool a_apply_induction = false,
       bool a_counter_example = false,
       bool a_all_violations = false,

libraries/lps/include/mcrl2/lps/invariant_eliminator.h
@@ -17,6 +17,7 @@
 
 #include "mcrl2/aterm/aterm2.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 #include "mcrl2/data/detail/bdd_prover.h"
 #include "mcrl2/lps/specification.h"
 
@@ -99,7 +100,7 @@ class Invariant_Eliminator
       const mcrl2::data::rewriter::strategy a_rewrite_strategy = mcrl2::data::rewriter::jitty,
       const int a_time_limit = 0,
       const bool a_path_eliminator = false,
-      const mcrl2::data::detail::SMT_Solver_Type a_solver_type = mcrl2::data::detail::solver_type_cvc,
+      const mcrl2::data::detail::smt_solver_type a_solver_type = mcrl2::data::detail::solver_type_cvc,
       const bool a_apply_induction = false,
       const bool a_simplify_all = false
     );

libraries/lps/include/mcrl2/lps/tools.h
@@ -15,10 +15,14 @@
 #include <string>
 #include "mcrl2/core/print.h"
 #include "mcrl2/data/rewriter.h"
+#include "mcrl2/data/detail/prover/solver_type.h"
 
 namespace mcrl2 {
 
 namespace lps {
+
+void lpsbinary(const std::string& input_filename,
+               const std::string& output_filename);
 
 void lpsconstelm(const std::string& input_filename,
                  const std::string& output_filename,
@@ -32,6 +36,23 @@ void lpsconstelm(const std::string& input_filename,
 void lpsinfo(const std::string& input_filename,
              const std::string& input_file_message
             );
+
+void lpsinvelm(const std::string& input_filename,
+               const std::string& output_filename,
+               const std::string& invariant_filename,
+               const std::string& dot_file_name,
+               data::rewriter::strategy rewrite_strategy,
+               data::detail::smt_solver_type solver_type,
+               const size_t summand_number,
+               const bool no_check,
+               const bool no_elimination,
+               const bool simplify_all,
+               const bool all_violations,
+               const bool counter_example,
+               const bool path_eliminator,
+               const bool apply_induction,
+               const int time_limit
+              );
 
 void lpsparelm(const std::string& input_filename,
                const std::string& output_filename
@@ -49,6 +70,19 @@ void lpsrewr(const std::string& input_filename,
              bool benchmark,
              unsigned long bench_times
             );
+
+void lpssumelm(const std::string& input_filename,
+               const std::string& output_filename,
+               const bool decluster);
+
+void lpssuminst(const std::string& input_filename,
+                const std::string& output_filename,
+                const std::string& sorts_string,
+                const bool finite_sorts_only,
+                const bool tau_summands_only);
+
+void lpsuntime(const std::string& input_filename,
+               const std::string& output_filename);
 
 void txtlps(const std::string& input_filename,
             const std::string& output_filename

libraries/lps/source/confluence_checker.cpp
@@ -148,7 +148,7 @@ data_expression get_equation_from_assignments(
       }
       a_assignments_1=pop_front(a_assignments_1);
     }
-    else 
+    else
     {
       if (!a_assignments_2.empty() && v_variable == a_assignments_2.front().lhs())
       {
@@ -451,7 +451,7 @@ Confluence_Checker::Confluence_Checker(
   mcrl2::data::rewriter::strategy a_rewrite_strategy,
   int a_time_limit,
   bool a_path_eliminator,
-  SMT_Solver_Type a_solver_type,
+  smt_solver_type a_solver_type,
   bool a_apply_induction,
   bool a_no_marking,
   bool a_check_all,
@@ -460,7 +460,7 @@ Confluence_Checker::Confluence_Checker(
   std::string const& a_dot_file_name):
   f_disjointness_checker(lps::linear_process_to_aterm(a_lps.process())),
   f_invariant_checker(a_lps, a_rewrite_strategy, a_time_limit, a_path_eliminator, a_solver_type, false, false, 0),
-  f_bdd_prover(a_lps.data(), used_data_equation_selector(a_lps.data()), a_rewrite_strategy, 
+  f_bdd_prover(a_lps.data(), used_data_equation_selector(a_lps.data()), a_rewrite_strategy,
                      a_time_limit, a_path_eliminator, a_solver_type, a_apply_induction),
   f_lps(a_lps),
   f_no_marking(a_no_marking),

commit 128058ac4df9705d6aae51e57b3fc30bd0e5969e

Fix command_line_interface_test.
Test failure was introduced in r10038.

git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10044 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/data/include/mcrl2/data/detail/prover/bdd_path_eliminator.h
@@ -210,7 +210,7 @@ class BDD_Path_Eliminator: public BDD_Simplifier
     /// expression with an expression of sort Bool  as guard, and a then-branch and an else-branch that again follow
     /// these restrictions
     /// \param a_bdd A binary decision diagram.
-    virtual data_expression simplify(const data_expression a_bdd)
+    virtual data_expression simplify(const data_expression& a_bdd)
     {
       return aux_simplify(a_bdd, data_expression_list());
     }

libraries/data/include/mcrl2/data/detail/prover/bdd_simplifier.h
@@ -66,7 +66,7 @@ class BDD_Simplifier
     /// following restrictions: It either represents the constant true or the constant false, or it is an if-then-else
     /// expression with an expression of sort Bool  as guard, and a then-branch and an else-branch that again follow these
     /// restrictions
-    virtual data_expression simplify(data_expression a_bdd)
+    virtual data_expression simplify(const data_expression& a_bdd)
     {
       return a_bdd;
     }

libraries/utilities/test/command_line_interface_test.cpp
@@ -43,7 +43,7 @@ inline void string_to_strategy_test(std::string const& strategy)
 template < bool b >
 inline void string_to_prover_type_test(std::string const& prover_type)
 {
-  string_to_type_test< mcrl2::data::detail::SMT_Solver_Type, b >(prover_type);
+  string_to_type_test< mcrl2::data::detail::smt_solver_type, b >(prover_type);
 }
 
 BOOST_AUTO_TEST_CASE(border_invalid)