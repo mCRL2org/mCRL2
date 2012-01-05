d356bf95a38221d407b69d54d6f67eebcf1069f9
Wieger Wesselink <j.w.wesselink@tue.nl>
2012-01-05 15:10:46 +0000

    - Fixed run_pbes2bool. Needed since the output of pbes2bool is no longer written to stderr, but to stdout.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10094 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/build/mcrl2_tools.py
+++ libraries/core/build/mcrl2_tools.py
@@ -95,7 +95,7 @@ def last_word(line):
 # returns True, False or None if a timeout occurs
 def run_pbes2bool(filename, timeout = 3):
     add_temporary_files(filename)
-    dummy, text = timeout_command('pbes2bool', filename, timeout)
+    text, dummy = timeout_command('pbes2bool', filename, timeout)
     if text == None:
         print 'WARNING: timeout on %s' % filename
         return None
