90d02196c3bba7b9e61a58f02e3e69c3494a0958
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-02-07 15:43:13 +0000

    Do not show collapsed parts of documentation in print.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10251 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- doc/sphinx/source/themes/mcrl2/static/mcrl2.js
+++ doc/sphinx/source/themes/mcrl2/static/mcrl2.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   $("div.collapse").children().addClass("collapsed");
+  $("div.collapse").addClass("collapsed");
   $("div.collapse p.admonition-title").bind("click", function (e) {
     $(this).parent().children().toggleClass("collapsed");
+    $(this).parent().toggleClass("collapsed");
   });
 });
