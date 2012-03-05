28f27e9a3d20305ec58449fbb62f215a831c6bf2
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-03-05 07:51:42 +0000

    Merge r10317 from release branch.
    Patch for compiling rewriters for distribution with Mac OSX 10.5+
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10359 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/MCRL2ConfigureComplingRewriters.cmake
+++ scripts/MCRL2ConfigureComplingRewriters.cmake
@@ -68,11 +68,93 @@ if( CMAKE_RUNTIME_OUTPUT_DIRECTORY )
 else( CMAKE_RUNTIME_OUTPUT_DIRECTORY )
   set( MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/mcrl2compilerewriter" )
 endif( CMAKE_RUNTIME_OUTPUT_DIRECTORY )
+
+if( APPLE AND MCRL2_SINGLE_BUNDLE AND CMAKE_INSTALL_PREFIX STREQUAL "/" ) 
+  set( R_CMAKE_INSTALL_PREFIX "..")
+  set( R_Boost_INCLUDE_DIRS "../Boost" )
+  set( R_Boost_LIBRARY_DIRS "../Boost/lib" )
+
+  set( R_MCRL2_BOOST_HEADER_FILES
+    boost/assert.hpp
+    boost/bind.hpp
+    boost/call_traits.hpp
+    boost/checked_delete.hpp
+    boost/concept_check.hpp
+    boost/config.hpp
+    boost/current_function.hpp
+    boost/get_pointer.hpp
+    boost/implicit_cast.hpp
+    boost/integer_traits.hpp
+    boost/is_placeholder.hpp
+    boost/iterator.hpp
+    boost/lexical_cast.hpp
+    boost/limits.hpp
+    boost/mem_fn.hpp
+    boost/memory_order.hpp
+    boost/next_prior.hpp
+    boost/noncopyable.hpp
+    boost/ref.hpp
+    boost/shared_ptr.hpp
+    boost/smart_ptr.hpp
+    boost/static_assert.hpp
+    boost/throw_exception.hpp
+    boost/type.hpp
+    boost/utility.hpp
+    boost/visit_each.hpp
+  )
+
+  set ( R_MCRL2_BOOST_HEADER_DIRS
+    boost/bind
+    boost/concept
+    boost/config
+    boost/detail
+    boost/exception
+    boost/iterator
+    boost/mpl
+    boost/numeric
+    boost/preprocessor
+    boost/range
+    boost/smart_ptr
+    boost/type_traits
+    boost/utility
+  )  
+  
+  foreach( hfile ${R_MCRL2_BOOST_HEADER_FILES})
+    string(FIND ${hfile} "/" IDX REVERSE)
+    string(SUBSTRING ${hfile} 0 ${IDX} DEST)
+    install(FILES "${Boost_INCLUDE_DIRS}/${hfile}" DESTINATION "Boost/${DEST}/")
+  endforeach( hfile ${R_MCRL2_BOOST_HEADER_FILES}) 
+
+  foreach( hdir ${R_MCRL2_BOOST_HEADER_DIRS})
+    string(FIND ${hdir} "/" IDX REVERSE)
+    string(SUBSTRING ${hdir} 0 ${IDX} DEST)
+    install(DIRECTORY "${Boost_INCLUDE_DIRS}/${hdir}" DESTINATION "Boost/${DEST}/")
+  endforeach( hdir ${R_MCRL2_BOOST_HEADER_DIRS}) 
+
+
+else( APPLE AND MCRL2_SINGLE_BUNDLE AND CMAKE_INSTALL_PREFIX STREQUAL "/" ) 
+  set( R_CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+  set( R_Boost_INCLUDE_DIRS "${Boost_INCLUDE_DIRS}" )
+  set( R_Boost_LIBRARY_DIRS "${Boost_LIBRARY_DIRS}" )
+endif( APPLE AND MCRL2_SINGLE_BUNDLE AND CMAKE_INSTALL_PREFIX STREQUAL "/" ) 
+
+# Set MacOS-X specific compile flags
+if( CMAKE_OSX_ARCHITECTURES )
+  set(R_MCRL2_OSX_ARCH  "-arch ${CMAKE_OSX_ARCHITECTURES}" )
+endif( CMAKE_OSX_ARCHITECTURES )
+if( CMAKE_OSX_SYSROOT )
+  set(R_MCRL2_OSX_ARCH  "${R_MCRL2_OSX_ARCH} -isysroot ${CMAKE_OSX_SYSROOT}" )
+endif( CMAKE_OSX_SYSROOT )
+if( CMAKE_OSX_DEPLOYMENT_TARGET )
+  set(R_MCRL2_OSX_ARCH "${R_MCRL2_OSX_ARCH} -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}" )
+endif( CMAKE_OSX_DEPLOYMENT_TARGET )
+
 configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/build/autoconf/mcrl2compilerewriter.in" "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}" @ONLY )
+
 add_executable( mcrl2compilerewriter IMPORTED IMPORTED_LOCATION "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}" )
 install(
   PROGRAMS "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}"
-  DESTINATION bin/
+  DESTINATION ${MCRL2_BIN_DIR}/
 )
 
 ENDIF( UNIX )
