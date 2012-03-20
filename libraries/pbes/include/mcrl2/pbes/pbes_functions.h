dbba589ccf607d751ef1e132d913e57655cda2ae
Wieger Wesselink <j.w.wesselink@tue.nl>
2012-03-20 10:11:13 +0000

    - Added some pbes utility functions, written by Gijs Kant.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10391 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/pbes/include/mcrl2/pbes/pbes_functions.h
@@ -0,0 +1,118 @@
+// Author(s): Gijs Kant
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file pbes/include/mcrl2/pbes/pbes_functions.h
+/// \brief add your file description here.
+
+#ifndef PBES_INCLUDE_MCRL2_PBES_FUNCTIONS_H
+#define PBES_INCLUDE_MCRL2_PBES_FUNCTIONS_H
+
+#include "mcrl2/pbes/traverser.h"
+#include "mcrl2/pbes/pbes_expression.h"
+
+namespace mcrl2 {
+
+namespace pbes_system {
+
+/// \cond INTERNAL_DOCS
+/// \brief Visitor for checking if a pbes object is a simple pbes expression.
+struct is_simple_expression_traverser: public pbes_expression_traverser<is_simple_expression_traverser>
+{
+  typedef pbes_expression_traverser<is_simple_expression_traverser> super;
+  using super::enter;
+  using super::leave;
+  using super::operator();
+
+#if BOOST_MSVC
+#include "mcrl2/core/detail/traverser_msvc.inc.h"
+#endif
+
+  bool result;
+
+  is_simple_expression_traverser()
+    : result(true)
+  {}
+
+  void enter(const propositional_variable_instantiation& x)
+  {
+    result = false;
+  }
+};
+/// \endcond
+
+/// \brief Determines if an expression is a simple expression.
+/// An expression is simple if it is free of propositional variables.
+/// \param x a PBES object
+/// \return true if x is a simple expression.
+template <typename T>
+bool is_simple_expression(const T& x)
+{
+  is_simple_expression_traverser f;
+  f(x);
+  return f.result;
+}
+
+/// \brief Test for a disjunction
+/// \param t A PBES expression
+/// \return True if it is a disjunction and not a simple expression
+inline bool is_non_simple_disjunct(const pbes_expression& t)
+{
+  return is_pbes_or(t) && !is_simple_expression(t);
+}
+
+/// \brief Test for a conjunction
+/// \param t A PBES expression
+/// \return True if it is a conjunction and not a simple expression
+inline bool is_non_simple_conjunct(const pbes_expression& t)
+{
+  return is_pbes_and(t) && !is_simple_expression(t);
+}
+
+namespace pbes_expr {
+
+/// \brief Splits a disjunction into a sequence of operands
+/// Given a pbes expression of the form p1 || p2 || .... || pn, this will yield a
+/// set of the form { p1, p2, ..., pn }, assuming that pi does not have a || as main
+/// function symbol.
+/// \param expr A PBES expression
+/// \return A sequence of operands
+inline
+atermpp::vector<pbes_expression> split_disjuncts(const pbes_expression& expr)
+{
+  using namespace accessors;
+  atermpp::vector<pbes_expression> result;
+
+  utilities::detail::split(expr, std::back_insert_iterator<atermpp::vector<pbes_expression> >(result), is_non_simple_disjunct, left, right);
+
+  return result;
+}
+
+/// \brief Splits a conjunction into a sequence of operands
+/// Given a pbes expression of the form p1 && p2 && .... && pn, this will yield a
+/// set of the form { p1, p2, ..., pn }, assuming that pi does not have a && as main
+/// function symbol.
+/// \param expr A PBES expression
+/// \return A sequence of operands
+inline
+atermpp::vector<pbes_expression> split_conjuncts(const pbes_expression& expr)
+{
+  using namespace accessors;
+  atermpp::vector<pbes_expression> result;
+
+  utilities::detail::split(expr, std::back_insert_iterator<atermpp::vector<pbes_expression> >(result), is_non_simple_conjunct, left, right);
+
+  return result;
+}
+
+} // namespace pbes_expr
+
+} // namespace pbes_system
+
+} // namespace mcrl2
+
+#endif // PBES_INCLUDE_MCRL2_PBES_FUNCTIONS_H
