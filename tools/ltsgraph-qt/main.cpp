464c7d99b8064bd481f54b17d6042341e25639bb
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-04-12 13:33:49 +0000

    Fix build of ltsgraph-qt
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10478 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/ltsgraph-qt/main.cpp
+++ tools/ltsgraph-qt/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
     aterm::ATinit(&stack);
 
     mcrl2::log::logger::set_reporting_level(mcrl2::log::verbose);
-    mCRL2log(mcrl2::log::status) << "Long sentence.\n";
-    mCRL2log(mcrl2::log::status) << "Short.\n";
+    mCRL2log(mcrl2::log::verbose) << "Long sentence.\n";
+    mCRL2log(mcrl2::log::verbose) << "Short.\n";
 
     QApplication a(argc, argv);
     MainWindow w;
