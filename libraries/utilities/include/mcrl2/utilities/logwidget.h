106c24709a0398c3a2f182e3df808c2374704bbe
Ruud Koolen <r.p.j.koolen@tue.nl>
2012-04-23 14:03:21 +0000

    Moved LogWidget to the new mcrl2_utilities_qt project.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10508 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/utilities/include/mcrl2/utilities/logwidget.h
@@ -0,0 +1,68 @@
+// Author(s): Rimco Boudewijns
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef MCRL2_UTILITIES_LOGWIDGET_H
+#define MCRL2_UTILITIES_LOGWIDGET_G
+
+#include <QWidget>
+#include <QDateTime>
+#include "mcrl2/utilities/logger.h"
+
+using namespace mcrl2::log;
+
+namespace Ui
+{
+  class LogWidget;
+}
+
+namespace mcrl2
+{
+
+namespace utilities
+{
+
+namespace qt
+{
+
+class LogRelay : public QObject, public output_policy
+{
+  Q_OBJECT
+
+  public:
+    void output(const log_level_t level, const std::string& hint, const time_t timestamp, const std::string& msg);
+
+  signals:
+    void logMessage(QString level, QString hint, QDateTime timestamp, QString message);
+};
+
+class LogWidget: public QWidget
+{
+  Q_OBJECT
+
+  public:
+    LogWidget(QWidget *parent = 0);
+    ~LogWidget();
+
+  signals:
+    void logMessage(QString level, QString hint, QDateTime timestamp, QString message, QString formattedMessage);
+
+  private slots:
+    void writeMessage(QString level, QString hint, QDateTime timestamp, QString message);
+
+  private:
+    Ui::LogWidget *m_ui;
+    LogRelay m_relay;
+};
+
+} // namespace qt
+
+} // namespace utilities
+
+} // namespace mcrl2
+
+#endif // MCRL2_UTILITIES_LOGWIDGET_H
---
+++ libraries/utilities/source/logwidget.cpp
@@ -0,0 +1,39 @@
+// Author(s): Rimco Boudewijns
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "mcrl2/utilities/logwidget.h"
+#include "ui_logwidget.h"
+
+using namespace mcrl2::utilities;
+using namespace mcrl2::utilities::qt;
+
+void LogRelay::output(const log_level_t level, const std::string& hint, const time_t timestamp, const std::string& msg)
+{
+  emit logMessage(QString::fromStdString(log_level_to_string(level)), QString::fromStdString(hint), QDateTime::fromTime_t(timestamp), QString::fromStdString(msg));
+}
+
+LogWidget::LogWidget(QWidget *parent)
+  : QWidget(parent),
+    m_ui(new Ui::LogWidget)
+{
+  m_ui->setupUi(this);
+  connect(&m_relay, SIGNAL(logMessage(QString, QString, QDateTime, QString)), this, SLOT(writeMessage(QString, QString, QDateTime, QString)));
+  mcrl2_logger::register_output_policy(m_relay);
+}
+
+LogWidget::~LogWidget()
+{
+  delete m_ui;
+}
+
+void LogWidget::writeMessage(QString level, QString hint, QDateTime timestamp, QString message)
+{
+  QString formattedMessage = QString("[%1 %2] %3 %4").arg(timestamp.toString("hh:mm:ss")).arg(level).arg(hint).arg(message).trimmed();
+  m_ui->editOutput->appendPlainText(formattedMessage);
+  emit logMessage(level, hint, timestamp, message, formattedMessage);
+}
