a68e2c547fd4afce83f4a2f66b986b9ed34cd2ed
Jan Friso Groote <j.f.groote@tue.nl>
2012-05-14 15:05:38 +0000

    Explicitly transformed a propositional_variable_with_variables to an aterm_appl, as the former is an object that is too big to put into an aterm_appl.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10559 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/detail/pbesinst_rewriter.h
+++ libraries/pbes/include/mcrl2/pbes/detail/pbesinst_rewriter.h
@@ -105,7 +105,7 @@ struct pbesinst_rewrite_builder: public enumerate_quantifiers_builder<pbes_expre
   term_type visit_propositional_variable(const term_type& x, const propositional_variable_type& v, pbesinst_substitution_function& sigma)
   {
     term_type y = super::visit_propositional_variable(x, v, sigma);
-    term_type result = term_type(rename(y), y.variables(), atermpp::make_list(y));
+    term_type result = term_type(rename(y), y.variables(), atermpp::make_list(atermpp::aterm_appl(y)));
     return result;
   }
 };
