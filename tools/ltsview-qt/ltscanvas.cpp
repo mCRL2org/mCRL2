0adba1e878fc2228e54c4fb857a636a19a3b2279
Ruud Koolen <r.p.j.koolen@tue.nl>
2012-06-14 08:19:15 +0000

    ltsview-qt: properly solved bug #1028.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10699 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/ltsview-qt/ltscanvas.cpp
+++ tools/ltsview-qt/ltscanvas.cpp
@@ -64,10 +64,7 @@ void LtsCanvas::clusterPositionsChanged()
   m_visualizer->computeBoundsInfo(width, height);
   m_nearPlane = 1.0f;
   m_baseDepth = 0.866f * height + width + m_nearPlane;
-  m_farPlane = m_baseDepth * 2 * width;
-  // Hack to ensure that m_nearPlane < m_farPlane
-  if (m_farPlane < m_nearPlane)
-      m_farPlane = 1000.0f;
+  m_farPlane = 4 * m_baseDepth;
   update();
 }
 
@@ -119,7 +116,6 @@ void LtsCanvas::initializeGL()
   glCullFace(GL_BACK);
 
   glClearDepth(1.0);
-
 }
 
 void LtsCanvas::resizeGL(int width, int height)
@@ -144,7 +140,6 @@ void LtsCanvas::paintGL()
 void LtsCanvas::render(bool light)
 {
   glEnable(GL_DEPTH_TEST);
-
   glDepthFunc(GL_LESS);
 
   glClearColor(
@@ -292,7 +287,7 @@ void LtsCanvas::render(bool light)
   GLenum error = glGetError();
   if (error != GL_NO_ERROR)
   {
-     mCRL2log(mcrl2::log::error) << "OpenGL error: " << gluErrorString(error) << std::endl;
+    mCRL2log(mcrl2::log::error) << "OpenGL error: " << gluErrorString(error) << std::endl;
   }
 }
 
