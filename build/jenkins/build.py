5ae1801aaf5d16fa3302ba1a76272907d614988f
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-08-20 13:44:52 +0000

    Add package script for Jenkins
    - Factor out utilities from build.py into util.py
    - Add file that packages with CPack, depending on platform.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10924 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- build/jenkins/build.py
+++ build/jenkins/build.py
@@ -2,14 +2,9 @@ import os
 import sys
 import shutil
 import platform
-import subprocess
 import multiprocessing
 
-workspace = os.environ['WORKSPACE']
-buildthreads = os.environ['BUILD_THREADS'] if 'BUILD_THREADS' in os.environ else 0
-buildtype = os.environ['buildtype']
-compiler = os.environ['compiler']
-label = os.environ['label']
+from util import *
 
 #
 # Try to remove CMake cache. We can wait the extra few minutes if it means we don't
@@ -19,50 +14,6 @@ try:
   os.remove(os.path.join(workspace, 'CMakeCache.txt'))
 except:
   pass
-
-def log(*lines):
-  print '\n##\n## ' + '\n## '.join(lines) + '\n##\n'
-  sys.stdout.flush()
-
-def call(name, command, **kwargs):
-  log('Running ' + name, ' '.join(command))
-  return subprocess.call(command, **kwargs)
-
-def which(name, flags=os.X_OK):
-    """Search PATH for executable files with the given name.
-   
-    On newer versions of MS-Windows, the PATHEXT environment variable will be
-    set to the list of file extensions for files considered executable. This
-    will normally include things like ".EXE". This fuction will also find files
-    with the given name ending with any of these extensions.
-
-    On MS-Windows the only flag that has any meaning is os.F_OK. Any other
-    flags will be ignored.
-   
-    @type name: C{str}
-    @param name: The name for which to search.
-   
-    @type flags: C{int}
-    @param flags: Arguments to L{os.access}.
-   
-    @rtype: C{list}
-    @param: A list of the full paths to files found, in the
-    order in which they were found.
-    """
-    result = None # []
-    exts = filter(None, os.environ.get('PATHEXT', '').split(os.pathsep))
-    path = os.environ.get('PATH', None)
-    if path is None:
-      return None # []
-    for p in os.environ.get('PATH', '').split(os.pathsep):
-      p = os.path.join(p, name)
-      if os.access(p, flags):
-        return p # result.append(p)
-      for e in exts:
-        pext = p + e
-        if os.access(pext, flags):
-          return pext # result.append(pext)
-    return result
 
 #
 # Configuration axis: compiler
---
+++ build/jenkins/package.py
@@ -0,0 +1,21 @@
+import platform
+from util import *
+
+#
+# Package
+#
+cpack_options = []
+if label in ["fedora-amd64"]:
+  cpack_options = ["-G", "RPM"]
+elif label in ["ubuntu-x86", "ubuntu-amd64"]:
+  cpack_options = ["-G", "DEB"]
+elif label in ["windows-x86", "windows-amd64"]:
+  cpack_options = ["-G", "NSIS"]
+
+if platform.system() == 
+cpack_command = ['cpack'] + cpack_options
+
+cpack_result = call('CPack', cpack_command)
+if cpack_result:
+  log('CPack returned ' + str(cpack_result))
+
