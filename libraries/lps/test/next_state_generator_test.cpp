2ac79246c3ac7b8811752508ac1a48c1da63931b
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-08-23 06:19:24 +0000

    Remove the old nextstate generator, simulator and state space exploration code.
    This has been superseeded by the implementation by Ruud Koolen.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10957 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/test/next_state_generator_test.cpp
+++ libraries/lps/test/next_state_generator_test.cpp
@@ -19,6 +19,8 @@
 #include "mcrl2/lps/parse.h"
 #include "mcrl2/lps/state.h"
 
+#include "test_specifications.h"
+
 using namespace mcrl2;
 using namespace mcrl2::data;
 using namespace mcrl2::data::detail;
@@ -97,62 +99,31 @@ void test_next_state_generator(const specification& lps_spec, size_t expected_st
   BOOST_CHECK(transition_labels.size() == expected_transition_labels);
 }
 
-BOOST_AUTO_TEST_CASE(test_abp)
+BOOST_AUTO_TEST_CASE(single_state_test)
 {
   std::string text(
-    "sort Error = struct e;\n"
-    "    D = struct d1 | d2;\n"
-
-    "act  i;\n"
-    "     c6,r6,s6: Error;\n"
-    "     c6,r6,s6,c5,r5,s5: Bool;\n"
-    "     c3,r3,s3: Error;\n"
-    "     c3,r3,s3,c2,r2,s2: D # Bool;\n"
-    "     s4,r1: D;\n"
-
-    "proc P(s30_S: Pos, d_S: D, b_S: Bool, s31_K: Pos, d_K: D, b_K: Bool, s32_L: Pos, b_L: Bool, s33_R: Pos, d_R: D, b_R: Bool) =\n"
-    "       sum d0_S: D.\n"
-    "         (s30_S == 1) ->\n"
-    "         r1(d0_S) .\n"
-    "         P(s30_S = 2, d_S = d0_S)\n"
-    "     + sum e0_K: Bool.\n"
-    "         (s31_K == 2) ->\n"
-    "         i .\n"
-    "         P(s31_K = if(e0_K, 4, 3), d_K = if(e0_K, d1, d_K), b_K = if(e0_K, true, b_K))\n"
-    "     + sum e1_L: Bool.\n"
-    "         (s32_L == 2) ->\n"
-    "         i .\n"
-    "         P(s32_L = if(e1_L, 4, 3), b_L = if(e1_L, true, b_L))\n"
-    "     + (s33_R == 2) ->\n"
-    "         s4(d_R) .\n"
-    "         P(s33_R = 3, d_R = d1)\n"
-    "     + sum e2_R: Bool.\n"
-    "         (s32_L == 1 && if(e2_R, s33_R == 4, s33_R == 3)) ->\n"
-    "         c5(if(e2_R, !b_R, b_R)) .\n"
-    "         P(s32_L = 2, b_L = if(e2_R, !b_R, b_R), s33_R = 1, d_R = d1, b_R = if(e2_R, b_R, !b_R))\n"
-    "     + (s31_K == 4 && s33_R == 1) ->\n"
-    "         c3(e) .\n"
-    "         P(s31_K = 1, d_K = d1, b_K = true, s33_R = 4, d_R = d1)\n"
-    "     + sum e3_R: Bool.\n"
-    "         ((s31_K == 3 && s33_R == 1) && if(e3_R, !b_R, b_R) == b_K) ->\n"
-    "         c3(d_K, if(e3_R, !b_R, b_R)) .\n"
-    "         P(s31_K = 1, d_K = d1, b_K = true, s33_R = if(e3_R, 4, 2), d_R = if(e3_R, d1, d_K))\n"
-    "     + (s30_S == 2 && s31_K == 1) ->\n"
-    "         c2(d_S, b_S) .\n"
-    "         P(s30_S = 3, s31_K = 2, d_K = d_S, b_K = b_S)\n"
-    "     + (s30_S == 3 && s32_L == 4) ->\n"
-    "         c6(e) .\n"
-    "         P(s30_S = 2, s32_L = 1, b_L = true)\n"
-    "     + sum e_S: Bool.\n"
-    "         ((s30_S == 3 && s32_L == 3) && if(e_S, b_S, !b_S) == b_L) ->\n"
-    "         c6(if(e_S, b_S, !b_S)) .\n"
-    "         P(s30_S = if(e_S, 1, 2), d_S = if(e_S, d1, d_S), b_S = if(e_S, !b_S, b_S), s32_L = 1, b_L = true)\n"
+    "act  a;\n"
+    "proc P(s3: Pos) =\n"
+    "       (s3 == 1) ->\n"
+    "        a .\n"
+    "        P(s3 = 2)\n"
     "     + delta;\n"
-
-    "init P(1, d1, true, 1, d1, true, 1, true, 1, d1, true);\n"
+    "init P(1);\n"
   );
 
   specification spec = parse_linear_process_specification(text);
+
+  test_initial_state_successors(spec);
+  // Test all 2**3 possible feature flags
+  for (size_t i = 0; i < 8; i++)
+  {
+    test_next_state_generator(spec, 2, 1, 1, i & 1, i & 2, i & 4);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_abp)
+{
+  specification spec = parse_linear_process_specification(LINEAR_ABP);
 
   test_initial_state_successors(spec);
   // Test all 2**3 possible feature flags
