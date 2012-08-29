a90160cf9c17e02adcc05e45550091d50f27148b
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-08-29 09:59:37 +0000

    Change RelocateInstallTree to include Qt dynamic libraries on Windows.
    Warning: this still needs proper testing on all platforms, sorry for any inconvenience!
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10990 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/RelocateInstallTree.cmake
+++ scripts/RelocateInstallTree.cmake
@@ -6,44 +6,51 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-# This script relocates the prerequisited shared libraries for a 
-# tool/project. 
+# This script relocates the prerequisited shared libraries for a
+# tool/project.
 
 # CMake 2.6 only supports relocating libraries for the Mac OSX.
 # CMake 2.8 support also relocation libraries for windows.
 # Since we support version 2.6 and up, we can only facilitate
-# relocation of shared libraries only for  Mac OSX. 
+# relocation of shared libraries only for  Mac OSX.
 
-if( APPLE AND BUILD_SHARED_LIBS AND NOT MCRL2_SINGLE_BUNDLE )
-if(EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.app" )
+SET(plugin_dest_dir bin)
+SET(qtconf_dest_dir bin)
+set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}")
+if(APPLE)
+  set(plugin_dest_dir bin/${PROJECT_NAME}.app/Contents/MacOS)
+  set(qtconf_dest_dir bin/${PROJECT_NAME}.app/Contents/Resources)
+  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}.app")
+endif()
+if(WIN32)
+  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}.exe")
+endif()
 
 # install a qt.conf file
 # this inserts some cmake code into the install script to write the file
 INSTALL(CODE "
-    file(WRITE \"\${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app/Contents/Resources/qt.conf\" \"\")
+    file(WRITE \"\${CMAKE_INSTALL_PREFIX}/${qtconf_dest_dir}/qt.conf\" \"\")
     " COMPONENT Runtime)
 
-# When a QtGui Library is a shared library, copy the required files manually. 
-if( ${QT_QTGUI_LIBRARY} MATCHES "^.*${CMAKE_SHARED_LIBRARY_SUFFIX}" )
-
-  if(EXISTS "${QT_LIBRARY_DIR}/Resources/qt_menu.nib")
-  # Install the required qt_menu into the Contents/Resources
-  install(DIRECTORY 
-      "${QT_LIBRARY_DIR}/Resources/qt_menu.nib" DESTINATION "${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app/Contents/Resources/")
-  else()
-      message(WARNING "${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app is probably corrupt.")
-  endif()
-endif()
-
 # Set libraries that need to be imported into bundle
-set(DIRS 
+set(DIRS
     ${Boost_LIBRARY_DIRS}
     ${QT_LIBRARY_DIRS} )
 
-install(CODE "
-    include(BundleUtilities) 
-    fixup_bundle(\"${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app\" \"\" \"${DIRS}\")
-    " COMPONENT Runtime)
-endif(EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.app" )
+# On Apple, the MacPorts build of Qt uses a slightly different structure
+# Therefore, we need to copy the qt_menu.nib file over to a decent location.
+if(APPLE AND ${QT_QTGUI_LIBRARY} MATCHES "^.*${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  if(EXISTS "${QT_LIBRARY_DIR}/Resources/qt_menu.nib")
+    install(DIRECTORY
+      "${QT_LIBRARY_DIR}/Resources/qt_menu.nib" DESTINATION "${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app/Contents/Resources/")
+    else()
+      message(WARNING "${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app is probably corrupt.")
+    endif()
+endif()
 
-endif( APPLE AND BUILD_SHARED_LIBS AND NOT MCRL2_SINGLE_BUNDLE )
+INSTALL(CODE "
+    file(GLOB_RECURSE QTPLUGINS
+      \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
+    include(BundleUtilities)
+    fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${DIRS}\")
+    " COMPONENT Runtime)
