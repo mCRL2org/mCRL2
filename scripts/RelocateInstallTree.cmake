21cd45a8fbdd76950f4cc86c447f4d80b00b308d
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-08-29 12:54:12 +0000

    Another attempt at fixing install tree relocation
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@10993 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/RelocateInstallTree.cmake
+++ scripts/RelocateInstallTree.cmake
@@ -1,4 +1,4 @@
-# Authors: Frank Stappers
+# Authors: Frank Stappers, Jeroen Keiren
 # Copyright: see the accompanying file COPYING or copy at
 # https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
 #
@@ -9,36 +9,56 @@
 # This script relocates the prerequisited shared libraries for a
 # tool/project.
 
-# CMake 2.6 only supports relocating libraries for the Mac OSX.
-# CMake 2.8 support also relocation libraries for windows.
-# Since we support version 2.6 and up, we can only facilitate
-# relocation of shared libraries only for  Mac OSX.
+# CMake 2.8 supports relocation libraries for Mac OSX and windows.
 
-SET(plugin_dest_dir bin)
-SET(qtconf_dest_dir bin)
-set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}")
+# This is based on the example on http://www.cmake.org/Wiki/BundleUtilitiesExample
+# Since we do not include Qt plugins, these have been left out.
+
+# Source files, Moc headers, ui files and resource files have been added in
+# the CMake script that includes this script.
+# Here we only need to take care of the installation stuff
+
+# WARNING: Relocation is only enabled for GUI tools
+# At the moment we detect GUI tools using an extremely ugly check,
+# we rely on MCRL2MacOSXBundleInformation to be included before this file,
+# and check whether the MACOSX_BUNDLE_NAME has been set.
+#
+# TODO: Make this into a proper solution
+if(NOT(MACOSX_BUNDLE_NAME))
+  return()
+endif()
+
+#-------------------------------------------------------------------------------
+# Now the installation stuff below
+#-------------------------------------------------------------------------------
+set(qtconf_dest_dir bin)
+set(APPS "\${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}")
 if(APPLE)
-  set(plugin_dest_dir bin/${PROJECT_NAME}.app/Contents/MacOS)
   set(qtconf_dest_dir bin/${PROJECT_NAME}.app/Contents/Resources)
-  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}.app")
+  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.app")
 endif()
 if(WIN32)
-  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/\${PROJECT_NAME}.exe")
+  set (APPS "\${CMAKE_INSTALL_PREFIX}/bin/${PROJECT_NAME}.exe")
 endif()
 
+#--------------------------------------------------------------------------------
 # install a qt.conf file
 # this inserts some cmake code into the install script to write the file
 INSTALL(CODE "
     file(WRITE \"\${CMAKE_INSTALL_PREFIX}/${qtconf_dest_dir}/qt.conf\" \"\")
     " COMPONENT Runtime)
 
-# Set libraries that need to be imported into bundle
-set(DIRS
-    ${Boost_LIBRARY_DIRS}
-    ${QT_LIBRARY_DIRS} )
+#--------------------------------------------------------------------------------
+# Use BundleUtilities to get all other dependencies for the application to work.
+# It takes a bundle or executable along with possible plugins and inspects it
+# for dependencies.  If they are not system dependencies, they are copied.
+
+# directories to look for dependencies
+SET(DIRS ${QT_LIBRARY_DIRS})
 
 # On Apple, the MacPorts build of Qt uses a slightly different structure
-# Therefore, we need to copy the qt_menu.nib file over to a decent location.
+# Therefore, we need to copy the qt_menu.nib file over to a decent location,
+# such that it can be found by fixup_bundle.
 if(APPLE)
   if(${QT_QTGUI_LIBRARY} MATCHES "^.*${CMAKE_SHARED_LIBRARY_SUFFIX}")
     if(EXISTS "${QT_LIBRARY_DIR}/Resources/qt_menu.nib")
@@ -50,9 +70,12 @@ if(APPLE)
   endif()
 endif()
 
+# Now the work of copying dependencies into the bundle/package
+# The quotes are escaped and variables to use at install time have their $ escaped
+# An alternative is the do a configure_file() on a script and use install(SCRIPT  ...).
+# Note that the image plugins depend on QtSvg and QtXml, and it got those copied
+# over.
 INSTALL(CODE "
-    file(GLOB_RECURSE QTPLUGINS
-      \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
     include(BundleUtilities)
-    fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${DIRS}\")
+    fixup_bundle(\"${APPS}\" \"\" \"${DIRS}\")
     " COMPONENT Runtime)
