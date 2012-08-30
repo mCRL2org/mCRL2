d74cbd6def6c172ee3a06ed495db5da525db38b3
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-08-30 09:56:57 +0000

    Clean up and restructure the packaging script
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11010 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/MCRL2Packaging.cmake
+++ scripts/MCRL2Packaging.cmake
@@ -32,67 +32,62 @@ set(CPACK_TOPLEVEL_TAG "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
 set(CPACK_SOURCE_PACKAGE_FILE_NAME ${CPACK_TOPLEVEL_TAG})
 set(CPACK_PACKAGE_FILE_NAME ${CPACK_TOPLEVEL_TAG}_${CXX_COMPILER_ARCHITECTURE})
 
+# Record the version in the package, for proper version reporting of the tools
+# and documentation
+configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/build/SourceVersion.in" "${CMAKE_CURRENT_SOURCE_DIR}/build/SourceVersion" @ONLY )
+
+# -----------------
+# Binary installers
+# -----------------
+
+# Create Desktop link to mcrl2-gui
+set(CPACK_CREATE_DESKTOP_LINKS mcrl2-gui)
+# Mark the executables for which links have to be generated in the
+# menus.
+set(CPACK_PACKAGE_EXECUTABLES "grapemcrl2;grapemcrl2" "ltsgraph;ltsgraph" "ltsview;ltsview" "diagraphica;diagraphica" "lpsxsim;lpsxsim" "mcrl2-gui;mcrl2-gui" "mcrl2xi;mcrl2xi")
+
 # Group the COMPONENTS such that we get a decent installer
 set(CPACK_COMPONENT_APPLICATIONS_GROUP "Runtime")
 set(CPACK_COMPONENT_LIBRARIES_GROUP "Development")
 set(CPACK_COMPONENT_HEADERS_GROUP "Development")
 set(CPACK_COMPONENT_EXAMPLE_GROUP "Documentation")
 
-# For a windows installer
-set(CPACK_ALL_INSTALL_TYPES Full Default)
+# Always install the tools
+set(CPACK_COMPONENT_APPLICATIONS_REQUIRED TRUE)
+
+# For generators that can generate more than one package; make sure that only
+# a single package with all components is generated.
+set(CPACK_COMPONENTS_ALL_IN_ONE_PACKAGE TRUE)
+
+# If we build with shared libraries, make sure they are included in the package
+if(BUILD_SHARED_LIBS)
+  set(CPACK_COMPONENT_APPLICATIONS_DEPENDS Libraries)
+endif()
+
+# For non-windows platforms we need to include libraries and headers, in order
+# to build the compiling rewriters
+if(NOT WIN32)
+  #set(CPACK_COMPONENT_APPLICATIONS_DEPENDS Libraries)
+  set(CPACK_COMPONENT_APPLICATIONS_DEPENDS Headers)
+endif()
+
+# For a windows installer create two default configurations,
+# Default only installs tools and examples
+# Full in addition installs the libraries and headers.
+set(CPACK_ALL_INSTALL_TYPES Default Full)
 set(CPACK_COMPONENT_APPLICATIONS_INSTALL_TYPES Full Default)
 set(CPACK_COMPONENT_LIBRARIES_INSTALL_TYPES Full)
 set(CPACK_COMPONENT_HEADERS_INSTALL_TYPES Full)
-set(CPACK_COMPONENT_EXAMPLE_INSTALL_TYPES Full Default)
+set(CPACK_COMPONENT_EXAMPLES_INSTALL_TYPES Full Default)
 
-# Always install the tools
-set(CPACK_COMPONENT_APPLCIATIONS_REQUIRED)
-
-configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/build/SourceVersion.in" "${CMAKE_CURRENT_SOURCE_DIR}/build/SourceVersion" @ONLY )
-
-if( NOT APPLE )
-  if( NOT MSVC )
-    string(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
-    if(CMAKE_BUILD_TYPE MATCHES "release")
-      #Strip files for Release packages
-      SET( CPACK_STRIP_FILES true )
-    endif(CMAKE_BUILD_TYPE MATCHES "release")
-  else( NOT MSVC )
+# For a release build, strip the executables
+# Note that there is a bug on Apple for which we need to disallow this.
+string(TOLOWER ${CMAKE_BUILD_TYPE} LC_CMAKE_BUILD_TYPE)
+if(LC_CMAKE_BUILD_TYPE MATCHES "release")
+  if(NOT APPLE)
     SET( CPACK_STRIP_FILES true )
-  endif( NOT MSVC )
-endif( NOT APPLE )
-
-# Create Desktop link to mcrl2-gui
-set(CPACK_CREATE_DESKTOP_LINKS mcrl2-gui)
-set(CPACK_PACKAGE_EXECUTABLES "grapemcrl2;grapemcrl2" "ltsgraph;ltsgraph" "ltsview;ltsview" "diagraphica;diagraphica" "lpsxsim;lpsxsim" "mcrl2-gui;mcrl2-gui" "mcrl2xi;mcrl2xi")
-
-IF(APPLE AND MCRL2_SINGLE_BUNDLE)
-  SET(CPACK_SET_DESTDIR TRUE)
-
-  configure_file(${CMAKE_SOURCE_DIR}/postflight.sh.in ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
-
-  SET(CMAKE_POSTFLIGHT_SCRIPT
-    ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
-  SET(CPACK_POSTUPGRADE_SCRIPT
-    ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
-
-  set(CPACK_PACKAGE_DEFAULT_LOCATION "/Applications")
-
-ENDIF(APPLE AND MCRL2_SINGLE_BUNDLE)
-
-# NSIS VARIABLES
-SET(CPACK_NSIS_DISPLAY_NAME "mCRL2")
-SET(CPACK_NSIS_PACKAGE_NAME "mCRL2")
-SET(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}\\\\build\\\\installer\\\\mcrl2-install-logo.bmp")
-
-# Fix issue where mCRL2 gets installed into "Program Files (x86)" in Win64.
-if(CMAKE_CL_64)
-  set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
-endif(CMAKE_CL_64)
-
-#Extend PATH variable with mCRL2_INSTALL_DIR/bin if CPACK_NSIS_MODIFY_PATH = ON
-SET(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}/${MCRL2_BIN_DIR}")
-SET(CPACK_NSIS_MODIFY_PATH ON)
+  endif()
+endif()
 
 # README and COPYING require a .txt extention when be used with MacOSX's PackageMaker
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/COPYING" "${CMAKE_CURRENT_BINARY_DIR}/COPYING.txt" COPYONLY)
@@ -100,6 +95,15 @@ configure_file("${CMAKE_CURRENT_SOURCE_DIR}/README"  "${CMAKE_CURRENT_BINARY_DIR
 
 set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_BINARY_DIR}/COPYING.txt )
 set(CPACK_RESOURCE_FILE_README  ${CMAKE_CURRENT_BINARY_DIR}/README.txt )
+
+# --------------------------------
+# Platform specific configurations
+# --------------------------------
+
+set(MCRL2_BOOST_VER "1.35" )
+
+# Linux
+# -----
 
 #Variables for RPM packaging
 set(CPACK_RPM_PACKAGE_LICENSE "Boost Software License, Version 1.0")
@@ -117,22 +121,6 @@ set(CPACK_RPM_PACKAGE_DESCRIPTION "the mCRL2 formal specification language tools
  specifications.  Moreover, state spaces can be manipulated, visualised and
  analysed.")
 set(CPACK_RPM_PACKAGE_VENDOR "Technische Universiteit Eindhoven (TU/e)")
-
-# Distro versions
-# /etc/debian_version
-# /etc/gentoo-release
-# /etc/knoppix-version
-# /etc/mandrake-release
-# /etc/pardus-release
-# /etc/puppyversion
-# /etc/redhat-release
-# /etc/sabayon-release
-# /etc/slackware-version
-# /etc/SuSE-release
-# /etc/xandros-desktop-version
-# /etc/zenwalk-version
-
-set(MCRL2_BOOST_VER "1.35" )
 
 # OpenSuSE RPM dependencies
 if(EXISTS /etc/SuSE-release )
@@ -152,4 +140,41 @@ if(EXISTS /etc/debian_version )
   set(CPACK_DEBIAN_PACKAGE_DEPENDS "gcc, debhelper (>= 5), libboost-dev (>=${MCRL2_BOOST_VER}), libglu1-mesa-dev (>= 7.0.1)")
 endif(EXISTS /etc/debian_version )
 
+# Apple
+# -----
+
+IF(APPLE AND MCRL2_SINGLE_BUNDLE)
+  SET(CPACK_SET_DESTDIR TRUE)
+
+  configure_file(${CMAKE_SOURCE_DIR}/postflight.sh.in ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
+
+  SET(CMAKE_POSTFLIGHT_SCRIPT
+    ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
+  SET(CPACK_POSTUPGRADE_SCRIPT
+    ${CMAKE_CURRENT_BINARY_DIR}/postflight.sh)
+
+  set(CPACK_PACKAGE_DEFAULT_LOCATION "/Applications")
+
+ENDIF(APPLE AND MCRL2_SINGLE_BUNDLE)
+
+# Windows
+# -------
+
+# NSIS VARIABLES
+SET(CPACK_NSIS_DISPLAY_NAME "mCRL2")
+SET(CPACK_NSIS_PACKAGE_NAME "mCRL2")
+SET(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}\\\\build\\\\installer\\\\mcrl2-install-logo.bmp")
+
+# Workaround
+# Fix issue where mCRL2 gets installed into "Program Files (x86)" in Win64.
+if(CMAKE_CL_64)
+  set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+endif(CMAKE_CL_64)
+
+#Extend PATH variable with mCRL2_INSTALL_DIR/bin if CPACK_NSIS_MODIFY_PATH = ON
+SET(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}/${MCRL2_BIN_DIR}")
+SET(CPACK_NSIS_MODIFY_PATH ON)
+
+# Include CPack specific stuff
+# ----------------------------
 include(CPack)
