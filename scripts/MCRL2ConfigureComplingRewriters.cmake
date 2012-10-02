6d774cc1acd4ffea4bd5c1e68d4611bb7dcf4022
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-10-02 07:31:26 +0000

    Merge r11124, r11128 from trunk
    r11124:
    Added linking to aterm library for resolving lazy symbol binding failure:
    Symbol not found: __ZN5aterm11ATmakeAppl5EmPNS_6_ATermES1_S1_S1_S1_
    
    Failure occurs on OS-X 10.6 on with "pipes.mcl2" example. Example is available upon request.
    
    r11128:
    Proper fix for compiling rewriters.
    
    This is an improvement over the fix in r11124, that in addition cleans up the configuration of the compiling rewriter dramatically.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/release@11137 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/MCRL2ConfigureComplingRewriters.cmake
+++ scripts/MCRL2ConfigureComplingRewriters.cmake
@@ -14,9 +14,11 @@ if(NOT UNIX)
   return()
 endif()
 
+# Configure compiler
 set(CC  ${CMAKE_C_COMPILER})
 set(CXX ${CMAKE_CXX_COMPILER})
 
+# Configure the default build options
 if(CMAKE_BUILD_TYPE)
   string(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE)
   if(CMAKE_BUILD_TYPE MATCHES "release")
@@ -30,41 +32,24 @@ if(CMAKE_BUILD_TYPE)
   endif()
 endif()
 
-set(R_CXXFLAGS "${R_CXXFLAGS} -std=c++98")
-set(R_CPPFLAGS "-DNDEBUG")
+# Do not use debug symbols in the rewriter, and build using the appropriate
+# flags for the aterm library
+set(R_CXXFLAGS "${R_CXXFLAGS} -std=c++98 -DNDEBUG ${ATERM_FLAGS}")
 
+# Make sure the shared library for the rewriter is build using position
+# independent code, if we were configured as static build ourselves, otherwise
+# use the defaults.
 if(NOT BUILD_SHARED_LIBS)
-  set(R_SCXXFLAGS "-fPIC")
+  set(R_CXXFLAGS "${R_CXXFLAGS} -fPIC")
 else()
-  set(R_SCXXFLAGS ${BUILD_SHARED_LIBS_OPTION})
+  set(R_CXXFLAGS "${R_CXXFLAGS} ${BUILD_SHARED_LIBS_OPTION}")
 endif()
 
+# Make sure we use shared linking.
 if(NOT APPLE)
-  set(R_SLDFLAGS "-shared")
+  set(R_LDFLAGS "-shared")
 else ()
-  set(R_SLDFLAGS "-bundle -undefined dynamic_lookup")
-endif()
-
-set(R_STAGE_INCLUDE_DIRS "")
-if(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
-  set( MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/mcrl2compilerewriter" )
-  set( R_STAGE_INCLUDE_DIRS "${R_STAGE_INCLUDE_DIRS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/aterm/include\"" )
-  set( R_STAGE_INCLUDE_DIRS "${R_STAGE_INCLUDE_DIRS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/atermpp/include\"" )
-  set( R_STAGE_INCLUDE_DIRS "${R_STAGE_INCLUDE_DIRS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/utilities/include\"" )
-  set( R_STAGE_INCLUDE_DIRS "${R_STAGE_INCLUDE_DIRS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/core/include\"" )
-  set( R_STAGE_INCLUDE_DIRS "${R_STAGE_INCLUDE_DIRS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/data/include\"" )
-else()
-  set( MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/mcrl2compilerewriter" )
-endif()
-
-if( APPLE AND MCRL2_SINGLE_BUNDLE AND CMAKE_INSTALL_PREFIX STREQUAL "/" )
-  set( R_CMAKE_INSTALL_PREFIX "..")
-  set( R_Boost_INCLUDE_DIRS "$${BOOST_INCLUDE}" )
-  set( R_Boost_LIBRARY_DIRS "$${BOOST_LIB}" )
-else()
-  set( R_CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
-  set( R_Boost_INCLUDE_DIRS "${Boost_INCLUDE_DIRS}" )
-  set( R_Boost_LIBRARY_DIRS "${Boost_LIBRARY_DIRS}" )
+  set(R_LDFLAGS "-bundle -undefined dynamic_lookup")
 endif()
 
 # Set MacOS-X specific compile flags
@@ -80,15 +65,50 @@ if( CMAKE_OSX_DEPLOYMENT_TARGET )
   set(R_MCRL2_OSX_ARCH "${R_MCRL2_OSX_ARCH} -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}" )
 endif( CMAKE_OSX_DEPLOYMENT_TARGET )
 
+set(R_CXXFLAGS "${R_CXXFLAGS} ${R_MCRL2_OSX_ARCH}")
+set(R_LDFLAGS "${R_LDFLAGS} ${R_MCRL2_OSX_ARCH}")
+
+# Configure including/linking the mcrl2 libraries and Boost.
+if( APPLE AND MCRL2_SINGLE_BUNDLE AND CMAKE_INSTALL_PREFIX STREQUAL "/" )
+  set(R_LDFLAGS "${R_LDFLAGS} -L\"../lib/mcrl2\"")
+  set(R_LDFLAGS "${R_LDFLAGS} -L\"${BOOST_LIB}\"")
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"../include\"")
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${BOOST_INCLUDE}\"")
+else()
+  set(R_LDFLAGS "${R_LDFLAGS} -L\"${CMAKE_INSTALL_PREFIX}/lib/mcrl2\"")
+  set(R_LDFLAGS "${R_LDFLAGS} -L\"${Boost_LIBRARY_DIRS}\"")
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_INSTALL_PREFIX}/include\"")
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${Boost_INCLUDE_DIRS}\"")
+endif()
+
+# Configure including and linking for the stage build.
+if(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+  set( MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/mcrl2compilerewriter" )
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/aterm/include\"" )
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/atermpp/include\"" )
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/utilities/include\"" )
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/core/include\"" )
+  set(R_CXXFLAGS "${R_CXXFLAGS} -I\"${CMAKE_CURRENT_SOURCE_DIR}/libraries/data/include\"" )
+  set(R_LDFLAGS "${R_LDFLAGS} -L\"${CMAKE_LIBRARY_OUTPUT_DIRECTORY}\"")
+else()
+  set( MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/mcrl2compilerewriter" )
+endif()
+
+# Link against the aterm library; these symbols are not always exported by the
+# calling executable
+set(R_LDFLAGS "${R_LDFLAGS} -lmcrl2_aterm")
+
+# Finally, include all the information in the mcrl2compilerewriter script
 configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/build/autoconf/mcrl2compilerewriter.in" "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}" @ONLY )
 
+# Mark the script as executable, and install it to the bianry directory.
 add_executable( mcrl2compilerewriter IMPORTED IMPORTED_LOCATION "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}" )
 install(
   PROGRAMS "${MCRL2_COMPILE_REWRITER_SCRIPT_LOCATION}"
   DESTINATION ${MCRL2_BIN_DIR}/
 )
 
-
+# Last but not least, generate the man pages.
 if (MCRL2_MAN_PAGES)
   add_custom_target(mcrl2compilerewriter_man_page ALL)
   install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/doc/sphinx/source/user_manual/tools/man/mcrl2compilerewriter.1 DESTINATION share/man/man1)
