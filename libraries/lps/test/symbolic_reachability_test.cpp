3de428e3a7e91a3029ecad35f4535f4b370a9012
Wieger Wesselink <j.w.wesselink@tue.nl>
2012-10-10 16:33:18 +0000

    - Added missing copyright notice.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11225 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/test/symbolic_reachability_test.cpp
+++ libraries/lps/test/symbolic_reachability_test.cpp
@@ -1,3 +1,14 @@
+// Author(s): Jeroen van der Wulp
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file symbolic_reachability_test.cpp
+/// \brief Add your file description here.
+
 #include <algorithm>
 #include <iterator>
 #include <iostream>
