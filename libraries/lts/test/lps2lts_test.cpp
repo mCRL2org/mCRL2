fd0421e91059cafd974c3d90d6e98a3fef4c3247
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-10-30 12:13:46 +0000

    Clean up some tests.
    * In the lps_linearization_test, it is completely unclear what the purpose
      of test_large_specification is. Since it causes timeouts I remove it.
    * The specification "block" in the same test is run in the performance
      measurements.
    
    The flag MCRL2_SKIP_LONG_TESTS is now superfluous, as it should be.
    
    I propose removing the flag from CMake as well.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11336 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lts/test/lps2lts_test.cpp
+++ libraries/lts/test/lps2lts_test.cpp
@@ -444,7 +444,6 @@ BOOST_AUTO_TEST_CASE(test_well_typedness_of_length_of_list_of_numbers)
   check_lps2lts_specification(spec, 1, 1, 1);
 }
 
-#ifndef MCRL2_SKIP_LONG_TESTS
 #if 0 // This test has been disabled; it was decided not to fix this issue.
 // The following example illustrates that enumeration can sometimes need
 // a large stack depth.
@@ -469,7 +468,6 @@ BOOST_AUTO_TEST_CASE(test_deep_stack)
   check_lps2lts_specification(spec, 3, 524289, 2);
 }
 #endif // false
-#endif // MCRL2_SKIP_LONG_TESTS
 
 
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
