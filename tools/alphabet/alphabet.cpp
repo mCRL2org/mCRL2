9f36fc63ed6f254becde79ed770a9cfcc86a5c68
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-11-16 11:24:38 +0000

    Merge all revisions up to and including r11394 from trunk
    - Made a separate function to print conditions of conditional expressions, since an optimization in the print_condition for data expressions is not applicable.
    
    
    Conflicts:
    
    	libraries/process/test/print_test.cpp
    
    - Fixed issue 1117. There was an error in the precedence level of printing an if_then_else expression. The hard coded precedence values have been removed and/or replaced by values relative according to the process::precedence function values. The error was found when debugging a problem with alphabet reduction problem applied to the bke protocol. This problem is now solved.
    
    - Added some tests for this case.
    
    
    - Made a rewrite of the specification of alphabet reduction.
    
    
    - Added an experimental tool for applying alphabet reduction to a process specification.
    
    
    - Removed mcrl2_gui dependencies.
    
    
    Fix handling of command line arguments with spaces.
    
    
    - Simplified rename_inverse, by implementing it in terms of rename.
    
    
    - Fixed application of rename_inverse to an allow_set.
    
    - Updated alphabet test.
    
    
    Fix build of alphabet reductions on mammoth (gcc (GCC) 4.4.4 20100630 (Red Hat
    4.4.4-10))
    
    
    Conflicts:
    
    	libraries/process/include/mcrl2/process/alphabet.h
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/aterm@11395 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ tools/alphabet/alphabet.cpp
@@ -0,0 +1,99 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file alphabet.cpp
+/// \brief This tool reads a process specification,
+/// and applies alphabet reduction to it.
+
+#include "boost.hpp" // precompiled headers
+
+#define TOOLNAME "alphabet"
+#define AUTHOR "Wieger Wesselink"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "mcrl2/lps/tools.h"
+#include "mcrl2/process/alphabet.h"
+#include "mcrl2/process/parse.h"
+#include "mcrl2/utilities/input_output_tool.h"
+#include "mcrl2/atermpp/aterm_init.h"
+
+using namespace mcrl2;
+using namespace mcrl2::utilities;
+using namespace mcrl2::utilities::tools;
+
+void alphabet(const std::string& input_filename, const std::string& output_filename, bool use_new_implementation)
+{
+  std::string text;
+  if (input_filename.empty())
+  {
+    text = utilities::read_text(std::cin);
+  }
+  else
+  {
+    text = utilities::read_text(input_filename);
+  }
+
+  process::process_specification result;
+
+  if (use_new_implementation)
+  {
+    result = process::parse_process_specification(text, false);
+    process::alphabet_reduce(result);
+  }
+  else
+  {
+    result = process::parse_process_specification(text, true);
+  }
+
+  std::ofstream out(output_filename.c_str());
+  out << process::pp(result);
+}
+
+class alphabet_tool : public input_output_tool
+{
+  typedef input_output_tool super;
+
+  protected:
+    bool m_use_new_implementation;
+
+    void parse_options(const command_line_parser& parser)
+    {
+      super::parse_options(parser);
+      m_use_new_implementation = parser.options.count("use-new-implementation") > 0;
+    }
+
+    void add_options(interface_description& desc)
+    {
+      super::add_options(desc);
+      desc.add_option("use-new-implementation", "use the new implementation", 'n');
+    }
+
+  public:
+    alphabet_tool() : super(
+        TOOLNAME,
+        AUTHOR,
+        "applies alphabet reduction to a process specification",
+        "Applies alphabet reduction to the mCRL2 specification in INFILE and writes the "
+        "result to OUTFILE.")
+    {}
+
+    bool run()
+    {
+      alphabet(input_filename(), output_filename(), m_use_new_implementation);
+      return true;
+    }
+};
+
+int main(int argc, char** argv)
+{
+  MCRL2_ATERMPP_INIT(argc, argv)
+
+  return alphabet_tool().execute(argc, argv);
+}
