37f8f8c43333a61e5b47eaea852b998e72a4eeea
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-11-20 05:50:33 +0000

    Merge r11399, r11400 from trunk.
    This should finally fix packaging of the tools on MacOSX.
    
    r11399:
    Added Qt libraries to distribution package for OS-X, by using the DeployQt4-module.
    This module is available within CMake as of version 2.8.7.
    
    Fixes #1106 (once gained access to JFG's Mac...)
    
    r11400:
    Fixed compilation for regression tests.
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/release@11403 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/RelocateInstallTree.cmake
+++ scripts/RelocateInstallTree.cmake
@@ -23,73 +23,21 @@
 # we rely on MCRL2MacOSXBundleInformation to be included before this file,
 # and check whether the MACOSX_BUNDLE_NAME has been set.
 #
-if(NOT(MACOSX_BUNDLE_NAME))
+
+if(APPLE AND NOT(MACOSX_BUNDLE_NAME))
   return()
 endif()
 
 # Warning: this seems to only make sense on Windows and MacOSX!
-if(NOT(APPLE OR WIN32))
-  return()
-endif()
-
-#-------------------------------------------------------------------------------
-# Now the installation stuff below
-#-------------------------------------------------------------------------------
-set(plugin_dest_dir ${MCRL2_BIN_DIR})
-set(qtconf_dest_dir ${MCRL2_BIN_DIR})
-set(APPS "\${CMAKE_INSTALL_PREFIX}/${MCRL2_BIN_DIR}/${PROJECT_NAME}")
-if(APPLE)
-  set(plugin_dest_dir ${MCRL2_BIN_DIR}/${PROJECT_NAME}.app/Contents/MacOS)
-  set(qtconf_dest_dir ${MCRL2_BIN_DIR}/${PROJECT_NAME}.app/Contents/Resources)
-  set (APPS "\${CMAKE_INSTALL_PREFIX}/${MCRL2_BIN_DIR}/${PROJECT_NAME}.app")
-endif()
-if(WIN32)
-  set (APPS "\${CMAKE_INSTALL_PREFIX}/${MCRL2_BIN_DIR}/${PROJECT_NAME}.exe")
-endif()
-
-#--------------------------------------------------------------------------------
-# install a qt.conf file
-# this inserts some cmake code into the install script to write the file
-INSTALL(CODE "
-    file(WRITE \"\${CMAKE_INSTALL_PREFIX}/${qtconf_dest_dir}/qt.conf\" \"\")
-    " COMPONENT Applications)
-
-#--------------------------------------------------------------------------------
-# Use BundleUtilities to get all other dependencies for the application to work.
-# It takes a bundle or executable along with possible plugins and inspects it
-# for dependencies.  If they are not system dependencies, they are copied.
-
-# directories to look for dependencies
-# Note: dlls for QT are installed in QT_BINARY_DIR if it is installed through
-# the windows installer.
-SET(DIRS ${QT_LIBRARY_DIR} ${QT_BINARY_DIR})
-
-# On Apple, the MacPorts build of Qt uses a slightly different structure
-# Therefore, we need to copy the qt_menu.nib file over to a decent location,
-# such that it can be found by fixup_bundle.
-if(APPLE)
-  if(${QT_QTGUI_LIBRARY} MATCHES "^.*${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    if(EXISTS "${QT_LIBRARY_DIR}/Resources/qt_menu.nib")
-      install(DIRECTORY
-        "${QT_LIBRARY_DIR}/Resources/qt_menu.nib" DESTINATION "${CMAKE_INSTALL_PREFIX}/${MCRL2_BIN_DIR}/${PROJECT_NAME}.app/Contents/Resources/")
-      else()
-        message(WARNING "${CMAKE_INSTALL_PREFIX}/${MCRL2_BIN_DIR}/${PROJECT_NAME}.app is probably corrupt.")
-      endif()
-  endif()
-endif()
-
 if(APPLE OR WIN32)
+    include(DeployQt4)
+    if(APPLE)
+        set(EXECUTABLE "${MCRL2_BIN_DIR}/${PROJECT_NAME}.app")
+    elseif(WIN32)
+        set(EXECUTABLE "${MCRL2_BIN_DIR}/${PROJECT_NAME}${CMAKE_EXECUTABLE_SUFFIX}")
+    endif()
 
-# Now the work of copying dependencies into the bundle/package
-# The quotes are escaped and variables to use at install time have their $ escaped
-# An alternative is the do a configure_file() on a script and use install(SCRIPT  ...).
-# Note that the image plugins depend on QtSvg and QtXml, and it got those copied
-# over.
-INSTALL(CODE "
-    file(GLOB_RECURSE QTPLUGINS
-	  \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
-    include(BundleUtilities)
-    fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${DIRS}\")
-    " COMPONENT Applications)
-
+    # Synopsis:
+    # INSTALL_QT4_EXECUTABLE(<executable> [<qtplugins> <libs> <dirs> <plugins_dir> <request_qt_conf> <component>])
+    install_qt4_executable(${EXECUTABLE} "" "" "" "" "" Applications)
 endif()
