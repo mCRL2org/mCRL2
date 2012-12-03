39242d1c436c98e347219cff908ad6b2bb4bd56e
Jeroen Keiren <j.j.a.keiren@tue.nl>
2012-12-03 14:30:11 +0000

    Clean up define for ssize_t in attempt to fix the windows build of the aterm branch
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/aterm@11428 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/atermpp/include/mcrl2/atermpp/detail/atypes.h
+++ libraries/atermpp/include/mcrl2/atermpp/detail/atypes.h
@@ -33,28 +33,22 @@ typedef size_t HashNumber;
 static const size_t npos=(size_t)(-1);
 
 /* Avoid warnings under windows, by renaming all strdup's into _strdup's,
-   and renaming the _strdup into strdup under other platforms than windows.
-   Furthermore, ssize_t is not defined on windows. */
-#if defined(_MSC_VER) || defined(WIN32) || defined(WIN64)
-#if defined(ssize_t)
-#else
-#ifndef HAVE_SSIZE_T
-#if defined(WIN64) ||  defined(_WIN64) ||  defined(__WIN64__)
-/* int64 is not supported by all GCC */
-typedef __int64 ssize_t;
-#else
-#ifndef __MINGW32__
-typedef int ssize_t;
-#endif
-#endif
-/* prevent ssize_t redefinitions in other libraries */
-#define HAVE_SSIZE_T
-#endif
-
-#endif
-#else
+ * and renaming the _strdup into strdup under other platforms than windows. */
+#ifndef _MSC_VER
 #define _strdup strdup
 #endif
+
+/* ssize_t is not defined on windows, define it here. */
+#ifdef _MSC_VER
+#if !defined(ssize_t) && !defined(HAVE_SSIZE_T)
+#ifdef _WIN64
+typedef __int64 ssize_t;
+#else
+typedef _W64 int ssize_t;
+#endif // _WIN64
+#define HAVE_SSIZE_T
+#endif // !defined(ssize_t) && !defined(HAVE_SSIZE_T)
+#endif // _MSC_VER
 
 /* Integers in BAF are always exactly 32 or 64 bits.  The size must be fixed so that
  *  * BAF terms can be exchanged between platforms. */
