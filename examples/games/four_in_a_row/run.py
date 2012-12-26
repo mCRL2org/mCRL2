0971e40f8bc79293ca964fb39ecf5ec625428c59
Jan Friso Groote <j.f.groote@tue.nl>
2012-12-26 10:01:50 +0000

    Made the python script for four in a row far more effective.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/aterm@11448 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- examples/games/four_in_a_row/run.py
+++ examples/games/four_in_a_row/run.py
@@ -1,7 +1,9 @@
 import os
 
-os.system('mcrl22lps -v four_in_a_row.mcrl2 four_in_a_row.lps')
+os.system('mcrl22lps -vD four_in_a_row.mcrl2 temp.lps')
+os.system('lpssuminst -v temp.lps | lpsparunfold -l -sBoard -v -n5 | lpsparunfold -l -sRow -v -n7 | lpsrewr -v > temp1.lps')
+os.system('lpsconstelm -v -c -rjittyc temp1.lps temp2.lps')
 # The state space of four in a row is big, so the statement below will not terminate.
-os.system('lps2lts -vrjittyc four_in_a_row.lps')
+os.system('lps2lts -v -ftree -rjittyc --cached --prune temp2.lps') 
 
 
