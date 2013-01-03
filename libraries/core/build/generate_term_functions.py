130c3c56f8f191be89202bed7c7fb6977db56484
Wieger Wesselink <j.w.wesselink@tue.nl>
2013-01-03 15:49:13 +0000

    - Reverted a change in the generation script that has not been applied to the code.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/aterm@11456 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/core/build/generate_term_functions.py
+++ libraries/core/build/generate_term_functions.py
@@ -108,7 +108,7 @@ bool %(check_name)s(Term t)
 '''
 CHECK_TERM_TYPE = '''  // check the type of the term
   atermpp::aterm term(atermpp::aterm_traits<Term>::term(t));
-  if (!term.type_is_appl())
+  if (term.type() != AT_APPL)
   {
     return false;
   }
