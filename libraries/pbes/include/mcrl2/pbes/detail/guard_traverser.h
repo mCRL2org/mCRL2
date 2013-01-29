b1b21def939c7f792c392b96ac7997dd64dd519e
Wieger Wesselink <j.w.wesselink@tue.nl>
2013-01-29 14:44:11 +0000

    - Changed internal representation of guards from multimap to vector. This is done to ensure that the order of the elements doesn't change.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11511 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/detail/guard_traverser.h
+++ libraries/pbes/include/mcrl2/pbes/detail/guard_traverser.h
@@ -24,42 +24,42 @@ namespace detail {
 
 struct guard_expression
 {
-	atermpp::multimap<propositional_variable_instantiation, pbes_expression> guards;
+  atermpp::vector<std::pair<propositional_variable_instantiation, pbes_expression> > guards;
   pbes_expression condition;
 
   bool is_simple() const
   {
-  	return guards.empty();
+    return guards.empty();
   }
 
   void add_guard(const pbes_expression& guard)
   {
-  	if (is_simple())
-  	{
-  	  condition = utilities::optimized_and(guard, condition);
-  	}
-  	else
-  	{
-      for (atermpp::multimap<propositional_variable_instantiation, pbes_expression>::iterator i = guards.begin(); i != guards.end(); ++i)
+    if (is_simple())
+    {
+      condition = utilities::optimized_and(guard, condition);
+    }
+    else
+    {
+      for (atermpp::vector<std::pair<propositional_variable_instantiation, pbes_expression> >::iterator i = guards.begin(); i != guards.end(); ++i)
       {
-    	  i->second = utilities::optimized_and(guard, i->second);
+        i->second = utilities::optimized_and(guard, i->second);
       }
     }
   }
 
   void negate()
   {
-  	if (is_simple())
-  	{
-  	  condition = utilities::optimized_not(condition);
-  	}
-  	else
-  	{
-      for (atermpp::multimap<propositional_variable_instantiation, pbes_expression>::iterator i = guards.begin(); i != guards.end(); ++i)
+    if (is_simple())
+    {
+      condition = utilities::optimized_not(condition);
+    }
+    else
+    {
+      for (atermpp::vector<std::pair<propositional_variable_instantiation, pbes_expression> >::iterator i = guards.begin(); i != guards.end(); ++i)
       {
-    	  i->second = utilities::optimized_not(i->second);
+        i->second = utilities::optimized_not(i->second);
       }
-  	}
+    }
   }
 
   guard_expression(const pbes_expression& condition_ = pbes_system::true_())
@@ -70,18 +70,18 @@ struct guard_expression
 inline
 std::ostream& operator<<(std::ostream& out, const guard_expression& x)
 {
-	if (x.is_simple())
-	{
-	  out << "condition = " << pbes_system::pp(x.condition) << std::endl;
-	}
-	else
-	{
-	  for (atermpp::multimap<propositional_variable_instantiation, pbes_expression>::const_iterator i = x.guards.begin(); i != x.guards.end(); ++i)
-	  {
-	  	out << pbes_system::pp(i->first) << " guard = " << pbes_system::pp(i->second) << std::endl;
-	  }
-	}
-	return out;
+  if (x.is_simple())
+  {
+    out << "condition = " << pbes_system::pp(x.condition) << std::endl;
+  }
+  else
+  {
+    for (atermpp::vector<std::pair<propositional_variable_instantiation, pbes_expression> >::const_iterator i = x.guards.begin(); i != x.guards.end(); ++i)
+    {
+      out << pbes_system::pp(i->first) << " guard = " << pbes_system::pp(i->second) << std::endl;
+    }
+  }
+  return out;
 }
 
 /// \brief Computes a multimap of propositional variable instantiations and the corresponding guards from a PBES expression
@@ -119,111 +119,111 @@ struct guard_traverser: public pbes_expression_traverser<guard_traverser>
 
   void leave(const data::data_expression& x)
   {
-  	push(guard_expression(x));
+    push(guard_expression(x));
   }
 
   void leave(const pbes_system::propositional_variable_instantiation& x)
   {
-  	guard_expression node;
-  	node.guards.insert(std::make_pair(x, pbes_system::true_()));
-  	push(node);
+    guard_expression node;
+    node.guards.push_back(std::make_pair(x, pbes_system::true_()));
+    push(node);
   }
 
   void leave(const pbes_system::true_& x)
   {
-  	push(guard_expression(x));
+    push(guard_expression(x));
   }
 
   void leave(const pbes_system::false_& x)
   {
-  	push(guard_expression(x));
+    push(guard_expression(x));
   }
 
   void leave(const pbes_system::not_& x)
   {
-  	top().negate();
+    top().negate();
   }
 
   void leave(const pbes_system::and_& x)
   {
-  	guard_expression right = pop();
-  	guard_expression left = pop();
-  	if (left.is_simple() && right.is_simple())
+    guard_expression right = pop();
+    guard_expression left = pop();
+    if (left.is_simple() && right.is_simple())
     {
-    	left.condition = utilities::optimized_and(left.condition, right.condition);
-    	push(left);
+      left.condition = utilities::optimized_and(left.condition, right.condition);
+      push(left);
     }
-  	else if (left.is_simple())
-  	{
-  		right.add_guard(left.condition);
-  		push(right);
-  	}
-  	else if (right.is_simple())
+    else if (left.is_simple())
     {
-  		left.add_guard(right.condition);
-  		push(left);
+      right.add_guard(left.condition);
+      push(right);
+    }
+    else if (right.is_simple())
+    {
+      left.add_guard(right.condition);
+      push(left);
     }
     else
     {
-    	left.add_guard(right.condition);
-    	right.add_guard(left.condition);
-    	left.guards.insert(right.guards.begin(), right.guards.end());
-    	push(left);
+      left.add_guard(right.condition);
+      right.add_guard(left.condition);
+      left.guards.insert(left.guards.end(), right.guards.begin(), right.guards.end());
+      push(left);
     }
   }
 
   void leave(const pbes_system::or_& x)
   {
-  	guard_expression right = pop();
-  	guard_expression left = pop();
-  	if (left.is_simple() && right.is_simple())
+    guard_expression right = pop();
+    guard_expression left = pop();
+    if (left.is_simple() && right.is_simple())
     {
-    	left.condition = utilities::optimized_or(left.condition, right.condition);
-    	push(left);
+      left.condition = utilities::optimized_or(left.condition, right.condition);
+      push(left);
     }
-  	else if (left.is_simple())
-  	{
-  		right.add_guard(utilities::optimized_not(left.condition));
-  		push(right);
-  	}
-  	else if (right.is_simple())
+    else if (left.is_simple())
     {
-  		left.add_guard(utilities::optimized_not(right.condition));
-  		push(left);
+      right.add_guard(utilities::optimized_not(left.condition));
+      push(right);
+    }
+    else if (right.is_simple())
+    {
+      left.add_guard(utilities::optimized_not(right.condition));
+      push(left);
     }
     else
     {
-    	left.guards.insert(right.guards.begin(), right.guards.end());
-    	push(left);
+      left.guards.insert(left.guards.end(), right.guards.begin(), right.guards.end());
+      push(left);
     }
   }
 
   void leave(const pbes_system::imp& x)
   {
-  	guard_expression right = pop();
-  	guard_expression left = pop();
-  	left.negate();
-  	if (left.is_simple() && right.is_simple())
+    guard_expression right = pop();
+    guard_expression left = pop();
+    left.negate();
+    if (left.is_simple() && right.is_simple())
     {
-    	left.condition = utilities::optimized_and(left.condition, right.condition);
-    	push(left);
+      left.condition = utilities::optimized_and(left.condition, right.condition);
+      push(left);
     }
-  	else if (left.is_simple())
-  	{
-  		right.add_guard(left.condition);
-  		push(right);
-  	}
-  	else if (right.is_simple())
+    else if (left.is_simple())
     {
-  		left.add_guard(right.condition);
-  		push(left);
+      right.add_guard(left.condition);
+      push(right);
+    }
+    else if (right.is_simple())
+    {
+      left.add_guard(right.condition);
+      push(left);
     }
     else
     {
-    	left.add_guard(right.condition);
-    	right.add_guard(left.condition);
-    	left.guards.insert(right.guards.begin(), right.guards.end());
-    	push(left);
+      left.add_guard(right.condition);
+      right.add_guard(left.condition);
+      left.guards.insert(left.guards.end(), right.guards.begin(), right.guards.end());
+      push(left);
     }
   }
 };
@@ -233,5 +233,32 @@ struct guard_traverser: public pbes_expression_traverser<guard_traverser>
 } // namespace pbes_system
 
 } // namespace mcrl2
+
+namespace atermpp
+{
+
+template<>
+struct aterm_traits<std::pair<mcrl2::pbes_system::propositional_variable_instantiation, mcrl2::pbes_system::pbes_expression> >
+{
+  static void protect(const std::pair<mcrl2::pbes_system::propositional_variable_instantiation, mcrl2::pbes_system::pbes_expression>& t)
+  {
+  	t.first.protect();
+    t.second.protect();
+  }
+
+  static void unprotect(const std::pair<mcrl2::pbes_system::propositional_variable_instantiation, mcrl2::pbes_system::pbes_expression>& t)
+  {
+  	t.first.unprotect();
+    t.second.unprotect();
+  }
+
+  static void mark(const std::pair<mcrl2::pbes_system::propositional_variable_instantiation, mcrl2::pbes_system::pbes_expression>& t)
+  {
+  	t.first.mark();
+    t.second.mark();
+  }
+};
+
+} // namespace atermpp
 
 #endif // MCRL2_PBES_DETAIL_GUARD_TRAVERSER_H
