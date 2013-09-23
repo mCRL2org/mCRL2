9e542944d79b5e829dbfc45a02c0e2758bee08c8
Sjoerd Cranen <s.cranen@tue.nl>
2013-09-23 18:00:28 +0000

    Merging [12121], [12122], [12123], [12125] into release branch.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/branches/release@12126 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/MCRL2MacOSXBundleInformation.cmake
+++ scripts/MCRL2MacOSXBundleInformation.cmake
@@ -6,16 +6,19 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-# Get copyright text from file
-file(READ "${CMAKE_SOURCE_DIR}/COPYING" MCRL2_COPYRIGHT_TEXT)
+function(macosx_set_packaging_info ICONFILE ICONLOCATION)
+  # Get copyright text from file
+  file(READ "${CMAKE_SOURCE_DIR}/COPYING" MCRL2_COPYRIGHT_TEXT)
 
-# Raise warning if MCRL2_VERSION is not set.
-if( NOT(MCRL2_VERSION) )
-  message(WARNING "MCRL2_VERSION is not set." )
-endif( NOT(MCRL2_VERSION) )
+  # Raise warning if MCRL2_VERSION is not set.
+  if( NOT(MCRL2_VERSION) )
+    message(WARNING "MCRL2_VERSION is not set." )
+  endif( NOT(MCRL2_VERSION) )
 
-# Set default bundle information
-SET(MACOSX_BUNDLE_NAME ${PROJECT_NAME})
-SET(MACOSX_BUNDLE_ICON_FILE ${PROJECT_NAME}.icns)
-SET(MACOSX_BUNDLE_COPYRIGHT ${MCRL2_COPYRIGHT_TEXT})
-SET(MACOSX_BUNDLE_BUNDLE_VERSION ${MCRL2_VERSION}) 
+  # Set default bundle information
+  SET(MACOSX_BUNDLE_NAME ${PROJECT_NAME})
+  SET(MACOSX_BUNDLE_ICON_FILE ${CMAKE_SOURCE_DIR}/build/packaging/icons/${ICONFILE}.icns)
+  SET(MACOSX_BUNDLE_COPYRIGHT ${MCRL2_COPYRIGHT_TEXT})
+  SET(MACOSX_BUNDLE_BUNDLE_VERSION ${MCRL2_VERSION}) 
+  SET_SOURCE_FILES_PROPERTIES(${MACOSX_BUNDLE_ICON_FILE} PROPERTIES MACOSX_PACKAGE_LOCATION ${ICONLOCATION})
+endfunction(macosx_set_packaging_info)
