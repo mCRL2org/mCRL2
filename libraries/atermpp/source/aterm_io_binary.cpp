9840878feee217aa97d6859f48b55fe27e6fe863
Jan Friso Groote <j.f.groote@tue.nl>
2013-10-07 18:12:44 +0000

    Removed a compiler warning on non windows systems.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12164 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/atermpp/source/aterm_io_binary.cpp
+++ libraries/atermpp/source/aterm_io_binary.cpp
@@ -30,7 +30,11 @@ using detail::writeInt;
 
 using namespace std;
 
-static void aterm_io_init(std::basic_ios<char>& s)
+static void aterm_io_init(std::basic_ios<char>& 
+#ifdef WIN32 // This suppresses a compiler warning.
+s
+#endif 
+)
 {
   /* Check for reasonably sized aterm (32 bits, 4 bytes)     */
   /* This check might break on perfectly valid architectures */
