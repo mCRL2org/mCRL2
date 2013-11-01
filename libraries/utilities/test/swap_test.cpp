b84940aaf91435305cef1c69879b9672d75782eb
Wieger Wesselink <j.w.wesselink@tue.nl>
2013-11-01 10:33:57 +0000

    - Added a test for swap, made by Jeroen Keiren. It turns out that swap for template classes needs to be done using ADL.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12194 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/utilities/test/swap_test.cpp
@@ -0,0 +1,102 @@
+// Author(s): Jeroen Keiren
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file down_cast_test.cpp
+/// \brief Test for down casting
+
+#include <boost/test/included/unit_test_framework.hpp>
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+namespace nsp
+{
+class swappable
+{
+protected:
+  int val_;
+  
+public:
+  swappable(int x) : val_(x)
+  {}
+  
+  void swap(swappable& other)
+  {
+    std::cerr << "swap method called" << std::endl;
+    int tmp = other.val_;
+    other.val_ = val_;
+    val_ = tmp;
+  }
+  
+  int val() const
+  {
+    return val_;
+  }
+};
+  
+  inline bool operator<(const swappable& x, const swappable& y)
+  {
+    return x.val() < y.val();
+  }
+}
+
+// The following approach does not work on recent versions of Clang using C++11,
+// whereas according to the standard and existing documentation it should.
+/*
+namespace std
+{
+  template<>
+  void swap(nsp::swappable& x, nsp::swappable& y)
+  {
+    std::cerr << "std::swap specialisation" << std::endl;
+    x.swap(y);
+  }
+}
+ */
+namespace nsp
+{
+  void swap(swappable& x, swappable& y)
+  {
+    std::cerr << "nsp::swap overload" << std::endl;
+    x.swap(y);
+  }
+}
+
+// This test case check whether, if both std::swap and
+// nsp::swap are visible, the nsp::swap is called.
+BOOST_AUTO_TEST_CASE(swapping_element)
+{
+  using std::swap;
+  using namespace nsp;
+  swappable x(1);
+  swappable y(2);
+  swap(x,y);
+  BOOST_CHECK(x.val() == 2);
+  BOOST_CHECK(y.val() == 1);
+}
+
+// This test case checks wheter, if a vector is sorted and
+// swap is called internally, then nsp::swap is called through
+// argument dependent lookup (ADL) instead of std::swap.
+// if nsp::swap is called, a message to this effect is printed.
+BOOST_AUTO_TEST_CASE(swapping_vector)
+{
+  std::vector<nsp::swappable> v;
+  nsp::swappable x(1);
+  nsp::swappable y(2);
+  nsp::swappable z(3);
+  v.push_back(y);
+  v.push_back(x);
+  std::sort(v.begin(),v.end());
+}
+
+boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
+{
+  return 0;
+}
