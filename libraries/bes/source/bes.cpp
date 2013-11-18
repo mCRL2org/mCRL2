c7661b676db85b13197e7bf8e3d3438734a78578
Wieger Wesselink <j.w.wesselink@tue.nl>
2013-11-18 20:16:49 +0000

    - Added missing file to the repository (noted by Gijs Kant).
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12239 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/bes/source/bes.cpp
@@ -0,0 +1,35 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file bes.cpp
+/// \brief
+
+#include "mcrl2/bes/print.h"
+
+namespace mcrl2
+{
+
+namespace bes
+{
+
+//--- start generated bes overloads ---//
+std::string pp(const bes::and_& x) { return bes::pp< bes::and_ >(x); }
+std::string pp(const bes::boolean_equation& x) { return bes::pp< bes::boolean_equation >(x); }
+std::string pp(const bes::boolean_equation_system& x) { return bes::pp< bes::boolean_equation_system >(x); }
+std::string pp(const bes::boolean_expression& x) { return bes::pp< bes::boolean_expression >(x); }
+std::string pp(const bes::boolean_variable& x) { return bes::pp< bes::boolean_variable >(x); }
+std::string pp(const bes::false_& x) { return bes::pp< bes::false_ >(x); }
+std::string pp(const bes::imp& x) { return bes::pp< bes::imp >(x); }
+std::string pp(const bes::not_& x) { return bes::pp< bes::not_ >(x); }
+std::string pp(const bes::or_& x) { return bes::pp< bes::or_ >(x); }
+std::string pp(const bes::true_& x) { return bes::pp< bes::true_ >(x); }
+//--- end generated bes overloads ---//
+
+} // namespace bes
+
+} // namespace mcrl2
