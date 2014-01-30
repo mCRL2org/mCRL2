a4929820971607ff2f3a402e5c3d529cbd4569bb
Wieger Wesselink <j.w.wesselink@tue.nl>
2014-01-30 12:08:44 +0000

    - Added a condition to the case (X, e) -> (Y, d1, e), requested by Tim.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12396 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/detail/stategraph_algorithm.h
+++ libraries/pbes/include/mcrl2/pbes/detail/stategraph_algorithm.h
@@ -1203,8 +1203,12 @@ class stategraph_algorithm
             if (q != component_index.end()) // (Y, k1) in C
             {
               std::size_t k1 = q->second;
-              mCRL2log(log::debug1, "stategraph") << "case 1 k1 = " << print_index(k1) << std::endl;
-              insert_local_control_flow_graph_edge(V, todo, u, Y, k1, e);
+              if (e == mapped_value(i->dest, k1, data::undefined_data_expression()))
+              {
+                // dest(X, i, k1) = e
+                mCRL2log(log::debug1, "stategraph") << "case 1 k1 = " << print_index(k1) << std::endl;
+                insert_local_control_flow_graph_edge(V, todo, u, Y, k1, e);
+              }
             }
             // case 2: (X, e) -> (Y, e)
             else
