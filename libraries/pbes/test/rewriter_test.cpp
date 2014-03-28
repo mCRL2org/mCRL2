53aebf8c13c4db1f44e5097be541f6597a06cf47
Wieger Wesselink <j.w.wesselink@tue.nl>
2014-03-28 07:58:57 +0000

    Adresses #1273 Added a unit test for ticket 1273. It demonstrates that the simplifying rewriter handles the expression 'exists m: Nat. val(n == 0)' as expected. So the problem lies somewhere else.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12543 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/test/rewriter_test.cpp
+++ libraries/pbes/test/rewriter_test.cpp
@@ -237,6 +237,10 @@ void test_simplifying_rewriter()
   test_simplify(R, r, "forall n: Nat. val(b) && Y(n)"                                   , "val(b) && forall n: Nat. Y(n)");
   test_simplify(R, r, "forall n: Nat. val(b)"                                           , "val(b)");
   test_simplify(R, r, "forall m: Nat. val(m > 2) && X"                                  , "(forall m: Nat. val(m > 2)) && X");
+
+  // ticket 1273
+  test_simplify(R, r, "exists m: Nat. val(n == 0)"                                      , "val(n == 0)");
+
   // test_expressions(R, "Y(n+p) && Y(p+n)"                                                , "Y(n+p)");
   // test_expressions(R, "exists m:Nat. val( m== p) && Y(m)"                               , "Y(p)");
   // test_expressions(R, "X && (Y(p) || X)"                                                , "X");
