faea6589c16816416fdb23437ffe8baeaa82ebcf
Sjoerd Cranen <s.cranen@tue.nl>
2014-05-07 17:27:41 +0000

    Trying to remove some of the warnings that pbespgsolve is causing: - Removed an unused function (is_empty) - Changed auto_ptr to unique_ptr (auto_ptr is deprecated) - Changed verti and edgei types to be size_t - Applied a more appropriate patch for unused variables than was done in changeset [12667]. This fix will probably get rid of #1278 (but let's wait for Mr. Jenkins to see if this is true).
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12683 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tools/pbespgsolve/Graph.h
+++ tools/pbespgsolve/Graph.h
@@ -23,8 +23,8 @@
 #include "compatibility.h"
 
 // Note: these should be unsigned types; some algorithms depend on it!
-typedef uint64_t verti;    //!< type used to number vertices
-typedef uint64_t edgei;    //!< type used to number edges
+typedef size_t verti;    //!< type used to number vertices
+typedef size_t edgei;    //!< type used to number edges
 
 #define NO_VERTEX ((verti)-1)
 
