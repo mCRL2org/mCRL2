ea2e7fbbe9ebe831a4cda5abe4199b8470f225aa
Sjoerd Cranen <s.cranen@tue.nl>
2014-07-08 17:08:45 +0000

    Corrected mistake in test code.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12887 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/CheckCXX11Features/cxx11-test-make_unsigned.cpp
+++ scripts/CheckCXX11Features/cxx11-test-make_unsigned.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-  typedef typename std::make_unsigned<int>::type u_type;
+  typedef std::make_unsigned<int>::type u_type;
   u_type i = (u_type)-1;
   int j = (int)-1;
   std::cout << "i = " << i << std::endl;
