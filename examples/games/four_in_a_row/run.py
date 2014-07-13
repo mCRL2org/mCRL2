8a9ffcf85f62c6a3167159c9d48d0737b63afefc
Jan Friso Groote <j.f.groote@tue.nl>
2014-07-13 15:46:38 +0000

    Slightly refined four in a row and its script even more. Constant elimination did not take conditions into account, not leading to the elimination of all already filled fields. The final transformed pbes was not rewritten, also leaving some residual non rewritten terms in the final pbes.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@12934 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- examples/games/four_in_a_row/run.py
+++ examples/games/four_in_a_row/run.py
@@ -2,15 +2,15 @@ import os
 
 os.system('mcrl22lps -vD four_in_a_row.mcrl2 temp.lps')
 os.system('lpssuminst -v temp.lps | lpsparunfold -l -sBoard -v -n6 | lpsparunfold -l -sRow -v -n8 | lpsrewr -v > temp1.lps')
-os.system('lpsconstelm -v -c -rjittyc temp1.lps temp2.lps')
+os.system('lpsconstelm -v -rjittyc temp1.lps temp2.lps')
 # The state space of four in a row is big, so the statement below will not terminate.
 # Reducing the number of columns, e.g. to four instead of default 7 does lead to a result.
 # In that case the there are 16M states. Solving the formulas red_wins.mcf requires 7M bes
 # variables (with result false). Five columns lead to appr. 100Mstates.
-os.system('lps2lts -v -rjittyc --prune temp2.lps') 
+os.system('lps2lts -v -rjittyc temp2.lps') 
 # Also pbes2bool does not tend to terminate on the full game, as it is too big.
 os.system('lps2pbes -vfred_wins.mcf temp2.lps temp.pbes')
-os.system('pbesrewr -pquantifier-all temp.pbes | pbesconstelm -v > temp1.pbes')
+os.system('pbesrewr -pquantifier-all temp.pbes | pbesconstelm -v | pbesrewr -v > temp1.pbes')
 os.system('pbes2bool -rjittyc -v -s2 temp1.pbes')
 
 
