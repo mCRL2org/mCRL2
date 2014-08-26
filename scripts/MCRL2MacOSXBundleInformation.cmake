cbc71c2ae68dc5b4b59d1fb3c92829c13d69e9e6
Sjoerd Cranen <s.cranen@tue.nl>
2014-08-26 09:17:20 +0000

    Fix MacOSX application icons. Addresses #1245.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13023 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- scripts/MCRL2MacOSXBundleInformation.cmake
+++ scripts/MCRL2MacOSXBundleInformation.cmake
@@ -6,19 +6,14 @@
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
-function(macosx_set_packaging_info ICONFILE ICONLOCATION)
+function(macosx_set_packaging_info ICONFILE)
   # Get copyright text from file
   file(READ "${CMAKE_SOURCE_DIR}/COPYING" MCRL2_COPYRIGHT_TEXT)
 
-  # Raise warning if MCRL2_VERSION is not set.
-  if( NOT(MCRL2_VERSION) )
-    message(WARNING "MCRL2_VERSION is not set." )
-  endif( NOT(MCRL2_VERSION) )
-
-  # Set default bundle information
+  # Set default bundle information 
+  SET(MACOSX_ICON_PATH ${CMAKE_SOURCE_DIR}/build/packaging/icons/${ICONFILE}.icns PARENT_SCOPE)
   SET(MACOSX_BUNDLE_NAME ${PROJECT_NAME} PARENT_SCOPE)
-  SET(MACOSX_BUNDLE_ICON_FILE ${CMAKE_SOURCE_DIR}/build/packaging/icons/${ICONFILE}.icns PARENT_SCOPE)
+  SET(MACOSX_BUNDLE_ICON_FILE ${ICONFILE}.icns PARENT_SCOPE)
   SET(MACOSX_BUNDLE_COPYRIGHT ${MCRL2_COPYRIGHT_TEXT} PARENT_SCOPE)
-  SET(MACOSX_BUNDLE_BUNDLE_VERSION ${MCRL2_VERSION} PARENT_SCOPE) 
-  SET_SOURCE_FILES_PROPERTIES(${MACOSX_BUNDLE_ICON_FILE} PROPERTIES MACOSX_PACKAGE_LOCATION ${ICONLOCATION})
+  SET(MACOSX_BUNDLE_BUNDLE_VERSION ${MCRL2_VERSION} PARENT_SCOPE)
 endfunction(macosx_set_packaging_info)
