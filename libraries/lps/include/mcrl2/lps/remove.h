afc44ef0f8049c795e26e59a95e4de5a0fd711b3
Wieger Wesselink <j.w.wesselink@tue.nl>
2014-10-08 11:00:53 +0000

    - Moved contents of lps_parameter_remover.h into lps/remove.h.
    - Generalized remove_trivial_summands to stochastic LPSs.
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13137 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lps/include/mcrl2/lps/remove.h
+++ libraries/lps/include/mcrl2/lps/remove.h
@@ -14,7 +14,6 @@
 
 #include "mcrl2/data/replace.h"
 #include "mcrl2/data/substitutions/mutable_map_substitution.h"
-#include "mcrl2/lps/detail/lps_parameter_remover.h"
 #include "mcrl2/lps/replace.h"
 #include "mcrl2/utilities/detail/container_utility.h"
 
@@ -58,6 +57,116 @@ struct is_singleton_sort
   }
 };
 
+/// \brief Function object for removing parameters from LPS data types.
+/// These parameters can be either process parameters or free variables.
+/// Assignments to these parameters are removed as well.
+template <typename SetContainer>
+struct lps_parameter_remover
+{
+  const SetContainer& to_be_removed;
+
+  lps_parameter_remover(const SetContainer& to_be_removed_)
+    : to_be_removed(to_be_removed_)
+  {}
+
+  /// \brief Removes parameters from a set container.
+  template <typename SetContainer1>
+  void remove_set_container(SetContainer1& c) const
+  {
+    for (auto i = to_be_removed.begin(); i != to_be_removed.end(); ++i)
+    {
+      c.erase(*i);
+    }
+  }
+
+  /// \brief Removes parameters from a list of variables.
+  data::variable_list remove_list_copy(const data::variable_list& l) const
+  {
+  	using utilities::detail::contains;
+
+    std::vector<data::variable> result;
+    for (auto i = l.begin(); i != l.end(); ++i)
+    {
+      if (!contains(to_be_removed, *i))
+      {
+        result.push_back(*i);
+      }
+    }
+    return data::variable_list(result.begin(), result.end());
+  }
+
+  /// \brief Removes parameters from a list of assignments.
+  /// Assignments to removed parameters are removed.
+  data::assignment_list remove_list_copy(const data::assignment_list& l) const
+  {
+    // TODO: make this implementation more efficient
+    std::vector<data::assignment> a(l.begin(), l.end());
+    a.erase(std::remove_if(a.begin(), a.end(), data::detail::has_left_hand_side_in(to_be_removed)), a.end());
+    return data::assignment_list(a.begin(), a.end());
+  }
+
+  /// \brief Removes parameters from the elements of a term list
+  template <typename TermList>
+  void remove_list(TermList& l) const
+  {
+    l = remove_list_copy(l);
+  }
+
+  /// \brief Removes parameters from the elements of a container
+  template <typename Container>
+  void remove_container(Container& c) const
+  {
+    for (auto i = c.begin(); i != c.end(); ++i)
+    {
+      remove(*i);
+    }
+  }
+
+  /// \brief Removes parameters from a summand
+  /// \param s A summand
+  void remove(action_summand& s) const
+  {
+    remove_list(s.assignments());
+  }
+
+  /// \brief Removes parameters from a process_initializer
+  /// \param s A process_initializer
+  void remove(process_initializer& i) const
+  {
+    i = process_initializer(remove_list_copy(i.assignments()));
+  }
+
+  /// \brief Removes parameters from a linear_process
+  /// \param s A linear_process
+  void remove(linear_process& p) const
+  {
+    remove_list(p.process_parameters());
+    remove_container(p.action_summands());
+  }
+
+  /// \brief Removes parameters from a linear process specification
+  /// \param spec A linear process specification
+  void remove(specification& spec) const
+  {
+    remove(spec.process());
+    remove(spec.initial_process());
+    remove_set_container(spec.global_variables());
+  }
+
+  template <typename Term>
+  void operator()(Term& t)
+  {
+    remove(t);
+  }
+};
+
+/// \brief Utility function to create an lps_parameter_remover.
+template <typename SetContainer>
+lps_parameter_remover<SetContainer> make_lps_parameter_remover(const SetContainer& to_be_removed)
+{
+  return lps_parameter_remover<SetContainer>(to_be_removed);
+}
+
 } // namespace detail
 
 /// \brief Rewrites an LPS data type.
@@ -70,10 +179,10 @@ void remove_parameters(Object& o, const SetContainer& to_be_removed)
 
 /// \brief Removes summands with condition equal to false from a linear process specification
 /// \param spec A linear process specification
-inline
-void remove_trivial_summands(specification& spec)
+template <typename Specification>
+void remove_trivial_summands(Specification& spec)
 {
-  action_summand_vector& v = spec.process().action_summands();
+  auto& v = spec.process().action_summands();
   v.erase(std::remove_if(v.begin(), v.end(), lps::detail::is_trivial_summand()), v.end());
 
   deadlock_summand_vector& w = spec.process().deadlock_summands();
