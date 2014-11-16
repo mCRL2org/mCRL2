727a26d6681c64def46c20bf513a1e0f6920f5b1
Jan Friso Groote <j.f.groote@tue.nl>
2014-11-16 21:16:18 +0000

    It is now possible to read and write probabilistic .aut files.
    All tests succeed.
    
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13195 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lts/include/mcrl2/lts/probabilistic_label.h
@@ -0,0 +1,225 @@
+// Author(s): Jan Friso Groote
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+/** \file
+ *
+ * \brief This file contains a class that contains labels for probabilistic transitions.
+ *        These consist of a 64 bit enumerator and denominator. This library maintains
+ *        precision as long as the enuemerator and denominator fit in 64 bits positive numbers.
+ *        If they do not fit, rounding is applied.
+ * \author Jan Friso Groote
+ */
+
+
+#ifndef MCRL2_LTS_PROBABILISTIC_LABEL_H
+#define MCRL2_LTS_PROBABILISTIC_LABEL_H
+
+// #include "mcrl2/utilities/exception.h"
+// #include "mcrl2/utilities/text_utility.h"
+
+namespace mcrl2
+{
+namespace lts
+{
+namespace detail
+{
+
+/** \brief This class contains labels for probabilistic transistions, consisting of a numerator and a denumerator.
+ *  \details There are methods to add and subtract such labels. The numbers are used as 64 bit integers.
+ *           If the integers do not fit into 64 bit, the numerator and denumerator are adapted to make
+ *           them fit. This means that this library is not exact, but employs rounding at times.
+ *           
+ */
+class probabilistic_label
+{
+  public:
+    typedef unsigned long number_t;
+  protected:
+    number_t m_enumerator;
+    number_t m_denominator;
+
+    // An algorithm to calculate the greatest common divisor
+    static number_t greatest_common_divisor(number_t x, number_t y)
+    {
+      if (x==0) return y;
+      if (y==0) return x;
+      if (x>y)
+      {
+        const number_t temp=x; x=y; y=temp; // swap(x,y)
+      }
+     
+      number_t remainder=y % x;
+      while (remainder!=0)
+      {
+        y=x;
+        x=remainder;
+        remainder=y % x;
+      }
+      return x;
+    }
+
+    void remove_common_factors(number_t& enumerator, number_t& denominator)
+    {
+      number_t gcd=greatest_common_divisor(enumerator,denominator);
+      while (gcd!=1)
+      {
+        enumerator=enumerator/gcd;
+        denominator=denominator/gcd;
+      }
+    }
+
+  public:
+
+    /* \brief Default constructor. The label will contain the default string.
+     */
+    probabilistic_label()
+     : m_enumerator(0),
+       m_denominator(1)
+    {}
+
+    /* \brief Constructor on the basis of two numbers. The denominator must not be 0.
+     */
+    probabilistic_label(const number_t enumerator, const number_t denominator)
+     : m_enumerator(enumerator),
+       m_denominator(denominator)
+    {
+      assert(m_denominator!=0);
+      remove_common_factors(m_enumerator,m_denominator);
+    }
+
+    /* \brief A constructor, where the enumerator and denominator are constructed
+     *        from two strings of digits.
+     */
+    probabilistic_label(const std::string& enumerator, const std::string& denominator)
+    {
+      assert(enumerator.size()<=denominator.size());
+      
+      // Check whether the denominator fits in number_t.
+      if (denominator.size()>=std::numeric_limits<number_t>::digits10)
+      {
+        std::string shortened_denominator=denominator.substr(0,std::numeric_limits<number_t>::digits10);
+        m_denominator=std::stoull(shortened_denominator);
+        int shortened_enumerator_size=enumerator.size()+std::numeric_limits<number_t>::digits10-denominator.size();
+        if (shortened_enumerator_size<=0)
+        {
+          m_enumerator=0;
+        }
+        else
+        {
+          std::string shortened_enumerator= enumerator.substr(0,shortened_enumerator_size);
+          m_enumerator=std::stoull(shortened_enumerator);
+        }
+        return;
+      }
+      m_enumerator=std::stoull(enumerator);
+      m_denominator=std::stoull(denominator);
+      assert(m_denominator!=0);
+      remove_common_factors(m_enumerator,m_denominator);
+      assert(m_enumerator<=m_denominator);
+    }
+
+    /* \brief Return the enumerator of the label.
+    */
+    number_t enumerator() const
+    {
+      return m_enumerator;
+    }
+
+    /* \brief Return the denominator of the label.
+    */
+    number_t denominator() const
+    {
+      return m_denominator;
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator==(const probabilistic_label& other) const
+    {
+      assert(greatest_common_divisor(other.m_enumerator,other.m_denominator)==1);
+      assert(greatest_common_divisor(m_enumerator,m_denominator)==1);
+      return this->m_enumerator==other.m_enumerator && this->m_denominator==other.m_denominator;
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator!=(const probabilistic_label& other) const
+    {
+      return !this->operator==(other);
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator<(const probabilistic_label& other) const
+    {
+      // float is used to guard against overflow.
+      return float(this->m_enumerator)*float(other.m_denominator)<float(other.m_enumerator)*float(this->m_denominator);
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator<=(const probabilistic_label& other) const
+    {
+      return !this->operator>(other);
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator>(const probabilistic_label& other) const
+    {
+      return other.operator<(*this);
+    }
+
+    /* \brief Standard comparison operator.
+    */
+    bool operator>=(const probabilistic_label& other) const
+    {
+      return other.operator<=(*this);
+    }
+
+    /* \brief Standard addition operator.
+     */
+    probabilistic_label operator+(const probabilistic_label& other) const
+    {
+      // TODO: Guard against overflow.
+      number_t enumerator=this->enumerator()*other.denominator()+other.enumerator()*this->denominator();
+      number_t denominator=this->denominator()*other.denominator();
+      return probabilistic_label(enumerator,denominator);
+    }
+
+    
+    /* \brief Standard subtraction operator.
+     */
+    probabilistic_label operator-(const probabilistic_label& other) const
+    {
+      // TODO: Guard against overflow.
+      assert(this->enumerator()*other.denominator()>=other.enumerator()*this->denominator());
+      number_t enumerator=this->enumerator()*other.denominator()-other.enumerator()*this->denominator();
+      number_t denominator=this->denominator()*other.denominator();
+      return probabilistic_label(enumerator,denominator);
+    }
+};
+
+/* \brief A pretty print operator on action labels, returning it as a string.
+*/
+inline std::string pp(const probabilistic_label& l)
+{
+  std::stringstream s;
+  s << l.enumerator() << "/" << l.denominator();
+  return s.str();
+}
+
+} // namespace detail
+
+} // namespace lts
+} // namespace mcrl2
+
+#endif // MCRL2_LTS_PROBABILISTIC_LABEL_H
+
+
