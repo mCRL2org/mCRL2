a88fc169bbfd40835ff3ca57d6b439d444f7252a
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-02 13:03:41 +0000

    - Added a number of trac tickets to the regression tests
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13315 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tests/regression/test.py
+++ tests/regression/test.py
@@ -6,6 +6,8 @@ sys.path += [os.path.join(os.path.dirname(__file__), '..', 'python')]
 import testrunner
 from testing import run_test
 
+MCRL2_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
+
 class TestRunner(testrunner.TestRunner):
     def __init__(self):
         self.tests = []
@@ -13,18 +15,44 @@ class TestRunner(testrunner.TestRunner):
         # add pbessolve tests
         index = 0
         for filename in sorted(glob.glob(os.path.join(os.path.dirname(__file__), 'pbessolve', '*.txt'))):
-            self.tests.append(('pbessolve', 'pbessolve{0}'.format(index), filename))
+            self.addtest('pbessolve{0}'.format(index), '../specifications/pbessolve.yml', [filename])
             index = index + 1
+
+        # add trac tickets tests
+        self.addtest('ticket_283',  '../specifications/mcrl22lps.yml', ['tickets/283/1.mcrl2'])
+        self.addtest('ticket_325',  '../specifications/alphabet.yml', ['tickets/325/1.mcrl2'])
+        #self.addtest('ticket_352',  '../specifications/alphabet.yml', ['tickets/352/1.mcrl2'], )
+        #self.addtest('ticket_397',  '../specifications/mcrl22lps.yml', ['tickets/397/1.mcrl2'])
+        #self.addtest('ticket_1090', '../specifications/lps2pbes.yml', ['tickets/1090/form.mcf', 'tickets/1090/spec.mcrl2'])
+        self.addtest('ticket_1093', '../specifications/alphabet.yml', ['tickets/1093/1.mcrl2'])
+        #self.addtest('ticket_1122', '../specifications/txt2lps.yml', ['tickets/1122/1.mcrl2'])
+        self.addtest('ticket_1127', '../specifications/mcrl22lps.yml', ['tickets/1127/1.mcrl2'])
+        self.addtest('ticket_1143', '../specifications/pbesrewr-onepoint.yml', ['tickets/1143/1.txt'])
+        self.addtest('ticket_1144', '../specifications/lpsbisim2pbes.yml', ['tickets/1144/test1.txt', 'tickets/1144/test2.txt'])
+        #self.addtest('ticket_1167', '../specifications/countstates.yml', [MCRL2_ROOT + '/examples/academic/abp/abp.mcrl2'])
+        self.addtest('ticket_1234', '../specifications/lpsbinary.yml', [MCRL2_ROOT + '/examples/academic/cabp/cabp.mcrl2'])
+        self.addtest('ticket_1241', '../specifications/alphabet.yml', ['tickets/1241/1.mcrl2'])
+        #self.addtest('ticket_1247', '../specifications/alphabet.yml', ['tickets/1247/1.mcrl2'])
+        self.addtest('ticket_1249', '../specifications/alphabet.yml', ['tickets/1249/1.mcrl2'])
+        self.addtest('ticket_1315', '../specifications/alphabet.yml', ['tickets/1315/1.mcrl2'])
+        self.addtest('ticket_1316', '../specifications/alphabet.yml', ['tickets/1316/1.mcrl2'])
+        self.addtest('ticket_1317', '../specifications/alphabet.yml', ['tickets/1317/1.mcrl2'])
+        self.addtest('ticket_1318', '../specifications/alphabet.yml', ['tickets/1318/1.mcrl2'])
+        self.addtest('ticket_1319', '../specifications/alphabet.yml', ['tickets/1319/1.mcrl2'])
+        self.addtest('ticket_1321', '../specifications/alphabet.yml', ['tickets/1321/1.mcrl2'])
+
+    def addtest(self, name, testfile, inputfiles):
+        self.tests.append((name, testfile, inputfiles))
 
     def name(self, testnum):
         if testnum < len(self.tests):
-            return self.tests[testnum][1]
+            (name, testfile, inputfiles) = self.tests[testnum]
+            return name
 
     def run(self, testnum):
         if testnum < len(self.tests):
-            testfile = '../specifications/pbessolve.yml'
-            filename = self.tests[testnum][2]
-            run_test(testfile, [filename])
+            (name, testfile, inputfiles) = self.tests[testnum]
+            run_test(testfile, inputfiles)
         else:
             raise RuntimeError('Invalid test number')
 
