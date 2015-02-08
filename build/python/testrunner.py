d8f776a26feb121edb11871d3ddc885f0086434c
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-08 12:29:45 +0000

    - Fixed erroneous checks for existence of command line arguments
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13360 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- build/python/testrunner.py
+++ build/python/testrunner.py
@@ -11,11 +11,11 @@ class TestRunner(object):
         is located in <_source_path>/build/python, and that the mCRL2 executables are located in
         <_source_path>/stage/bin (an in-source build). For out-of-source build trees, the location
         of the executables must be passed as a command-line option.
-        ''' 
+        '''
         self._args = self._get_commandline_parser().parse_args()
         self._source_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
         self._tool_path = self._args.toolpath or os.path.join(self._source_path, 'stage', 'bin')
-  
+
     def _get_commandline_parser(self):
         '''
         Returns an argparse.ArgumentParser. May be overridden in derived classes to add arguments.
@@ -28,21 +28,21 @@ class TestRunner(object):
         parser.add_argument('--toolpath', dest='toolpath', metavar='DIR', type=str, action='store',
                             help='Assume that the mCRL2 executables are located in this directory.')
         return parser
-  
+
     def main(self):
-        if self._args.names is not None:
+        if self._args.names:
             for name in self.names():
                 print name
-        elif self._args.run is not None:
+        elif self._args.run:
             try:
                 self.run(self._args.run)
             except Exception as e:
                 sys.exit(str(e))
-  
+
     def run(self, testnum):
         '''Runs the name of the test with number testnum.'''
         raise NotImplemented
-  
+
     def names(self):
         '''Iterates through the test names defined by this test runner.'''
         return []
