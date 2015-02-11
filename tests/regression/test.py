53f0da12c35fec2ff368b00c04fa4a6c5c9f7e7a
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-11 15:29:58 +0000

    - Added some regression tests for triangular confluence, supplied by Djurre van der Wal. Note that checks for the number of confluent tau's still need to be added
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13397 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tests/regression/test.py
+++ tests/regression/test.py
@@ -18,14 +18,6 @@ class TestRunner(testrunner.TestRunner):
                       if file.endswith('.txt')]) + \
                      [('ticket_283',  'mcrl22lps', ['tickets/283/1.mcrl2']),
                       ('ticket_325',  'alphabet', ['tickets/325/1.mcrl2']),
-                     #('ticket_352',  'alphabet', ['tickets/352/1.mcrl2']),
-                     #('ticket_397',  'mcrl22lps', ['tickets/397/1.mcrl2']),
-                     #('ticket_1090', 'lps2pbes', ['tickets/1090/form.mcf', 'tickets/1090/spec.mcrl2']),
-
-                     # TODO: increase timeout value
-                     #('ticket_1093', 'alphabet', ['tickets/1093/1.mcrl2']),
-
-                     #('ticket_1122', 'txt2lps', ['tickets/1122/1.mcrl2']),
                       ('ticket_1127', 'mcrl22lps', ['tickets/1127/1.mcrl2']),
                       ('ticket_1143', 'pbesrewr-onepoint', ['tickets/1143/1.txt']),
                       ('ticket_1144', 'lpsbisim2pbes', ['tickets/1144/test1.txt', 'tickets/1144/test2.txt']),
@@ -33,14 +25,27 @@ class TestRunner(testrunner.TestRunner):
                          {'nodes': {'l5': {'value': 74}}}),
                       ('ticket_1234', 'lpsbinary', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
                       ('ticket_1241', 'alphabet', ['tickets/1241/1.mcrl2']),
-                     #('ticket_1247', 'alphabet', ['tickets/1247/1.mcrl2']),
                       ('ticket_1249', 'alphabet', ['tickets/1249/1.mcrl2']),
                       ('ticket_1315', 'alphabet', ['tickets/1315/1.mcrl2']),
                       ('ticket_1316', 'alphabet', ['tickets/1316/1.mcrl2']),
                       ('ticket_1317', 'alphabet', ['tickets/1317/1.mcrl2']),
                       ('ticket_1318', 'alphabet', ['tickets/1318/1.mcrl2']),
                       ('ticket_1319', 'alphabet', ['tickets/1319/1.mcrl2']),
-                      ('ticket_1321', 'alphabet', ['tickets/1321/1.mcrl2'])]
+                      ('ticket_1321', 'alphabet', ['tickets/1321/1.mcrl2']),
+                      ('lpsconfcheck_triangular_cabp', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
+                      ('lpsconfcheck_triangular_trains', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                      ('lpsconfcheck_triangular_no_sums_trains', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                     ]
+                     # TODO: increase timeout value
+                     #('lpsconfcheck_triangular_no_sums_chatbox', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/industrial/chatbox/chatbox.mcrl2']),
+                     #('ticket_1247', 'alphabet', ['tickets/1247/1.mcrl2']),
+                     #('ticket_352',  'alphabet', ['tickets/352/1.mcrl2']),
+                     #('ticket_397',  'mcrl22lps', ['tickets/397/1.mcrl2']),
+                     #('ticket_1090', 'lps2pbes', ['tickets/1090/form.mcf', 'tickets/1090/spec.mcrl2']),
+                     # TODO: increase timeout value
+                     #('ticket_1093', 'alphabet', ['tickets/1093/1.mcrl2']),
+                     #('ticket_1122', 'txt2lps', ['tickets/1122/1.mcrl2']),
+
         self.settings = {'toolpath': self._tool_path,
                          'verbose': self._args.verbose,
                          'cleanup_files': True}
