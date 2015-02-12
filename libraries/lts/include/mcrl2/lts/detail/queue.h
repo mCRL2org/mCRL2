cdbf7fa29f96260a2afdf0cc220c853a16562dbb
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-12 08:19:59 +0000

    - Fixed include for UINT_MAX
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13401 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/lts/include/mcrl2/lts/detail/queue.h
+++ libraries/lts/include/mcrl2/lts/detail/queue.h
@@ -9,7 +9,7 @@
 #ifndef MCRL2_LTS_DETAIL_QUEUE_H
 #define MCRL2_LTS_DETAIL_QUEUE_H
 
-#include <limits>
+#include <climits>
 #include <cassert>
 #include <deque>
 #include "mcrl2/utilities/logger.h"
