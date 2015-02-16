94f5617e0ede9fc85276a45f5d1c941bde3bc059
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-16 15:23:34 +0000

    - Made the test function for the regression tests configurable
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13418 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tests/regression/test.py
+++ tests/regression/test.py
@@ -7,34 +7,35 @@ import os
 import sys
 sys.path += [os.path.join(os.path.dirname(__file__), '..', '..', 'build', 'python')]
 sys.path += [os.path.join(os.path.dirname(__file__), '..', 'python')]
+from testing import run_yml_test
 import testrunner
 
 class TestRunner(testrunner.TestRunner):
     def __init__(self):
         super(TestRunner, self).__init__()
         self.test_path = os.path.join(os.getcwd(), os.path.dirname(__file__))
-        self.tests = sorted([('regression_pbessolve_{}'.format(file[:-4]), 'pbessolve', ['pbessolve/{}'.format(file)])
+        self.tests = sorted([(run_yml_test, 'regression_pbessolve_{}'.format(file[:-4]), 'pbessolve', ['pbessolve/{}'.format(file)])
                       for file in os.listdir(os.path.join(self.test_path, 'pbessolve'))
                       if file.endswith('.txt')]) + \
-                     [('ticket_283',  'mcrl22lps', ['tickets/283/1.mcrl2']),
-                      ('ticket_325',  'alphabet', ['tickets/325/1.mcrl2']),
-                      ('ticket_1127', 'mcrl22lps', ['tickets/1127/1.mcrl2']),
-                      ('ticket_1143', 'pbesrewr-onepoint', ['tickets/1143/1.txt']),
-                      ('ticket_1144', 'lpsbisim2pbes', ['tickets/1144/test1.txt', 'tickets/1144/test2.txt']),
-                      ('ticket_1167', 'countstates', [self._source_path + '/examples/academic/abp/abp.mcrl2'],
-                         {'nodes': {'l5': {'value': 74}}}),
-                      ('ticket_1234', 'lpsbinary', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
-                      ('ticket_1241', 'alphabet', ['tickets/1241/1.mcrl2']),
-                      ('ticket_1249', 'alphabet', ['tickets/1249/1.mcrl2']),
-                      ('ticket_1315', 'alphabet', ['tickets/1315/1.mcrl2']),
-                      ('ticket_1316', 'alphabet', ['tickets/1316/1.mcrl2']),
-                      ('ticket_1317', 'alphabet', ['tickets/1317/1.mcrl2']),
-                      ('ticket_1318', 'alphabet', ['tickets/1318/1.mcrl2']),
-                      ('ticket_1319', 'alphabet', ['tickets/1319/1.mcrl2']),
-                      ('ticket_1321', 'alphabet', ['tickets/1321/1.mcrl2']),
-                      ('lpsconfcheck_triangular_cabp', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
-                      ('lpsconfcheck_triangular_trains', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
-                      ('lpsconfcheck_triangular_no_sums_trains', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                     [
+                       (run_yml_test, 'ticket_283',  'mcrl22lps', ['tickets/283/1.mcrl2']),
+                       (run_yml_test, 'ticket_325',  'alphabet', ['tickets/325/1.mcrl2']),
+                       (run_yml_test, 'ticket_1127', 'mcrl22lps', ['tickets/1127/1.mcrl2']),
+                       (run_yml_test, 'ticket_1143', 'pbesrewr-onepoint', ['tickets/1143/1.txt']),
+                       (run_yml_test, 'ticket_1144', 'lpsbisim2pbes', ['tickets/1144/test1.txt', 'tickets/1144/test2.txt']),
+                       (run_yml_test, 'ticket_1167', 'countstates', [self._source_path + '/examples/academic/abp/abp.mcrl2'], {'nodes': {'l5': {'value': 74}}}),
+                       (run_yml_test, 'ticket_1234', 'lpsbinary', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
+                       (run_yml_test, 'ticket_1241', 'alphabet', ['tickets/1241/1.mcrl2']),
+                       (run_yml_test, 'ticket_1249', 'alphabet', ['tickets/1249/1.mcrl2']),
+                       (run_yml_test, 'ticket_1315', 'alphabet', ['tickets/1315/1.mcrl2']),
+                       (run_yml_test, 'ticket_1316', 'alphabet', ['tickets/1316/1.mcrl2']),
+                       (run_yml_test, 'ticket_1317', 'alphabet', ['tickets/1317/1.mcrl2']),
+                       (run_yml_test, 'ticket_1318', 'alphabet', ['tickets/1318/1.mcrl2']),
+                       (run_yml_test, 'ticket_1319', 'alphabet', ['tickets/1319/1.mcrl2']),
+                       (run_yml_test, 'ticket_1321', 'alphabet', ['tickets/1321/1.mcrl2']),
+                       (run_yml_test, 'lpsconfcheck_triangular_cabp', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
+                       (run_yml_test, 'lpsconfcheck_triangular_trains', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                       (run_yml_test, 'lpsconfcheck_triangular_no_sums_trains', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
                      ]
                      # TODO: increase timeout value
                      #('lpsconfcheck_triangular_no_sums_chatbox', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/industrial/chatbox/chatbox.mcrl2']),
@@ -57,13 +58,12 @@ class TestRunner(testrunner.TestRunner):
 
     def names(self):
         for test in self.tests:
-            yield test[0]
+            yield test[1]
 
     def run(self, testnum):
-        from testing import run_yml_test
         if testnum < len(self.tests):
             test = self.tests[testnum]
-            name, specification, inputfiles = test[:3]
+            testfunc, name, specification, inputfiles = test[:4]
 
             # The specification identifies a YAML file in the specifications directory
             specification = '{}/tests/specifications/{}.yml'.format(self._source_path, specification)
@@ -72,8 +72,8 @@ class TestRunner(testrunner.TestRunner):
             # settings
             settings = {}
             settings.update(self.settings)
-            if len(test) > 3:
-                settings.update(test[3])
+            if len(test) > 4:
+                settings.update(test[4])
 
             # Interpret relative input file paths as being relative to the regression test
             # directory.
@@ -85,7 +85,7 @@ class TestRunner(testrunner.TestRunner):
             os.chdir(name)
 
             # Run the test
-            run_yml_test(name, specification, inputfiles, settings)
+            testfunc(name, specification, inputfiles, settings)
 
             # If the test did not throw an exception, we assume it was successful and remove
             # the directory we created. We assume that run_yml_test has deleted any files it
