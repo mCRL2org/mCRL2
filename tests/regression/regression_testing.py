b1a794abd22de5e10b6d61c0afede6f005f19d51
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-18 17:06:21 +0000

    - Made separate functions for the countstates and lpsconfcheck tests. This makes it much easier to specify command line arguments and expected results.
    - Adapted the regression testing script such that it can work with these functions
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13438 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ tests/regression/regression_testing.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#~ Copyright 2015 Wieger Wesselink.
+#~ Distributed under the Boost Software License, Version 1.0.
+#~ (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
+import os
+import sys
+sys.path += [os.path.join(os.path.dirname(__file__), '..', 'python')]
+
+from text_utility import write_text
+from testing import run_yml_test, cleanup_files
+
+MCRL2_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+MCRL2_INSTALL_DIR = os.path.join(MCRL2_ROOT, 'stage', 'bin')
+
+def ymlfile(file):
+    return '{}/tests/specifications/{}.yml'.format(MCRL2_ROOT, file)
+
+# Example:
+# inputfiles: ['1.mcrl2']
+# command_line_options: ['-xT']
+# expected_result: (0, 10) meaning 0 confluent tau summands (out of 10)
+def run_lpsconfcheck_test(name, inputfiles, command_line_options, expected_result, settings):
+    settings.update({'tools': {'t2': {'args': command_line_options}}})
+    settings.update({'result': "result = l6.value and l7.value['confluent_tau_summands'] == {}".format(expected_result)})
+    testfile = ymlfile('lpsconfcheck')
+    run_yml_test(name, testfile, inputfiles, settings)
+
+# expected_result is the expected number of states
+def run_countstates_test(name, inputfiles, expected_result, settings):
+    settings.update({'nodes': {'l5': {'value': expected_result}}})
+    testfile = ymlfile('countstates')
+    run_yml_test(name, testfile, inputfiles, settings)
+
+if __name__ == '__main__':
+    import copy
+    settings = {'toolpath': MCRL2_INSTALL_DIR, 'verbose': False, 'cleanup_files': True}
+    testdir = 'output'
+    if not os.path.exists(testdir):
+        os.mkdir(testdir)
+    os.chdir(testdir)
+    run_lpsconfcheck_test('lpsconfcheck_triangular', [MCRL2_ROOT + '/examples/academic/cabp/cabp.mcrl2'], ['-xT'], (0, 10), copy.deepcopy(settings))
+    run_countstates_test('countstates_abp', [MCRL2_ROOT + '/examples/academic/abp/abp.mcrl2'], 74, copy.deepcopy(settings))
--- tests/regression/test.py
+++ tests/regression/test.py
@@ -8,35 +8,41 @@ import sys
 sys.path += [os.path.join(os.path.dirname(__file__), '..', '..', 'build', 'python')]
 sys.path += [os.path.join(os.path.dirname(__file__), '..', 'python')]
 from testing import run_yml_test
+from regression_testing import *
 import testrunner
 
 class TestRunner(testrunner.TestRunner):
     def __init__(self):
         super(TestRunner, self).__init__()
+        ymlfile = self.ymlfile
+        mcrl2file = self.mcrl2file
+        abspath = self.abspath
         self.test_path = os.path.join(os.getcwd(), os.path.dirname(__file__))
-        self.tests = sorted([(run_yml_test, 'regression_pbessolve_{}'.format(file[:-4]), 'pbessolve', ['pbessolve/{}'.format(file)])
-                      for file in os.listdir(os.path.join(self.test_path, 'pbessolve'))
-                      if file.endswith('.txt')]) + \
+        pbessolve_files = sorted(os.listdir(os.path.join(self.test_path, 'pbessolve')))
+        self.tests = [('regression_pbessolve_{}'.format(file[:-4]), run_yml_test, [ymlfile('pbessolve'), [abspath('pbessolve/{}'.format(file))]]) for file in pbessolve_files] + \
                      [
-                       (run_yml_test, 'ticket_283',  'mcrl22lps', ['tickets/283/1.mcrl2']),
-                       (run_yml_test, 'ticket_325',  'alphabet', ['tickets/325/1.mcrl2']),
-                       (run_yml_test, 'ticket_1127', 'mcrl22lps', ['tickets/1127/1.mcrl2']),
-                       (run_yml_test, 'ticket_1143', 'pbesrewr-onepoint', ['tickets/1143/1.txt']),
-                       (run_yml_test, 'ticket_1144', 'lpsbisim2pbes', ['tickets/1144/test1.txt', 'tickets/1144/test2.txt']),
-                       (run_yml_test, 'ticket_1167', 'countstates', [self._source_path + '/examples/academic/abp/abp.mcrl2'], {'nodes': {'l5': {'value': 74}}}),
-                       (run_yml_test, 'ticket_1234', 'lpsbinary', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
-                       (run_yml_test, 'ticket_1241', 'alphabet', ['tickets/1241/1.mcrl2']),
-                       (run_yml_test, 'ticket_1249', 'alphabet', ['tickets/1249/1.mcrl2']),
-                       (run_yml_test, 'ticket_1315', 'alphabet', ['tickets/1315/1.mcrl2']),
-                       (run_yml_test, 'ticket_1316', 'alphabet', ['tickets/1316/1.mcrl2']),
-                       (run_yml_test, 'ticket_1317', 'alphabet', ['tickets/1317/1.mcrl2']),
-                       (run_yml_test, 'ticket_1318', 'alphabet', ['tickets/1318/1.mcrl2']),
-                       (run_yml_test, 'ticket_1319', 'alphabet', ['tickets/1319/1.mcrl2']),
-                       (run_yml_test, 'ticket_1321', 'alphabet', ['tickets/1321/1.mcrl2']),
-                       (run_yml_test, 'lpsconfcheck_triangular_cabp', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
-                       (run_yml_test, 'lpsconfcheck_triangular_trains', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
-                       (run_yml_test, 'lpsconfcheck_triangular_no_sums_trains', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                       ('regression_ticket_283' , run_yml_test,         [ ymlfile('mcrl22lps'),         [abspath('tickets/283/1.mcrl2')]                                       ] ),
+                       ('regression_ticket_325' , run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/325/1.mcrl2')]                                       ] ),
+                       ('regression_ticket_1127', run_yml_test,         [ ymlfile('mcrl22lps'),         [abspath('tickets/1127/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1143', run_yml_test,         [ ymlfile('pbesrewr-onepoint'), [abspath('tickets/1143/1.txt')]                                        ] ),
+                       ('regression_ticket_1144', run_yml_test,         [ ymlfile('lpsbisim2pbes'),     [abspath('tickets/1144/test1.txt'), abspath('tickets/1144/test2.txt')] ] ),
+                       ('regression_ticket_1167', run_countstates_test, [ [mcrl2file('/examples/academic/abp/abp.mcrl2')], 74                                                  ] ),
+                       ('regression_ticket_1234', run_yml_test,         [ ymlfile('lpsbinary'),         [mcrl2file('/examples/academic/cabp/cabp.mcrl2')]                      ] ),
+                       ('regression_ticket_1241', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1241/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1249', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1249/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1315', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1315/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1316', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1316/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1317', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1317/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1318', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1318/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1319', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1319/1.mcrl2')]                                      ] ),
+                       ('regression_ticket_1321', run_yml_test,         [ ymlfile('alphabet'),          [abspath('tickets/1321/1.mcrl2')]                                      ] ),
                      ]
+                     #(run_yml_test, 'lpsconfcheck_triangular_cabp', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/cabp/cabp.mcrl2']),
+                     #(run_yml_test, 'lpsconfcheck_triangular_trains', 'lpsconfcheck_triangular', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                     #(run_yml_test, 'lpsconfcheck_triangular_no_sums_trains', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/academic/trains/trains.mcrl2']),
+                     # + \
+                     #generate_test('lpsconfcheck_triangular_cabp', 'run_lpsconfcheck_test', [self._source_path + '/examples/academic/cabp/cabp.mcrl2'], ['-xT'], (0, 10)) + \
+                     #[]
                      # TODO: increase timeout value
                      #('lpsconfcheck_triangular_no_sums_chatbox', 'lpsconfcheck_triangular_no_sums', [self._source_path + '/examples/industrial/chatbox/chatbox.mcrl2']),
                      #('ticket_1247', 'alphabet', ['tickets/1247/1.mcrl2']),
@@ -51,6 +57,15 @@ class TestRunner(testrunner.TestRunner):
                          'verbose': self._args.verbose,
                          'cleanup_files': True}
 
+    def ymlfile(self, name):
+        return '{}/tests/specifications/{}.yml'.format(self._source_path, name)
+
+    def mcrl2file(self, file):
+        return self._source_path + file
+
+    def abspath(self, file):
+        return os.path.join(self.test_path, file)
+
     def _get_commandline_parser(self):
         parser = super(TestRunner, self)._get_commandline_parser()
         parser.add_argument('--verbose', dest='verbose', action='store_true', help='Display additional progress messages.')
@@ -58,34 +73,28 @@ class TestRunner(testrunner.TestRunner):
 
     def names(self):
         for test in self.tests:
-            yield test[1]
+            yield test[0]
 
     def run(self, testnum):
         if testnum < len(self.tests):
             test = self.tests[testnum]
-            testfunc, name, specification, inputfiles = test[:4]
-
-            # The specification identifies a YAML file in the specifications directory
-            specification = '{}/tests/specifications/{}.yml'.format(self._source_path, specification)
+            name, testfunc, arguments = test[:3]
 
             # Create a settings dict based on self.settings and, optionally, the test-specific
             # settings
             settings = {}
             settings.update(self.settings)
-            if len(test) > 4:
-                settings.update(test[4])
+            if len(test) > 3:
+                settings.update(test[3])
 
-            # Interpret relative input file paths as being relative to the regression test
-            # directory.
-            inputfiles = [file if os.path.isabs(file) else os.path.join(self.test_path, file)
-                          for file in inputfiles]
+            arguments.append(settings)
 
             # Create sandbox directory for the test to run in
             os.mkdir(name)
             os.chdir(name)
 
             # Run the test
-            testfunc(name, specification, inputfiles, settings)
+            testfunc(name, *arguments)
 
             # If the test did not throw an exception, we assume it was successful and remove
             # the directory we created. We assume that run_yml_test has deleted any files it
@@ -98,4 +107,4 @@ class TestRunner(testrunner.TestRunner):
 
 if __name__ == "__main__":
     sys.exit(TestRunner().main())
-    #TestRunner().run(25)
+    #TestRunner().run(5)
