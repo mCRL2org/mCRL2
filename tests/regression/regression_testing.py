bb593364720925ca1c54096e3dad84a11eaa34b7
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-02-25 09:23:48 +0000

    - Also put regression tests in a dictionary
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13469 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- tests/regression/regression_testing.py
+++ tests/regression/regression_testing.py
@@ -13,6 +13,12 @@ from testcommand import YmlTest
 MCRL2_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 MCRL2_INSTALL_DIR = os.path.join(MCRL2_ROOT, 'stage', 'bin')
 
+def abspath(file):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), file))
+
+def mcrl2file(file):
+    return os.path.abspath(MCRL2_ROOT + file)
+
 def ymlfile(file):
     return '{}/tests/specifications/{}.yml'.format(MCRL2_ROOT, file)
 
@@ -47,6 +53,45 @@ class PbesstategraphTest(YmlTest):
     def __init__(self, name, inputfiles, command_line_options, settings = dict()):
         super(PbesstategraphTest, self).__init__(name, ymlfile('pbesstategraph'), inputfiles, settings)
         self.set_command_line_options('t2', command_line_options)
+
+regression_tests = {
+    'ticket_283'    : lambda name, settings: YmlTest(name, ymlfile('mcrl22lps'),         [abspath('tickets/283/1.mcrl2')], settings),
+    'ticket_325'    : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/325/1.mcrl2')], settings),
+    'ticket_904'    : lambda name, settings: YmlTest(name, ymlfile('mcrl22lps'),         [abspath('tickets/904/1.mcrl2')], settings),
+    'ticket_952'    : lambda name, settings: YmlTest(name, ymlfile('txt2pbes'),          [abspath('tickets/952/1.txt')], settings),
+    'ticket_1090'   : lambda name, settings: YmlTest(name, ymlfile('ticket_1090'),       [abspath('tickets/1090/1.mcrl2'), abspath('tickets/1090/1.mcf')], settings),
+    'ticket_1114a'  : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1114/1.mcrl2')], settings),
+    'ticket_1114b'  : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1114/2.mcrl2')], settings),
+    'ticket_1143'   : lambda name, settings: PbesrewrTest(name, [abspath('tickets/1143/1.txt')], 'quantifier-one-point'),
+    'ticket_1144'   : lambda name, settings: YmlTest(name, ymlfile('lpsbisim2pbes'),     [abspath('tickets/1144/test1.txt'), abspath('tickets/1144/test2.txt')], settings),
+    'ticket_1167'   : lambda name, settings: CountStatesTest(name, [mcrl2file('/examples/academic/abp/abp.mcrl2')], 74),
+    'ticket_1206'   : lambda name, settings: YmlTest(name, ymlfile('lps2lts'),           [abspath('tickets/1206/1.mcrl2')], settings),
+    'ticket_1218'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1218/1.mcrl2')], settings),
+    'ticket_1234'   : lambda name, settings: YmlTest(name, ymlfile('lpsbinary'),         [mcrl2file('/examples/academic/cabp/cabp.mcrl2')], settings),
+    'ticket_1241'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1241/1.mcrl2')], settings),
+    'ticket_1249'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1249/1.mcrl2')], settings),
+    'ticket_1297'   : lambda name, settings: YmlTest(name, ymlfile('ticket_1297'),       [abspath('tickets/1297/1.mcrl2')], settings),
+    'ticket_1301'   : lambda name, settings: YmlTest(name, ymlfile('ticket_1301'),       [abspath('tickets/1301/1.mcrl2'), abspath('tickets/1301/1.mcf')], settings),
+    'ticket_1311'   : lambda name, settings: PbesstategraphTest(name,                    [abspath('tickets/1311/1.txt')], ['-g'], settings),
+    'ticket_1314'   : lambda name, settings: YmlTest(name, ymlfile('ticket_1314'),       [abspath('tickets/1314/1.mcrl2'), abspath('tickets/1314/1.mcf')], settings),
+    'ticket_1315'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1315/1.mcrl2')], settings),
+    'ticket_1316'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1316/1.mcrl2')], settings),
+    'ticket_1317'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1317/1.mcrl2')], settings),
+    'ticket_1318'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1318/1.mcrl2')], settings),
+    'ticket_1319'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1319/1.mcrl2')], settings),
+    'ticket_1320'   : lambda name, settings: YmlTest(name, ymlfile('lps2pbes'),          [abspath('tickets/1320/1.mcrl2'), abspath('tickets/1320/1.mcf')], settings),
+    'ticket_1321'   : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1321/1.mcrl2')], settings),
+    'ticket_1322'   : lambda name, settings: YmlTest(name, ymlfile('pbesstategraph'),    [abspath('tickets/1322/1.txt')], settings),
+    'lpsconfcheck_1': lambda name, settings: LpsconfcheckCtauTest(name, [mcrl2file('/examples/academic/cabp/cabp.mcrl2')], 'T', (0, 18), settings),
+    'lpsconfcheck_2': lambda name, settings: LpsconfcheckCtauTest(name, [mcrl2file('/examples/academic/trains/trains.mcrl2')], 'T', (0, 9), settings),
+    }
+
+pbessolve_tests = { 'pbessolve_{}'.format(filename[:-4]) : lambda name, settings: YmlTest(name, ymlfile('pbessolve'), [abspath('pbessolve/{}'.format(filename))]) for filename in sorted(os.listdir(abspath('pbessolve'))) }
+
+slow_regression_tests = {
+    'ticket_1093'    : lambda name, settings: YmlTest(name, ymlfile('alphabet'),          [abspath('tickets/1093/1.mcrl2')], settings.update({ 'timeout': 300, 'memlimit': 500000000 })),
+    'lpsconfcheck_3' : lambda name, settings: LpsconfcheckCtauTest(name, [mcrl2file('/examples/industrial/chatbox/chatbox.mcrl2')], 'x', (40, 72), settings.update({ 'timeout': 300, 'memlimit': 500000000 })),
+    }
 
 if __name__ == '__main__':
     settings = {'toolpath': MCRL2_INSTALL_DIR, 'verbose': False, 'cleanup_files': True}
--- tests/regression/test.py
+++ tests/regression/test.py
@@ -8,56 +8,16 @@ import os
 import sys
 sys.path += [os.path.join(os.path.dirname(__file__), '..', 'python')]
 import testing
-from regression_testing import *
-from testcommand import YmlTest
-
-def abspath(file):
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), file))
+from regression_testing import regression_tests, pbessolve_tests
 
 class TestRunner(testing.TestRunner):
     def __init__(self):
         super(TestRunner, self).__init__()
-        ymlfile = self.ymlfile
-        mcrl2file = self.mcrl2file
-        pbessolve_files = sorted(os.listdir(abspath('pbessolve')))
-        self.tests = [
-                       YmlTest('ticket_283',  ymlfile('mcrl22lps'),         [abspath('tickets/283/1.mcrl2')]),
-                       YmlTest('ticket_325' , ymlfile('alphabet'),          [abspath('tickets/325/1.mcrl2')]),
-                       YmlTest('ticket_904',  ymlfile('mcrl22lps'),         [abspath('tickets/904/1.mcrl2')]),
-                       YmlTest('ticket_952' , ymlfile('txt2pbes'),          [abspath('tickets/952/1.txt')]),
-                       YmlTest('ticket_1090', ymlfile('ticket_1090'),       [abspath('tickets/1090/1.mcrl2'), abspath('tickets/1090/1.mcf')]),
-                       #YmlTest('ticket_1093', ymlfile('alphabet'),          [abspath('tickets/1093/1.mcrl2')], { 'timeout': 300, 'memlimit': 500000000 }),
-                       YmlTest('ticket_1114a', ymlfile('alphabet'),         [abspath('tickets/1114/1.mcrl2')]),
-                       YmlTest('ticket_1114b', ymlfile('alphabet'),         [abspath('tickets/1114/2.mcrl2')]),
-                       PbesrewrTest('ticket_1143', [abspath('tickets/1143/1.txt')], 'quantifier-one-point'),
-                       YmlTest('ticket_1144', ymlfile('lpsbisim2pbes'),     [abspath('tickets/1144/test1.txt'), abspath('tickets/1144/test2.txt')]),
-                       CountStatesTest('ticket_1167', [mcrl2file('/examples/academic/abp/abp.mcrl2')], 74),
-                       YmlTest('ticket_1206', ymlfile('lps2lts'),           [abspath('tickets/1206/1.mcrl2')]),
-                       YmlTest('ticket_1218', ymlfile('alphabet'),          [abspath('tickets/1218/1.mcrl2')]),
-                       YmlTest('ticket_1234', ymlfile('lpsbinary'),         [mcrl2file('/examples/academic/cabp/cabp.mcrl2')]),
-                       YmlTest('ticket_1241', ymlfile('alphabet'),          [abspath('tickets/1241/1.mcrl2')]),
-                       YmlTest('ticket_1249', ymlfile('alphabet'),          [abspath('tickets/1249/1.mcrl2')]),
-                       YmlTest('ticket_1297', ymlfile('ticket_1297'),       [abspath('tickets/1297/1.mcrl2')]),
-                       YmlTest('ticket_1301', ymlfile('ticket_1301'),       [abspath('tickets/1301/1.mcrl2'), abspath('tickets/1301/1.mcf')]),
-                       PbesstategraphTest('ticket_1311',                    [abspath('tickets/1311/1.txt')], ['-g']),
-                       YmlTest('ticket_1314', ymlfile('ticket_1314'),       [abspath('tickets/1314/1.mcrl2'), abspath('tickets/1314/1.mcf')]),
-                       YmlTest('ticket_1315', ymlfile('alphabet'),          [abspath('tickets/1315/1.mcrl2')]),
-                       YmlTest('ticket_1316', ymlfile('alphabet'),          [abspath('tickets/1316/1.mcrl2')]),
-                       YmlTest('ticket_1317', ymlfile('alphabet'),          [abspath('tickets/1317/1.mcrl2')]),
-                       YmlTest('ticket_1318', ymlfile('alphabet'),          [abspath('tickets/1318/1.mcrl2')]),
-                       YmlTest('ticket_1319', ymlfile('alphabet'),          [abspath('tickets/1319/1.mcrl2')]),
-                       YmlTest('ticket_1320', ymlfile('lps2pbes'),          [abspath('tickets/1320/1.mcrl2'), abspath('tickets/1320/1.mcf')]),
-                       YmlTest('ticket_1321', ymlfile('alphabet'),          [abspath('tickets/1321/1.mcrl2')]),
-                       YmlTest('ticket_1322', ymlfile('pbesstategraph'),    [abspath('tickets/1322/1.txt')]),
-                       LpsconfcheckCtauTest('lpsconfcheck_1', [mcrl2file('/examples/academic/cabp/cabp.mcrl2')], 'T', (0, 18)),
-                       LpsconfcheckCtauTest('lpsconfcheck_2', [mcrl2file('/examples/academic/trains/trains.mcrl2')], 'T', (0, 9)),
-                       #LpsconfcheckCtauTest('lpsconfcheck_3', [mcrl2file('/examples/industrial/chatbox/chatbox.mcrl2')], 'x', (40, 72), { 'timeout': 300, 'memlimit': 500000000 } ),
-                     ] + \
-                     [
-                       YmlTest('regression_pbessolve_{}'.format(file[:-4]), ymlfile('pbessolve'), [abspath('pbessolve/{}'.format(file))]) for file in pbessolve_files
-                     ]
-        for test in self.tests:
-            test.name = 'regression_' + test.name
+        self.tests = []
+        for name in sorted(regression_tests):
+            self.tests.append(regression_tests[name]('regression_{}'.format(name), self.settings))
+        for name in sorted(pbessolve_tests):
+            self.tests.append(pbessolve_tests[name]('regression_{}'.format(name), self.settings))
 
 if __name__ == "__main__":
     sys.exit(TestRunner().main())
