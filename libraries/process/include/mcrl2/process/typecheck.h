f5eafffcb3265a62759b7faa60342f4743352155
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-07-16 14:34:09 +0000

    - Made a start with a process expression type checker implemented using the traverser framework
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13654 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/process/include/mcrl2/process/typecheck.h
+++ libraries/process/include/mcrl2/process/typecheck.h
@@ -63,6 +63,22 @@ struct data_expression_typechecker: protected data::data_type_checker
     return data::data_type_checker::check_sort_is_declared(x);
   }
 
+  sorts_list TypeListsIntersect(const sorts_list& TypeListList1, const sorts_list& TypeListList2)
+  {
+    // returns the intersection of the 2 type list lists
+    sorts_list Result;
+
+    for (auto i=TypeListList2.begin(); i!=TypeListList2.end(); ++i)
+    {
+      const data::sort_expression_list TypeList2= *i;
+      if (InTypesL(TypeList2,TypeListList1))
+      {
+        Result.push_front(TypeList2);
+      }
+    }
+    return atermpp::reverse(Result);
+  }
+
   /** \brief     Type check a data expression.
    *  Throws a mcrl2::runtime_error exception if the expression is not well typed.
    *  \param[in] x A data expression that has not been type checked.
@@ -86,6 +102,54 @@ struct data_expression_typechecker: protected data::data_type_checker
   }
 };
 
+bool MActEq(core::identifier_string_list MAct1, core::identifier_string_list MAct2);
+
+inline
+bool MActIn(core::identifier_string_list MAct, action_name_multiset_list MActs)
+{
+  //returns true if MAct is in MActs
+  for (action_name_multiset_list::const_iterator i=MActs.begin(); i!=MActs.end(); ++i)
+    if (MActEq(MAct,i->names()))
+    {
+      return true;
+    }
+
+  return false;
+}
+
+inline
+bool MActEq(core::identifier_string_list MAct1, core::identifier_string_list MAct2)
+{
+  //returns true if the two multiactions are equal.
+  if (MAct1.size()!=MAct2.size())
+  {
+    return false;
+  }
+  if (MAct1.empty())
+  {
+    return true;
+  }
+  core::identifier_string Act1=MAct1.front();
+  MAct1=MAct1.tail();
+
+  //remove Act1 once from MAct2. if not there -- return ATfalse.
+  core::identifier_string_list NewMAct2;
+  for (; !MAct2.empty(); MAct2=MAct2.tail())
+  {
+    core::identifier_string Act2=MAct2.front();
+    if (Act1==Act2)
+    {
+      MAct2=atermpp::reverse(NewMAct2)+MAct2.tail();
+      return MActEq(MAct1,MAct2);
+    }
+    else
+    {
+      NewMAct2.push_front(Act2);
+    }
+  }
+  return false;
+}
+
 struct typecheck_builder: public process_expression_builder<typecheck_builder>
 {
   typedef process_expression_builder<typecheck_builder> super;
@@ -94,15 +158,79 @@ struct typecheck_builder: public process_expression_builder<typecheck_builder>
   data_expression_typechecker& m_data_typechecker;
   std::map<core::identifier_string, data::sort_expression> m_variables;
   const std::map<core::identifier_string, data::sort_expression_list>& m_equation_sorts;
+  const std::map<core::identifier_string, sorts_list>& m_actions;
 
   typecheck_builder(data_expression_typechecker& data_typechecker,
                     const std::map<core::identifier_string, data::sort_expression>& variables,
-                    const std::map<core::identifier_string, data::sort_expression_list>& equation_sorts
+                    const std::map<core::identifier_string, data::sort_expression_list>& equation_sorts,
+                    const std::map<core::identifier_string, sorts_list>& actions
                    )
     : m_data_typechecker(data_typechecker),
       m_variables(variables),
-      m_equation_sorts(equation_sorts)
+      m_equation_sorts(equation_sorts),
+      m_actions(actions)
   {}
+
+  const sorts_list& action_sorts(const core::identifier_string& x)
+  {
+    auto i = m_actions.find(x);
+    assert(i != m_actions.end());
+    return i->second;
+  }
+
+  void check_action_declared(const core::identifier_string& a, const process_expression& x)
+  {
+    if (m_actions.find(a) == m_actions.end())
+    {
+      throw mcrl2::runtime_error("Undefined action " + core::pp(a) + " (typechecking " + core::pp(x) + ")");
+    }
+  }
+
+  void check_actions_declared(const core::identifier_string_list& act_list, const process_expression& x)
+  {
+    std::set<core::identifier_string> Acts;
+    for (const core::identifier_string& a: act_list)
+    {
+      check_action_declared(a, x);
+      if (!Acts.insert(a).second)  // The action was already in the set.
+      {
+        mCRL2log(log::warning) << "Used action " << a << " twice (typechecking " << x << ")" << std::endl;
+      }
+    }
+  }
+
+  template <typename Container>
+  void check_not_empty(const Container& c, const std::string& msg, const process_expression& x)
+  {
+    if (c.empty())
+    {
+      mCRL2log(log::warning) << msg << " (typechecking " << x << ")" << std::endl;
+    }
+  }
+
+  template <typename T>
+  void check_not_equal(const T& first, const T& second, const std::string& msg, const process_expression& x)
+  {
+    if (first == second)
+    {
+      mCRL2log(log::warning) << msg << " " << first << "(typechecking " << x << ")" << std::endl;
+    }
+  }
+
+  bool has_empty_intersection(const sorts_list& s1, const sorts_list& s2)
+  {
+    std::set<data::sort_expression_list> v1(s1.begin(), s1.end());
+    std::set<data::sort_expression_list> v2(s2.begin(), s2.end());
+    return utilities::detail::has_empty_intersection(v1, v2);
+  }
+
+  void check_rename_common_type(const core::identifier_string& a, const core::identifier_string& b, const process_expression& x)
+  {
+    if (has_empty_intersection(action_sorts(a), action_sorts(b)))
+    {
+      throw mcrl2::runtime_error("renaming action " + core::pp(a) + " into action " + core::pp(b) + ": these two have no common type (typechecking " + process::pp(x) + ")");
+    }
+  }
 
   process_expression apply(const untyped_parameter_identifier& x)
   {
@@ -112,30 +240,220 @@ struct typecheck_builder: public process_expression_builder<typecheck_builder>
 
   process_expression apply(const process::hide& x)
   {
-//    const core::identifier_string_list& act_list = x.hide_set();
-//    if (act_list.empty())
-//    {
-//      mCRL2log(log::warning) << "Hiding empty set of actions (typechecking " << x << ")" << std::endl;
-//    }
-//
-//    std::set<core::identifier_string> Acts;
-//    for (const core::identifier_string& a: act_list)
-//    {
-//      //Actions must be declared
-//      if (m_actions.count(a) == 0)
-//      {
-//        throw mcrl2::runtime_error("Hiding an undefined action " + core::pp(a) + " (typechecking " + core::pp(x) + ")");
-//      }
-//      if (!Acts.insert(a).second)  // The action was already in the set.
-//      {
-//        mCRL2log(log::warning) << "Hiding action " << a << " twice (typechecking " << x << ")" << std::endl;
-//      }
-//    }
-//    return hide(act_list, (*this)(x.operand()));
+    check_not_empty(x.hide_set(), "Hiding empty set of actions", x);
+    check_actions_declared(x.hide_set(), x);
+    return process::hide(x.hide_set(), (*this).apply(x.operand()));
+  }
+
+  process_expression apply(const process::block& x)
+  {
+    check_not_empty(x.block_set(), "Blocking empty set of actions", x);
+    check_actions_declared(x.block_set(), x);
+    return block(x.block_set(), (*this).apply(x.operand()));
+  }
+
+  process_expression apply(const process::rename& x)
+  {
+    check_not_empty(x.rename_set(), "Renaming empty set of actions", x);
+
+    std::set<core::identifier_string> actions;
+    for (const rename_expression& r: x.rename_set())
+    {
+      check_not_equal(r.source(), r.target(), "Renaming action into itself:", x);
+      check_action_declared(r.source(), x);
+      check_action_declared(r.target(), x);
+      check_rename_common_type(r.source(), r.target(), x);
+      if (!actions.insert(r.source()).second) // The element was already in the set.
+      {
+        throw mcrl2::runtime_error("renaming action " + core::pp(r.source()) + " twice (typechecking " + process::pp(x) + ")");
+      }
+    }
+    return rename(x.rename_set(), (*this).apply(x.operand()));
+  }
+
+  //comm: like renaming multiactions (with the same parameters) to action/tau
+  process_expression apply(const process::comm& x)
+  {
+    check_not_empty(x.comm_set(), "synchronizing empty set of (multi)actions", x);
+    if (!x.comm_set().empty())
+    {
+      core::identifier_string_list ActsFrom;
+
+      for (const communication_expression& c: x.comm_set())
+      {
+        const core::identifier_string_list& cnames = c.action_name().names();
+        assert(!cnames.empty());
+
+        if (cnames.size() == 1)
+        {
+          throw mcrl2::runtime_error("using synchronization as renaming/hiding of action " + core::pp(cnames.front()) + " into " + core::pp(c.name()) + " (typechecking " + process::pp(x) + ")");
+        }
+
+        //Actions must be declared
+        sorts_list ResTypes;
+        if (!core::is_nil(c.name()))
+        {
+          const std::map<core::identifier_string,sorts_list >::const_iterator j=m_actions.find(c.name());
+          if (j == m_actions.end())
+          {
+            throw mcrl2::runtime_error("synchronizing to an undefined action " + core::pp(c.name()) + " (typechecking " + process::pp(x) + ")");
+          }
+          ResTypes = j->second;
+        }
+
+        for (const core::identifier_string& Act: cnames)
+        {
+          auto j = m_actions.find(Act);
+          sorts_list Types;
+          if (j == m_actions.end())
+          {
+            throw mcrl2::runtime_error("synchronizing an undefined action " + core::pp(Act) + " in (multi)action " + core::pp(cnames) + " (typechecking " + process::pp(x) + ")");
+          }
+          Types = j->second;
+          ResTypes = m_data_typechecker.TypeListsIntersect(ResTypes, Types);
+          if (ResTypes.empty())
+          {
+            throw mcrl2::runtime_error("synchronizing action " + core::pp(Act) + " from (multi)action " + core::pp(cnames) +
+                              " into action " + core::pp(c.name()) + ": these have no common type (typechecking " + process::pp(x) + ")");
+          }
+        }
+
+        //the multiactions in the lhss of comm should not intersect.
+        //make the list of unique actions
+        core::identifier_string_list Acts;
+        for (const core::identifier_string& Act: cnames)
+        {
+          if (std::find(Acts.begin(), Acts.end(), Act) == Acts.end())
+          {
+            Acts.push_front(Act);
+          }
+        }
+        for (const core::identifier_string& Act: Acts)
+        {
+          if (std::find(ActsFrom.begin(), ActsFrom.end(), Act) != ActsFrom.end())
+          {
+            throw mcrl2::runtime_error("synchronizing action " + core::pp(Act) + " in different ways (typechecking " + process::pp(x) + ")");
+          }
+          else
+          {
+            ActsFrom.push_front(Act);
+          }
+        }
+      }
+    }
+    return comm(x.comm_set(), (*this).apply(x.operand()));
+  }
+
+  process_expression apply(const process::allow& x)
+  {
+    if (x.allow_set().empty())
+    {
+      mCRL2log(log::warning) << "allowing empty set of (multi) actions (typechecking " << x << ")" << std::endl;
+    }
+    else
+    {
+      action_name_multiset_list MActs;
+      for (const action_name_multiset& A: x.allow_set())
+      {
+        const core::identifier_string_list& MAct = A.names();
+
+        //Actions must be declared
+        for (const core::identifier_string& Act: MAct)
+        {
+          if (m_actions.count(Act)==0)
+          {
+            throw mcrl2::runtime_error("allowing an undefined action " + core::pp(Act) + " in (multi)action " + core::pp(MAct) + " (typechecking " + process::pp(x) + ")");
+          }
+        }
+        if (MActIn(MAct,MActs))
+        {
+          mCRL2log(log::warning) << "allowing (multi)action " << MAct << " twice (typechecking " << x << ")" << std::endl;
+        }
+        else
+        {
+          MActs.push_front(MAct);
+        }
+      }
+    }
+    return allow(x.allow_set(), (*this).apply(x.operand()));
+  }
+
+  process_expression apply(const process::at& x)
+  {
+    // const process_expression NewProc=TraverseActProcVarConstP(Vars,t.operand());
+    // data::data_expression Time=t.time_stamp();
+    // const data::sort_expression NewType=TraverseVarConsTypeD(Vars,Vars,Time,ExpandNumTypesDown(data::sort_real::real_()));
+    //
+    // data::sort_expression temp;
+    // if (!TypeMatchA(data::sort_real::real_(),NewType,temp))
+    // {
+    //   //upcasting
+    //   data::sort_expression CastedNewType;
+    //   try
+    //   {
+    //     std::map<core::identifier_string,data::sort_expression> dummy_table;
+    //     CastedNewType=UpCastNumericType(data::sort_real::real_(),NewType,Time,Vars,Vars,dummy_table,false);
+    //   }
+    //   catch (mcrl2::runtime_error &e)
+    //   {
+    //     throw mcrl2::runtime_error(std::string(e.what()) + "\ncannot (up)cast time value " + data::pp(Time) + " to type Real");
+    //   }
+    // }
+    //
+    // return at(NewProc,Time);
     return x;
   }
 
+  process_expression apply(const process::if_then& x)
+  {
+    data::data_expression condition = m_data_typechecker(x.condition(), data::sort_bool::bool_(), m_variables);
+    return if_then(condition, (*this).apply(x.then_case()));
+  }
 
+  process_expression apply(const process::if_then_else& x)
+  {
+    data::data_expression condition = m_data_typechecker(x.condition(), data::sort_bool::bool_(), m_variables);
+    return if_then_else(condition, (*this).apply(x.then_case()), (*this).apply(x.else_case()));
+  }
+
+  process_expression apply(const process::sum& x)
+  {
+    try
+    {
+      auto m_variables_copy = m_variables;
+      for (const data::variable& v: x.variables())
+      {
+        m_variables[v.name()] = v.sort();
+      }
+      process_expression operand = (*this).apply(x.operand());
+      m_variables = m_variables_copy;
+      return sum(x.variables(), operand);
+    }
+    catch (mcrl2::runtime_error& e)
+    {
+      throw mcrl2::runtime_error(std::string(e.what()) + "\nwhile typechecking " + process::pp(x));
+    }
+  }
+
+  process_expression apply(const process::stochastic_operator& x)
+  {
+    try
+    {
+      auto m_variables_copy = m_variables;
+      for (const data::variable& v: x.variables())
+      {
+        m_variables[v.name()] = v.sort();
+      }
+      data::data_expression distribution = m_data_typechecker(x.distribution(), data::sort_real::real_(), m_variables);
+      process_expression operand = (*this).apply(x.operand());
+      m_variables = m_variables_copy;
+      return stochastic_operator(x.variables(), distribution, operand);
+    }
+    catch (mcrl2::runtime_error& e)
+    {
+      throw mcrl2::runtime_error(std::string(e.what()) + "\nwhile typechecking " + process::pp(x));
+    }
+  }
 
 //  process_expression apply(const data::data_expression& x)
 //  {
@@ -158,10 +476,11 @@ inline
 typecheck_builder make_typecheck_builder(
                     data_expression_typechecker& data_typechecker,
                     const std::map<core::identifier_string, data::sort_expression>& variables,
-                    const std::map<core::identifier_string, data::sort_expression_list>& equation_sorts
+                    const std::map<core::identifier_string, data::sort_expression_list>& equation_sorts,
+                    const std::map<core::identifier_string, sorts_list>& actions
                    )
 {
-  return typecheck_builder(data_typechecker, variables, equation_sorts);
+  return typecheck_builder(data_typechecker, variables, equation_sorts, actions);
 }
 
 } // namespace detail
