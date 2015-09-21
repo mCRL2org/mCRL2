95fa3d1c4c15ba61aa9e4637cb0a3ef44d00edb3
Wieger Wesselink <j.w.wesselink@tue.nl>
2015-09-21 10:10:15 +0000

    - Wrote some python functions to generate missing files (downloads-release.inc / downloads-nightly.inc), and to apply substitutions on these files.
    - Fixed the path to the mCRL2 syntax file in the dparser module of the documentation.
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13777 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- doc/sphinx/source/__init__.py
+++ doc/sphinx/source/__init__.py
@@ -1,10 +1,26 @@
+#!/usr/bin/env python
+
+#~ Copyright 201? Sjoerd Cranen
+#~           2015 Wieger Wesselink
+#~ Distributed under the Boost Software License, Version 1.0.
+#~ (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+
 import os
+import re
 import shutil
 import subprocess
 import logging
 import sphinx
 
 _LOG = logging.getLogger('util')
+
+def read_text(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+def write_text(filename, text):
+    with open(filename, 'w') as f:
+        f.write(text)
 
 def clone_rst(src, dst):
   if os.path.isdir(src):
@@ -23,13 +39,13 @@ def clone_rst(src, dst):
 #
 def call(name, cmdline, stdin=None):
   '''Runs cmdline and pipes stdin to the process' stdin. The standard output of
-  the process is returned as a string. Any errors and warnings will be logged 
-  using the name provided. Raises a RuntimeError if the return code of the 
+  the process is returned as a string. Any errors and warnings will be logged
+  using the name provided. Raises a RuntimeError if the return code of the
   process was nonzero.'''
   _LOG.debug('Running "{0}"'.format(' '.join(cmdline)))
-  proc = subprocess.Popen(cmdline, 
-           stdin=subprocess.PIPE, 
-           stdout=subprocess.PIPE, 
+  proc = subprocess.Popen(cmdline,
+           stdin=subprocess.PIPE,
+           stdout=subprocess.PIPE,
            stderr=subprocess.PIPE)
   out, err = proc.communicate(stdin)
   if proc.returncode:
@@ -40,6 +56,27 @@ def call(name, cmdline, stdin=None):
   if err:
     _LOG.warning('{0} error output: {1}'.format(name, err.strip()))
   return out
+
+def generate_download_inc_files(temp_dev, temp_usr, version, release_tag='@MCRL2_MAJOR_VERSION@', nightly_tag='@MCRL2_VERSION@'):
+  '''Generates the files downloads-release.inc and downloads-nightly.inc. The mCRL2
+  version number is substituted.
+  '''
+  release_file_template = os.path.join(temp_usr, 'downloads-release.inc.template')
+  nightly_file_template = os.path.join(temp_usr, 'downloads-nightly.inc.template')
+  release_text = read_text(release_file_template)
+  nightly_text = read_text(nightly_file_template)
+  version1 = re.sub(r'[\.M]', '', version[1])
+  dest = '{}.{}'.format(version[0], version1)
+  release_text = re.sub(release_tag, dest, release_text)
+  nightly_text = re.sub(nightly_tag, dest, nightly_text)
+  dev_release_file = os.path.join(temp_dev, 'downloads-release.inc')
+  dev_nightly_file = os.path.join(temp_dev, 'downloads-nightly.inc')
+  usr_release_file = os.path.join(temp_usr, 'downloads-release.inc')
+  usr_nightly_file = os.path.join(temp_usr, 'downloads-nightly.inc')
+  write_text(dev_release_file, release_text)
+  write_text(dev_nightly_file, nightly_text)
+  write_text(usr_release_file, release_text)
+  write_text(usr_nightly_file, nightly_text)
 
 def generate_rst(binpath, temppath, outpath, version):
   import developer_manual
@@ -57,12 +94,13 @@ def generate_rst(binpath, temppath, outpath, version):
   clone_rst(cfg_usr, temp_usr)
   developer_manual.generate_rst(temppath, outpath)
   user_manual.generate_rst(temppath, outpath, binpath)
+  generate_download_inc_files(temp_dev, temp_usr, version)
   if '.'.join(version) == version[0]:
     color='#406756'
   else:
     color='#c1272d'
   shared_opts = ['-bhtml',
-               '-D', 'version={0}'.format('.'.join(version)), 
+               '-D', 'version={0}'.format('.'.join(version)),
                '-D', 'release={0}'.format(version[0]),
                '-D', 'html_theme_options.relbarbgcolor={0}'.format(color)]
   if _LOG.getEffectiveLevel() > logging.WARNING:
@@ -72,10 +110,10 @@ def generate_rst(binpath, temppath, outpath, version):
 
   options = shared_opts + ['-c', cfg_dev, temp_dev, out_dev]
   print '<OPTIONS>', ' '.join(options)
-  
+
   sphinx.main( shared_opts +
-               ['-c', cfg_dev, 
+               ['-c', cfg_dev,
                temp_dev, out_dev])
   sphinx.main( shared_opts +
-               ['-c', cfg_usr, 
+               ['-c', cfg_usr,
                temp_usr, out_usr])
