6216594bd5639ae2a4a45dc454c48e95c4b32a95
Xiao Qi <xiaqqiax@gmail.com>
2015-10-28 22:46:46 +0000

    Correct justification printing to only print variables whose value agree with the solution of init
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@13865 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/bes/include/mcrl2/bes/justification.h
+++ libraries/bes/include/mcrl2/bes/justification.h
@@ -33,7 +33,7 @@ namespace mcrl2
 namespace bes
 {
 
-void print_justification_tree_rec(const boolean_equation_system& b, const vector<bool>& solution, bool init_solution, const map<boolean_variable, int>& index_of, int indent, boolean_expression expr, set<boolean_variable>& visited)
+void print_justification_tree_rec(const boolean_equation_system& b, const vector<bool>& solution, bool init_solution, const map<boolean_variable, size_t>& index_of, int indent, boolean_expression expr, set<boolean_variable>& visited)
 {
   if (is_and(expr) || is_or(expr))
   {
@@ -43,19 +43,39 @@ void print_justification_tree_rec(const boolean_equation_system& b, const vector
   else if (is_boolean_variable(expr))
   {
     boolean_variable X = atermpp::down_cast<boolean_variable>(expr);
-    cout << string(indent, ' ') << X;
+
     if (visited.count(X))
     {
-      cout << '*' << endl;
+      // X has been visited before. Stop here.
+      cout << string(indent, ' ') << X << "?" << endl;
       return;
     }
     visited.insert(X);
+
     if (index_of.count(X) == 0)
     {
-      cout << "?" << endl;
+      // X not found in the left-hand-side of any equation. There is some
+      // error in the solving algorithm.
+      cout << string(indent, ' ') << X << "?" << endl;
       return;
     }
-    cout << endl;
+    size_t idx = index_of.at(X);
+    if (idx >= solution.size())
+    {
+      // X is out of the range of the solution. There is some error in the
+      // solving algorithm.
+      cout << string(indent, ' ') << X << "?" << endl;
+      return;
+    }
+
+    if (solution[idx] != init_solution)
+    {
+      // X is not relevant for the justification.
+      return;
+    }
+
+    // Print justification rooted at X recursively.
+    cout << string(indent, ' ') << X << endl;
     print_justification_tree_rec(b, solution, init_solution, index_of, indent+1, b.equations()[index_of.at(X)].formula(), visited);
   }
   else if (is_true(expr) || is_false(expr))
@@ -74,7 +94,7 @@ void print_justification_tree_rec(const boolean_equation_system& b, const vector
 void print_justification_tree(const boolean_equation_system& b, const vector<bool>& solution, bool init_solution)
 {
   set<boolean_variable> visited;
-  map<boolean_variable, int> index_of;
+  map<boolean_variable, size_t> index_of;
   for (size_t i = 0; i < b.equations().size(); i++)
   {
     index_of[b.equations()[i].variable()] = i;
