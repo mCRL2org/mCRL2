c1ede74fecdf96daef6f741192fa40c0328df0f2
Wieger Wesselink <j.w.wesselink@tue.nl>
2016-10-22 11:28:58 +0000

    - If there are no matches for a process or action expression, then for each declaration the reason for not matching is printed.
    #Fixes 1364.
    
    
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@14255 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/process/include/mcrl2/process/detail/match_action_parameters.h
+++ libraries/process/include/mcrl2/process/detail/match_action_parameters.h
@@ -12,6 +12,8 @@
 #ifndef MCRL2_PROCESS_DETAIL_MATCH_ACTION_PARAMETERS_H
 #define MCRL2_PROCESS_DETAIL_MATCH_ACTION_PARAMETERS_H
 
+#include <sstream>
+#include <tuple>
 #include "mcrl2/core/detail/print_utility.h"
 #include "mcrl2/data/typecheck.h"
 #include "mcrl2/data/detail/data_utility.h"
@@ -23,11 +25,11 @@ namespace process {
 namespace detail {
 
 inline
-std::pair<bool, data::data_expression_vector> match_action_parameters(const data::data_expression_list& parameters,
-                                                                      const data::sort_expression_list& expected_sorts,
-                                                                      const data::detail::variable_context& variable_context,
-                                                                      data::data_type_checker& typechecker
-                                                                     )
+std::tuple<bool, data::data_expression_vector, std::string> match_action_parameters(const data::data_expression_list& parameters,
+                                                                                    const data::sort_expression_list& expected_sorts,
+                                                                                    const data::detail::variable_context& variable_context,
+                                                                                    data::data_type_checker& typechecker
+                                                                                   )
 {
   data::data_expression_vector result;
   auto i = parameters.begin();
@@ -38,12 +40,12 @@ std::pair<bool, data::data_expression_vector> match_action_parameters(const data
     {
       result.push_back(typechecker.typecheck_data_expression(*i, *j, variable_context));
     }
-    catch (mcrl2::runtime_error&)
+    catch (mcrl2::runtime_error& e)
     {
-      return { false, {} };
+      return std::make_tuple(false, data::data_expression_vector(), std::string(e.what()));
     }
   }
-  return { true, result };
+  return std::make_tuple(true, result, "");
 }
 
 inline
@@ -56,17 +58,39 @@ std::pair<data::data_expression_list, data::sort_expression_list> match_action_p
                                                                                          )
 {
   std::vector<data::data_expression_vector> matches;
+  std::vector<std::string> match_errors;
   for (const data::sort_expression_list& sorts: possible_parameter_sorts)
   {
     auto p = match_action_parameters(parameters, sorts, variable_context, typechecker);
-    if (p.first)
+    if (std::get<0>(p))
     {
-      matches.push_back(p.second);
+      matches.push_back(std::get<1>(p));
+    }
+    else
+    {
+      match_errors.push_back(std::get<2>(p));
     }
   }
   if (matches.empty())
   {
-    throw mcrl2::runtime_error("Could not find a matching " + msg + " declaration for " + core::pp(name) + core::detail::print_arguments(parameters) + ".");
+    std::ostringstream out;
+    out << "Could not find a matching " << msg << " declaration for " << name << core::detail::print_arguments(parameters) << ".";
+    auto i = possible_parameter_sorts.begin();
+    auto j = match_errors.begin();
+    for (; i != possible_parameter_sorts.end(); ++i, ++j)
+    {
+      out << "\n" << name << ": ";
+      for (auto k = i->begin(); k != i->end(); ++k)
+      {
+        if (k != i->begin())
+        {
+          out << " # ";
+        }
+        out << *k;
+      }
+      out << " does not match due to " << *j;
+    }
+    throw mcrl2::runtime_error(out.str());
   }
   if (matches.size() > 1)
   {
