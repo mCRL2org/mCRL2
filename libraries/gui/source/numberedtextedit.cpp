bcd0063d211c245e28f14eb1d51c1653555eb681
Jan Friso Groote <j.f.groote@tue.nl>
2016-11-29 19:37:58 +0000

    Changed the tab width of mcrl2xi from 80 to 20 as requested in bugreport #1333.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@14336 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/gui/source/numberedtextedit.cpp
+++ libraries/gui/source/numberedtextedit.cpp
@@ -17,101 +17,103 @@ using namespace mcrl2::gui::qt;
 
 class LineNumberArea : public QWidget
 {
-public:
-    LineNumberArea(NumberedTextEdit *editor) : QWidget(editor) {
+  public:
+    LineNumberArea(NumberedTextEdit *editor) : QWidget(editor) 
+    {
         m_edit = editor;
         setStyleSheet("Background: #DDD;");
         setAutoFillBackground(true);
     }
 
-    QSize sizeHint() const {
+    QSize sizeHint() const 
+    {
         return QSize(m_edit->lineNumberAreaWidth(), 0);
     }
 
-protected:
+  protected:
     void paintEvent(QPaintEvent *event)
     {
         m_edit->lineNumberAreaPaintEvent(event);
     }
 
-private:
+  private:
     NumberedTextEdit* m_edit;
 };
 
-NumberedTextEdit::NumberedTextEdit(QWidget *parent) :
-    QTextEdit(parent)
+NumberedTextEdit::NumberedTextEdit(QWidget *parent) : QTextEdit(parent)
 {
-    m_numberArea = new LineNumberArea(this);
-    connect(document(), SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth()));
+  setTabStopWidth(20);   // Limit the size of tab distances. The default of 80 is rather large. 
+  m_numberArea = new LineNumberArea(this);
+  connect(document(), SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth()));
 
-    updateLineNumberAreaWidth();
+  updateLineNumberAreaWidth();
 }
 
 int NumberedTextEdit::lineNumberAreaWidth()
 {
-    int blocks = document()->blockCount();
-    int digits = 1;
-    while (blocks >= 10)
-    {
-        blocks /= 10;
-        digits++;
-    }
-    return (5+fontMetrics().width('9')*digits);
+  int blocks = document()->blockCount();
+  int digits = 1;
+  while (blocks >= 10)
+  {
+      blocks /= 10;
+      digits++;
+  }
+  return (5+fontMetrics().width('9')*digits);
 }
 
 void NumberedTextEdit::lineNumberAreaPaintEvent(QPaintEvent * /*event*/)
 {
-    QPainter painter(m_numberArea);
-    painter.setPen(Qt::black);
+  QPainter painter(m_numberArea);
+  painter.setPen(Qt::black);
 
-    QTextBlock block = document()->firstBlock();
-    int line = 1;
-    int offset = verticalScrollBar()->value();
-    int lineheight = fontMetrics().height();
+  QTextBlock block = document()->firstBlock();
+  int line = 1;
+  int offset = verticalScrollBar()->value();
+  int lineheight = fontMetrics().height();
 
-    while (block.isValid())
+  while (block.isValid())
+  {
+    QPointF pos = document()->documentLayout()->blockBoundingRect(block).topLeft();
+
+    // Stop if we have done the last visible line
+    if (pos.y() > offset+viewport()->height())
     {
-        QPointF pos = document()->documentLayout()->blockBoundingRect(block).topLeft();
-
-        // Stop if we have done the last visible line
-        if (pos.y() > offset+viewport()->height())
-        {
-            break;
-        }
-
-        // Only draw if the line is visible
-        if (pos.y()+lineheight > offset)
-        {
-            painter.drawText(0, pos.y()-offset, m_numberArea->width()-3, lineheight, Qt::AlignRight, QString::number(line));
-        }
-
-        block = block.next();
-        line++;
+        break;
     }
+
+    // Only draw if the line is visible
+    if (pos.y()+lineheight > offset)
+    {
+        painter.drawText(0, pos.y()-offset, m_numberArea->width()-3, lineheight, Qt::AlignRight, QString::number(line));
+    }
+
+    block = block.next();
+    line++;
+  }
 }
 
 void NumberedTextEdit::updateLineNumberAreaWidth()
 {
-    // Offset the contents
-    setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+  // Offset the contents
+  setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
 
-    // And update the line number area
-    m_numberArea->repaint();
+  // And update the line number area
+  m_numberArea->repaint();
 }
 
 void NumberedTextEdit::resizeEvent(QResizeEvent *e)
 {
-    QTextEdit::resizeEvent(e);
+  QTextEdit::resizeEvent(e);
 
-    // Resize the line number area
-    QRect cr = contentsRect();
-    m_numberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+  // Resize the line number area
+  QRect cr = contentsRect();
+  m_numberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
 }
 
 void NumberedTextEdit::scrollContentsBy(int dx, int dy)
 {
-    QTextEdit::scrollContentsBy(dx, dy);
+  QTextEdit::scrollContentsBy(dx, dy);
 
-    // Update the line number area
-    m_numberArea->repaint();
+  // Update the line number area
+  m_numberArea->repaint();
 }
