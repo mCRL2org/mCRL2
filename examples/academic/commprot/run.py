4e026fc6c151d9665a765e0fb23c0f72c03ae75d
Jan Friso Groote <j.f.groote@tue.nl>
2017-05-06 08:48:48 +0000

    Made the commprot example, which has a large, and maybe unbounded statespace, run within finite time.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@14772 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- examples/academic/commprot/run.py
+++ examples/academic/commprot/run.py
@@ -1,7 +1,13 @@
 import os
-# The generated state space is large. This run is time consuming. 
+# The generated state space is huge, well over 10^8 states. Explicit verification of the state
+# space is therefore not possible. One can use highway search, using e.g. --todo-max=1000,
+# searching with 10000 lanes through the state space for deadlocks. The total number of 
+# states to be explored is limited to 100000 in this example. 
 os.system('mcrl22lps -v commprot.mcrl2 commprot.lps')
-os.system('lps2pbes -v -f -nodeadlock.mcf commprot.lps commprot.nodeadlock.pbes')
-os.system('pbes2bool -v -s2 -rjittyc commprot.nodeadlock.pbes')
+os.system('lps2lts -D --cached --todo-max=1000 -l100000 -v commprot.lps')
+# Checking the properties below does not work on an ordinary computer, given 
+# the size of the state space. 
+# os.system('lps2pbes -v -fnodeadlock.mcf commprot.lps commprot.nodeadlock.pbes')
+# os.system('pbes2bool -v -s2 -rjittyc commprot.nodeadlock.pbes')
 
 
