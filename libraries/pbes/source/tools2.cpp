9103cdcac5b1a6a2b1b0d159dc60aefe51d3064b
Wieger Wesselink <j.w.wesselink@tue.nl>
2017-06-29 09:47:43 +0000

    - Since lpsbisim2pbes has become a release tool, it should no longer be compiled in experimental_tools.cpp. Approved by Thomas.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@14959 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/source/tools2.cpp
+++ libraries/pbes/source/tools2.cpp
@@ -12,5 +12,6 @@
 #include "mcrl2/pbes/tools.h"
 
 #include "mcrl2/pbes/tools/lps2pbes.h"
+#include "mcrl2/pbes/tools/lpsbisim2pbes.h"
 #include "mcrl2/pbes/tools/pbesparelm.h"
 #include "mcrl2/pbes/tools/pbesstategraph.h"
