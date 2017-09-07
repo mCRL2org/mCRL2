7f76955900833e659b9d881423f23f3ee98db81d
Jan Friso Groote <j.f.groote@tue.nl>
2017-09-07 18:13:23 +0000

    Added a formula expressing that the first player always has a winning strategy in tic-tac-toe.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15039 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- examples/games/tictactoe/run.py
+++ examples/games/tictactoe/run.py
@@ -1,6 +1,8 @@
 import os
 
 os.system('mcrl22lps -v tictactoe.mcrl2 tictactoe.lps')
-os.system('lps2lts -v -rjittyc tictactoe.lps tictactoe.fsm')
+os.system('lps2lts -v -rjittyc tictactoe.lps tictactoe.lts')
+os.system('lps2pbes -vm -fhas_player_cross_a_winning_strategy.mcf tictactoe.lps tictactoe.pbes')
+os.system('pbes2bool -v -rjittyc tictactoe.pbes')
 
 
