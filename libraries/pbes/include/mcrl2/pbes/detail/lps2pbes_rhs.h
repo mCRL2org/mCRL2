888e55a190e7260537c56b68d62f37158055fdf7
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-02-23 10:15:57 +0000

    - Encode the number of action parameters in the names of the counter example variables.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15194 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/detail/lps2pbes_rhs.h
+++ libraries/pbes/include/mcrl2/pbes/detail/lps2pbes_rhs.h
@@ -151,10 +151,12 @@ struct lps2pbes_counter_example_parameters: public lps2pbes_parameters
     for (const lps::action_summand& summand: lps.action_summands())
     {
       const lps::multi_action& ai = summand.multi_action();
-      core::identifier_string pos = id_generator("Z" + multi_action_name(ai) + "_pos");
-      core::identifier_string neg = id_generator("Z" + multi_action_name(ai) + "_neg");
-      Zpos[ai] = propositional_variable(pos, d + action_variables(ai.actions()) + d1);
-      Zneg[ai] = propositional_variable(neg, d + action_variables(ai.actions()) + d1);
+      data::variable_list actvars = action_variables(ai.actions());
+      core::identifier_string pos = id_generator("Zneg_" + utilities::number2string(actvars.size()) +  "_" + multi_action_name(ai));
+      core::identifier_string neg = id_generator("Zpos_" + utilities::number2string(actvars.size()) +  "_" + multi_action_name(ai));
+
+      Zpos[ai] = propositional_variable(pos, d + actvars + d1);
+      Zneg[ai] = propositional_variable(neg, d + actvars + d1);
     }
   }
 
