20fb7933cb7f58a53c6a868c7a86b4be927193dc
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-03-02 11:39:34 +0000

    - Factored out the code to load an LTS file in a tool, to make this code reusable, and to avoid pollution of the tools that rely on it.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15221 21252fe5-60e8-0310-8ff0-c1ae1001594e

---
+++ libraries/lts/include/mcrl2/lts/detail/lts_load.h
@@ -0,0 +1,106 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://svn.win.tue.nl/trac/MCRL2/browser/trunk/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/lts/detail/lts_load.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_LTS_DETAIL_LTS_LOAD_H
+#define MCRL2_LTS_DETAIL_LTS_LOAD_H
+
+#include "mcrl2/lts/lts_io.h"
+#include "mcrl2/lts/lts_lts.h"
+#include "mcrl2/utilities/tool.h"
+
+namespace mcrl2 {
+
+namespace lts {
+
+namespace detail {
+
+inline
+void add_options(utilities::interface_description& desc)
+{
+  desc.add_option("data", utilities::make_file_argument("FILE"),
+                  "use FILE as the data and action specification. "
+                  "FILE must be a .mcrl2 file which does not contain an init clause. ", 'D');
+
+  desc.add_option("lps", utilities::make_file_argument("FILE"),
+                  "use FILE for the data and action specification. "
+                  "FILE must be a .lps file. ", 'l');
+
+  desc.add_option("mcrl2", utilities::make_file_argument("FILE"),
+                  "use FILE as the data and action specification for the LTS. "
+                  "FILE must be a .mcrl2 file. ", 'm');
+}
+
+// Load an LTS from ltsfilename, with additional files specified in the command line parser.
+// TODO: this incomprehensible interface for loading LTSs needs improvement.
+inline
+void load_lts(const utilities::command_line_parser& parser, const std::string& ltsfilename, lts_lts_t& result)
+{
+  data_file_type_t data_file_type = data_file_type_t::none_e;
+  std::string data_file;
+
+  if (parser.options.count("data"))
+  {
+    if (1 < parser.options.count("data"))
+    {
+      mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
+    }
+    data_file_type = data_file_type_t::data_e;
+    data_file = parser.option_argument("data");
+  }
+
+  if (parser.options.count("lps"))
+  {
+    if (1 < parser.options.count("lps") || data_file_type != data_file_type_t::none_e)
+    {
+      mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
+    }
+
+    data_file_type = data_file_type_t::lps_e;
+    data_file = parser.option_argument("lps");
+  }
+
+  if (parser.options.count("mcrl2"))
+  {
+    if (1 < parser.options.count("mcrl2") || data_file_type != data_file_type_t::none_e)
+    {
+      mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
+    }
+
+    data_file_type = data_file_type_t::mcrl2_e;
+    data_file = parser.option_argument("mcrl2");
+  }
+
+  lts_type input_type = detail::guess_format(ltsfilename);
+  load_lts(result, ltsfilename, input_type, data_file_type, data_file);
+}
+
+// extracts a specification from an LTS
+inline
+lps::specification extract_specification(const lts_lts_t& l)
+{
+  lps::action_summand_vector action_summands;
+  data::variable process_parameter("x", data::sort_pos::pos());
+  data::variable_list process_parameters({ process_parameter });
+  std::set<data::variable> global_variables;
+  // Add a single delta.
+  lps::deadlock_summand_vector deadlock_summands(1, lps::deadlock_summand(data::variable_list(), data::sort_bool::true_(), lps::deadlock()));
+  lps::linear_process lps(process_parameters, deadlock_summands, lps::action_summand_vector());
+  lps::process_initializer initial_process(data::assignment_list({ data::assignment(process_parameter, data::sort_pos::pos(l.initial_state() + 1)) }));
+  return lps::specification(l.data(), l.action_labels(), global_variables, lps, initial_process);
+}
+
+} // namespace detail
+
+} // namespace lts
+
+} // namespace mcrl2
+
+#endif // MCRL2_LTS_DETAIL_LTS_LOAD_H
--- tools/release/lts2pbes/lts2pbes.cpp
+++ tools/release/lts2pbes/lts2pbes.cpp
@@ -14,17 +14,12 @@
 
 #include <fstream>
 #include <iostream>
-#include <string>
 #include "mcrl2/bes/pbes_output_tool.h"
-#include "mcrl2/lts/lts_io.h"
+#include "mcrl2/lts/detail/lts_load.h"
 #include "mcrl2/modal_formula/algorithms.h"
 #include "mcrl2/modal_formula/parse.h"
-#include "mcrl2/modal_formula/state_formula.h"
-#include "mcrl2/pbes/io.h"
 #include "mcrl2/pbes/lts2pbes.h"
-#include "mcrl2/utilities/text_utility.h"
 #include "mcrl2/utilities/input_output_tool.h"
-#include "mcrl2/utilities/logger.h"
 
 using namespace mcrl2;
 using namespace mcrl2::utilities;
@@ -38,13 +33,10 @@ class lts2pbes_tool : public pbes_output_tool<input_output_tool>
 
   protected:
 
-    std::string infilename;
-    std::string outfilename;
     std::string formfilename;
-    mcrl2::lts::data_file_type_t data_file_type;
-    std::string data_file;
     bool preprocess_modal_operators;
     bool generate_counter_example;
+    lts::lts_lts_t l;
 
     void add_options(interface_description& desc)
     {
@@ -58,55 +50,12 @@ class lts2pbes_tool : public pbes_output_tool<input_output_tool>
 
       desc.add_hidden_option("counter-example",
                              "add counter example equations to the generated PBES", 'c');
-
-      desc.add_option("data", make_file_argument("FILE"),
-                      "use FILE as the data and action specification. "
-                      "FILE must be a .mcrl2 file which does not contain an init clause. ", 'D');
-
-      desc.add_option("lps", make_file_argument("FILE"),
-                      "use FILE for the data and action specification. "
-                      "FILE must be a .lps file. ", 'l');
-
-      desc.add_option("mcrl2", make_file_argument("FILE"),
-                      "use FILE as the data and action specification for the LTS. "
-                      "FILE must be a .mcrl2 file. ", 'm');
+      lts::detail::add_options(desc);
     }
 
     void parse_options(const command_line_parser& parser)
     {
       super::parse_options(parser);
-
-      if (parser.options.count("data"))
-      {
-        if (1 < parser.options.count("data"))
-        {
-          mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
-        }
-        data_file_type = mcrl2::lts::data_file_type_t::data_e;
-        data_file = parser.option_argument("data");
-      }
-
-      if (parser.options.count("lps"))
-      {
-        if (1 < parser.options.count("lps") || data_file_type != mcrl2::lts::data_file_type_t::none_e)
-        {
-          mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
-        }
-
-        data_file_type = mcrl2::lts::data_file_type_t::lps_e;
-        data_file = parser.option_argument("lps");
-      }
-
-      if (parser.options.count("mcrl2"))
-      {
-        if (1 < parser.options.count("mcrl2") || data_file_type != mcrl2::lts::data_file_type_t::none_e)
-        {
-          mCRL2log(log::warning) << "multiple data specification files are specified; can only use one.\n";
-        }
-
-        data_file_type = mcrl2::lts::data_file_type_t::mcrl2_e;
-        data_file = parser.option_argument("mcrl2");
-      }
 
       if (parser.options.count("formula"))
       {
@@ -115,9 +64,7 @@ class lts2pbes_tool : public pbes_output_tool<input_output_tool>
 
       preprocess_modal_operators = parser.options.count("preprocess-modal-operators") > 0;
       generate_counter_example = parser.options.count("counter-example") > 0;
-
-      infilename  = input_filename();
-      outfilename = output_filename();
+      lts::detail::load_lts(parser, input_filename(), l);
     }
 
   public:
@@ -128,35 +75,13 @@ class lts2pbes_tool : public pbes_output_tool<input_output_tool>
         "translates an LTS into an PBES",
         "Translates an LTS in INFILE and writes the resulting PBES to "
         "OUTFILE. If OUTFILE is not present, standard output is used. If INFILE is not "
-        "present, standard input is used."),
-      data_file_type(mcrl2::lts::data_file_type_t::none_e)
+        "present, standard input is used.")
     {}
-
-  protected:
-
-    // extracts a specification from an LTS
-    lps::specification extract_specification(const lts::lts_lts_t& l)
-    {
-      lps::action_summand_vector action_summands;
-      data::variable process_parameter("x", data::sort_pos::pos());
-      data::variable_list process_parameters({ process_parameter });
-      std::set<data::variable> global_variables;
-      // Add a single delta.
-      lps::deadlock_summand_vector deadlock_summands(1, lps::deadlock_summand(data::variable_list(), data::sort_bool::true_(), lps::deadlock()));
-      lps::linear_process lps(process_parameters, deadlock_summands, lps::action_summand_vector());
-      lps::process_initializer initial_process(data::assignment_list({ data::assignment(process_parameter, data::sort_pos::pos(l.initial_state() + 1)) }));
-      return lps::specification(l.data(), l.action_labels(), global_variables, lps, initial_process);
-    }
 
   public:
     bool run()
     {
-      const lts::lts_type input_type = lts::detail::guess_format(infilename);
-      lts::lts_lts_t l;
-      load_lts(l, infilename, input_type, data_file_type, data_file);
-
-      //load formula file
-      lps::specification lpsspec = extract_specification(l);
+      lps::specification lpsspec = lts::detail::extract_specification(l);
       std::ifstream from(formfilename.c_str());
       if (!from)
       {
