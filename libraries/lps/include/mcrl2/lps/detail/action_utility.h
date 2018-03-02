mCRL2org/mCRL2

commit 39a4607290698d7d9f417058199d6a80a5ba3142

Added a check to lps::is_well_typed() for duplicate definition of actions with different sorts
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15222 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/lps/include/mcrl2/lps/detail/action_utility.h
@@ -84,6 +84,25 @@ bool check_action_label_sorts(const process::action_label_list& action_labels, c
   return true;
 }
 
+/// \brief Check whether some action name is defined multiple times with different sorts
+/// \param actions_labels A sequence of action labels
+/// \return True if there are two action label with the same name but with different sorts
+inline
+bool check_action_label_duplicates(const process::action_label_list& actions_labels)
+{
+  std::map< core::identifier_string, data::sort_expression_list > sort_map;
+  for(const process::action_label& al: actions_labels)
+  {
+    auto find_result = sort_map.find(al.name());
+    if(find_result != sort_map.end() && find_result->second != al.sorts())
+    {
+      return false;
+    }
+    sort_map.insert(std::make_pair(al.name(), al.sorts()));
+  }
+  return true;
+}
+
 } // namespace detail
 
 } // namespace lps

libraries/lps/include/mcrl2/lps/is_well_typed.h
@@ -177,7 +177,7 @@ struct lps_well_typed_checker
   {
     if (a.has_time())
     {
-      check_time(a.time(), "deadlock");
+      check_time(a.time(), "multi_action");
     }
     return true;
   }
@@ -340,6 +340,12 @@ struct lps_well_typed_checker
     if (!(detail::check_action_label_sorts(spec.action_labels(), declared_sorts)))
     {
       error << "is_well_typed(specification) failed: some of the sorts occurring in the action labels " << lps::pp(spec.action_labels()) << " are not declared in the data specification " << data::pp(spec.data().sorts()) << std::endl;
+      return false;
+    }
+
+    if(!detail::check_action_label_duplicates(spec.action_labels()))
+    {
+      error << "is_well_typed(specification) failed: some action label in " << lps::pp(spec.action_labels()) << " is defined more than once with different sorts." << std::endl;
       return false;
     }
 

commit 7fbfcabafeeed7769fa53717f4f3fb63f9a22904

The type checking rule I added in r15222 was too strict, duplicate action names with different sorts should be allowed.
Many tests were broken by the change in r15222


git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15224 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/lps/include/mcrl2/lps/detail/action_utility.h
@@ -84,25 +84,6 @@ bool check_action_label_sorts(const process::action_label_list& action_labels, c
   return true;
 }
 
-/// \brief Check whether some action name is defined multiple times with different sorts
-/// \param actions_labels A sequence of action labels
-/// \return True if there are two action label with the same name but with different sorts
-inline
-bool check_action_label_duplicates(const process::action_label_list& actions_labels)
-{
-  std::map< core::identifier_string, data::sort_expression_list > sort_map;
-  for(const process::action_label& al: actions_labels)
-  {
-    auto find_result = sort_map.find(al.name());
-    if(find_result != sort_map.end() && find_result->second != al.sorts())
-    {
-      return false;
-    }
-    sort_map.insert(std::make_pair(al.name(), al.sorts()));
-  }
-  return true;
-}
-
 } // namespace detail
 
 } // namespace lps

libraries/lps/include/mcrl2/lps/is_well_typed.h
@@ -343,12 +343,6 @@ struct lps_well_typed_checker
       return false;
     }
 
-    if(!detail::check_action_label_duplicates(spec.action_labels()))
-    {
-      error << "is_well_typed(specification) failed: some action label in " << lps::pp(spec.action_labels()) << " is defined more than once with different sorts." << std::endl;
-      return false;
-    }
-
     // check 5)
     for (auto i = action_summands.begin(); i != action_summands.end(); ++i)
     {