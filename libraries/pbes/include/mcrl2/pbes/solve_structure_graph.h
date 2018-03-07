43d7edce2a3ed87a09b5173f5fdefd319e21c3ee
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-03-07 08:58:51 +0000

    - Made some progress towards merging the conjunctive and disjunctive parts of the Zielonka algorithm.
    git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@15239 21252fe5-60e8-0310-8ff0-c1ae1001594e

--- libraries/pbes/include/mcrl2/pbes/solve_structure_graph.h
+++ libraries/pbes/include/mcrl2/pbes/solve_structure_graph.h
@@ -39,35 +39,13 @@ bool contains(const structure_graph::vertex_set& V, const structure_graph::verte
 
 // Returns true if the vertex u satisfies the conditions for being added to the attractor set A.
 inline
-bool is_attractor_conjunctive(const structure_graph::vertex& u, const structure_graph::vertex_set& A)
+bool is_attractor(const structure_graph::vertex& u, const structure_graph::vertex_set& A, structure_graph::decoration_type decoration_self, structure_graph::decoration_type decoration_other)
 {
-  if (u.decoration != structure_graph::d_disjunction)
+  if (u.decoration != decoration_self)
   {
     return true;
   }
-  if (u.decoration != structure_graph::d_conjunction)
-  {
-    for (const structure_graph::vertex* v: u.successors)
-    {
-      if (v->enabled && !contains(A, v))
-      {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-}
-
-// Returns true if the vertex u satisfies the conditions for being added to the attractor set A.
-inline
-bool is_attractor_disjunctive(const structure_graph::vertex& u, const structure_graph::vertex_set& A)
-{
-  if (u.decoration != structure_graph::d_conjunction)
-  {
-    return true;
-  }
-  if (u.decoration != structure_graph::d_disjunction)
+  if (u.decoration != decoration_other)
   {
     for (const structure_graph::vertex* v: u.successors)
     {
@@ -83,7 +61,7 @@ bool is_attractor_disjunctive(const structure_graph::vertex& u, const structure_
 
 // Computes the conjunctive attractor set, by extending A.
 inline
-structure_graph::vertex_set compute_attractor_set_conjunctive(structure_graph::vertex_set A)
+structure_graph::vertex_set compute_attractor_set(structure_graph::vertex_set A, structure_graph::decoration_type decoration_self, structure_graph::decoration_type decoration_other)
 {
   typedef structure_graph::vertex vertex;
 
@@ -106,70 +84,10 @@ structure_graph::vertex_set compute_attractor_set_conjunctive(structure_graph::v
     todo.erase(todo.begin());
     assert(!contains(A, u));
 
-    if (is_attractor_conjunctive(*u, A))
+    if (is_attractor(*u, A, decoration_other, decoration_self))
     {
       // set strategy
-      if (u->decoration != structure_graph::d_disjunction)
-      {
-        for (const vertex* w: u->successors)
-        {
-          if (contains(A, w))
-          {
-            mCRL2log(log::debug) << "set strategy for node " << u->formula << " to " << w->formula << std::endl;
-            u->strategy = w;
-            break;
-          }
-        }
-        if (u->strategy == nullptr)
-        {
-          mCRL2log(log::debug) << "Error: no strategy for node " << u << std::endl;
-        }
-      }
-
-      A.insert(u);
-
-      for (const vertex* v: u->predecessors)
-      {
-        if (v->enabled && !contains(A, v))
-        {
-          todo.insert(v);
-        }
-      }
-    }
-  }
-
-  return A;
-}
-
-// Computes the disjunctive attractor set, by extending A.
-inline
-structure_graph::vertex_set compute_attractor_set_disjunctive(structure_graph::vertex_set A)
-{
-  typedef structure_graph::vertex vertex;
-
-  // put all predecessors of elements in A in todo
-  std::unordered_set<const vertex*> todo;
-  for (const vertex* u: A)
-  {
-    for (const vertex* v: u->predecessors)
-    {
-      if (v->enabled && !contains(A, v))
-      {
-        todo.insert(v);
-      }
-    }
-  }
-
-  while (!todo.empty())
-  {
-    const vertex* u = *todo.begin();
-    todo.erase(todo.begin());
-    assert(!contains(A, u));
-
-    if (is_attractor_disjunctive(*u, A))
-    {
-      // set strategy
-      if (u->decoration != structure_graph::d_conjunction)
+      if (u->decoration != decoration_other)
       {
         for (const vertex* w: u->successors)
         {
@@ -303,7 +221,7 @@ void log_vertex_set(const structure_graph::vertex_set& V, const std::string& nam
 
 // find a successor of u in U, or a random one if no successor in U exists
 inline
-const structure_graph::vertex* find_successor(const structure_graph::vertex* u, const structure_graph::vertex_set& U)
+const structure_graph::vertex* succ(const structure_graph::vertex* u, const structure_graph::vertex_set& U)
 {
   if (u->successors.empty())
   {
@@ -343,97 +261,60 @@ std::pair<structure_graph::vertex_set, structure_graph::vertex_set> solve_recurs
 
   if (is_empty(V))
   {
-    Wconj = vertex_set();
-    Wdisj = vertex_set();
+    return { vertex_set(), vertex_set() };
+  }
+
+  auto q = get_minmax_rank(V);
+  std::size_t m = std::get<0>(q);
+  const vertex_set& U = std::get<2>(q);
+
+  structure_graph::decoration_type decoration_self  = (m % 2 == 0) ? structure_graph::d_disjunction : structure_graph::d_conjunction;
+  structure_graph::decoration_type decoration_other = (m % 2 == 0) ? structure_graph::d_conjunction : structure_graph::d_disjunction;
+
+  // set strategy
+  for (const vertex* u: U)
+  {
+    if (u->decoration == decoration_self)
+    {
+      auto v = succ(u, U);
+      if (v)
+      {
+        u->strategy = v;
+        mCRL2log(log::debug) << "set initial strategy for node " << u->formula << " to " << u->strategy->formula << std::endl;
+      }
+    }
+  }
+
+  if (m % 2 == 0)
+  {
+    vertex_set A = compute_attractor_set(U, decoration_self, decoration_other);
+    std::tie(Wconj1, Wdisj1) = solve_recursive(V, A);
+    if (is_empty(Wconj1))
+    {
+      Wconj.clear();
+      Wdisj = set_union(A, Wdisj1);
+    }
+    else
+    {
+      vertex_set B = compute_attractor_set(Wconj1, decoration_other, decoration_self);
+      std::tie(Wconj, Wdisj) = solve_recursive(V, B);
+      Wconj = set_union(Wconj, B);
+    }
   }
   else
   {
-    auto q = get_minmax_rank(V);
-    std::size_t m = std::get<0>(q);
-    std::size_t h = std::get<1>(q);
-    const vertex_set& U = std::get<2>(q);
-    mCRL2log(log::debug) << "U = " << pp(U) << std::endl;
-
-    // set strategy
-    if (m % 2 == 0)
+    vertex_set A = compute_attractor_set(U, decoration_self, decoration_other);
+    std::tie(Wconj1, Wdisj1) = solve_recursive(V, A);
+    if (is_empty(Wdisj1))
     {
-      for (const vertex* u: U)
-      {
-        if (u->decoration == structure_graph::d_disjunction)
-        {
-          auto v = find_successor(u, U);
-          if (v)
-          {
-            u->strategy = v;
-            mCRL2log(log::debug) << "set initial strategy for node " << u->formula << " to " << u->strategy->formula << std::endl;
-          }
-        }
-      }
+      Wconj = set_union(A, Wconj1);
+      Wdisj.clear();
     }
     else
     {
-      for (const vertex* u: U)
-      {
-        if (u->decoration == structure_graph::d_conjunction)
-        {
-          auto v = find_successor(u, U);
-          if (v)
-          {
-            u->strategy = v;
-            mCRL2log(log::debug) << "set initial strategy for node " << u->formula << " to " << u->strategy->formula << std::endl;
-          }
-        }
-      }
-    }
-
-    if (h == m)
-    {
-      if (m % 2 == 0)
-      {
-        Wconj = vertex_set();
-        Wdisj = remove_disabled_vertices(V);
-      }
-      else
-      {
-        Wconj = remove_disabled_vertices(V);
-        Wdisj = vertex_set();
-      }
-    }
-    else if (m % 2 != 0)
-    {
-      vertex_set A = compute_attractor_set_conjunctive(U);
-      mCRL2log(log::debug) << "A = " << pp(A) << std::endl;
-      std::tie(Wconj1, Wdisj1) = solve_recursive(V, A);
-      if (is_empty(Wdisj1))
-      {
-        Wconj = set_union(A, Wconj1);
-        Wdisj.clear();
-      }
-      else
-      {
-        vertex_set B = compute_attractor_set_disjunctive(Wdisj1);
-        mCRL2log(log::debug) << "B = " << pp(B) << std::endl;
-        std::tie(Wconj, Wdisj) = solve_recursive(V, B);
-        Wdisj = set_union(Wdisj, B);
-      }
-    }
-    else
-    {
-      vertex_set A = compute_attractor_set_disjunctive(U);
-      mCRL2log(log::debug) << "A = " << pp(A) << std::endl;
-      std::tie(Wconj1, Wdisj1) = solve_recursive(V, A);
-      if (is_empty(Wconj1))
-      {
-        Wconj.clear();
-        Wdisj = set_union(A, Wdisj1);
-      }
-      else
-      {
-        vertex_set B = compute_attractor_set_conjunctive(Wconj1);
-        mCRL2log(log::debug) << "B = " << pp(B) << std::endl;
-        std::tie(Wconj, Wdisj) = solve_recursive(V, B);
-        Wconj = set_union(Wconj, B);
-      }
+      vertex_set B = compute_attractor_set(Wdisj1, decoration_other, decoration_self);
+      std::tie(Wconj, Wdisj) = solve_recursive(V, B);
+      Wdisj = set_union(Wdisj, B);
     }
   }
 
@@ -471,11 +352,11 @@ std::pair<structure_graph::vertex_set, structure_graph::vertex_set> solve_recurs
   // extend Vconj and Vdisj
   if (!Vconj.empty())
   {
-    Vconj = compute_attractor_set_conjunctive(Vconj);
+    Vconj = compute_attractor_set(Vconj, structure_graph::d_conjunction, structure_graph::d_disjunction);
   }
   if (!Vdisj.empty())
   {
-    Vdisj = compute_attractor_set_disjunctive(Vdisj);
+    Vdisj = compute_attractor_set(Vdisj, structure_graph::d_disjunction, structure_graph::d_conjunction);
   }
   log_vertex_set(Vconj, "Vconj after extension");
   log_vertex_set(Vdisj, "Vdisj after extension");
