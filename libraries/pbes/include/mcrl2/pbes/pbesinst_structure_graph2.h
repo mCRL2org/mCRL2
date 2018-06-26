eb7e9f5e8a2c320962dd3d3f87906b9100f203a8
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-06-26 18:52:34 +0200

    Implemented a new rewriter "Rplus" for the pbesinst_structure_graph2 algorithm.
    This new version of the rewriter should preserve counter example information in combination with optimization level 2 and 3.

--- libraries/pbes/include/mcrl2/pbes/pbesinst_structure_graph2.h
+++ libraries/pbes/include/mcrl2/pbes/pbesinst_structure_graph2.h
@@ -12,6 +12,7 @@
 #ifndef MCRL2_PBES_PBESINST_STRUCTURE_GRAPH2_H
 #define MCRL2_PBES_PBESINST_STRUCTURE_GRAPH2_H
 
+#include "mcrl2/data/undefined.h"
 #include "mcrl2/pbes/replace.h"
 #include "mcrl2/pbes/simple_structure_graph.h"
 #include "mcrl2/pbes/pbesinst_structure_graph.h"
@@ -20,16 +21,105 @@ namespace mcrl2 {
 
 namespace pbes_system {
 
-namespace detail {
-
-}
-
 /// \brief Adds an optimization to pbesinst_structure_graph.
 class pbesinst_structure_graph_algorithm2: public pbesinst_structure_graph_algorithm
 {
   protected:
     vertex_set S0;
     vertex_set S1;
+    pbes_expression b;
+
+    template<typename T>
+    pbes_expression expr(const T& x) const
+    {
+      return static_cast<const pbes_expression&>(x);
+    }
+
+    std::pair<pbes_expression, pbes_expression> Rplus(const pbes_expression& x)
+    {
+      if (is_true(x) || is_false(x))
+      {
+        return {x, x};
+      }
+      else if (is_propositional_variable_instantiation(x))
+      {
+        auto u = m_graph_builder.find_vertex(x);
+        if (u != structure_graph::undefined_vertex)
+        {
+          if (S0.contains(u))
+          {
+            return { expr(true_()), x };
+          }
+          else if (S1.contains(u))
+          {
+            return { expr(false_()), x };
+          }
+        }
+        return { expr(data::undefined_data_expression()), x };
+      }
+      else if (is_and(x))
+      {
+        const auto& x_ = atermpp::down_cast<and_>(x);
+        // TODO: use structured bindings
+        pbes_expression b1, b2, x1, x2;
+        std::tie(b1, x1) = Rplus(x_.left());
+        std::tie(b2, x2) = Rplus(x_.right());
+        if (is_true(b1) && is_true(b2))
+        {
+          return { expr(true_()), x };
+        }
+        else if (is_false(b1) && !is_false(b2))
+        {
+          return { expr(false_()), x1 };
+        }
+        else if (!is_false(b1) && is_false(b2))
+        {
+          return { expr(false_()), x2 };
+        }
+        else if (is_false(b1) && is_false(b2))
+        {
+          // TODO: use a heuristic for the smallest term (x1 or x2)
+          return { expr(false_()), x1 };
+        }
+        else // if (b1 == data::undefined_data_expression() && b2 == data::undefined_data_expression())
+        {
+          return {expr(data::undefined_data_expression()), x};
+        }
+      }
+      else if (is_or(x))
+      {
+        const auto& x_ = atermpp::down_cast<or_>(x);
+        // TODO: use structured bindings
+        pbes_expression b1, b2, x1, x2;
+        std::tie(b1, x1) = Rplus(x_.left());
+        std::tie(b2, x2) = Rplus(x_.right());
+        if (is_false(b1) && is_false(b2))
+        {
+          return { expr(false_()), x };
+        }
+        else if (is_true(b1) && !is_true(b2))
+        {
+          return { expr(true_()), x1 };
+        }
+        else if (!is_true(b1) && is_true(b2))
+        {
+          return { expr(true_()), x2 };
+        }
+        else if (is_true(b1) && is_true(b2))
+        {
+          // TODO: use a heuristic for the smallest term (x1 or x2)
+          return { expr(true_()), x1 };
+        }
+        else // if (b1 == data::undefined_data_expression() && b2 == data::undefined_data_expression())
+        {
+          return { expr(data::undefined_data_expression()), x };
+        }
+      }
+      else
+      {
+        throw mcrl2::runtime_error("Unexpected term " + pbes_system::pp(x) + " encountered in Rplus");
+      }
+    }
 
   public:
     typedef pbesinst_structure_graph_algorithm super;
@@ -50,28 +140,31 @@ class pbesinst_structure_graph_algorithm2: public pbesinst_structure_graph_algor
       // N.B. The sets S0 and S1 need to be resized, because new vertices may have been added.
       S0.resize(m_graph_builder.m_vertices.size());
       S1.resize(m_graph_builder.m_vertices.size());
-      return replace_propositional_variables(psi, [&](const propositional_variable_instantiation& x) {
-          auto u = m_graph_builder.find_vertex(x);
-          if (u != structure_graph::undefined_vertex)
-          {
-            if (S0.contains(u))
-            {
-              return static_cast<const pbes_expression&>(true_());
-            }
-            else if (S1.contains(u))
-            {
-              return static_cast<const pbes_expression&>(false_());
-            }
-          }
-          return static_cast<const pbes_expression&>(x);
-        }
-      );
+//      return replace_propositional_variables(psi, [&](const propositional_variable_instantiation& x) {
+//          auto u = m_graph_builder.find_vertex(x);
+//          if (u != structure_graph::undefined_vertex)
+//          {
+//            if (S0.contains(u))
+//            {
+//              return expr(true_());
+//            }
+//            else if (S1.contains(u))
+//            {
+//              return expr(false_());
+//            }
+//          }
+//          return expr(x);
+//        }
+//      );
+      pbes_expression x;
+      std::tie(b, x) = Rplus(psi);
+      return x;
     }
 
     void report_equation(const propositional_variable_instantiation& X, const pbes_expression& psi, std::size_t k) override
     {
       super::report_equation(X, psi, k);
-      if (is_true(psi))
+      if (is_true(b))
       {
         S0.insert(m_graph_builder.find_vertex(X));
         if (m_optimization > 2)
@@ -80,7 +173,7 @@ class pbesinst_structure_graph_algorithm2: public pbesinst_structure_graph_algor
           S0 = compute_attractor_set(G, S0, 0);
         }
       }
-      else if (is_false(psi))
+      else if (is_false(b))
       {
         S1.insert(m_graph_builder.find_vertex(X));
         if (m_optimization > 2)
