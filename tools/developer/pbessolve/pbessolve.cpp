8e25124ad91eede739f4e7b8b893305cca0b96d8
Wieger Wesselink <j.w.wesselink@tue.nl>
2018-07-05 09:49:42 +0200

    Renamed the optimization-level option of pbessolve to strategy, similar to pbes2bool.

--- tools/developer/pbessolve/pbessolve.cpp
+++ tools/developer/pbessolve/pbessolve.cpp
@@ -33,16 +33,15 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
   protected:
     typedef rewriter_tool<pbes_input_tool<input_tool>> super;
 
-    transformation_strategy m_transformation_strategy; // The strategy to substitute the value of variables with
-                                                       // a trivial rhs (aka true or false) in other equations when generating a BES.
-    search_strategy m_search_strategy;                 // The search strategy (depth first/breadth first)
+    search_strategy m_search_strategy;
+
+    // for doing a consistency check on the computed strategy
     bool m_check_strategy = false;
+
     std::string lpsfile;
     std::string ltsfile;
-    lts::lts_lts_t ltsspec;
 
-    // TODO: integrate these options with the search strategy?
-    int m_optimization = false; // can be 0, 1, 2 or 3
+    int m_strategy = 0; // can be 0, 1, 2, 3 or 4
 
     void add_options(utilities::interface_description& desc) override
     {
@@ -51,9 +50,7 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
       desc.add_option("search",
                  utilities::make_enum_argument<search_strategy>("SEARCH")
                    .add_value(breadth_first, true)
-                   .add_value(depth_first)
-                   .add_value(breadth_first_short)
-                   .add_value(depth_first_short),
+                   .add_value(depth_first),
                  "use search strategy SEARCH:",
                  'z');
       desc.add_option("lpsfile",
@@ -64,10 +61,34 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
                  utilities::make_optional_argument("NAME", "name"),
                  "The file containing the LTS that was used to generate the PBES. If this option is set, a counter example LTS will be generated.",
                  'g');
-      desc.add_option("optimization-level",
-                  utilities::make_optional_argument("NUMBER", "0"),
-                  "Apply optimization level (0 .. 4)",
-                  'l');
+      desc.add_option("strategy",
+                  utilities::make_optional_argument("STRATEGY", "0"),
+                      "use strategy STRATEGY:\n"
+                      "  '0' Compute all boolean equations which can be"
+                      " reached from the initial state, without"
+                      " optimization. This is is the most data efficient"
+                      " option per generated equation. (default)\n"
+                      "  '1' In addition to 0, remove self loops.\n"
+                      "  '2' Optimize by immediately substituting the right"
+                      " hand sides for already investigated variables that"
+                      " are true or false when generating an expression."
+                      " This is as memory efficient as 0.\n"
+                      "  '3' In addition to 2, also substitute variables that"
+                      " are true or false into an already generated right"
+                      " hand side. This can mean that certain variables"
+                      " become unreachable (e.g. X0 in X0 and X1, when X1"
+                      " becomes false, assuming X0 does not occur elsewhere."
+                      " It will be maintained which variables have become"
+                      " unreachable as these do not have to be investigated."
+                      " Depending on the PBES, this can reduce the size of"
+                      " the generated BES substantially but requires a"
+                      " larger memory footprint.\n"
+                      "  '4' In addition to 3, investigate for generated"
+                      " variables whether they occur on a loop, such that"
+                      " they can be set to true or false, depending on the"
+                      " fixed point symbol. This can increase the time"
+                      " needed to generate an equation substantially.",
+                 's');
     }
 
 
@@ -86,14 +107,18 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
       if (parser.options.count("ltsfile") > 0)
       {
         ltsfile = parser.option_argument("ltsfile");
-        ltsspec.load(ltsfile);
       }
       m_search_strategy = parser.option_argument_as<mcrl2::pbes_system::search_strategy>("search");
-      m_optimization = parser.option_argument_as<int>("optimization-level");
-      if (m_optimization < 0 || m_optimization > 4)
+      m_strategy = parser.option_argument_as<int>("strategy");
+      if (m_strategy < 0 || m_strategy > 4)
       {
-        throw mcrl2::runtime_error("An invalid value " + std::to_string(m_optimization) + " was specified for the optimization option.");
+        throw mcrl2::runtime_error("An invalid value " + std::to_string(m_strategy) + " was specified for the strategy option.");
       }
+    }
+
+    std::set<utilities::file_format> available_input_formats() const override
+    {
+      return { pbes_system::pbes_format_internal() };
     }
 
   public:
@@ -103,6 +128,10 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
               "Generate a BES from a PBES and solve it. ",
               "Solves (P)BES from INFILE. "
               "If INFILE is not present, stdin is used. "
+              "The PBES is first instantiated into a parity game, "
+              "which is then solved using Zielonka's algorithm. "
+              "It supports the generation of a witness or counter "
+              "example for the property encoded by the PBES."
              ),
       m_search_strategy(breadth_first)
     {}
@@ -112,7 +141,7 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
     {
       structure_graph G;
 
-      PbesInstAlgorithm algorithm(pbesspec, G, rewrite_strategy(), m_search_strategy, m_optimization);
+      PbesInstAlgorithm algorithm(pbesspec, G, rewrite_strategy(), m_search_strategy, m_strategy);
       mCRL2log(log::verbose) << "Generating parity game..." << std::endl;
       timer().start("instantiation");
       algorithm.run();
@@ -136,6 +165,8 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
       }
       else if (!ltsfile.empty())
       {
+        lts::lts_lts_t ltsspec;
+        ltsspec.load(ltsfile);
         lts::lts_lts_t evidence;
         timer().start("solving");
         bool result = solve_structure_graph_with_counter_example(G, ltsspec);
@@ -159,13 +190,13 @@ class pbessolve_tool: public rewriter_tool<pbes_input_tool<input_tool>>
       pbes_system::pbes pbesspec = pbes_system::detail::load_pbes(input_filename());
       pbes_system::algorithms::normalize(pbesspec);
 
-      if (m_optimization > 1)
+      if (m_strategy <= 1)
       {
-        run_algorithm<pbesinst_structure_graph_algorithm2>(pbesspec);
+        run_algorithm<pbesinst_structure_graph_algorithm>(pbesspec);
       }
       else
       {
-        run_algorithm<pbesinst_structure_graph_algorithm>(pbesspec);
+        run_algorithm<pbesinst_structure_graph_algorithm2>(pbesspec);
       }
       return true;
     }
