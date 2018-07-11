0e7bf062ff01c425b0325d4749d109cf5ddc9142
Maurice Laveaux <m.laveaux@tue.nl>
2018-07-11 17:51:45 +0200

    Added a benchmark script. Note that it uses the perf tool to obtain averages.

---
+++ libraries/atermpp/benchmark/benchmark.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Running simple benchmarks"
+perf stat -r10 benchmark_atermpp_integer_term_creation
+perf stat -r10 benchmark_atermpp_list_creation
+perf stat -r10 benchmark_atermpp_function_symbol_creation
+
+echo "Running nested function application benchmark from 0 to 32"
+for i in 0 1 2 4 7 8 10 12 16 18 20 24 28 32; do
+  perf stat -r10 benchmark_atermpp_function_application_creation $i
+done;
+
