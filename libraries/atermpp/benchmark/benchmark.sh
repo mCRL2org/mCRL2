eb33f6acf3117cf52be64ab9a0d7b37885f62079
Maurice Laveaux <m.laveaux@tue.nl>
2018-07-12 10:42:53 +0200

    Added the garbage collection benchmark to the script.

--- libraries/atermpp/benchmark/benchmark.sh
+++ libraries/atermpp/benchmark/benchmark.sh
@@ -4,6 +4,7 @@ echo "Running simple benchmarks"
 perf stat -r10 benchmark_atermpp_integer_term_creation
 perf stat -r10 benchmark_atermpp_list_creation
 perf stat -r10 benchmark_atermpp_function_symbol_creation
+perf stat -r10 benchmark_atermpp_garbage_collection_short
 
 echo "Running nested function application benchmark from 0 to 32"
 for i in 0 1 2 4 7 8 10 12 16 18 20 24 28 32; do
