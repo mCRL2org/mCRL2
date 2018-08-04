0a76f7bb5cf24a99b5401ab2a07f6652d760e27a
Thomas Neele <t.s.neele@tue.nl>
2018-08-04 11:34:46 +0200

    Fixed build of pg library example
    Also improved the layout and fixed some deprecation warnings.

--- libraries/pg/example/pg_solving.cpp
+++ libraries/pg/example/pg_solving.cpp
@@ -18,82 +18,88 @@
 
 int main(int argc, char *argv[])
 {
-    // you get these values from where-ever:
-    bool use_spm = true;    // use SPM solver?
-    bool use_rec = false;   // use recursive solver?
-    bool use_scc = true;    // use SCC decomposition?
-    bool verify  = true;    // verify solution?
+  // you get these values from where-ever:
+  bool use_spm = true;    // use SPM solver?
+  bool use_rec = false;   // use recursive solver?
+  bool use_scc = true;    // use SCC decomposition?
+  bool verify  = true;    // verify solution?
 
-    if (argc != 2)
-    {
-        printf("usage: %s <pbes>\n", argv[0]);
-        return 0;
-    }
-
-    std::auto_ptr<LiftingStrategyFactory> lift_strat_factory;
-    std::auto_ptr<ParityGameSolverFactory> subsolver_factory;
-    std::auto_ptr<ParityGameSolverFactory> solver_factory;
-
-    if (use_spm)
-    {
-        // Create a lifting strategy factory:
-        lift_strat_factory.reset(
-            new PredecessorLiftingStrategyFactory );
-
-        // Create a SPM solver factory:
-        solver_factory.reset(
-            new SmallProgressMeasuresSolverFactory(lift_strat_factory.get(), 2, true) );
-    }
-    else
-    if (use_rec)
-    {
-        // Create a recursive solver factory:
-        solver_factory.reset( new RecursiveSolverFactory );
-    }
-    else
-    {
-        printf("no solver chosen!\n");
-        return 1;
-    }
-
-    if (use_scc)
-    {
-        // Wrap solver factory into a component solver factory:
-        subsolver_factory = solver_factory;
-        solver_factory.reset(
-            new ComponentSolverFactory(*subsolver_factory) );
-    }
-
-    // Generate the game from a PBES:
-    verti goal_v;
-    ParityGame pg;
-    std::string path = argv[1];
-    if (path == "-") path = "";
-    pg.read_pbes(argv[1], &goal_v);  // N.B. mCRL2 could raise an exception here
-
-    // Create a solver:
-    std::auto_ptr<ParityGameSolver> solver(solver_factory->create(pg));
-
-    // Solve the game:
-    ParityGame::Strategy solution = solver->solve();
-    if (solution.empty())
-    {
-        printf("solving failed!\n");
-        return 1;
-    }
-
-    // Optional: verify the solution
-    if (verify && !pg.verify(solution, NULL))
-    {
-        printf("verification failed!\n");
-        return 1;
-    }
-
-    // Print winner:
-    if (pg.winner(solution, goal_v) == PLAYER_EVEN)
-        printf("Property holds.\n");
-    else
-        printf("Property DOES NOT hold.\n");
-
+  if (argc != 2)
+  {
+    printf("usage: %s <pbes>\n", argv[0]);
     return 0;
+  }
+
+  std::shared_ptr<LiftingStrategyFactory> lift_strat_factory;
+  std::shared_ptr<ParityGameSolverFactory> subsolver_factory;
+  std::shared_ptr<ParityGameSolverFactory> solver_factory;
+
+  if (use_spm)
+  {
+    // Create a lifting strategy factory:
+    lift_strat_factory.reset(
+        new PredecessorLiftingStrategyFactory );
+
+    // Create a SPM solver factory:
+    solver_factory.reset(
+        new SmallProgressMeasuresSolverFactory(lift_strat_factory, 2, true) );
+  }
+  else if (use_rec)
+  {
+    // Create a recursive solver factory:
+    solver_factory.reset( new RecursiveSolverFactory );
+  }
+  else
+  {
+    printf("no solver chosen!\n");
+    return 1;
+  }
+
+  if (use_scc)
+  {
+    // Wrap solver factory into a component solver factory:
+    subsolver_factory = solver_factory;
+    solver_factory.reset(
+        new ComponentSolverFactory(*subsolver_factory) );
+  }
+
+  // Generate the game from a PBES:
+  verti goal_v;
+  ParityGame pg;
+  std::string path = argv[1];
+  if (path == "-")
+  {
+    path = "";
+  }
+  pg.read_pbes(path, &goal_v);  // N.B. mCRL2 could raise an exception here
+
+  // Create a solver:
+  std::shared_ptr<ParityGameSolver> solver(solver_factory->create(pg));
+
+  // Solve the game:
+  ParityGame::Strategy solution = solver->solve();
+  if (solution.empty())
+  {
+    printf("solving failed!\n");
+    return 1;
+  }
+
+  // Optional: verify the solution
+  if (verify && !pg.verify(solution, NULL))
+  {
+    printf("verification failed!\n");
+    return 1;
+  }
+
+  // Print winner:
+  if (pg.winner(solution, goal_v) == PLAYER_EVEN)
+  {
+    printf("Property holds.\n");
+  }
+  else
+  {
+    printf("Property DOES NOT hold.\n");
+  }
+
+  return 0;
 }
