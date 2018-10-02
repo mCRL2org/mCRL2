a9fa0786dc202e627020a0d5c5c38a5d3c7c94de
Thomas Neele <t.s.neele@tue.nl>
2018-10-02 14:02:38 +0200

    Improved memory usage of pbessymbolicbisim a bit
    Previously, it would make many copies of ppg_equation instances. This
    can cost a lot of memory if there are many subblocks.

--- tools/experimental/pbessymbolicbisim/subblock.h
+++ tools/experimental/pbessymbolicbisim/subblock.h
@@ -32,8 +32,9 @@ class subblock
   typedef pbes_system::detail::ppg_equation equation_type_t;
 
 protected:
+  // pointer to a const equation_type_t object
   // invariant: m_equation.variable() == m_var
-  equation_type_t m_equation;
+  equation_type_t const* m_equation;
 
   pbes_system::propositional_variable m_var;
   data_expression m_char_func;
@@ -43,12 +44,13 @@ protected:
 
 public:
 
-  subblock()
-  : subblock(data_manipulators())
+  subblock(const data_manipulators& dm)
+  : m_equation(nullptr)
+  , m_dm(dm)
   {}
 
-  explicit subblock(const data_manipulators& dm)
-  : m_char_func(sort_bool::false_())
+  explicit subblock(const equation_type_t& eq, const data_manipulators& dm)
+  : m_equation(&eq)
   , m_dm(dm)
   {}
 
@@ -57,7 +59,7 @@ public:
   {}
 
   subblock(const equation_type_t& eq, const data_expression& char_func, const data_manipulators& dm, split_cache<subblock>* cache)
-  : m_equation(eq)
+  : m_equation(&eq)
   , m_var(eq.variable())
   , m_char_func(char_func)
   , m_dm(dm)
@@ -74,9 +76,9 @@ public:
     return m_char_func;
   }
 
-  equation_type_t equation() const
+  const equation_type_t& equation() const
   {
-    return m_equation;
+    return *m_equation;
   }
 
   bool has_transition(const std::list<subblock>& others) const
@@ -91,7 +93,7 @@ public:
     {
       return find_result->second;
     }
-    bool result = std::any_of(m_equation.summands().cbegin(), m_equation.summands().cend(), [&](const pbes_system::detail::ppg_summand& summ)
+    bool result = std::any_of(equation().summands().cbegin(), equation().summands().cend(), [&](const pbes_system::detail::ppg_summand& summ)
       {
         return summ.new_state().name() == other.m_var.name() &&
           m_dm.is_satisfiable(m_var.parameters() + summ.quantification_domain(),
@@ -110,7 +112,7 @@ public:
 
   bool is_empty()
   {
-    return m_char_func == sort_bool::false_();
+    return m_char_func == sort_bool::false_() || m_char_func == data_expression();
   }
 
   std::pair<subblock,subblock> split(const std::list<subblock>& others, const std::vector<subblock>& /*subblock_list*/) const
@@ -127,7 +129,7 @@ public:
       return std::make_pair(*this, subblock(m_dm));
     }
     data_expression transition_exists = sort_bool::false_();
-    for(const summand_type_t& cl: m_equation.summands())
+    for(const summand_type_t& cl: equation().summands())
     {
       // matching_subblocks expresses whether the summand cl
       // can be used to take a transition from this subblock to
@@ -159,39 +161,39 @@ public:
     transition_exists = one_point_rule_rewrite(m_dm.rewr(transition_exists));
     data_expression block2_body = m_dm.rewr(
         sort_bool::and_(
-          make_application(m_char_func, m_equation.variable().parameters()),
+          make_application(m_char_func, equation().variable().parameters()),
           sort_bool::not_(transition_exists)
         ));
-    if(!m_dm.is_satisfiable(m_equation.variable().parameters(), block2_body))
+    if(!m_dm.is_satisfiable(equation().variable().parameters(), block2_body))
     {
       // There are no (X,v) without transitions in this block
       // Therefore, we cannot split
       return std::make_pair(*this, subblock(m_dm));
     }
     data_expression block2 =
-      make_abstraction(lambda_binder(), m_equation.variable().parameters(),
+      make_abstraction(lambda_binder(), equation().variable().parameters(),
         block2_body
       );
-    block2 = m_dm.simpl->at(m_equation.variable())->apply(block2);
+    block2 = m_dm.simpl->at(equation().variable())->apply(block2);
     data_expression block1 =
-      make_abstraction(lambda_binder(), m_equation.variable().parameters(),
+      make_abstraction(lambda_binder(), equation().variable().parameters(),
         sort_bool::and_(
-          make_application(m_char_func, m_equation.variable().parameters()),
+          make_application(m_char_func, equation().variable().parameters()),
           transition_exists
         )
       );
 
-    block1 = m_dm.simpl->at(m_equation.variable())->apply(block1);
-    if(block1 == make_abstraction(lambda_binder(), m_equation.variable().parameters(), sort_bool::false_()))
+    block1 = m_dm.simpl->at(equation().variable())->apply(block1);
+    if(block1 == make_abstraction(lambda_binder(), equation().variable().parameters(), sort_bool::false_()))
     {
       throw mcrl2::runtime_error("Found an empty block1. This is most likely caused by a bug.");
     }
-    if(block2 == make_abstraction(lambda_binder(), m_equation.variable().parameters(), sort_bool::false_()))
+    if(block2 == make_abstraction(lambda_binder(), equation().variable().parameters(), sort_bool::false_()))
     {
       throw mcrl2::runtime_error("Found an empty block2. This is most likely caused by a bug.");
     }
-    subblock pos_block(m_equation, block1, m_dm, m_cache);
-    subblock neg_block(m_equation, block2, m_dm, m_cache);
+    subblock pos_block(equation(), block1, m_dm, m_cache);
+    subblock neg_block(equation(), block2, m_dm, m_cache);
 
     // TODO temporarily disabled this, since it depends on whether we are using
     // optimisations.
