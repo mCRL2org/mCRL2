ac2b8a72ade9b5dfc2462be9b4fe437c185b8195
Maurice Laveaux <m.laveaux@tue.nl>
2018-12-13 18:25:50 +0100

    Added a utility class to easily make classes noncopyable.
     * Inherit from noncopyable will disallow copy construction and copy assignment.

---
+++ libraries/utilities/include/mcrl2/utilities/noncopyable.h
@@ -0,0 +1,31 @@
+// Author(s): Maurice Laveaux
+// Copyright: see the accompanying file COPYING or copy at
+// https://github.com/mCRL2org/mCRL2/blob/master/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef MCRL2_UTILITIES_NONCOPYABLE_H_
+#define MCRL2_UTILITIES_NONCOPYABLE_H_
+
+namespace mcrl2
+{
+namespace utilities
+{
+
+/// \brief Inherit from this class to prevent it from being copyable.
+/// \details Declares the copy (assignment) constructors as deleted.
+class noncopyable
+{
+public:
+  noncopyable() = default;
+  noncopyable(const noncopyable&) = delete;
+  noncopyable& operator=(const noncopyable&) = delete;
+};
+
+}
+}
+
+#endif // MCRL2_UTILITIES_NONCOPYABLE_H_
