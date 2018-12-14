6aa39cf6c63236382956fd4231c20b6fefd3e62e
Maurice Laveaux <m.laveaux@tue.nl>
2018-12-14 11:54:43 +0100

    Introduced an unprotected_aterm that does not change the reference count.
     * Can be used instead of _aterm* or aterm* in various places.
     * Is used as the argument of a term application as the arguments are no
       longer protected.
     * Cleaned up the implementation of an underlying term and added the ability
       to mark terms for the mark-and-sweep garbage collection.

--- libraries/atermpp/include/mcrl2/atermpp/aterm.h
+++ libraries/atermpp/include/mcrl2/atermpp/aterm.h
@@ -12,15 +12,12 @@
 #ifndef MCRL2_ATERMPP_ATERM_H
 #define MCRL2_ATERMPP_ATERM_H
 
+#include "mcrl2/atermpp/type_traits.h"
+#include "mcrl2/atermpp/detail/aterm.h"
+
 #include <string>
 #include <sstream>
 #include <iostream>
-#include <cassert>
-#include <functional>
-
-#include <type_traits>
-#include "mcrl2/atermpp/detail/aterm.h"
-#include "mcrl2/atermpp/type_traits.h"
 
 /// \brief The main namespace for the aterm++ library.
 namespace atermpp
@@ -28,263 +25,226 @@ namespace atermpp
 
 typedef void(*term_callback)(const aterm&);
 
-void add_creation_hook(const function_symbol&, term_callback);
-void add_deletion_hook(const function_symbol&, term_callback);
+extern void add_creation_hook(const function_symbol&, term_callback);
+extern void add_deletion_hook(const function_symbol&, term_callback);
 
-class aterm
+/// \brief An unprotected term does not change the reference count of the
+///        shared term when it is copied or moved.
+class unprotected_aterm
 {
-  public:
-    template < typename T >
-    friend class term_appl;
+public:
+  template < typename T >
+  friend class term_appl;
 
-    template < typename T >
-    friend class term_list;
+  template < typename T >
+  friend class term_list;
 
-    friend void detail::free_term_aux(detail::_aterm* t, detail::_aterm*& terms_to_be_removed);
+  friend detail::_aterm* detail::address(const unprotected_aterm& t);
 
-    friend void detail::initialise_aterm_administration();
+public:
 
-    template <class Term, class Iter, class ATermConverter>
-    friend detail::_aterm *detail::make_list_backward(Iter first, Iter last, const ATermConverter& convert_to_aterm);
+  unprotected_aterm() noexcept :
+    m_term(nullptr)
+  {}
 
-    template <class Term, class Iter, class ATermConverter, class ATermFilter>
-    friend detail::_aterm *detail::make_list_backward(Iter first, Iter last, const ATermConverter& convert_to_aterm, const ATermFilter& aterm_filter);
+  unprotected_aterm(detail::_aterm* term) noexcept :
+    m_term(term)
+  {}
 
-    template <class Term, class Iter, class ATermConverter>
-    friend detail::_aterm *detail::make_list_forward(Iter first, Iter last, const ATermConverter& convert_to_aterm);
+  /// \return True iff this term is an term_appl.
+  /// \details This function has constant complexity.
+  ///          It is defined as !type_is_int() && !type_is_list().
+  bool type_is_appl() const noexcept
+  {
+    return !type_is_int() && !type_is_list();
+  }
 
-    template <class Term, class Iter, class ATermConverter, class ATermFilter>
-    friend detail::_aterm *detail::make_list_forward(Iter first, Iter last, const ATermConverter& convert_to_aterm, const  ATermFilter& aterm_filter);
+  /// \return True iff this term has internal structure of an aterm_int.
+  /// \details This function has constant complexity.
+  bool type_is_int() const noexcept
+  {
+    const function_symbol& f=m_term->function();
+    return f == detail::g_as_int;
+  }
 
-    friend detail::_aterm* detail::address(const aterm& t);
- 
-  protected:
-    detail::_aterm* m_term;
+  /// \returns True iff this term has the structure of an term_list
+  /// \details This function has constant complexity.
+  bool type_is_list() const noexcept
+  {
+    const function_symbol& f=m_term->function();
+    return f == detail::g_as_list || f == detail::g_as_empty_list;
+  }
 
+  /// \return true iff t is equal to the current term.
+  /// \details Terms are stored in a maximally shared way. This
+  ///         means that this equality operator can be calculated
+  ///         in constant time.
+  bool operator ==(const unprotected_aterm& t) const
+  {
+    return m_term == t.m_term;
+  }
 
-    static detail::_aterm* static_undefined_aterm;
-    static detail::_aterm *static_empty_aterm_list;
+  /// \brief Inequality operator on two aterms.
+  /// \details See note at the == operator. This operator requires constant time.
+  /// \param t A term to which the current term is compared.
+  /// \return false iff t is equal to the current term.
+  bool operator !=(const unprotected_aterm& t) const
+  {
+    return m_term!=t.m_term;
+  }
 
-    inline std::size_t reference_count() const noexcept
-    {
-      assert(m_term!=nullptr);
-      assert(m_term->reference_count()>0);
-      return m_term->reference_count();
-    }
+  /// \brief Comparison operator for two aterms.
+  /// \details This operator requires constant time. It compares
+  ///         the addresses where terms are stored. That means
+  ///         that the outcome of this operator is only stable
+  ///         as long as aterms are not garbage collected.
+  /// \param t A term to which the current term is compared.
+  /// \return True iff the current term is smaller than the argument.
+  bool operator <(const unprotected_aterm& t) const
+  {
+    return m_term<t.m_term;
+  }
 
-    inline std::size_t decrease_reference_count() noexcept
-    {
-      assert(m_term!=nullptr);
-      assert(m_term->reference_count()>0);
-      m_term->decrease_reference_count();
-      return m_term->reference_count();
-    }
+  /// \brief Comparison operator for two aterms.
+  /// \details This operator requires constant time. See note at the operator <.
+  /// \param t A term to which the current term is compared.
+  /// \return True iff the current term is larger than the argument.
+  bool operator >(const unprotected_aterm& t) const
+  {
+    return m_term>t.m_term;
+  }
 
-    template <bool CHECK>
-    void increase_reference_count() noexcept
-    {
-      assert(m_term!=nullptr);
-      if (CHECK) assert(m_term->reference_count()>0);
-      m_term->increase_reference_count();
-    }
+  /// \brief Comparison operator for two aterms.
+  /// \details This operator requires constant time. See note at the operator <.
+  /// \param t A term to which the current term is compared.
+  /// \return True iff the current term is smaller or equal than the argument.
+  bool operator <=(const unprotected_aterm& t) const
+  {
+    return m_term<=t.m_term;
+  }
 
-    void copy_term(const aterm& t) noexcept
-    {
-      /* It is important that the reference count of m_term is decreased after
-         the reference count of t.m_term is increased, as otherwise if the terms are exactly
-         the same, the reference count can temporarily become 0. */
-      const_cast<aterm&>(t).increase_reference_count<true>();
-      m_term->decrease_reference_count();
-      m_term=t.m_term;
-    }
+  /// \brief Comparison operator for two aterms.
+  /// \details This operator requires constant time. See note at the operator <.
+  /// \param t A term to which the current term is compared.
+  /// \return True iff the current term is larger or equalthan the argument.
+  bool operator >=(const unprotected_aterm& t) const
+  {
+    return m_term>=t.m_term;
+  }
 
-    // An aterm has a function symbol, which can also be an AS_EMPTY_LIST,
-    // AS_INT and AS_LIST. This is for internal use only. 
-    const function_symbol& function() const
-    {
-      return m_term->function();
-    }
+  /// \brief Returns true if this term is not equal to the term assigned by
+  ///        the default constructor of aterms, term_appl<T>'s and aterm_int.
+  /// \details The default constructor of a term_list<T> is the empty list, on which
+  ///          the operator defined yields true. This operation is more efficient
+  ///          than comparing the current term with an aterm(), term_appl<T>() or an
+  ///          aterm_int().
+  /// \return A boolean indicating whether this term equals the default constructor.
+  bool defined() const
+  {
+    return m_term != nullptr;
+  }
 
-  public: // Should be protected, but this cannot yet be done due to a problem
-          // in the compiling rewriter.
-    explicit aterm(detail::_aterm *t) noexcept 
-      : m_term(t) 
-    {
-      // Note that reference_count can be 0, as this term can just be constructed,
-      // and is now handed over to become a real aterm.
-      increase_reference_count<false>();
-    }
+  /// \brief Swaps this term with its argument.
+  /// \details This operation is more efficient than exchanging terms by an assignment,
+  ///          as swapping does not require to change the protection of terms.
+  /// \param t The term with which this term is swapped.
+  void swap(unprotected_aterm& t) noexcept
+  {
+    std::swap(m_term, t.m_term);
+  }
 
-  public:
+  /// \returns The function symbol of the term, which can also be an AS_EMPTY_LIST,
+  ///          AS_INT and AS_LIST.
+  /// \details This is for internal use only.
+  const function_symbol& function() const
+  {
+    return m_term->function();
+  }
 
-    /// \brief Default constructor.
-    aterm() noexcept
-      : m_term(static_undefined_aterm)
-    {
-      increase_reference_count<false>();
-    }
-
-    /// \brief Copy constructor.
-    /// \param t Term that is copied.
-    aterm(const aterm& t) noexcept 
-      : m_term(t.m_term)
-    {
-      increase_reference_count<true>();
-    }
-
-    /// \brief Move constructor.
-    /// \param t Term that is moved to this.
-    aterm(aterm&& t) noexcept 
-      : m_term(t.m_term) 
-    { 
-      t.increase_reference_count<true>();  // The use of swap is not possible, 
-                                           // as a term needs to be constructed. 
-    } 
-
-    /// \brief Assignment operator.
-    /// \param t a term to be assigned.
-    aterm& operator=(const aterm& t) noexcept
-    {
-      copy_term(t);
-      return *this;
-    }
-
-    /// \brief Move assignment operator.
-    /// \param t a term to be assigned.
-    aterm& operator=(aterm&& t) noexcept
-    {
-      swap(t);
-      return *this;
-    }
-
-    /// \brief Destructor.
-    ~aterm () noexcept
-    {
-      decrease_reference_count();
-    }
-
-    /// \brief Returns whether this term is a term_appl.
-    /// \details This function has constant complexity.
-    ///          It is defined as !type_is_int() && !type_is_list().
-    /// \return True iff term is an term_appl.
-    bool type_is_appl() const
-    {
-      return !type_is_int() && !type_is_list();
-    }
-
-    /// \brief Returns whether this term has the internal structure of an aterm_int.
-    /// \details This function has constant complexity.
-    /// \return True iff term is an term_int.
-    bool type_is_int() const
-    {
-      return m_term->function()==detail::function_adm.AS_INT;
-    }
-
-    /// \brief Returns whether this term has the structure of an term_list
-    /// \details This function has constant complexity.
-    /// \return True iff term is an term_list.
-    bool type_is_list() const
-    {
-      const function_symbol& f=m_term->function();
-      return f==detail::function_adm.AS_LIST|| f==detail::function_adm.AS_EMPTY_LIST;
-    }
-
-    /// \brief Equality function on two aterms.
-    /// \details Terms are stored in a maximally shared way. This
-    ///         means that this equality operator can be calculated
-    ///         in constant time.
-    /// \param t A term to which the current term is compared.
-    /// \return true iff t is equal to the current term.
-    bool operator ==(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term==t.m_term;
-    }
-
-    /// \brief Inequality operator on two aterms.
-    /// \details See note at the == operator. This operator requires constant time.
-    /// \param t A term to which the current term is compared.
-    /// \return false iff t is equal to the current term.
-    bool operator !=(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term!=t.m_term;
-    }
-
-    /// \brief Comparison operator for two aterms.
-    /// \details This operator requires constant time. It compares
-    ///         the addresses where terms are stored. That means
-    ///         that the outcome of this operator is only stable
-    ///         as long as aterms are not garbage collected.
-    /// \param t A term to which the current term is compared.
-    /// \return True iff the current term is smaller than the argument.
-    bool operator <(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term<t.m_term;
-    }
-
-    /// \brief Comparison operator for two aterms.
-    /// \details This operator requires constant time. See note at the operator <.
-    /// \param t A term to which the current term is compared.
-    /// \return True iff the current term is larger than the argument.
-    bool operator >(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term>t.m_term;
-    }
-
-    /// \brief Comparison operator for two aterms.
-    /// \details This operator requires constant time. See note at the operator <.
-    /// \param t A term to which the current term is compared.
-    /// \return True iff the current term is smaller or equal than the argument.
-    bool operator <=(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term<=t.m_term;
-    }
-
-    /// \brief Comparison operator for two aterms.
-    /// \details This operator requires constant time. See note at the operator <.
-    /// \param t A term to which the current term is compared.
-    /// \return True iff the current term is larger or equalthan the argument.
-    bool operator >=(const aterm& t) const
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      return m_term>=t.m_term;
-    }
-
-    /// \brief Returns true if this term is not equal to the term assigned by
-    ///        the default constructor of aterms, term_appl<T>'s and aterm_int.
-    /// \details The default constructor of a term_list<T> is the empty list, on which
-    ///          the operator defined yields true. This operation is more efficient
-    ///          than comparing the current term with an aterm(), term_appl<T>() or an
-    ///          aterm_int().
-    /// \return A boolean indicating whether this term equals the default constructor.
-    bool defined() const
-    {
-      assert(m_term->reference_count()>0);
-      return this->function()!=function_symbol();
-    }
-
-    /// \brief Swaps this term with its argument.
-    /// \details This operation is more efficient than exchanging terms by an assignment,
-    ///          as swapping does not require to change the protection of terms.
-    /// \param t The term with which this term is swapped.
-    void swap(aterm& t) noexcept
-    {
-      assert(m_term->reference_count()>0);
-      assert(t.m_term->reference_count()>0);
-      using std::swap;
-      swap(m_term,t.m_term);
-    }
+protected:
+  detail::_aterm* m_term;
 };
+
+/// \brief The aterm base class that provides protection of the underlying shared terms.
+class aterm : public unprotected_aterm
+{
+public:
+  aterm() noexcept = default;
+  ~aterm()
+  {
+    if (defined())
+    {
+      m_term->decrement_reference_count();
+    }
+  }
+
+  /// \brief Takes ownership of the passed underlying term.
+  /// \todo Should be protected, but this cannot yet be done due to a problem
+  ///       in the compiling rewriter.
+  explicit aterm(detail::_aterm *t) noexcept
+  {
+    t->increment_reference_count();
+    m_term = t;
+  }
+
+  // This class has a non-trivial destructor so explicitly define the copy and move operators.
+  aterm(const aterm& other) noexcept
+  {
+    m_term = other.m_term;
+    if (defined())
+    {
+      m_term->increment_reference_count();
+    }
+  }
+
+  aterm& operator=(const aterm& other) noexcept
+  {
+    // Increment first to prevent the same term from becoming reference zero temporarily.
+    if (other.defined())
+    {
+      other.m_term->increment_reference_count();
+    }
+
+    // Decrement the reference from the term that is currently referred to.
+    if (defined())
+    {
+      m_term->decrement_reference_count();
+    }
+
+    m_term = other.m_term;
+    return *this;
+  }
+
+  aterm(aterm&& other) noexcept
+  {
+    std::swap(m_term, other.m_term);
+  }
+
+  aterm& operator=(aterm&& other) noexcept
+  {
+    if (defined())
+    {
+      m_term->decrement_reference_count();
+    }
+
+    m_term = other.m_term;
+    other.m_term = nullptr;
+    return *this;
+  }
+
+protected:
+  /// \brief A copy constructor that does not protect the term.
+  void copy_term(const aterm& t) noexcept
+  {
+    m_term = t.m_term;
+  }
+};
+
+namespace detail
+{
+  /// \brief A term that represents an empty list.
+  extern aterm g_is_empty_list;
+}
 
 template <class Term1, class Term2>
 struct is_convertible : public
@@ -330,9 +290,9 @@ const DerivedCont& container_cast(const Cont<Base>& t,
 
 /// \brief A cast form an aterm derived class to a class that inherits in possibly multiple steps from this class.
 /// \details The derived class is not allowed to contain extra fields. This conversion does not require runtime computation
-//          effort. Also see down_cast. 
-/// \param t The term that is converted. 
-/// \return A term of type Derived. 
+///          effort. Also see down_cast.
+/// \param t The term that is converted.
+/// \return A term of type Derived.
 template <class Derived, class Base>
 const Derived& vertical_cast(const Base& t,
                           typename std::enable_if<is_convertible<Base, Derived>::value>::type* = nullptr)
@@ -373,7 +333,7 @@ const Derived& deprecated_cast(const Base& t,
 std::ostream& operator<<(std::ostream& out, const aterm& t);
 
 /// \brief Transform an aterm into a string representation.
-/// \brief This function also prints terms that are derived from aterms. 
+/// \brief This function also prints terms that are derived from aterms.
 /// \param t The input aterm.
 /// \return A string.
 inline std::string pp(const aterm& t)
@@ -414,27 +374,11 @@ namespace std
 ///          swap function must be provided for derived classes.
 /// \param t1 The first term
 /// \param t2 The second term
-
 template <>
-inline void swap(atermpp::aterm& t1, atermpp::aterm& t2) noexcept
+inline void swap(atermpp::unprotected_aterm& t1, atermpp::unprotected_aterm& t2) noexcept
 {
   t1.swap(t2);
 }
-
-/// \brief specialization of the standard std::hash function.
-template<>
-struct hash<atermpp::aterm>
-{
-  // Default constructor, required for const qualified hash functions. 
-  hash()
-  {}
-
-  std::size_t operator()(const atermpp::aterm& t) const
-  {
-    std::hash<atermpp::detail::_aterm*> aterm_hasher;
-    return aterm_hasher(atermpp::detail::address(t));
-  }
-};
 
 } // namespace std
 
--- libraries/atermpp/include/mcrl2/atermpp/detail/aterm.h
+++ libraries/atermpp/include/mcrl2/atermpp/detail/aterm.h
@@ -6,199 +6,93 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-/// \file mcrl2/atermpp/detail/aterm.h
-/// \brief This file contains the _aterm class, which is the
-///        class to which an aterm points. Each _aterm consists
-///        of a function symbol, a reference count, used for garbage
-///        collection and a next pointer used in the hash tables to
-///        locate a term on the basis of the function symbol and 
-///        arguments. Each _aterm contains an arbitrary number of
-///        arguments after m_next, as indicated in the function symbol.
-///        These arguments are not listed explicitly in the class 
-///        below, but room is reserved for them when creating this
-///        term. 
 
 #ifndef DETAIL_ATERM_H
 #define DETAIL_ATERM_H
 
-#include <cstddef>
 #include "mcrl2/atermpp/detail/atypes.h"
-#include "mcrl2/atermpp/detail/function_symbol_constants.h"
 #include "mcrl2/atermpp/function_symbol.h"
+#include "mcrl2/utilities/noncopyable.h"
+#include "mcrl2/utilities/shared_reference.h"
+#include "mcrl2/atermpp/type_traits.h"
+
+#include <limits>
 
 namespace atermpp
 {
 
-class aterm;
+class unprotected_aterm;
+
+constexpr static std::size_t MarkedReferenceCount = std::numeric_limits<std::size_t>::max();
 
 namespace detail
 {
 
-static const std::size_t IN_FREE_LIST(-1);
+/// \brief Can be used to check whether all elements in the parameter pack are derived from aterms.
+template<typename ...Terms>
+using are_terms = and_<std::is_convertible<Terms, unprotected_aterm>...>;
 
-class _aterm
+/// \brief This is the class to which an aterm points. Each _aterm consists
+///        of a function symbol and a reference count used for garbage
+///        collection.
+class _aterm : public mcrl2::utilities::shared_reference_counted<_aterm, GlobalThreadSafe>, mcrl2::utilities::noncopyable
 {
-  protected:
-    function_symbol m_function_symbol;
-    std::size_t  m_reference_count;
-    _aterm* m_next;
+public:
+  using ref = mcrl2::utilities::shared_reference<_aterm>;
 
-  public:
-    _aterm()=delete;
-    _aterm(const _aterm &)=delete;
-    _aterm(_aterm &&)=delete;
-    _aterm& operator=(const _aterm&)=delete;
-    _aterm& operator=(_aterm&&)=delete;
+  /// \brief Create a term from a function symbol.
+  _aterm(const function_symbol& symbol) :
+    m_function_symbol(symbol)
+  {}
 
-    function_symbol& function() noexcept
-    {
-      return m_function_symbol;
-    }
-
-    const function_symbol& function() const noexcept
-    {
-      return m_function_symbol;
-    }
-
-    void decrease_reference_count() noexcept
-    {
-      assert(!reference_count_indicates_is_in_freelist());
-      assert(!reference_count_is_zero());
-      --m_reference_count;
-    } 
-
-    void increase_reference_count() noexcept
-    {
-      assert(!reference_count_indicates_is_in_freelist());
-      ++m_reference_count;
-    } 
-
-    void reset_reference_count(const bool check=true) noexcept
-    {
-      if (check) assert(reference_count_indicates_is_in_freelist());
-      m_reference_count=0;
-    } 
-
-    bool reference_count_is_zero() const noexcept
-    {
-      return m_reference_count==0;
-    } 
-
-    std::size_t reference_count() const noexcept
-    {
-      return m_reference_count;
-    }
-
-    void set_reference_count_indicates_in_freelist(const bool check=true) noexcept
-    {
-      if (check) assert(!reference_count_indicates_is_in_freelist());
-      m_reference_count=IN_FREE_LIST;
-    }
-
-    bool reference_count_indicates_is_in_freelist() const noexcept
-    {
-      return m_reference_count==IN_FREE_LIST;
-    }
-
-    _aterm* next() const noexcept
-    {
-      return m_next;
-    }
-
-    void set_next(_aterm* n) noexcept
-    {
-      m_next=n;
-    }
-};
-
-static const std::size_t TERM_SIZE=sizeof(_aterm)/sizeof(std::size_t);
-
-detail::_aterm* allocate_term(const std::size_t size);
-void remove_from_hashtable(_aterm *t);
-void free_term(detail::_aterm* t);
-void free_term_aux(detail::_aterm* t, detail::_aterm*& terms_to_be_removed);
-
-extern detail::_aterm* * aterm_hashtable;
-extern std::size_t aterm_table_mask;
-extern std::size_t total_nodes_in_hashtable;
-
-void call_creation_hook(_aterm*);
-
-inline void insert_in_hashtable(_aterm *t, const std::size_t hnr)
-{
-  t->set_next(detail::aterm_hashtable[hnr]);
-  detail::aterm_hashtable[hnr] = t;
-  total_nodes_in_hashtable++;
-}
-
-inline _aterm* term_appl0(const function_symbol& sym)
-{
-  assert(sym.arity()==0);
-
-  const std::hash<function_symbol> function_symbol_hasher;
-  std::size_t hnr = function_symbol_hasher(sym);
-
-  _aterm *cur = detail::aterm_hashtable[hnr & detail::aterm_table_mask];
-
-  while (cur)
+  function_symbol& function() noexcept
   {
-    if (cur->function()==sym)
-    {
-      return cur;
-    }
-    cur = cur->next();
+    return m_function_symbol;
   }
 
-  cur = detail::allocate_term(detail::TERM_SIZE);
-  /* Delay masking until after allocate */
-  hnr &= detail::aterm_table_mask;
-  new (&const_cast<detail::_aterm*>(cur)->function()) function_symbol(sym);
+  const function_symbol& function() const noexcept
+  {
+    return m_function_symbol;
+  }
 
-  insert_in_hashtable(cur,hnr);
+  /// \brief Mark this term to be garbage collected.
+  /// \details Changes the reference count, so only apply whenever !is_reachable().
+  void mark()
+  {
+    assert(!is_reachable());
+    m_reference_count = MarkedReferenceCount;
+    increment_reference_count_changes();
+  }
 
-  call_creation_hook(cur);
+  /// \brief Remove the mark from a term.
+  /// \details Changes the reference count, so only apply whenever it was marked.
+  void reset()
+  {
+    assert(is_marked());
+    m_reference_count = 0;
+    increment_reference_count_changes();
+  }
 
-  return cur;
-}
+  /// \returns True whenever this term has been marked.
+  bool is_marked() const noexcept
+  {
+    return m_reference_count == MarkedReferenceCount;
+  }
 
+  /// \returns True whenever the term is reachable, either marked or reference count above zero.
+  bool is_reachable() const noexcept
+  {
+    return m_reference_count > 0;
+  }
 
-template <class Term, class Iter, class ATermConverter>
-_aterm *make_list_backward(Iter first, Iter last, const ATermConverter& convert_to_aterm);
-template <class Term, class Iter, class ATermConverter, class ATermFilter>
-_aterm *make_list_backward(Iter first, Iter last, const ATermConverter& convert_to_aterm, const ATermFilter& aterm_filter);
-template <class Term, class Iter, class ATermConverter>
-_aterm *make_list_forward(Iter first, Iter last, const ATermConverter& convert_to_aterm);
-template <class Term, class Iter, class ATermConverter, class ATermFilter>
-_aterm *make_list_forward(Iter first, Iter last, const ATermConverter& convert_to_aterm, const ATermFilter& aterm_filter);
+private:
+  function_symbol m_function_symbol;
+};
 
-// Provides the address where the data belonging to this aterm is stored.
-inline _aterm* address(const aterm& t); 
-
-inline std::size_t hash_value_aterm_int(const std::size_t val)
-{
-  return val;
-}
+/// \brief Provides the address where the data belonging to this aterm is stored.
+inline _aterm* address(const unprotected_aterm& t);
 
 } // namespace detail
 } // namespace atermpp
-
-namespace std
-{
-
-// \brief specialisation of the hash function.
-template<>
-struct hash<atermpp::detail::_aterm*>
-{
-  // Default constructor, required for const qualified hash functions. 
-  hash()
-  {}
-
-  std::size_t operator()(const atermpp::detail::_aterm* t) const
-  {
-    return reinterpret_cast<std::size_t>(t)>>4;
-  }
-};
-
-} // namespace detail
 
 #endif /* DETAIL_ATERM_H */
