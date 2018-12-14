7f5cbea5a2aff1556a776d9efd06cf7ee52dfe9f
Maurice Laveaux <m.laveaux@tue.nl>
2018-12-14 11:36:42 +0100

    Added a function symbol pool that stores all function symbols.
     * Cleaned up the underlying function symbol interface.
     * Make use of the shared_reference to handle reference counting.
     * Added heterogeneous hash and equivalence functions.
     * Simplified the function symbol generation implementation.

--- libraries/atermpp/include/mcrl2/atermpp/detail/function_symbol.h
+++ libraries/atermpp/include/mcrl2/atermpp/detail/function_symbol.h
@@ -12,179 +12,55 @@
 #ifndef DETAIL_FUNCTION_SYMBOL_H
 #define DETAIL_FUNCTION_SYMBOL_H
 
+#include "mcrl2/utilities/noncopyable.h"
+#include "mcrl2/utilities/shared_reference.h"
+
 #include <string>
-#include <unordered_map>
-
-namespace atermpp
-{
-class function_symbol;
-class function_symbol_generator;
-
-namespace detail
-{
-struct constant_function_symbols;
-void initialise_aterm_administration();
-void initialise_function_map_administration();
-
-// A function is determined by its name and its arity. 
-class _function_symbol_primary_data
-{
-  protected:
-    const std::string m_name;  // The primary data cannot be altered after creation. 
-    const std::size_t m_arity;
-
-  public:
-    _function_symbol_primary_data(const std::string& name, std::size_t arity)
-     : m_name(name), m_arity(arity)
-    {}
-
-    const std::string& name() const
-    {
-      return m_name;
-    }
-
-    std::size_t arity() const
-    {
-      return m_arity;
-    }
-
-    bool operator==(const _function_symbol_primary_data& f) const
-    {
-      return m_arity==f.m_arity && m_name==f.m_name;
-    }
-};
-
-// Each function symbol has a reference count, and sometimes a sequence number
-// as special information. 
-class _function_symbol_auxiliary_data
-{
-  protected:
-    std::size_t m_reference_count;
-
-  public:
-
-    _function_symbol_auxiliary_data(std::size_t reference_count)
-     : m_reference_count(reference_count)
-    {}
-
-    std::size_t reference_count() const
-    {
-      return m_reference_count;
-    }
-
-    std::size_t& reference_count()
-    {
-      return m_reference_count;
-    }
-};
-
-// set index such that no function symbol exists with the name 'prefix + std::to_string(n)'
-// for all values n >= index
-extern std::size_t get_sufficiently_large_postfix_index(const std::string& prefix_);
-
-// Ugly class, to be replaced by a lambda function.
-struct index_increaser
-{
-  std::size_t* m_initial_index;
-  std::size_t* m_index;
-
-  index_increaser()
-   : m_index(nullptr)
-  {}
-
-  index_increaser(std::size_t& initial_index, std::size_t& index)
-   : m_initial_index(&initial_index), m_index(&index)
-  {}
-
-  index_increaser(const index_increaser& other)
-   : m_initial_index(other.m_initial_index),
-     m_index(other.m_index)
-  {}
-
-  index_increaser& operator=(const index_increaser& other)
-  {
-    m_initial_index=other.m_initial_index;
-    m_index=other.m_index;
-    return *this;
-  }
-
-  void operator ()(std::size_t new_index)
-  {
-    *m_initial_index=new_index;
-    if (*m_index<new_index)
-    {
-      *m_index=new_index;
-    }
-  }
-};
-
-// register a prefix for a function symbol, such that the index of this prefix can be increased when
-// some other process makes a function symbol with the same prefix.
-extern void register_function_symbol_prefix_string(const std::string& prefix, index_increaser& increase_index);
-
-// deregister a prefix for a function symbol.
-extern void deregister_function_symbol_prefix_string(const std::string& prefix);
-
-} // namespace detail
-} // namespace atermpp
-
-namespace std
-{
-
-// Specialisation of the standard hash function for _function_symbol.
-template<>
-struct hash<atermpp::detail::_function_symbol_primary_data>
-{
-  std::size_t operator()(const atermpp::detail::_function_symbol_primary_data& f) const
-  {
-    std::hash<std::string> string_hasher;
-    std::size_t h=string_hasher(f.name());
-    return (h ^ f.arity());
-  }
-};
-}
+#include <cstddef>
 
 namespace atermpp
 {
 namespace detail
 {
 
-// A function symbol is a pair of essential and auxiliary information, stored in an unordered set.
-typedef std::pair<const _function_symbol_primary_data, _function_symbol_auxiliary_data> _function_symbol;
+/// \brief Enables thread safety for the global term and function symbol pools.
+/// \details The reason to declare it here is because the function symbol is included by everything.
+constexpr static bool GlobalThreadSafe = false;
 
-
-// This is the class to store function symbols. It is just an unordered map.
-class function_symbol_store_class: 
-        public std::unordered_map<detail::_function_symbol_primary_data, 
-                                  detail::_function_symbol_auxiliary_data>
+/// \brief Stores the data for a function symbol (name, arity) pair.
+class _function_symbol : public mcrl2::utilities::shared_reference_counted<_function_symbol, GlobalThreadSafe>, private mcrl2::utilities::noncopyable
 {
-  protected:
-    bool& m_function_symbol_store_is_defined;
+public:
+  /// \brief A shared reference for this object.
+  using ref = mcrl2::utilities::shared_reference<_function_symbol>;
 
-  public:
-  
-    // Constructor. Also initialise other basic aterm data structures.
-    function_symbol_store_class(bool& function_symbol_store_is_defined)
-      : m_function_symbol_store_is_defined(function_symbol_store_is_defined)
-    {
-      if (!m_function_symbol_store_is_defined)
-      {
-        m_function_symbol_store_is_defined=true;
-      }
-    }
+  _function_symbol(const std::string& name, std::size_t arity) :
+     m_arity(arity),
+     m_name(name)
+  {}
 
-    // \brief Destructor. Indicate that the function store does not exist anymore.
-    ~function_symbol_store_class()
-    {
-      m_function_symbol_store_is_defined=false;
-    }
+  const std::string& name() const noexcept
+  {
+    return m_name;
+  }
+
+  std::size_t arity() const noexcept
+  {
+    return m_arity;
+  }
+
+  bool operator==(const _function_symbol& f) const noexcept
+  {
+    return m_arity == f.m_arity && m_name == f.m_name;
+  }
+
+private:
+  const std::size_t m_arity;
+  const std::string m_name;
 };
+
 } // namespace detail
 } // namespace atermpp
-
-
-
-
 
 #endif // DETAIL_FUNCTION_SYMBOL_H
 
---
+++ libraries/atermpp/include/mcrl2/atermpp/detail/function_symbol_hash.h
@@ -0,0 +1,83 @@
+// Author(s): Maurice Laveaux.
+// Copyright: see the accompanying file COPYING or copy at
+// https://github.com/mCRL2org/mCRL2/blob/master/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef MCRL2_ATERMPP_DETAIL_FUNCTION_SYMBOL_HASH_H_
+#define MCRL2_ATERMPP_DETAIL_FUNCTION_SYMBOL_HASH_H_
+
+#include "mcrl2/atermpp/function_symbol.h"
+
+#include <assert.h>
+#include <string>
+#include <stddef.h>
+
+namespace std
+{
+  /// \brief Specialisation of the standard hash function for _function_symbol.
+  template<>
+  struct hash<atermpp::detail::_function_symbol>
+  {
+    std::size_t operator()(const atermpp::detail::_function_symbol& f) const
+    {
+      std::hash<std::string> string_hasher;
+      std::size_t h = string_hasher(f.name());
+      return (h ^ f.arity());
+    }
+  };
+} // namespace std
+
+namespace atermpp
+{
+namespace detail
+{
+
+/// \brief Computes the hash for given function symbol objects and for the
+///        function_symbol_key.
+struct function_symbol_hasher
+{
+  inline std::size_t operator() (const _function_symbol& symbol) const noexcept;
+  inline std::size_t operator() (const std::string& name, std::size_t arity) const noexcept;
+};
+
+/// \brief True iff the given function symbols are equal to eachother or to
+///        the given key.
+struct function_symbol_equals
+{
+  using is_transparent = void;
+
+  inline bool operator() (const _function_symbol& first, const _function_symbol& second) const noexcept;
+  inline bool operator()(const _function_symbol& symbol, const std::string& name, std::size_t arity) const noexcept;
+};
+
+std::size_t function_symbol_hasher::operator() (const _function_symbol& symbol) const noexcept
+{
+  std::hash<_function_symbol> function_symbol_hasher;
+  return function_symbol_hasher(symbol);
+}
+
+std::size_t function_symbol_hasher::operator() (const std::string& name, std::size_t arity) const noexcept
+{
+  std::hash<std::string> string_hasher;
+  std::size_t h = string_hasher(name);
+  return (h ^ arity);
+}
+
+bool function_symbol_equals::operator() (const _function_symbol& first, const _function_symbol& second) const noexcept
+{
+  return first == second;
+}
+
+bool function_symbol_equals::operator()(const _function_symbol& symbol, const std::string& name, std::size_t arity) const noexcept
+{
+  return (symbol.name() == name) && (symbol.arity() == arity);
+}
+
+} // namespace detail
+} // namespace atermpp
+
+#endif // MCRL2_ATERMPP_DETAIL_FUNCTION_SYMBOL_HASH_H_
