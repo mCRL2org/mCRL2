2845d4beb372dca2fb076bddccafd094d3d7de85
Wieger Wesselink <j.w.wesselink@tue.nl>
2019-02-14 13:47:38 +0100

    Store states in an indexed_set, just like in lps2lts.

--- libraries/lps/include/mcrl2/lps/generate_lts.h
+++ libraries/lps/include/mcrl2/lps/generate_lts.h
@@ -18,6 +18,7 @@
 #include <tuple>
 #include <type_traits>
 #include <utility>
+#include "mcrl2/atermpp/indexed_set.h"
 #include "mcrl2/data/consistency.h"
 #include "mcrl2/data/enumerator.h"
 #include "mcrl2/lps/state.h"
@@ -27,11 +28,18 @@ namespace mcrl2 {
 
 namespace lps {
 
-/// \brief The skip operation
-struct skip
+/// \brief The skip operation with one argument
+struct skip1
 {
   template <typename T>
   void operator()(const T&) const {}
+};
+
+/// \brief The skip operation with three arguments
+struct skip3
+{
+  template <typename T1, typename T2, typename T3>
+  void operator()(const T1&, const T2&, const T3&) const {}
 };
 
 // The states are identified by numbers in the interval [0 ... number_of_states).
@@ -62,8 +70,8 @@ struct labeled_transition_system
 
   std::vector<transition> transitions;
   std::vector<std::string> action_labels;
-  std::size_t initial_state;
-  std::size_t number_of_states;
+  std::size_t initial_state = 0;
+  std::size_t number_of_states = 0;
 
   void add_transition(std::size_t from, std::size_t label, std::size_t to)
   {
@@ -104,10 +112,10 @@ make_state(const data::data_expression_list& x,
 }
 
 /// \brief Simple algorithm to generate an LTS from a linear process specification
-/// \param report_state Callback function that reports the states that are disovered. Every state is reported only once
-/// \param report_transition Callback function that reports the transtions that are found
+/// \param report_state Callback function that reports the states that are disovered. Every state is reported only once.
+/// \param report_transition Callback function that reports the transitions that are found.
 /// \pre lpsspec may not have any 'global' variables
-template <typename StateType, typename ReportState = skip, typename ReportTransition = skip>
+template <typename StateType, typename ReportState = skip1, typename ReportTransition = skip3>
 void generate_lts(const specification& lpsspec,
                   const data::rewriter& r,
                   ReportState report_state = ReportState(),
@@ -115,16 +123,16 @@ void generate_lts(const specification& lpsspec,
                  )
 {
   typedef data::enumerator_list_element_with_substitution<> enumerator_element;
+  typedef StateType state_type;
 
+  atermpp::indexed_set<state_type> discovered;
+  std::deque<std::size_t> todo;
   data::mutable_indexed_substitution<> sigma;
   const data::variable_list& process_parameters = lpsspec.process().process_parameters();
   const std::size_t n = process_parameters.size();
-  data::data_expression_list init = lpsspec.initial_process().state(process_parameters);
-  StateType d0 = make_state<StateType>(init, process_parameters.size(), [&](const data::data_expression& x) { return x; });
-
-  std::deque<StateType> todo{d0};
-  std::unordered_set<StateType> discovered{d0};
-  report_state(d0);
+  bool accept_solutions_with_variables = false;
+  data::enumerator_identifier_generator id_generator;
+  data::enumerator_algorithm<data::rewriter, data::rewriter> E(r, lpsspec.data(), r, id_generator, accept_solutions_with_variables);
 
   auto rewrite_data_expression_list = [&](const data::data_expression_list& v)
   {
@@ -133,7 +141,7 @@ void generate_lts(const specification& lpsspec,
 
   auto rewrite_state = [&](const data::data_expression_list& v)
   {
-    return make_state<StateType>(v, n, [&](const data::data_expression& x) { return r(x, sigma); });
+    return make_state<state_type>(v, n, [&](const data::data_expression& x) { return r(x, sigma); });
   };
 
   auto rewrite_action = [&](const process::action& a)
@@ -141,27 +149,22 @@ void generate_lts(const specification& lpsspec,
     return process::action(a.label(), rewrite_data_expression_list(a.arguments()));
   };
 
-  auto rewrite_multi_action = [&](const lps::multi_action& m)
+  auto rewrite_action_list = [&](const process::action_list& actions)
   {
-    const process::action_list& actions = m.actions();
-    if (m.has_time())
-    {
-      return lps::multi_action(process::action_list(actions.begin(), actions.end(), [&](const process::action& a) { return rewrite_action(a); }), r(m.time(), sigma));
-    }
-    else
-    {
-      return lps::multi_action(process::action_list(actions.begin(), actions.end(), [&](const process::action& a) { return rewrite_action(a); }));
-    }
+    return process::action_list(actions.begin(), actions.end(), [&](const process::action& a) { return rewrite_action(a); });
   };
 
-  bool accept_solutions_with_variables = false;
-  data::enumerator_identifier_generator id_generator;
-  data::enumerator_algorithm<data::rewriter, data::rewriter> E(r, lpsspec.data(), r, id_generator, accept_solutions_with_variables);
+  data::data_expression_list init = lpsspec.initial_process().state(process_parameters);
+  state_type d0 = make_state<state_type>(init, process_parameters.size(), [&](const data::data_expression& x) { return x; });
+  report_state(d0);
+  auto k = discovered.put(d0);
+  todo.push_back(k.first);
 
   while (!todo.empty())
   {
-    StateType d = todo.front();
+    std::size_t i = todo.front();
     todo.pop_front();
+    const state_type& d = discovered.get(i);
 
     auto di = d.begin();
     auto pi = process_parameters.begin();
@@ -178,15 +181,15 @@ void generate_lts(const specification& lpsspec,
                   [&](const enumerator_element& p)
                   {
                     p.add_assignments(variables, sigma, r);
-                    multi_action a = rewrite_multi_action(summand.multi_action());
-                    StateType d1 = rewrite_state(summand.next_state(process_parameters));
-                    if (discovered.find(d1) == discovered.end())
+                    process::action_list a = rewrite_action_list(summand.multi_action().actions());
+                    state_type d1 = rewrite_state(summand.next_state(process_parameters));
+                    auto j = discovered.put(d1);
+                    if (j.second)
                     {
-                      todo.push_back(d1);
-                      discovered.insert(d1);
+                      todo.push_back(j.first);
                       report_state(d1);
                     }
-                    report_transition(d, a, d1);
+                    report_transition(i, a, j.first);
                     return false;
                   },
                   data::is_false
@@ -207,18 +210,8 @@ void generate_lts(const specification& lpsspec, const data::rewriter& r, labeled
   typedef process::action_list action_type;
   typedef StateType state_type;
 
-  std::unordered_map<state_type, std::size_t> states;
+  std::size_t number_of_states = 0;
   std::unordered_map<action_type, std::size_t> actions;
-
-  auto add_state = [&](const state_type& s)
-  {
-    auto i = states.find(s);
-    if (i == states.end())
-    {
-      i = states.emplace(std::make_pair(s, states.size())).first;
-    }
-    return i->second;
-  };
 
   auto add_action = [&](const action_type& a)
   {
@@ -235,24 +228,23 @@ void generate_lts(const specification& lpsspec, const data::rewriter& r, labeled
 
   generate_lts<state_type>(lpsspec,
                            r,
-                           [&](const state_type& d)
+                           [&](const state_type&)
                            {
-                             add_state(d);
+                             number_of_states++;
                            },
-                           [&](const state_type& d, const lps::multi_action& m, const state_type& d1)
+                           [&](std::size_t from, const action_type& a, std::size_t to)
                            {
-                             std::size_t from = states.find(d)->second;
-                             std::size_t label = add_action(m.actions());
-                             std::size_t to = states.find(d1)->second;
+                             std::size_t label = add_action(a);
                              result.add_transition(from, label, to);
                            }
   );
 
-  result.number_of_states = states.size();
+  result.initial_state = 0;
+  result.number_of_states = number_of_states;
   result.action_labels.resize(actions.size());
   for (const auto& p: actions)
   {
-    result.action_labels[p.second] = process::pp(p.first);
+    result.action_labels[p.second] = lps::pp(lps::multi_action(p.first));
   }
 }
 
