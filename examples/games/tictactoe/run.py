afa6de1d67a52cb29385ffa2accd4d62f3df960f
Thomas Neele <t.s.neele@tue.nl>
2019-03-12 11:46:21 +0100

    Added a formula which expresses that naught cannot afford a wrong move
    This gives a branching counter-example that is relatively easy to
    understand.

--- examples/games/tictactoe/run.py
+++ examples/games/tictactoe/run.py
@@ -1,8 +1,10 @@
 import os
 
 os.system('mcrl22lps -v tictactoe.mcrl2 tictactoe.lps')
-os.system('lps2lts -v -rjittyc tictactoe.lps tictactoe.lts')
-os.system('lps2pbes -vm -fhas_player_cross_a_winning_strategy.mcf tictactoe.lps tictactoe.pbes')
-os.system('pbes2bool -v -rjittyc tictactoe.pbes')
+# os.system('lps2lts -v -rjittyc tictactoe.lps tictactoe.lts')
+os.system('lts2pbes -v -fhas_player_cross_a_winning_strategy.mcf tictactoe.lts tictactoe.winning_strategy.pbes')
+os.system('lts2pbes -vc -fone_wrong_move.mcf tictactoe.lts tictactoe.wrong_move.pbes')
+os.system('pbessolve -v tictactoe.winning_strategy.pbes')
+os.system('pbessolve -v -f tictactoe.lts tictactoe.wrong_move.pbes')
 
 
