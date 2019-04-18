371884b482ebeb374fcda832649abe4c93182e7b
Wieger Wesselink <j.w.wesselink@tue.nl>
2019-04-18 09:20:36 +0200

    Replaced a runtime check by an assertion.

---
+++ libraries/lts/include/mcrl2/lts/stochastic_lts_builder.h
@@ -0,0 +1,24 @@
+// Author(s): Wieger Wesselink
+// Copyright: see the accompanying file COPYING or copy at
+// https://github.com/mCRL2org/mCRL2/blob/master/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/lts/stochastic_lts_builder.h
+/// \brief add your file description here.
+
+#ifndef MCRL2_LTS_STOCHASTIC_LTS_BUILDER_H
+#define MCRL2_LTS_STOCHASTIC_LTS_BUILDER_H
+
+namespace mcrl2 {
+
+namespace lts {
+
+
+} // namespace lts
+
+} // namespace mcrl2
+
+#endif // MCRL2_LTS_STOCHASTIC_LTS_BUILDER_H
