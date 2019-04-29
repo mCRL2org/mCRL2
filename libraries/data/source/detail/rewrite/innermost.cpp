dbfc5b491e89eafd9fda63044e5c7e0e9bbb8e39
Maurice Laveaux <m.laveaux@tue.nl>
2019-04-29 14:07:03 +0200

    Moved static functions to a single utility header.

--- libraries/data/source/detail/rewrite/innermost.cpp
+++ libraries/data/source/detail/rewrite/innermost.cpp
@@ -11,6 +11,7 @@
 
 #include "mcrl2/utilities/logger.h"
 #include "mcrl2/data/bool.h"
+#include "mcrl2/data/detail/rewrite/utility.h"
 
 #include <assert.h>
 
@@ -202,98 +203,6 @@ void InnermostRewriter::mark_normal_form(const data_expression& term)
   if (EnableNormalForms)
   {
     m_normal_forms.emplace(term);
-  }
-}
-
-/// \brief Matches a single left-hand side with the given term and creates the substitution.
-template<typename Substitution>
-static bool match_lhs(const data_expression& term,  const data_expression& lhs, Substitution& sigma)
-{
-  if (is_function_symbol(lhs))
-  {
-    return term == lhs;
-  }
-  else if (is_variable(lhs))
-  {
-    const auto& var = static_cast<const variable&>(lhs);
-
-    if (sigma.count(var))
-    {
-      // If the variable was already assigned they must match.
-      return sigma.at(var) == term;
-    }
-    else
-    {
-      // Else substitute the given term for the current variable.
-      sigma[var] = term;
-      return true;
-    }
-  }
-  else
-  {
-    // The term and lhs are applications.
-    const application& lhs_appl  = static_cast<const application&>(lhs);
-    const application& term_appl = static_cast<const application&>(term);
-
-    // Both must have the same arity, the head symbol must match and their arguments must match.
-    if (lhs_appl.size() != term_appl.size())
-    {
-      return false;
-    }
-
-    if (!match_lhs(term_appl.head(), lhs_appl.head(), sigma))
-    {
-      return false;
-    }
-
-    for (std::size_t i = 0; i < term_appl.size(); i++)
-    {
-      if (!match_lhs(term_appl[i], lhs_appl[i], sigma))
-      {
-        return false;
-      }
-    }
-
-    return true;
-  }
-}
-
-/// \brief A capture-avoiding substitution of sigma applied to the given term.
-template<typename Substitution>
-static data_expression capture_avoiding_substitution(const data_expression& term, const Substitution& sigma)
-{
-  // C(x, sigma, V) = sigma(x), where x is a variable
-  if (is_variable(term))
-  {
-    const auto& var = static_cast<const variable&>(term);
-    return sigma.at(var);
-  }
-  // C(f, sigma, V) = f, where f is a function symbol.
-  else if (is_function_symbol(term))
-  {
-    return term;
-  }
-  // C(lambda x . t, sigma, V) = lambda y . C(t, sigma[x := y], V), where x and y are variables.
-  else if (is_abstraction(term))
-  {
-    const auto& abstraction = static_cast<const class abstraction&>(term);
-    assert(false);
-  }
-  // C(t(t_1, ..., t_n, sigma, V) = C(t, sigma, V) ( C(t_1, sigma, V), ..., C(t_n, sigma, V) )
-  else
-  {
-    assert(is_application(term));
-    const auto& appl = static_cast<const application&>(term);
-
-    // Substitution of all arguments.
-    std::vector<data_expression> arguments(appl.size());
-    for (std::size_t index = 0; index < appl.size(); ++index)
-    {
-      arguments[index] = capture_avoiding_substitution(appl[index], sigma);
-    }
-
-    // Construct the application, also subsituting the head.
-    return application(capture_avoiding_substitution(appl.head(), sigma), arguments.begin(), arguments.end());
   }
 }
 
