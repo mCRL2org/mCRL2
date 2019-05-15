21049ebcb044f7ef4c142dce39d74a93bbfb2583
Thomas Neele <t.s.neele@tue.nl>
2019-05-15 16:46:01 +0200

    Restored the runscript of the bke example and improved its property

--- examples/academic/bke/run.py
+++ examples/academic/bke/run.py
@@ -1,11 +1,5 @@
 import os
 
-os.system('mcrl22lps -vD bke.mcrl2 bke.lps')
-# Running any transformation does not have any effect, due to the following
-# error:
-# Error while exploring state space: error: term does not evaluate to true or false: address(E) == bad_address
-# (JK: Checked 14/9/2010)
-#os.system('lps2pbes -v -f nodeadlock.mcf bke.lps bke.nodeadlock.pbes')
-#os.system('pbes2bool -v bke.nodeadlock.pbes')
-
-
+os.system('mcrl22lps -v bke.mcrl2 bke.lps')
+os.system('lps2pbes -v -f secret_not_leaked.mcf bke.lps bke.secret_not_leaked.pbes')
+os.system('pbessolve -v bke.secret_not_leaked.pbes')
