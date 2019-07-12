9412c19c9b023610fc570e5300e70de801c77bd6
Thomas Neele <t.s.neele@tue.nl>
2019-07-12 14:09:00 +0200

    Data types are sorted topologically

--- libraries/smt2/include/mcrl2/smt2/utilities.h
+++ libraries/smt2/include/mcrl2/smt2/utilities.h
@@ -1,4 +1,4 @@
-// Author(s): Thomas Neele
+// Author(s): Thomas Neele, Ruud Koolen
 // Copyright: see the accompanying file COPYING or copy at
 // https://github.com/mCRL2org/mCRL2/blob/master/COPYING
 //
@@ -8,6 +8,8 @@
 
 #ifndef MCRL2_SMT_UTILITIES_H
 #define MCRL2_SMT_UTILITIES_H
+
+#include <queue>
 
 #include "mcrl2/core/identifier_string.h"
 #include "mcrl2/data/data_expression.h"
@@ -63,6 +65,68 @@ data::function_symbol make_recogniser_func(const data::function_symbol& cons)
 {
   data::function_sort sort(data::sort_expression_list({ cons.sort().target_sort() }), data::sort_bool::bool_());
   return data::function_symbol(make_recogniser_name(cons), sort);
+}
+
+inline
+template<class T>
+std::vector<T> topological_sort(std::map<T, std::set<T>> dependencies)
+{
+  std::queue<T> vertices_without_dependencies;
+  std::map<T, std::set<T> > reverse_dependencies;
+  for (const auto& p: dependencies)
+  {
+    const T& node = p.first;
+    const std::set<T>& successors = p.second;
+    for (const T& succ: successors)
+    {
+      reverse_dependencies[succ].insert(node);
+    }
+
+    if (successors.empty())
+    {
+      vertices_without_dependencies.push(node);
+    }
+  }
+
+  std::vector<T> output;
+  output.reserve(dependencies.size());
+
+  while (!vertices_without_dependencies.empty())
+  {
+    T vertex = vertices_without_dependencies.front();
+    vertices_without_dependencies.pop();
+    output.push_back(vertex);
+
+    const std::set<T>& successors = reverse_dependencies[vertex];
+    for (const T& succ: successors)
+    {
+      dependencies[succ].erase(vertex);
+      if (dependencies[succ].empty())
+      {
+        vertices_without_dependencies.push(succ);
+      }
+    }
+  }
+
+  if (output.size() != dependencies.size())
+  {
+    for (const T& node: output)
+    {
+      dependencies.erase(node);
+    }
+    assert(!dependencies.empty());
+    //TODO: SMT2.5 format supports mutually recursive data types
+    std::ostringstream out;
+    for(const auto& p: dependencies)
+    {
+      out << p.first << ", ";
+    }
+    throw translation_error("Dependency loop trying to resolve dependencies: {" + out.str() + "}");
+  }
+  else
+  {
+    return output;
+  }
 }
 
 } // namespace smt
