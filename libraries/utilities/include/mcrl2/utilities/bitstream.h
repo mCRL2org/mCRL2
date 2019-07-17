ac9971ad724cfcea8d71adb88009d6c891d7a232
Maurice Laveaux <m.laveaux@tue.nl>
2019-07-17 11:07:42 +0200

    Improved the documentation and comments in the bitstream.

--- libraries/utilities/include/mcrl2/utilities/bitstream.h
+++ libraries/utilities/include/mcrl2/utilities/bitstream.h
@@ -25,29 +25,31 @@ constexpr std::size_t integer_encoding_size()
   return ((sizeof(T) + 1) * 8) / 7;
 }
 
-/// \brief A bitstream provided per bit writing of data to any stream (including stdout). Internally uses
-///        bitpacking and buffering for compact and efficient IO.
+/// \brief A bitstream provides per bit writing of data to any stream (including stdout).
+/// \details Internally uses bitpacking and buffering for compact and efficient IO.
 class obitstream
 {
 public:
+  /// \brief Provides the stream on which the write function operate.
   obitstream(std::ostream& stream);
   ~obitstream() { flush(); }
 
-  /// \brief Write the nr_bits least significant bits from val to this stream.
+  /// \brief Write the num_of_bits least significant bits in descending order from value.
   /// @param val      Variable that contains the bits.
   /// @param nr_bits  Number of bits to write to the output stream.
-  void write_bits(std::size_t val, const std::size_t nr_bits);
+  void write_bits(std::size_t value, const std::size_t num_of_bits);
 
   /// \brief Write the given string to the output stream.
   /// \details Encoded in bits using <length, string>
   void write_string(const std::string& string);
 
-  /// \brief Write the given value to the output stream using most significant bit encoding.
-  void write_integer(std::size_t val);
+  /// \brief Write the given value to the output stream.
+  /// \details Uses most significant bit encoding.
+  void write_integer(std::size_t value);
 
 private:
   /// \brief Flush the remaining bits in the buffer to the output stream.
-  /// \details Note that this aligns it to the next byte, so if bits_in_buffer is 7 then a single zero bit is added additionally.
+  /// \details Note that this aligns it to the next byte, e.g. when bits_in_buffer is 6 then two zero bits are added redundantly.
   void flush();
 
   /// \brief Writes size bytes from the given buffer.
@@ -55,42 +57,45 @@ private:
 
   std::ostream& stream;
 
-  /// \brief Buffer that is filled starting from bit 127 when reading or writing
-  std::bitset<128> read_write_buffer = 0;
+  /// \brief Buffer that is filled starting from bit 127 when writing
+  std::bitset<128> write_buffer = 0;
 
-  std::size_t  bits_in_buffer = 0; ///< how many bits in bit_buffer are used.
+  std::size_t  bits_in_buffer = 0; ///< how many bits in are used in the buffer.
 
-  std::uint8_t integer_buffer[integer_encoding_size<std::size_t>()]; ///< Reserved space to space an n byte integer.
+  std::uint8_t integer_buffer[integer_encoding_size<std::size_t>()]; ///< Reserved space to store an n byte integer.
 };
 
+/// \brief The counterpart of obitstream, guarantees that the same data is read as has been written when calling the read operators
+///        in the same sequence as the corresponding write operators.
 class ibitstream
 {
 public:
+  /// \brief Provides the stream on which the read function operate.
   ibitstream(std::istream& stream);
 
-  /// \brief Reads an n-bit integer from the input stream.
-  /// \param nr_bits  Number of bits to read from the input stream.
-  std::size_t read_bits(const unsigned int nr_bits);
+  /// \brief Reads an num_of_bits bits from the input stream and stores them in the least significant part (in descending order) of the return value.
+  /// \param num_of_bits Number of bits to read from the input stream.
+  std::size_t read_bits(const unsigned int num_of_bits);
 
-  /// \returns A pointer to a character string, remains valid until the next readString call.
+  /// \returns A pointer to the read string.
+  /// \details Remains valid until the next call to read_string.
   const char* read_string();
 
-  /// \returns A natural number that was read from the binary stream encoded using most significant bit encoding.
+  /// \returns A natural number that was read from the binary stream encoded in most significant bit encoding.
   std::size_t read_integer();
 
 private:
-  /// \brief Read size bytes into the given buffer.
+  /// \brief Read size bytes into the provided buffer.
   void read(std::size_t size, std::uint8_t* buffer);
 
   std::istream& stream;
 
-  /// \brief Buffer that is filled starting from bit 127 when reading or writing
-  std::bitset<128> read_write_buffer = 0;
+  /// \brief Buffer that is filled starting from bit 127 when reading.
+  std::bitset<128> read_buffer = 0;
 
-  std::size_t bits_in_buffer = 0; ///< how many bits in bit_buffer are used.
+  std::size_t bits_in_buffer = 0; ///< how many bits in the buffer are used.
 
-  std::vector<char> m_text_buffer;
-
+  std::vector<char> m_text_buffer; ///< A temporary buffer to store char array strings.
 };
 
 } // namespace utilities
