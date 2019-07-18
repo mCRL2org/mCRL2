1eff6555ac9ab15c5560122063dfd4e0a7d7cca7
Maurice Laveaux <m.laveaux@tue.nl>
2019-07-18 14:17:43 +0200

    Use fixed-width encoding for indices.
     * Based on the number of terms (or function symbols) that have been written
       (or read) it is possible to determine the worst-case number of bits that are
       needed to index the arguments and head symbol of a term. Namely, log(n) + 1.
     * To avoid recomputation before writing or reading every index the width is
       only updated when the number of elements change.
     * For 1394-fin this reduces the amount of storage required by 20 percent
       or almost 30 percent improvement compared to the original BAF format.

--- libraries/atermpp/source/aterm_io_binary.cpp
+++ libraries/atermpp/source/aterm_io_binary.cpp
@@ -25,12 +25,13 @@
 #include "mcrl2/utilities/unordered_map.h"
 
 #include <cassert>
+#include <cmath>
 
 namespace atermpp
 {
 using namespace mcrl2::utilities;
 
-static const std::size_t BAF_MAGIC = 0xbaf;
+static constexpr std::size_t BAF_MAGIC = 0xbaf;
 
 /// \brief The BAF_VERSION constant is the version number of the ATerms written in BAF
 ///        format. As of 29 August 2013 this version number is used by the mCRL2
@@ -44,7 +45,7 @@ static const std::size_t BAF_MAGIC = 0xbaf;
 /// 23 November 2013  : version changed to 0x0302 (introduction of index for variable types)
 /// 24 September 2014 : version changed to 0x0303 (introduction of stochastic distribution)
 ///  2 April 2017     : version changed to 0x0304 (removed a few superfluous fields in the format)
-static const std::size_t BAF_VERSION = 0x0304;
+static constexpr std::size_t BAF_VERSION = 0x0304;
 
 /// \brief Each packet has a header consisting of a type and an invisible bit (indicating no output).
 enum class packet_type
@@ -56,13 +57,14 @@ enum class packet_type
 };
 
 /// \brief The number of bits needed to store an element of packet_type.
-constexpr std::size_t packet_bits = 2;
+static constexpr unsigned int packet_bits = 2;
 
 binary_aterm_output::binary_aterm_output(std::ostream& stream)
   : m_stream(stream)
 {
   // The term with function symbol index 0 indicates the end of the stream, its actual value does not matter.
-  m_function_symbol.insert(detail::g_as_int);
+  m_function_symbols.insert(detail::g_as_int);
+  m_function_symbol_index_width = 1;
 
   // Write the header of the binary aterm format.
   m_stream.write_integer(0);
@@ -117,12 +119,12 @@ void binary_aterm_output::write_term(const aterm& term)
 
         // Write the packet identifier, followed by the indices of its function symbol and arguments.
         m_stream.write_bits(static_cast<std::size_t>(is_output ? packet_type::aterm_output : packet_type::aterm), packet_bits);
-        m_stream.write_integer(symbol_index);
+        m_stream.write_bits(symbol_index, function_symbol_index_width());
         for (const auto& argument : current.term)
         {
           auto result = m_terms.find(argument);
           assert(result != m_terms.end());
-          m_stream.write_integer(result->second);
+          m_stream.write_bits(result->second, term_index_width());
         }
       }
 
@@ -130,6 +132,7 @@ void binary_aterm_output::write_term(const aterm& term)
       {
         // Output terms are not shared and thus can be forgotten.
         m_terms.insert(current.term);
+        m_term_index_width = static_cast<std::uint8_t>(std::log2(m_terms.size()) + 1);
       }
 
       stack.pop();
@@ -149,11 +152,24 @@ void binary_aterm_output::write_term(const aterm& term)
   while (!stack.empty());
 }
 
+unsigned int binary_aterm_output::term_index_width()
+{
+  assert(m_term_index_width == static_cast<unsigned int>(std::log2(m_terms.size()) + 1));
+  return m_term_index_width;
+}
+
+unsigned int binary_aterm_output::function_symbol_index_width()
+{
+  assert(m_function_symbol_index_width == static_cast<unsigned int>(std::log2(m_function_symbols.size()) + 1));
+  return m_function_symbol_index_width;
+}
+
 binary_aterm_input::binary_aterm_input(std::istream& is)
   : m_stream(is)
 {
   // The term with function symbol index 0 indicates the end of the stream.
-  m_function_symbol.emplace_back();
+  m_function_symbols.emplace_back();
+  m_function_symbol_index_width = 1;
 
   // Read the binary aterm format header.
   if (m_stream.read_integer() != 0 || m_stream.read_integer() != BAF_MAGIC)
@@ -171,9 +187,9 @@ binary_aterm_input::binary_aterm_input(std::istream& is)
 
 std::size_t binary_aterm_output::write_function_symbol(const function_symbol& symbol)
 {
-  auto result = m_function_symbol.find(symbol);
+  auto result = m_function_symbols.find(symbol);
 
-  if (result != m_function_symbol.end())
+  if (result != m_function_symbols.end())
   {
     return result->second;
   }
@@ -183,7 +199,8 @@ std::size_t binary_aterm_output::write_function_symbol(const function_symbol& sy
     m_stream.write_bits(static_cast<std::size_t>(packet_type::function_symbol), packet_bits);
     m_stream.write_string(symbol.name());
     m_stream.write_integer(symbol.arity());
-    auto result = m_function_symbol.insert(symbol);
+    auto result = m_function_symbols.insert(symbol);
+    m_function_symbol_index_width = static_cast<unsigned int>(std::log2(m_function_symbols.size()) + 1);
     return result.first->second;
   }
 }
@@ -201,12 +218,13 @@ aterm binary_aterm_input::read_term()
       // Read a single function symbol and insert it into the already read function symbols.
       std::string name = m_stream.read_string();
       std::size_t arity = m_stream.read_integer();
-      m_function_symbol.emplace_back(name, arity);
+      m_function_symbols.emplace_back(name, arity);
+      m_function_symbol_index_width = static_cast<unsigned int>(std::log2(m_function_symbols.size()) + 1);
     }
     else if (packet == packet_type::aterm || packet == packet_type::aterm_output)
     {
       // First read the function symbol of the following term.
-      function_symbol symbol = m_function_symbol[m_stream.read_integer()];
+      function_symbol symbol = m_function_symbols[m_stream.read_bits(function_symbol_index_width())];
 
       if (!symbol.defined())
       {
@@ -218,7 +236,7 @@ aterm binary_aterm_input::read_term()
       std::vector<aterm> arguments(symbol.arity());
       for (std::size_t argument = 0; argument < symbol.arity(); ++argument)
       {
-        arguments[argument] = m_terms[m_stream.read_integer()];
+        arguments[argument] = m_terms[m_stream.read_bits(term_index_width())];
       }
 
       if (packet == packet_type::aterm_output)
@@ -230,6 +248,7 @@ aterm binary_aterm_input::read_term()
       {
         // Construct the term appl from the function symbol and the already read arguments and insert it.
         m_terms.emplace_back(aterm_appl(symbol, arguments.begin(), arguments.end()));
+        m_term_index_width = static_cast<unsigned int>(std::log2(m_terms.size()) + 1);
       }
     }
     else if (packet == packet_type::aterm_int)
@@ -237,8 +256,21 @@ aterm binary_aterm_input::read_term()
       // Read the integer from the stream and construct an aterm_int.
       std::size_t value = m_stream.read_integer();
       m_terms.emplace_back(aterm_int(value));
+      m_term_index_width = static_cast<unsigned int>(std::log2(m_terms.size()) + 1);
     }
   }
+}
+
+unsigned int binary_aterm_input::term_index_width()
+{
+  assert(m_term_index_width == static_cast<unsigned int>(std::log2(m_terms.size()) + 1));
+  return m_term_index_width;
+}
+
+unsigned int binary_aterm_input::function_symbol_index_width()
+{
+  assert(m_function_symbol_index_width == static_cast<unsigned int>(std::log2(m_function_symbols.size()) + 1));
+  return m_function_symbol_index_width;
 }
 
 void write_term_to_binary_stream(const aterm& t, std::ostream& os)
