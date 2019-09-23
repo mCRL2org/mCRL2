95f39d64d8d43c54728578a369892d038351a54c
Wessel Sinnema <wesselsinnema1@me.com>
2019-09-23 09:32:18 +0200

    Upgrade OpenGL widget

--- tools/release/ltsview/ltscanvas.h
+++ tools/release/ltsview/ltscanvas.h
@@ -10,7 +10,7 @@
 #define LTSCANVAS_H
 
 #include <QCursor>
-#include <QGLWidget>
+#include <QOpenGLWidget>
 #include <QImage>
 #include <QMouseEvent>
 #include <QPoint>
@@ -19,7 +19,7 @@
 
 #include "visualizer.h"
 
-class LtsCanvas : public QGLWidget
+class LtsCanvas : public QOpenGLWidget
 {
   Q_OBJECT
 
