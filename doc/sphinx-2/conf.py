e4de0e7eb58d96bac76497d68b90c8042a258b7c
Thom Castermans <t.h.a.castermans@tue.nl>
2019-09-27 16:52:06 +0200

    Delete old temporary path from exclude_patterns

--- doc/sphinx-2/conf.py
+++ doc/sphinx-2/conf.py
@@ -71,8 +71,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '_temp/rst/libraries', '.svn', '.git',
-                    'Thumbs.db', '.DS_Store', 'sphinx-venv']
+exclude_patterns = ['_build', '.svn', '.git',
+                    'Thumbs.db', '.DS_Store',
+                    'sphinx-venv']
 
 
 # -- Options for HTML output -------------------------------------------------
