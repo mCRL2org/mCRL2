c0de5b32045b1782b63098db60a7f31b9036fe48
Wieger Wesselink <j.w.wesselink@tue.nl>
2019-12-16 19:03:02 +0100

    Cleaned up the pbesparelm code.

--- libraries/pbes/include/mcrl2/pbes/parelm.h
+++ libraries/pbes/include/mcrl2/pbes/parelm.h
@@ -15,10 +15,7 @@
 #include "mcrl2/utilities/detail/iota.h"
 #include "mcrl2/utilities/reachable_nodes.h"
 
-namespace mcrl2
-{
-
-namespace pbes_system
+namespace mcrl2::pbes_system
 {
 
 /// \brief Algorithm class for the parelm algorithm
@@ -38,7 +35,7 @@ class pbes_parelm_algorithm
     /// \param t A PBES expression
     /// \param bound_variables A sequence of data variables
     /// \return The unbound variables in \p t that are not contained in \p bound_variables
-    std::set<data::variable> unbound_variables(const pbes_expression& t, const data::variable_list& bound_variables) const
+    static std::set<data::variable> unbound_variables(const pbes_expression& t, const data::variable_list& bound_variables)
     {
       bool search_propositional_variables = false;
       return detail::find_free_variables(t, bound_variables, search_propositional_variables);
@@ -48,7 +45,7 @@ class pbes_parelm_algorithm
     /// \param variables A sequence of data variables
     /// \param d A data variable
     /// \return The index of \p d in \p v, or -1 if the variable wasn't found
-    int variable_index(const data::variable_list& variables, const data::variable& d) const
+    static int variable_index(const data::variable_list& variables, const data::variable& d)
     {
       int index = 0;
       for (const data::variable& v: variables)
@@ -62,48 +59,13 @@ class pbes_parelm_algorithm
       return -1;
     }
 
-    /// \cond INTERNAL_DOCS
-
-    /// \brief FUNCTION_DESCRIPTION
-    // \brief Prints a container to standard error
-    // \param v A container
-    // \param message A string
-    /// \param v PARAM_DESCRIPTION
-    /// \param message A string
-    template <typename Container>
-    void print_container(const Container& v, const std::string& message = "<variables>") const
-    {
-      mCRL2log(log::info) << message << std::endl;
-      for (auto i = v.begin(); i != v.end(); ++i)
-      {
-        mCRL2log(log::info) << *i << " ";
-      }
-      mCRL2log(log::info) << std::endl;
-    }
-
-    /// \brief Prints a map to standard error
-    /// \param v A map container
-    /// \param message A string
-    template <typename Container>
-    void print_map(const Container& v, const std::string& message = "<variables>") const
-    {
-      mCRL2log(log::info) << message << std::endl;
-      for (auto i = v.begin(); i != v.end(); ++i)
-      {
-        mCRL2log(log::info) << i->first << " -> " << i->second << std::endl;
-      }
-      mCRL2log(log::info) << std::endl;
-    }
-
-    /// \endcond
-
     /// \brief Finds the predicate variable to which the data parameter with the given index belongs.
     /// Here index refers to the cumulative index in the array obtained by concatening all parameters
     /// of the predicate variables in the pbes \p p.
     /// \param p A pbes
     /// \param index A positive number
     /// \return The name of the predicate variable that corresponds with \p index
-    core::identifier_string find_predicate_variable(const pbes& p, std::size_t index) const
+    static core::identifier_string find_predicate_variable(const pbes& p, std::size_t index)
     {
       std::size_t offset = 0;
       for (const pbes_equation& eqn: p.equations())
@@ -119,12 +81,11 @@ class pbes_parelm_algorithm
     }
 
   public:
-
     /// \brief Runs the parelm algorithm. The pbes \p is modified by the algorithm
     /// \param p A pbes
-    void run(pbes& p) const
+    void run(pbes& p)
     {
-      data::variable_list fvars(p.global_variables().begin(), p.global_variables().end());
+      data::variable_list global_variables(p.global_variables().begin(), p.global_variables().end());
       std::vector<data::variable> predicate_variables;
 
       // compute a mapping from propositional variable names to offsets
@@ -139,20 +100,18 @@ class pbes_parelm_algorithm
       std::size_t N = offset; // # variables
 
       // compute the initial set v of significant variables
-      std::set<std::size_t> v;
+      std::set<std::size_t> significant_variables;
       offset = 0;
       for (pbes_equation& eqn: p.equations())
       {
-        std::set<data::variable> uvars = unbound_variables(eqn.formula(), fvars);
-        for (const data::variable& w: uvars)
+        for (const data::variable& w: unbound_variables(eqn.formula(), global_variables))
         {
           int k = variable_index(eqn.variable().parameters(), w);
           if (k < 0)
           {
-            mCRL2log(log::error) << "<variable error>" << data::pp(w) << std::endl;
-            continue;
+            throw mcrl2::runtime_error("<variable error>" + data::pp(w));
           }
-          v.insert(offset + k);
+          significant_variables.insert(offset + k);
         }
         offset += eqn.variable().parameters().size();
       }
@@ -190,7 +149,7 @@ class pbes_parelm_algorithm
       }
 
       // compute the indices s of the parameters that need to be removed
-      std::vector<std::size_t> r = utilities::reachable_nodes(G, v.begin(), v.end());
+      std::vector<std::size_t> r = utilities::reachable_nodes(G, significant_variables.begin(), significant_variables.end());
       std::sort(r.begin(), r.end());
       std::vector<std::size_t> q(N);
       utilities::detail::iota(q.begin(), q.end(), 0);
@@ -204,65 +163,75 @@ class pbes_parelm_algorithm
       for (pbes_equation& eqn: p.equations())
       {
         std::size_t maxindex = index + eqn.variable().parameters().size();
-        auto slast = std::find_if(sfirst, s.end(), std::bind(std::greater_equal<std::size_t>(), std::placeholders::_1, maxindex));
+        auto slast = std::find_if(sfirst, s.end(), [&](std::size_t i) { return i >= maxindex; });
         if (slast > sfirst)
         {
           std::vector<std::size_t> w(sfirst, slast);
-          std::transform(w.begin(), w.end(), w.begin(), std::bind(std::minus<std::size_t>(), std::placeholders::_1, index));
+          std::transform(w.begin(), w.end(), w.begin(), [&](std::size_t i) { return i - index; });
           removals[eqn.variable().name()] = w;
         }
         index = maxindex;
         sfirst = slast;
       }
 
-      // print debug output
       if (mCRL2logEnabled(log::debug))
       {
-        mCRL2log(log::debug) << "\ninfluential parameters:" << std::endl;
-        for (std::size_t i: v)
-        {
-          core::identifier_string X1 = find_predicate_variable(p, i);
-          data::variable v1 = predicate_variables[i];
-          mCRL2log(log::debug) << "(" + core::pp(X1) + ", " + data::pp(v1) + ")\n";
-        }
-        mCRL2log(log::debug) << "\ndependencies:" << std::endl;
-        typedef boost::graph_traits<graph>::edge_iterator edge_iterator;
-        std::pair<edge_iterator, edge_iterator> e = edges(G);
-        edge_iterator first = e.first;
-        edge_iterator last  = e.second;
-        for (; first != last; ++first)
-        {
-          edge_descriptor f = *first;
-          std::size_t i1 = boost::source(f, G);
-          core::identifier_string X1 = find_predicate_variable(p, i1);
-          data::variable v1 = predicate_variables[i1];
-          std::size_t i2 = boost::target(f, G);
-          core::identifier_string X2 = find_predicate_variable(p, i2);
-          data::variable v2 = predicate_variables[i2];
-          std::string left  = "(" + core::pp(X1) + ", " + data::pp(v1) + ")";
-          std::string right = "(" + core::pp(X2) + ", " + data::pp(v2) + ")";
-          mCRL2log(log::debug) << left << " -> " << right << std::endl;
-        }
+        print_dependencies(p, predicate_variables, significant_variables, G);
       }
 
       // print verbose output
       if (mCRL2logEnabled(log::verbose))
       {
-        mCRL2log(log::verbose) << "\nremoving the following parameters:" << std::endl;
-        for (std::map<core::identifier_string, std::vector<std::size_t> >::const_iterator i = removals.begin(); i != removals.end(); ++i)
-        {
-          core::identifier_string X1 = i->first;
-
-          for (std::size_t j: (i->second))
-          {
-            data::variable v1 = predicate_variables[j + propvar_offsets[X1]];
-            mCRL2log(log::verbose) << "(" + core::pp(X1) + ", " + data::pp(v1) + ")\n";
-          }
-        }
+        print_removed_parameters(predicate_variables, propvar_offsets, removals);
       }
 
       // remove the parameters
       pbes_system::algorithms::remove_parameters(p, removals);
+    }
+
+    void print_removed_parameters(const std::vector<data::variable>& predicate_variables,
+                                  const std::map<core::identifier_string, std::size_t>& propvar_offsets,
+                                  const std::map<core::identifier_string, std::vector<std::size_t>>& removals) const
+    {
+      mCRL2log(log::verbose) << "\nremoving the following parameters:" << std::endl;
+      for (auto& removal: removals)
+      {
+        core::identifier_string X1 = removal.first;
+        for (std::size_t j: removal.second)
+        {
+          data::variable v1 = predicate_variables[j + propvar_offsets.at(X1)];
+          mCRL2log(log::verbose) << "(" + core::pp(X1) + ", " + data::pp(v1) + ")\n";
+        }
+      }
+    }
+
+    static void print_dependencies(const pbes& p, const std::vector<data::variable>& predicate_variables, const std::set<std::size_t>& significant_variables, const graph& G)
+    {
+      mCRL2log(log::debug) << "\ninfluential parameters:" << std::endl;
+      for (std::size_t i: significant_variables)
+      {
+        core::identifier_string X1 = find_predicate_variable(p, i);
+        data::variable v1 = predicate_variables[i];
+        mCRL2log(log::debug) << "(" + core::pp(X1) + ", " + data::pp(v1) + ")\n";
+      }
+      mCRL2log(log::debug) << "\ndependencies:" << std::endl;
+      typedef boost::graph_traits<graph>::edge_iterator edge_iterator;
+      std::pair<edge_iterator, edge_iterator> e = edges(G);
+      edge_iterator first = e.first;
+      edge_iterator last  = e.second;
+      for (; first != last; ++first)
+      {
+        edge_descriptor f = *first;
+        std::size_t i1 = boost::source(f, G);
+        core::identifier_string X1 = find_predicate_variable(p, i1);
+        data::variable v1 = predicate_variables[i1];
+        std::size_t i2 = boost::target(f, G);
+        core::identifier_string X2 = find_predicate_variable(p, i2);
+        data::variable v2 = predicate_variables[i2];
+        std::string left  = "(" + core::pp(X1) + ", " + data::pp(v1) + ")";
+        std::string right = "(" + core::pp(X2) + ", " + data::pp(v2) + ")";
+        mCRL2log(log::debug) << left << " -> " << right << std::endl;
+      }
     }
 };
 
@@ -275,8 +244,6 @@ void parelm(pbes& p)
   algorithm.run(p);
 }
 
-} // namespace pbes_system
-
-} // namespace mcrl2
+} // namespace mcrl2::pbes_system
 
 #endif // MCRL2_PBES_PARELM_H
