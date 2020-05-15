312f5f0d60633d3484d162d43ebe0d451663a5d3
Jan Friso Groote <J.F.Groote@tue.nl>
2020-05-15 11:01:55 +0200

    Simplified the prover_rewriter and moved it to a .h file
    Simplified the prover_rewriter by removing explicit new/delete
    constructs in the constructor/destructor.
    Removed a copied pointer to a standard rewriter, which was
    already available.
    
    As the code became very simple, moved the code to a .h file
    and removed the .cpp file.

--- libraries/data/include/mcrl2/data/detail/rewrite/with_prover.h
+++ libraries/data/include/mcrl2/data/detail/rewrite/with_prover.h
@@ -24,20 +24,58 @@ namespace detail
 class RewriterProver: public Rewriter
 {
   public:
-    BDD_Prover* prover_obj;
-    std::shared_ptr<detail::Rewriter> rewr_obj;
+    BDD_Prover prover_obj;
 
     typedef Rewriter::substitution_type substitution_type;
 
   public:
-    RewriterProver(const data_specification& data_spec, mcrl2::data::rewriter::strategy strat, const used_data_equation_selector& equations_selector);
-    virtual ~RewriterProver();
+    RewriterProver(const data_specification& data_spec, mcrl2::data::rewriter::strategy strat, 
+                   const used_data_equation_selector& equations_selector)
+      : Rewriter(data_spec, equations_selector),
+        prover_obj(data_spec, equations_selector, strat)
+    {
+    }
+    
+    virtual ~RewriterProver()
+    {}
 
-    rewrite_strategy getStrategy();
+    rewrite_strategy getStrategy()
+    {
+      switch (prover_obj.get_rewriter()->getStrategy())
+      {
+        case jitty:
+          return jitty_prover;
+#ifdef MCRL2_JITTYC_AVAILABLE
+        case jitty_compiling:
+          return jitty_compiling_prover;
+#endif
+        default:
+          throw mcrl2::runtime_error("invalid rewrite strategy");
+      }
+    }
+
 
     data_expression rewrite(
-         const data_expression &Term,
-         substitution_type &sigma);
+         const data_expression& t,
+         substitution_type& sigma)
+    {
+      // The prover rewriter should also work on terms with other types than Bool. 
+      // Up till 14/5/2020 this was not the case. The old code is left here, to
+      // restore it easily, in case it turns out that the jittyp rewriter cannot
+      // fruitfully be applied on expressions, other than those of type bool. 
+      //
+      // if (mcrl2::data::data_expression(t).sort() == mcrl2::data::sort_bool::bool_())
+      // {
+        prover_obj.set_substitution(sigma);
+        prover_obj.set_formula(t);
+        return prover_obj.get_bdd();
+      // }
+      // else
+      //{
+      //  return prover_obj.get_rewriter()->rewrite(t,sigma);
+      //}
+    }
+
 
 };
 
