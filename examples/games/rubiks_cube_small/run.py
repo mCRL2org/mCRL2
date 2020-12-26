14dddde49f1abe20c06d0658ddf7197b4c9e6d26
Jan Friso Groote <J.F.Groote@tue.nl>
2020-12-26 13:42:43 +0100

    Added as an example the small 2x2x2 rubick's cube
    The mCRL2 specification of this small 2x2x2 rubick's cube allows
    it to generate the state space completely, being less than 90M states.
    This can for instance be used to find the minimal sequence of turns
    to solve the puzzle. The enclosed run.py file does this concretely
    for one scrambled cube. It is easy to add the colors of a scrambled
    cube and let the system solve the cube, although this is not
    particularly efficient, as it simply generates all possible rotations
    until a situation is reached that corresponds to a solved cube.
    
    Tempting remaining work could be to derive
    a cookbook using which based on the colors of the cube the
    optimal rotation could be done to solve it.

---
+++ examples/games/rubiks_cube_small/run.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('mcrl22lps -f small_cube.mcrl2 temp.lps')
+os.system('lpsparunfold -l temp.lps -v -n4 -s"List(List(Color))" | lpsconstelm -v | lpsrewr > temp1.lps')
+os.system('lpsparunfold -l temp1.lps -v -n8 -s"List(Color)" | lpsconstelm -v | lpsrewr > temp2.lps')
+os.system('lps2lts -asolved -t1 -v -rjittyc temp2.lps')
+os.system('tracepp temp2.lps_act_0_solved.trc')
