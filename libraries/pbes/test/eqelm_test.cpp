9545e1f931056519cba5e1de8d5e5f1dff4c9f1c
Maurice Laveaux <m.laveaux@tue.nl>
2021-07-20 11:38:42 +0200

    Made the eqelm_test robust as well.

--- libraries/pbes/test/eqelm_test.cpp
+++ libraries/pbes/test/eqelm_test.cpp
@@ -28,6 +28,7 @@ std::string t1 =
   " init X(0, 0);                          \n"
   ;
 std::string x1 = "binding_variables = X(m: Nat)";
+std::string x2 = "binding_variables = X(n: Nat)";
 
 // Example provided by Tim Willemse.
 // The parameters n and m are not equivalent, and thus should not
@@ -38,10 +39,11 @@ std::string t2 =
   "                               \n"
   "init X(0, 0);                  \n"
   ;
-std::string x2 = "binding_variables = X(n,m: Nat)";
+std::string x3 = "binding_variables = X(n,m: Nat)";
 
+/// The test succeeds if any element in expected_results is satisfied.
 void test_pbes(const std::string& pbes_spec,
-               const std::string& expected_result,
+               const std::vector<std::string>& expected_results,
                bool /* compute_conditions */,
                bool /* remove_equations = true */,
                const std::string& msg = "")
@@ -69,20 +71,37 @@ void test_pbes(const std::string& pbes_spec,
   }
 
   pbes_system::detail::pbes_property_map info1(q);
-  pbes_system::detail::pbes_property_map info2(expected_result);
-  std::string diff = info1.compare(info2);
-  if (!diff.empty())
-  {
-    std::cerr << "\n------ FAILED TEST ------ " << msg << std::endl;
-    std::cerr << "--- expected result" << std::endl;
-    std::cerr << expected_result << std::endl;
-    std::cerr << "--- found result" << std::endl;
-    std::cerr << info1.to_string() << std::endl;
-    std::cerr << "--- differences" << std::endl;
-    std::cerr << diff << std::endl;
-  }
-  BOOST_CHECK(diff.empty());
+  std::vector<std::string> failed;
 
+  for (const std::string& expected_result : expected_results)
+  {
+      pbes_system::detail::pbes_property_map info2(expected_result);
+      std::string diff = info1.compare(info2);
+      if (!diff.empty())
+      {
+        std::stringstream result;
+        result << "--- expected result" << std::endl;
+        result << expected_result << std::endl;
+        result << "--- found result" << std::endl;
+        result << info1.to_string() << std::endl;
+        result << "--- differences" << std::endl;
+        result << diff << std::endl;
+        failed.push_back(result.str());
+      }
+  }
+
+  if (failed.size() == expected_results.size())
+  {
+      std::cerr << "\n------ FAILED TEST ------ " << msg << std::endl;
+      std::cerr << "None of the expected results match: \n\n";
+
+      for (const std::string& result : failed)
+      {
+          std::cerr << result;
+      }
+  }
+
+  BOOST_CHECK(failed.size() < expected_results.size());
 }
 
 void test_eqelm(const std::string& pbes_spec, const bool expected_outcome)
@@ -116,7 +135,7 @@ BOOST_AUTO_TEST_CASE(eqelm_test1)
 {
   bool compute_conditions = false;
   bool remove_equations = true;
-  test_pbes(t1, x1, compute_conditions, remove_equations, "test 1");
-  test_pbes(t2, x2, compute_conditions, remove_equations, "test 2");
+  test_pbes(t1, {x1, x2}, compute_conditions, remove_equations, "test 1");
+  test_pbes(t2, {x3}, compute_conditions, remove_equations, "test 2");
   test_eqelm(random1, false);
 }
