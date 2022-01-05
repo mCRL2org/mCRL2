0ef891a9755f5170dba017b2bfded432f23efc49
Keiren, J.J.A <j.j.a.keiren@tue.nl>
2022-01-05 16:59:19 +0100

    Change file and menu structure.
    This already makes the structure of the documentation a lot clearer, and puts the user manual and the developer docs into a single structure.

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -54,7 +54,7 @@ copyright = f'2011-{datetime.datetime.now().year}, {author}'
 # -- General configuration ---------------------------------------------------
 
 # The master document is in the user_manual directory.
-master_doc = 'user_manual/index'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -108,9 +108,6 @@ html_sidebars = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Additional files that are copied as such to the output directory, for example the index.html.
-html_extra_path = ['_html']
 
 html_css_files = [
     'css/mcrl2.css'
