9e2618ba1c46ac053605213f932492a8fd223fda
rvinktue <91969760+rvinktue@users.noreply.github.com>
2022-09-16 18:01:08 +0200

    UI changes and togglable between old LTS and using tree acceleration
    TODO: OpenGL repaint() call takes too long for large graphs and the UI becomes unresponsive.

--- tools/release/ltsgraph/layoututility.cpp
+++ tools/release/ltsgraph/layoututility.cpp
@@ -12,153 +12,28 @@
   @file layoututility.cpp
   @author R. Vink
 
-  This file contains implementation of classes facilitating layout algorithms through data structures
+  This file contains implementation of functions for classes facilitating layout algorithms through data structures
 
 */
 
 #include "layoututility.h"
-#include <cmath>
-
-#ifndef FLT_EPSILON
-#define FLT_EPSILON 1.192092896e-07F
-#endif
-
-template< class T >
-TreeNode<T> emptyTreeNode(){
-    TreeNode<T> node;
-    node.children = -1;
-    return node;
-}
-
-template < class T, int num_children, class _Iterator >
-GeometricTree<T, num_children, _Iterator>::GeometricTree(float theta, T minbounds, T maxbounds) : m_theta(theta), m_minbounds(minbounds), m_maxbounds(maxbounds), m_nodes(1)
-{
-    m_data = {emptyTreeNode<T>()};
-};
-
-template<>
-Octree::GeometricTree(float theta, QVector3D minbounds, QVector3D maxbounds) : m_theta(theta), m_minbounds(minbounds), m_maxbounds(maxbounds), m_nodes(1){
-    m_data = {emptyTreeNode<QVector3D>()};
-}
-
-template < class T, int num_children, class _Iterator >
-void GeometricTree<T, num_children, _Iterator>::add_children(){
-    if (m_data.size() < m_nodes + num_children){
-        for (int i = 0; i < num_children; i++) m_data.emplace_back(emptyTreeNode<T>());
-    }else{
-        for (int i = 0; i < num_children; i++) m_data[m_nodes+i].children = -1;
-    }
-    m_nodes += num_children;
-}
-
-template<>
-void Octree::sub_insert(const QVector3D& node_pos, int i, QVector3D& node_minbound, QVector3D& node_extents){
-    // Index has to be within m_data range
-    assert(i >= 0 && i < m_nodes);
-
-    if (m_data[i].children == TreeNodeTypes::EMPTY_NODE){
-        // we can insert here
-        m_data[i].pos = node_pos;
-        m_data[i].children = TreeNodeTypes::LEAF_NODE;
-        return; // node has been inserted
-    }else if(m_data[i].children == TreeNodeTypes::LEAF_NODE){
-      if ((node_pos - m_data[i].pos).lengthSquared() < FLT_EPSILON)
-      {
-        // trying to insert position that is already in tree
-        return;
-      }
-        // we need to subdivide the current node
-        m_data[i].children = 1;
-        m_data[i].offset = m_nodes - i;
-        add_children();
-
-        // now we have subdivided we need to move the current point down
-        QVector3D relative_pos = (m_data[i].pos - node_minbound) / node_extents; 
-        int dx = (int)round(relative_pos.x());
-        int dy = (int)round(relative_pos.y());
-        int dz = (int)round(relative_pos.z());
-        // Encode children in x y z order of least significant bit
-        int rel_child_index = (dz << 2) | (dy << 1) | dx;
-        int child_index = i+m_data[i].offset+rel_child_index;
-
-        // Move node information down
-        m_data[child_index].pos = m_data[i].pos;
-        m_data[child_index].children = 0; 
-    }
-
-    // if we make it this far we know for sure that the node has not been inserted so we need to recurse
-
-    // first we add the node to-be-inserted to the cumulative position and increment the number of children
-    m_data[i].pos += node_pos;
-    m_data[i].children += 1;
-
-    // then we find what child the to-be-inserted node has to go in
-    QVector3D relative_pos = (node_pos - node_minbound) / node_extents; 
-    int dx = (int)round(relative_pos.x());
-    int dy = (int)round(relative_pos.y());
-    int dz = (int)round(relative_pos.z());
-    // Encode children in x y z order of least significant bit
-    int rel_child_index = (dz << 2) | (dy << 1) | dx;
-    int child_index = i+m_data[i].offset+rel_child_index;
-    // and lastly we update the node bounds using the calculated dx, dy, dz values
-    node_extents *= 0.5;
-    node_minbound += QVector3D(dx, dy, dz)*node_extents;
-    // recurse
-    sub_insert(node_pos, child_index, node_minbound, node_extents);
-}
-
-template<>
-void Octree::insert(const QVector3D& node_pos){
-    // Tree has to exist
-    assert(m_nodes >= 1);
-    // Attempting to insert node outside of bounds should never happen
-    assert(node_pos.x() > m_minbounds.x() && node_pos.y() > m_minbounds.y() && node_pos.z() > m_minbounds.z());
-    assert(node_pos.x() < m_maxbounds.x() && node_pos.y() < m_maxbounds.y() && node_pos.z() < m_maxbounds.z());
-
-    // call recursive function
-    QVector3D node_extents = m_maxbounds - m_minbounds;
-    QVector3D node_minbounds(m_minbounds);
-    sub_insert(node_pos, 0, node_minbounds, node_extents);
-}
-
-template<>
-void Octree::sub_supnodes(const QVector3D& node_pos, int i, QVector3D& node_extents, std::vector<std::pair<int, QVector3D>>& super_nodes){
-    // we should never recurse on an empty subtree
-    assert(m_data[i].children != -1);
-
-    if (m_data[i].children == 0){
-        // has to be a supernode
-        super_nodes.push_back({1, m_data[i].pos});
-    }else{
-        // we check Barnes-Hut criterion
-        // if satisfied we stop recursing and add to super_nodes
-        QVector3D xS = m_data[i].pos / m_data[i].children;
-        if (node_extents.lengthSquared()/3 <= m_theta*m_theta*(node_pos-xS).lengthSquared()){
-            super_nodes.push_back({m_data[i].children, xS});
-        }else{
-            // iterate over all children and recurse if necessary
-            node_extents *= 0.5f;
-            for (int j = 0; j < 8; j++){
-                int child_index = i+m_data[i].offset+j; 
-                if (m_data[child_index].children >= 0){
-                    sub_supnodes(node_pos, child_index, node_extents, super_nodes);
-                }
-            }
-            node_extents *= 2;
-        }
-    }
-}
-
-template<>
-std::vector<std::pair<int, QVector3D>> Octree::getSuperNodes(const QVector3D& node_pos){
-    std::vector<std::pair<int, QVector3D>> super_nodes;
-    QVector3D extents = m_maxbounds - m_minbounds;
-    sub_supnodes(node_pos, 0, extents, super_nodes);
-    return super_nodes;
-}
-
 
 float octreeNorm(const QVector3D& vec) { return vec.lengthSquared(); }
 float octreeWidth(const QVector3D& vec) { return vec.lengthSquared()*0.333333333333333; }
+bool octreeEqual(const QVector3D& u, const QVector3D& v) { return (u-v).lengthSquared() < FLT_EPSILON ;}
+
+int octreeChildIndex(const QVector3D& rel_pos){
+    return ((rel_pos.z() > 0.5) << 2) |
+           ((rel_pos.y() > 0.5) << 1) |
+            (rel_pos.x() > 0.5);
+}
+
+
+void octreeUpdateMinbound(const QVector3D& rel_pos, const QVector3D& extents, QVector3D& minbound){
+    if (rel_pos.x() > 0.5) minbound.setX(minbound.x() + extents.x());
+    if (rel_pos.y() > 0.5) minbound.setY(minbound.y() + extents.y());
+    if (rel_pos.z() > 0.5) minbound.setZ(minbound.z() + extents.z());
+}
+
 float quadtreeNorm(const QVector2D& vec) { return vec.lengthSquared(); }
 float quadtreeWidth(const QVector2D& vec) { return vec.lengthSquared() * 0.5f; }
--- tools/release/ltsgraph/layoututility.h
+++ tools/release/ltsgraph/layoututility.h
@@ -12,7 +12,8 @@
   @file layoututility.h
   @author R. Vink
 
-  This file contains classes facilitating layout algorithms through data structures
+  This file contains classes facilitating layout algorithms through data
+  structures
 
 */
 
@@ -23,144 +24,225 @@
 #include <QVector3D>
 #include <vector>
 #include <mcrl2/utilities/logger.h>
-#include <stack>
-#include <iterator>
 
-template< class T >
-struct TreeNode{
-    T pos; // if the node has children, pos contains the sum of all inserted subnode positions
-    int offset;
-    // children also encodes information about the node
-    //   -1       : empty node
-    //    0       : leaf  node
-    //  otherwise : number of children in subtrees
-    int children;
+#include <cmath>
+
+#ifndef FLT_EPSILON
+#define FLT_EPSILON 1.192092896e-07F
+#endif
+
+template <class T> struct TreeNode
+{
+  T pos; // if the node has children, pos contains the sum of all inserted
+         // subnode positions
+  int offset;
+  // children also encodes information about the node
+  //    0       : empty node
+  //  otherwise : number of children in subtrees
+  int children;
 };
 
-namespace TreeNodeTypes{
-    const int EMPTY_NODE = -1;
-    const int LEAF_NODE = 0;
+template <class T> TreeNode<T> emptyTreeNode()
+{
+  TreeNode<T> node;
+  node.children = 0;
+  return node;
 }
 
-template< class T, int num_children, float(*norm)(const T&), float(*width)(const T&) >
-struct TreeIterator{
-    using iterator_category = std::forward_iterator_tag;
-    using value_type = TreeNode<T>;
-    using pointer = TreeNode<T>*;
-    using reference = TreeNode<T>&; 
-    using _Iterator_t = TreeIterator<T, num_children, norm, width>; 
+namespace TreeNodeTypes
+{
+const int EMPTY_NODE = 0;
+const int LEAF_NODE = 1;
+} // namespace TreeNodeTypes
 
-    TreeIterator(pointer ptr, const T& pos, T node_extents, float theta) 
-      : m_ptr(ptr), m_pos(pos), m_node_extents(node_extents), m_theta(theta)
-    {
-      traverse_stack = std::stack<int>({0});
-      child_index_stack = std::stack<char>({0});
-      if(m_ptr) ++(*this);
-    }
-
-    reference operator*() const {return *m_ptr; }
-    pointer operator->() {return m_ptr; }
-    
-    /// @brief Prefix increment
-    _Iterator_t& operator++() 
-    {
-      while (!traverse_stack.empty()){
-          while (!child_index_stack.empty() && child_index_stack.top() == num_children){
-              // move up the tree
-              m_ptr -= traverse_stack.top();
-              traverse_stack.pop();
-              child_index_stack.pop();
-              m_node_extents *= 2;
-          }
-          if (traverse_stack.empty())
-            break;
-          int n = traverse_stack.size();
-          assert(m_ptr->children > -1);
-          if (m_ptr->children == 0){
-              child_index_stack.top() = num_children; // marked for going back up next time
-              return *this; // found leaf node, cant go further down so it must be a super node
-          }else{
-              T xS = m_ptr->pos / m_ptr->children;
-
-              //Barnes-Hut criterion
-              if (width(m_node_extents) <= m_theta*m_theta*norm(m_pos - xS)){
-                child_index_stack.top() = num_children; // mark for going back up next time
-                return *this; // this is a supernode since the Barnes Hut citerion holds
-              }else{
-                  // iterate over all children
-                  m_node_extents *= 0.5f;
-                  while(child_index_stack.top() < num_children){
-                      int child_offset = m_ptr->offset + child_index_stack.top();
-                      child_index_stack.top()++;
-                      if ((m_ptr + child_offset)->children >= 0){
-                        // move down the tree
-                        traverse_stack.push(child_offset);
-                        child_index_stack.push(0);
-                        m_ptr += child_offset;
-                        return ++(*this); // recursion is not done yet
-                      }
-                  } // if this loop finishes; child_index_stack.top() == num_children is true and we are marked to move back up
-              }
-          }
-      }
-      m_ptr = nullptr;
-      return *this;
-    }
-    friend bool operator== (const TreeIterator& a, const TreeIterator& b) { return a.m_ptr == b.m_ptr; };
-    friend bool operator!= (const TreeIterator& a, const TreeIterator& b) { return a.m_ptr != b.m_ptr; };
-
-    private:
-        pointer m_ptr;
-        T dvec;
-        const T& m_pos;
-        T m_node_extents;
-        float m_theta;
-        std::stack<int> traverse_stack;
-        std::stack<char> child_index_stack;
-};
-
-template< class T, int num_children, class _Iterator >
-class GeometricTree{
+template <typename T, int num_children, 
+          bool (*equal)(const T&, const T&),
+          int (*get_child_index)(const T&),
+          bool (*criterion)(const TreeNode<T>&, const T&, const T&, const float&),
+          void (*update_minbound)(const T&, const T&, T&)>
+class GeometricTree
+{
   public:
-    GeometricTree(float theta, T minbounds, T maxbounds);
+  GeometricTree(float theta, T minbounds, T maxbounds)
+      : m_theta(theta), m_minbounds(minbounds), m_maxbounds(maxbounds),
+        m_nodes(1)
+  {
+    m_data = {emptyTreeNode<T>()};
+  };
 
-    void insert(const T& node_pos);
+  void insert(const T& node_pos){
+    // Tree has to exist
+    assert(m_nodes >= 1);
 
-    std::vector<std::pair<int, T>> getSuperNodes(const T& node_pos);
+    // call recursive function
+    T node_extents = m_maxbounds - m_minbounds;
+    T node_minbounds(m_minbounds);
+    sub_insert(node_pos, 0, node_minbounds, node_extents);
+  }
 
-    std::vector<TreeNode<T>> m_data;
+  std::vector<TreeNode<T>*>& getSuperNodes(const T& node_pos){
+    m_super_nodes.resize(0);
+    T extents = m_maxbounds - m_minbounds;
+    sub_supnodes(node_pos, 0, extents);
+    return m_super_nodes;
+  }
 
-    void setMinBounds(const T& bounds){ m_minbounds = bounds; }
-    void setMaxBounds(const T& bounds){ m_maxbounds = bounds; }
-    void setTheta(const float theta){ m_theta = theta; }
-    void reset(){
-      m_nodes = 1;
-      m_data[0].children = -1;
+  void calculatePositions(){
+    if (!m_sub_positions_calculated) calc_subpos(0);
+    m_sub_positions_calculated = true;
+  }
+
+  std::vector<TreeNode<T>> m_data;
+
+  void setMinBounds(const T& bounds)
+  {
+    m_minbounds = bounds;
+  }
+  void setMaxBounds(const T& bounds)
+  {
+    m_maxbounds = bounds;
+  }
+  void setTheta(const float theta)
+  {
+    m_theta = theta;
+  }
+  void reset()
+  {
+    m_sub_positions_calculated = false;
+    m_nodes = 1;
+    m_data[0].children = 0;
+  }
+
+  std::size_t size()
+  {
+    return m_nodes;
+  };
+
+  private:
+  void sub_insert(const T& node_pos, int i, T& node_minbound, T& node_extents)
+  {
+    // Index has to be within m_data range
+    assert(i >= 0 && i < m_nodes);
+
+    if (m_data[i].children == TreeNodeTypes::EMPTY_NODE)
+    {
+      // we can insert here
+      m_data[i].pos = node_pos;
+      m_data[i].children = 1;
+      return; // node has been inserted
+    }
+    else if (m_data[i].children == TreeNodeTypes::LEAF_NODE)
+    {
+      if (equal(node_pos, m_data[i].pos))
+      {
+        // trying to insert position that is already in tree
+        return;
+      }
+      // we need to subdivide the current node
+      m_data[i].offset = m_nodes - i;
+      add_children();
+
+      // now we have subdivided we need to move the current point down
+      T relative_pos = (m_data[i].pos - node_minbound) / node_extents;
+      
+      int child_index = i + m_data[i].offset + get_child_index(relative_pos);
+
+      // Move node information down
+      m_data[child_index].pos = m_data[i].pos;
+      m_data[child_index].children = TreeNodeTypes::LEAF_NODE;
     }
 
-    std::size_t size(){ return m_nodes; };
+    // if we make it this far we know for sure that the node has not been
+    // inserted so we need to recurse
 
-    _Iterator begin(const T& node_pos) { T extents = m_maxbounds - m_minbounds; return _Iterator(&m_data[0],node_pos, extents, m_theta); }
-    _Iterator end(const T& node_pos) {  T extents = m_maxbounds - m_minbounds; return _Iterator(nullptr, node_pos, extents, m_theta); }
-  private:
-    void sub_insert(const T& node_pos, int i, T& node_minbound, T& node_extents);
-    void sub_supnodes(const T& node_pos, int i, T& node_extents, std::vector<std::pair<int, T>>& super_nodes);
-    void calc_subpos(int i);
-    void add_children();
-    T m_minbounds;
-    T m_maxbounds;
-    float m_theta; ///< Barnes-Hut opening criterion
-    std::size_t m_nodes;   ///< Number of leaf nodes currently in tree
+    // first we add the node to-be-inserted to the cumulative position and
+    // increment the number of children
+    m_data[i].pos += node_pos;
+    m_data[i].children += 1;
+
+    // then we find what child the to-be-inserted node has to go in
+    T relative_pos = (node_pos - node_minbound) / node_extents;
+    int child_index = i + m_data[i].offset + get_child_index(relative_pos);
+    node_extents *= 0.5;
+    update_minbound(relative_pos, node_extents, node_minbound);
+    // recurse
+    sub_insert(node_pos, child_index, node_minbound, node_extents);
+  }
+  
+  void sub_supnodes(const T& node_pos, int i, T& node_extents){
+    // we should never recurse on an empty subtree
+    assert(m_data[i].children != TreeNodeTypes::EMPTY_NODE);
+
+    if (m_data[i].children == 1){
+        // has to be a supernode
+        m_super_nodes.push_back(&m_data[i]);
+    }else{
+        // we check criterion
+        // if satisfied we stop recursing and add to super_nodes
+        if (criterion(m_data[i], node_extents, node_pos, m_theta)){
+            m_super_nodes.push_back(&m_data[i]);
+        }else{
+            // iterate over all children and recurse if necessary
+            node_extents *= 0.5f;
+            for (int j = 0; j < num_children; j++){
+                int child_index = i+m_data[i].offset+j; 
+                if (m_data[child_index].children > 0){
+                    sub_supnodes(node_pos, child_index, node_extents);
+                }
+            }
+            node_extents *= 2;
+        }
+    }
+}
+  bool m_sub_positions_calculated = false;
+  void calc_subpos(int i){
+    if (m_data[i].children <= 1) return;
+    int base = i + m_data[i].offset;
+    double recip = 1.0/m_data[i].children;
+    m_data[i].pos = T();
+    for (int child = 0; child < num_children; child++){
+      calc_subpos(base + child);
+      m_data[i].pos += (m_data[base+child].children * recip) * m_data[base+child].pos;
+    }
+  }
+
+  void add_children()
+  {
+    if (m_data.size() < m_nodes + num_children)
+    {
+      for (int i = 0; i < num_children; i++)
+        m_data.emplace_back(emptyTreeNode<T>());
+    }
+    else
+    {
+      for (int i = 0; i < num_children; i++)
+        m_data[m_nodes + i].children = 0;
+    }
+    m_nodes += num_children;
+  }
+
+  T m_minbounds;
+  T m_maxbounds;
+  float m_theta;       ///< Barnes-Hut opening criterion
+  std::size_t m_nodes; ///< Number of leaf nodes currently in tree
+  std::vector<TreeNode<T>*> m_super_nodes;
 };
+
+// This template assumes that node.pos = xS, aka the center of mass of all contained nodes
+template < class T, float(*norm)(const T&), float(*width)(const T&) >
+bool BarnesHutCriterion(const TreeNode<T>& node, const T& extents, const T& pos, const float& m_theta){
+  return width(extents) <= m_theta*m_theta*norm(pos-node.pos);
+}
 
 float octreeNorm(const QVector3D&);
 float octreeWidth(const QVector3D&);
-using OctreeIterator = TreeIterator<QVector3D, 8, octreeNorm, octreeWidth>;
-using Octree = GeometricTree<QVector3D, 8, OctreeIterator>;
+bool octreeEqual(const QVector3D&, const QVector3D&);
+int octreeChildIndex(const QVector3D&);
+void octreeUpdateMinbound(const QVector3D&, const QVector3D&, QVector3D&);
+using Octree = GeometricTree<QVector3D, 8, octreeEqual, octreeChildIndex, BarnesHutCriterion<QVector3D, octreeNorm, octreeWidth>, octreeUpdateMinbound>;
 
-float quadtreeNorm(const QVector2D&);
-float quadtreeWidth(const QVector2D&);
-using QuadtreeIterator = TreeIterator<QVector2D, 4, quadtreeNorm, quadtreeWidth> ;
-using Quadtree = GeometricTree<QVector2D, 4, QuadtreeIterator>;
+// float quadtreeNorm(const QVector2D&);
+// float quadtreeWidth(const QVector2D&);
+// using Quadtree = GeometricTree<QVector2D, 4>;
 
 #endif
