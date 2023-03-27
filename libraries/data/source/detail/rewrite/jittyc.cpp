4bcaf591a640a657c6ed0b302d968be8b491083f
Maurice Laveaux <m.laveaux@tue.nl>
2023-03-27 14:13:37 +0200

    Fixed jittyc compilation issues

--- libraries/data/source/detail/rewrite/jittyc.cpp
+++ libraries/data/source/detail/rewrite/jittyc.cpp
@@ -1147,7 +1147,7 @@ class RewriterCompilingJitty::ImplementTree
           else 
           {
             s << m_padding << target_for_output << ".assign(" << variable_name.substr(1) 
-                           << ", this_rewriter->m_busy_flag, this_rewriter->m_forbidden_flag, this_rewriter->m_lock_depth);\n";
+                           << ", *this_rewriter->m_thread_aterm_pool);\n";
           }
         }
         else 
@@ -1191,7 +1191,7 @@ class RewriterCompilingJitty::ImplementTree
         //     "static_cast<const data_expression&>(this_rewriter->bound_variable_get(" << m_rewriter.bound_variable_index(v) << "))\n";
         s << m_padding << target_for_output << ".assign(" 
           << "static_cast<const data_expression&>(this_rewriter->bound_variable_get(" << m_rewriter.bound_variable_index(v) << ")), " 
-          << "this_rewriter), this_rewriter->m_busy_flag, this_rewriter->m_forbidden_flag, this_rewriter->m_lock_depth);\n";
+          << "this_rewriter), *this_rewriter->m_thread_aterm_pool);\n";
       }
       result_type << "data_expression";
       return;
@@ -2267,7 +2267,7 @@ public:
       {
         // m_stream << m_padding << target_for_output << " = " << ss.str() << ";\n";
         m_stream << m_padding << target_for_output << ".assign(" << ss.str() 
-                 << ", this_rewriter->m_busy_flag, this_rewriter->m_forbidden_flag, this_rewriter->m_lock_depth);\n";
+                 << ", *this_rewriter->m_thread_aterm_pool);\n";
       }
       return;
     }
