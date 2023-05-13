5f0a0e8e1fbd05b9518b7c16ea85f79438e6bc04
Maurice Laveaux <m.laveaux@tue.nl>
2023-05-13 14:26:36 +0200

    Apparently linearing ABP about 40 times takes several hours for these tests

--- libraries/modal_formula/test/monotonicity_test.cpp
+++ libraries/modal_formula/test/monotonicity_test.cpp
@@ -10,6 +10,7 @@
 /// \brief Tests for the is_monotonous function for state formulas.
 
 #define BOOST_TEST_MODULE modal_formula_monotonicity_test
+
 #include <boost/test/included/unit_test.hpp>
 
 #include "mcrl2/lps/detail/test_input.h"
@@ -129,7 +130,5 @@ BOOST_AUTO_TEST_CASE(test_elevator)
   run_monotonicity_test_case("(mu X . X) => true", lpsspec, true);
   run_monotonicity_test_case("(nu X . X) => true", lpsspec, true);
   run_monotonicity_test_case("!(mu X. (mu X. X))", lpsspec, true);
-
-  // trac ticket #1320
   run_monotonicity_test_case("!mu X. [true]X && mu X. [true]X", lpsspec, true);
 }
