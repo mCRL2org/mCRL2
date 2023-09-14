7730cc4636150c071e8c6ca2efc348510d7a551b
Maurice Laveaux <m.laveaux@tue.nl>
2023-09-14 12:43:54 +0200

    Removed the explicit timer from ltsgraph, updates still happen.

--- tools/release/ltsgraph/glwidget.cpp
+++ tools/release/ltsgraph/glwidget.cpp
@@ -21,10 +21,6 @@
 /// \brief Minimum distance for a drag to be registered (pixels)
 constexpr float DRAG_MIN_DIST = 20.0f;
 
-/// \brief Refreshing of OpenGL canvas properties
-constexpr int TARGET_FRAME_RATE = 60;
-constexpr int TARGET_FRAME_TIME = 1000 / TARGET_FRAME_RATE; // We round down.
-
 struct MoveRecord
 {
   virtual ~MoveRecord() = default;
@@ -171,13 +167,6 @@ GLWidget::GLWidget(Graph::Graph& graph, QWidget* parent)
   m_scene.setDevicePixelRatio(devicePixelRatio());
   mCRL2log(mcrl2::log::debug)
       << "Devicepixelratio: " << devicePixelRatio() << std::endl;
-
-  /// TODO: Move draw_timer to member variables so that drawing can be disabled
-  /// when graph stable and user does not interact with screen
-  // Set GLWidget to continuously update
-  QTimer* draw_timer = new QTimer(this);
-  connect(draw_timer, SIGNAL(timeout()), this, SLOT(update()));
-  draw_timer->start(TARGET_FRAME_TIME);
 }
 
 GLWidget::~GLWidget()
