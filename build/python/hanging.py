61b480f0ef21f9c8edfe3a5b9e9c1b4479c55243
Maurice Laveaux <m.laveaux@tue.nl>
2023-10-11 13:33:00 +0200

    Applied formatting suggestions to Python, removed unused file.

--- build/python/hanging.py
+++ build/python/hanging.py
@@ -2,9 +2,9 @@ import re
 import sys
 
 if len(sys.argv) != 2:
-  print('Usage: hanging.py <build_log>')
-  exit(-1)
-  
+    print("Usage: hanging.py <build_log>")
+    exit(-1)
+
 tests = set()
 found_tests = set()
 
@@ -16,20 +16,20 @@ finish_test = re.compile(r".*Test.*\#([0-9]*): .*")
 # [10:56:33] :		 [Step 1/1]         Start 516: random_pbesrewr-simplify-quantifiers-rewriter
 begin_test = re.compile(r".* Start\s*([0-9]*): .*")
 
-with open(sys.argv[1], 'r') as file:
-  for line in file.readlines():
-    result = begin_test.match(line)
-    if result is not None:
-      # Started test
-      print('Started test {}'.format(result.group(1)))
-      tests.add(int(result.group(1)))
+with open(sys.argv[1], "r", encoding="utf-8") as file:
+    for line in file.readlines():
+        result = begin_test.match(line)
+        if result is not None:
+            # Started test
+            print(f"Started test {result.group(1)}")
+            tests.add(int(result.group(1)))
 
-    result = finish_test.match(line)
-    if result is not None:
-      # Finished test
-      print('Finished test {}'.format(result.group(1)))
-      tests.discard(int(result.group(1)))
+        result = finish_test.match(line)
+        if result is not None:
+            # Finished test
+            print(f"Finished test {result.group(1)}")
+            tests.discard(int(result.group(1)))
 
-print('Hanging tests...')
+print("Hanging tests...")
 for test in tests:
-  print(test)
+    print(test)
