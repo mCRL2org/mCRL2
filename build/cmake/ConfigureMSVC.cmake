919eedee401ef8592a1ab33883d37bd94ef0058b
Maurice Laveaux <m.laveaux@tue.nl>
2023-10-25 16:17:02 +0200

    Removed try_add_xxx_flag and replaced it by add_compile_options.

--- build/cmake/ConfigureMSVC.cmake
+++ build/cmake/ConfigureMSVC.cmake
@@ -6,33 +6,55 @@ include_directories(build/workarounds/msvc)
 ## Set MSVC specific compiler flags
 ##---------------------------------------------------
 
-try_add_cxx_flag(/EHs)                # From MSDN:  Although Windows and Visual C++ support SEH, we strongly recommend 
-                                      #     that you use ISO-standard C++ exception handling (/EHs or /EHsc) because 
-                                      #     it makes code more portable and flexible.
-try_add_cxx_flag(/bigobj)             # Increases object addressable sections capacity to 4,294,967,296 (2^32), possibly required for template heavy code.
-try_add_cxx_flag(/std:c++17)          # Enable for conformance checking in Visual Studio 2017 for the C++17 standard.
-try_add_cxx_flag(/std:c17)
-try_add_cxx_flag(/W3      DEBUG)      # Increase the warning level.
-try_add_cxx_flag(/MP)                 # Enable multicore compilation.
-try_add_cxx_flag(/permissive-)		  # This option disables permissive behaviors, and sets the /Zc compiler options for strict conformance.
+function(add_cxx_flag FLAG)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${FLAG}>)
+endfunction()
 
-try_add_c_flag(/EHs)
-try_add_c_flag(/bigobj)
-try_add_c_flag(/std:c++17)
-try_add_c_flag(/std:c17)
-try_add_c_flag(/W3         DEBUG)
-try_add_c_flag(/MP)
-try_add_c_flag(/permissive-)
+# TODO: Adapt this generator expression
+function(add_cxx_debug_flag FLAG)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${FLAG}>)
+endfunction()
+
+function(add_c_flag FLAG)
+  add_compile_options($<$<COMPILE_LANGUAGE:C>:${FLAG}>)
+endfunction()
+
+function(add_c_debug_flag FLAG)
+  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:${FLAG}>)
+endfunction()
+
+add_cxx_flag(/wd4267)      # Disable size_t to smaller type conversion warnings.
+add_cxx_flag(/wd4244)      # Disable signed integer to smaller type conversion warnings.
+add_cxx_flag(/EHs)         # From MSDN:  Although Windows and Visual C++ support SEH, we strongly recommend 
+                           #     that you use ISO-standard C++ exception handling (/EHs or /EHsc) because 
+                           #     it makes code more portable and flexible.
+add_cxx_flag(/bigobj)      # Increases object addressable sections capacity to 4,294,967,296 (2^32), possibly required for template heavy code.
+add_cxx_flag(/std:c++17)   # Enable for conformance checking in Visual Studio 2017 for the C++17 standard.
+add_cxx_flag(/std:c17)
+add_cxx_flag(/MP)          # Enable multicore compilation.
+add_cxx_flag(/permissive-) # This option disables permissive behaviors, and sets the /Zc compiler options for strict conformance.
+add_cxx_debug_flag(/W3)    # Increase the warning level.
+
+add_c_flag(/wd4267)
+add_c_flag(/wd4244)
+add_c_flag(/EHs)
+add_c_flag(/bigobj)
+add_c_flag(/std:c++17)
+add_c_flag(/std:c17)
+add_c_flag(/MP)
+add_c_flag(/permissive-)
+add_c_debug_flag(/W3)
   
-add_definitions(-DNOMINMAX)                 # Don't let <windows.h> (re)define min and max
-add_definitions(-D_USE_MATH_DEFINES)        # Make <cmath> define M_PI, M_PI_2 etc.
-add_definitions(-D_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES) # Enables template overloads of standard CRT functions that call the more secure variants automatically.
-add_definitions(-D_CRT_SECURE_NO_WARNINGS)                 # Prevents many CRT deprecation warnings, especially in dparser.
-add_definitions(-DBOOST_ALL_NO_LIB=1)       # Tells the config system not to automatically select which libraries to link against. Normally if a compiler supports #pragma lib, 
+add_compile_definitions(NOMINMAX)                 # Don't let <windows.h> (re)define min and max
+add_compile_definitions(_USE_MATH_DEFINES)        # Make <cmath> define M_PI, M_PI_2 etc.
+add_compile_definitions(_CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES) # Enables template overloads of standard CRT functions that call the more secure variants automatically.
+add_compile_definitions(_CRT_SECURE_NO_WARNINGS)                 # Prevents many CRT deprecation warnings, especially in dparser.
+add_compile_definitions(BOOST_ALL_NO_LIB=1) # Tells the config system not to automatically select which libraries to link against. Normally if a compiler supports #pragma lib, 
                                             # then the correct library build variant will be automatically selected and linked against, simply by the act of including one of 
                                             # that library's headers. This macro turns that feature off. 
 
 if(MCRL2_ENABLE_MSVC_CCACHE)
+  # This changes the behaviour of MSVC to include debugging symbols into the object file, instead of generating a pdb file.
   string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
   string(REPLACE "/Zi" "/Z7" CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
   string(REPLACE "/Zi" "/Z7" CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
@@ -40,8 +62,8 @@ if(MCRL2_ENABLE_MSVC_CCACHE)
 endif()
 
 if(MCRL2_ENABLE_ADDRESSSANITIZER)
-    try_add_cxx_flag(/fsanitize=address)
+  add_cxx_flag(/fsanitize=address)
 endif()
 
 # increase the stack size
-set(CMAKE_EXE_LINKER_FLAGS "/STACK:32000000000 ${CMAKE_EXE_LINKER_FLAGS}")
+add_link_options("/STACK:32000000000")
