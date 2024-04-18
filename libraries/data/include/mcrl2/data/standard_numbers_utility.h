dcf49863eb12cee7e71b64543d1df0aede76943a
Jan Friso Groote <J.F.Groote@tue.nl>
2024-04-18 09:13:16 +0200

    Translation from Real's to doubles still incorrect.
    The translation was apparently made in a hurry, mixing up the order
    of digits. This should be repaired now.

--- libraries/data/include/mcrl2/data/standard_numbers_utility.h
+++ libraries/data/include/mcrl2/data/standard_numbers_utility.h
@@ -435,12 +435,26 @@ inline
 NUMERIC_TYPE positive_constant_to_value(const data_expression& n, typename std::enable_if<std::is_integral<NUMERIC_TYPE>::value>::type* = nullptr)
 {
 #ifdef Enable64bitNumbers
-  if (is_concat_digit_application(n))
+  if constexpr (std::is_integral<NUMERIC_TYPE>::value)
   {
-    mcrl2::runtime_error("Number " + pp(n) + " is too large to transform to a machine number.");
+    if (is_concat_digit_application(n))
+    {
+      mcrl2::runtime_error("Number " + pp(n) + " is too large to transform to a machine number.");
+    }
+    assert(is_most_significant_digit_application(n));
+    assert(atermpp::down_cast<machine_number>(sort_pos::arg(n)).value()>0);
+    return atermpp::down_cast<machine_number>(sort_pos::arg(n)).value();
   }
-  assert(is_most_significant_digit_application(n));
-  return atermpp::down_cast<machine_number>(sort_pos::arg(n)).value();
+  else
+  {
+    if (is_concat_digit_application(n))
+    {
+      return positive_constant_to_value<NUMERIC_TYPE>(sort_pos::arg1(n))*pow(2.0,64) +
+             atermpp::down_cast<machine_number>(sort_pos::arg2(n)).value();
+    }
+    assert(is_most_significant_digit_application(n));
+    return atermpp::down_cast<machine_number>(sort_pos::arg(n)).value();
+  }
 #else
   if (sort_pos::is_cdub_application(n))
   {
@@ -457,45 +471,6 @@ NUMERIC_TYPE positive_constant_to_value(const data_expression& n, typename std::
   return static_cast<NUMERIC_TYPE>(1);
 #endif
 }
-
-/// \brief Returns the NUMERIC_TYPE representation of a positive number
-/// \param n_in A data expression
-/// \pre is_positive_constant(n)
-/// \return Representation of n as sort NUMERIC_TYPE
-/// Transforms a positive constant n into number of sort NUMERIC_TYPE. Throws an exception when it does not fit. 
-/// the decimal representation of n.
-template <class NUMERIC_TYPE>
-inline
-NUMERIC_TYPE positive_constant_to_value(const data_expression& n, typename std::enable_if<std::is_floating_point<NUMERIC_TYPE>::value>::type* = nullptr)
-{
-#ifdef Enable64bitNumbers
-  data_expression m=n;
-  NUMERIC_TYPE result=0;
-  while (is_concat_digit_application(m))
-  {
-    result=result*pow(2.0,64)+atermpp::down_cast<machine_number>(sort_pos::arg2(m)).value();
-    m=sort_pos::arg1(m);
-  }
-  assert(is_most_significant_digit_application(m));
-  return result*pow(2.0,64)+atermpp::down_cast<machine_number>(sort_pos::arg(m)).value();
-#else
-  if (sort_pos::is_cdub_application(n))
-  {
-    NUMERIC_TYPE result = 2*positive_constant_to_value<NUMERIC_TYPE>(sort_pos::right(n));
-    if (sort_bool::is_true_function_symbol(sort_pos::left(n)))
-    {
-      result=result+1;
-    }
-    return result;
-  }
-
-  assert(sort_pos::is_c1_function_symbol(n));
-
-  return static_cast<NUMERIC_TYPE>(1);
-#endif
-}
-
-
 
 } // namespace sort_pos
 
@@ -627,15 +602,13 @@ inline NUMERIC_TYPE natural_constant_to_value(const data_expression& n)
   }
   else
   {
-    data_expression m=n;
-    NUMERIC_TYPE result=0;
-    while (is_concat_digit_application(m))
+    if (is_concat_digit_application(n))
     {
-      result=result*pow(2.0,64)+atermpp::down_cast<machine_number>(sort_nat::arg2(m)).value();
-      m=sort_nat::arg1(m);
+      return natural_constant_to_value<NUMERIC_TYPE>(sort_nat::arg1(n))*pow(2.0,64) +
+             atermpp::down_cast<machine_number>(sort_nat::arg2(n)).value();
     }
-    assert(is_most_significant_digit_nat_application(m));
-    return result*pow(2.0,64)+atermpp::down_cast<machine_number>(sort_nat::arg(m)).value();
+    assert(is_most_significant_digit_nat_application(n));
+    return atermpp::down_cast<machine_number>(sort_nat::arg(n)).value();
   }
 #else
   assert(is_natural_constant(n));
