3c16fd7444bfadb899f72d62640de8c5495b107f
Maurice Laveaux <m.laveaux@tue.nl>
2024-05-02 18:08:37 +0200

    Added some comments and made this run script more insightful

--- examples/academic/bakery/run.py
+++ examples/academic/bakery/run.py
@@ -2,10 +2,18 @@
 
 import subprocess
 
-mcrl22lps = subprocess.run(['mcrl22lps', '-v', '-f', '-D', '-n', '-w', 'bakery.mcrl2'], stdout=subprocess.PIPE, shell=True, check=True)
+mcrl22lps = subprocess.run(['mcrl22lps', '-f', '-D', '-n', '-w', 'bakery.mcrl2'], stdout=subprocess.PIPE, shell=True, check=True)
 lpsconstelm = subprocess.run(['lpsconstelm'], input=mcrl22lps.stdout, stdout=subprocess.PIPE, shell=True, check=True)
 subprocess.run(['lpsparelm', '-', 'bakery.lps'], input=lpsconstelm.stdout,  shell=True, check=True)
 
+# Two different abstractions used (an aggressive and less aggressive variation)
+# the following four properties can be proved/disproved using pbesabsinthe:
+#  1. get_at_least_number_circulating
+#  2. always_can_get_number
+#  3. request_must_eventually_enter
+#  4. request_can_eventually_enter
+
+# the remaining two properties cannot (currently) be proved/disproved using pbesabsinthe.
 for formula in [
     'nodeadlock',
     'request_can_eventually_enter',
@@ -14,7 +22,8 @@ for formula in [
     'always_can_get_number',
     'get_at_least_number_circulating',
 ]:
-    lps2pbes = subprocess.run(['lps2pbes', '-v', '-f', f'{formula}.mcf', 'bakery.lps'], stdout=subprocess.PIPE, shell=True, check=True)
+    print(f'Solving property {formula}')
+    lps2pbes = subprocess.run(['lps2pbes', '-f', f'{formula}.mcf', 'bakery.lps'], stdout=subprocess.PIPE, shell=True, check=True)
     pbesrewr = subprocess.run(['pbesrewr', '-psimplify'], input=lps2pbes.stdout, stdout=subprocess.PIPE, shell=True, check=True)
     pbesconstelm = subprocess.run(['pbesconstelm'], input=pbesrewr.stdout, stdout=subprocess.PIPE, shell=True, check=True)
     subprocess.run(['pbesparelm', '-', f'bakery.{formula}.pbes'], input=pbesconstelm.stdout, stdout=subprocess.PIPE, shell=True, check=True)
@@ -25,17 +34,17 @@ for formula in [
     # Instead we can abstract the data types to obtain over and underapproximations. These use the partial solvers of pbes2bool to terminate early.
     print('Answer by under-approximation: ')
     subprocess.run(['pbesabsinthe', '-sunder', '-a', 'abstraction.txt', f'bakery.{formula}.pbes', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
-    subprocess.run(['pbes2bool', '-v', '-s3', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
+    subprocess.run(['pbes2bool', '-s3', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
 
     print('Answer by over-approximation: ')
     subprocess.run(['pbesabsinthe', '-sover', '-a', 'abstraction.txt', f'bakery.{formula}.pbes', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
-    subprocess.run(['pbes2bool', '-v', '-s3', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
+    subprocess.run(['pbes2bool', '-s3', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
 
     # Alternatively, we can abstract more aggressive to ensure that the parity game is finite.
-    print('Answer by under-approximation: ')
+    print('Answer by aggressive under-approximation: ')
     subprocess.run(['pbesabsinthe', '-sunder', '-a', 'aggressive_abstraction.txt', f'bakery.{formula}.pbes', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
-    subprocess.run(['pbes2bool', '-v', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
+    subprocess.run(['pbes2bool', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
 
-    print('Answer by over-approximation: ')
+    print('Answer by aggressive over-approximation: ')
     subprocess.run(['pbesabsinthe', '-sover', '-a', 'aggressive_abstraction.txt', f'bakery.{formula}.pbes', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
-    subprocess.run(['pbes2bool', '-v', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
+    subprocess.run(['pbes2bool', '-rjitty', f'bakery.{formula}.absinthe.pbes'], shell=True, check=True)
