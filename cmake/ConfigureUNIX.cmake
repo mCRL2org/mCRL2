86c18a2716140354c1a491d93130e6ab5b53fe08
Maurice Laveaux <m.laveaux@tue.nl>
2024-11-27 14:13:56 +0100

    Fixed an issue with the standard GCC/clang checks.
    Added additional checks, as well enable both libc++ and libstd++ checks since the standard library used does not depend on the compiler.

--- cmake/ConfigureUNIX.cmake
+++ cmake/ConfigureUNIX.cmake
@@ -23,18 +23,20 @@ endif()
 ##---------------------------------------------------
 
 if (MCRL2_ENABLE_STD_CHECKS)
-  if(MCRL2_IS_CLANG)
-    # For libc++ (the LLVM standard library, what a naming scheme) there is also a debug mode
-    add_compile_definitions(_LIBCPP_ENABLE_DEBUG_MODE=1)
-  else()
-    # Enable libstdc++ debug checks and "fortify" mode, when code correctness is optional.
-    add_compile_definitions(_GLIBCXX_DEBUG)
-    add_compile_definitions(_FORTIFY_SOURCE=3)
+  # For libc++ (the LLVM standard library, what a naming scheme) there is also a debug mode
+  add_compile_definitions(_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_DEBUG)
+  add_compile_definitions(_LIBCPP_ABI_BOUNDED_ITERATORS=1)
+  add_compile_definitions(_LIBCPP_ABI_BOUNDED_ITERATORS_IN_VECTOR=1)
+  add_compile_definitions(_LIBCPP_ABI_BOUNDED_ITERATORS_IN_STRING=1)
+  add_compile_definitions(_LIBCPP_ABI_BOUNDED_UNIQUE_PTR=1)
+  add_compile_definitions(_LIBCPP_ABI_BOUNDED_ITERATORS_IN_STD_ARRAY=1)
+  add_compile_definitions(_LIBCPP_ENABLE_THREAD_SAFETY_ANNOTATIONS=1)
 
-    # These debug checks require basic optimisation for some reason, otherwise it gives a warning.
-    mcrl2_add_c_debug_flag(-O)
-  endif()
-
+  # Enable libstdc++ debug checks and "fortify" mode, when code correctness is optional.
+  add_compile_definitions(_GLIBCXX_DEBUG=1)
+  add_compile_definitions(_GLIBCXX_DEBUG_PEDANTIC=1)
+  add_compile_definitions(_FORTIFY_SOURCE=3)
+  add_compile_definitions(_GLIBCXX_ASSERTIONS=1)
 endif()
 
 ##---------------------------------------------------
@@ -57,6 +59,7 @@ mcrl2_add_cxx_debug_flag(-Wunused)
 mcrl2_add_cxx_debug_flag(-Wno-system-headers)
 mcrl2_add_cxx_debug_flag(-Woverloaded-virtual)
 mcrl2_add_cxx_debug_flag(-Wwrite-strings)
+mcrl2_add_cxx_debug_flag(ftrapv)
 
 # Change the thread local storage model to 'initial-exec', which applies additional
 # optimisations which assume that the shared library will never be dynamically loaded.
@@ -137,6 +140,7 @@ endif()
 mcrl2_add_c_flag(-pipe)
 mcrl2_add_c_flag(-ftls-model=initial-exec)
 mcrl2_add_c_flag(-fno-plt)
+mcrl2_add_c_flag(-ftrapv)
 
 # Explicitly disable some warnings for C third party libraries
 if(MCRL2_IS_CLANG)
