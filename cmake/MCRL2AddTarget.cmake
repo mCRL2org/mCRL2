18fa48260c37f5d97d371d7d7c31dcb35a269510
Maurice Laveaux <m.laveaux@tue.nl>
2024-12-19 12:36:12 +0100

    Another attempt at solving this problem

--- cmake/MCRL2AddTarget.cmake
+++ cmake/MCRL2AddTarget.cmake
@@ -56,11 +56,7 @@ function(mcrl2_add_library TARGET_NAME)
   mcrl2_install_header_files(${TARGET_INCLUDE_FILES})
 
   if (MCRL2_ENABLE_STD_CHECKS)
-    if(${MCRL2_IS_CLANG} OR APPLE)
-      target_link_libraries(${TARGET_NAME} PUBLIC -lstdc++_libbacktrace)
-    else()
-      target_link_libraries(${TARGET_NAME} PUBLIC -lbacktrace) 
-    endif()
+    target_link_libraries(${TARGET_NAME} PUBLIC -lstdc++_libbacktrace)
   endif()
 
   # Install the libraries only when we are using jittyc, or shared libraries.
