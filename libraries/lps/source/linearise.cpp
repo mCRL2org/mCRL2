mCRL2org/mCRL2

commit 16a30aedabd852d1f126706c364c90d0805b9274

Eliminate rest_is_null.
This seems a remnant of ancient history, when rest was stored as a pointer.

To eliminate, we use the invariant rest[i].empty() == rest_is_null[i].

libraries/lps/source/linearise.cpp
@@ -7851,6 +7851,8 @@ class specification_basic_type
         }
     };
 
+    /// Determine if there exists a communication expression a1|...|an -> b in comm_table
+    /// such that a1|...|an \subseteq m', where m' is the multiset of actionnames for multiaction m.
     process::action_label can_communicate(const action_list& m, comm_entry& comm_table)
     {
       /* this function indicates whether the actions in m
@@ -7969,9 +7971,7 @@ class specification_basic_type
 
       // the rest of actions of lhs that are not in m should be in n
       // rest[i] contains the part of n in which lhs i has to find matching actions
-      // rest_is_null[i] contains indications whether rest[i] is NULL.
       std::vector < action_list > rest(comm_table.size(),n);
-      std::vector < bool > rest_is_null(comm_table.size(),false);
 
       // check every lhs
       for (std::size_t i=0; i<comm_table.size(); ++i)
@@ -7986,7 +7986,6 @@ class specification_basic_type
           // .. find them in rest[i]
           if (rest[i].empty()) // no luck
           {
-            rest_is_null[i] = true;
             break;
           }
           // get first action in lhs i
@@ -7998,7 +7997,6 @@ class specification_basic_type
             rest[i].pop_front();
             if (rest[i].empty()) // no more
             {
-              rest_is_null[i] = true;
               break;
             }
           }
@@ -8011,7 +8009,7 @@ class specification_basic_type
           comm_table.tmp[i].pop_front();
         }
 
-        if (!rest_is_null[i]) // lhs was found in rest[i]
+        if (!rest[i].empty()) // lhs was found in rest[i]
         {
           return true;
         }

commit c1c4c343534b5607c587619e0eeb0f6a1f2a1b42

Fix regression introduced in 16a30aedabd852d1f126706c364c90d0805b9274
rest[i].empty() iff rest_is_null[i] is not an invariant (we only know rest_is_null[i] implies rest[i].empty())

libraries/lps/source/linearise.cpp
@@ -7949,7 +7949,11 @@ class specification_basic_type
 
           // the rest of actions of lhs that are not in m should be in n
           // rest[i] contains the part of n in which lhs i has to find matching actions
-          std::vector < action_list > rest(size(),n);
+          // if rest[i] cannot match the left hand side tmp[i], i.e., it becomes empty
+          // before matching all actions in the lhs, we set it to std::nullopt.
+          // N.B. when rest[i] becomes empty after matching all actions in the lhs,
+          // rest[i].empty() is a meaningful result: we have a successful match.
+          std::vector < std::optional<action_list> > rest(size(),n);
 
           // check every lhs
           for (std::size_t i=0; i<size(); ++i)
@@ -7958,37 +7962,40 @@ class specification_basic_type
             {
               continue;
             }
-            // as long as there are still unmatch actions in lhs i...
+            // as long as there are still unmatched actions in lhs i...
             while (!tmp[i].empty())
             {
+              assert(rest[i] != std::nullopt);
               // .. find them in rest[i]
-              if (rest[i].empty()) // no luck
+              if (rest[i]->empty()) // no luck
               {
+                rest[i] = std::nullopt;
                 break;
               }
               // get first action in lhs i
-              const identifier_string commname = tmp[i].front();
-              identifier_string restname = rest[i].front().label().name();
+              const identifier_string& commname = tmp[i].front();
+              identifier_string restname = rest[i]->front().label().name();
               // find it in rest[i]
               while (commname!=restname)
               {
-                rest[i].pop_front();
-                if (rest[i].empty()) // no more
+                rest[i]->pop_front();
+                if (rest[i]->empty()) // no more
                 {
+                  rest[i] = std::nullopt;
                   break;
                 }
-                restname = rest[i].front().label().name();
+                restname = rest[i]->front().label().name();
               }
               if (commname!=restname) // action was not found
               {
                 break;
               }
               // action found; try next
-              rest[i].pop_front();
+              rest[i]->pop_front();
               tmp[i].pop_front();
             }
 
-            if (!rest[i].empty()) // lhs was found in rest[i]
+            if (rest[i] != std::nullopt) // lhs was found in rest[i]
             {
               return true;
             }