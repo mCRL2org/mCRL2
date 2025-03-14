a05143a7a0970fb0b69899abf6d842c1c6878995
Jeroen Keiren <j.j.a.keiren@tue.nl>
2025-03-14 10:35:22 +0100

    Extend arguments for lpsreach.py script
    This allows more arguments to be passed to lpsreach, while using the hill-climbing algorithm to find a better variable order.

--- tools/release/lpsreach/lpsreach.py
+++ tools/release/lpsreach/lpsreach.py
@@ -6,9 +6,17 @@
 
 import argparse
 import os
+import pathlib
 import random
 import re
+import subprocess
 
+def dir_path(pathstr: str) -> pathlib.Path:
+    path = pathlib.Path(pathstr)
+    if path.is_dir():
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{pathstr} is not a valid path")
 
 def compute_cost(columns):
     m = len(columns)    # number of columns
@@ -41,7 +49,7 @@ def print_columns(columns):
 def maximize_cost(columns, iteration_count):
     m = len(columns)    # number of columns
     cost = compute_cost(columns)
-    print('initial solution has {} zeroes'.format(cost))
+    print(f'initial solution has {cost} zeroes')
     J = list(range(m))
     for i in range(iteration_count):
         pos1, pos2 = random.sample(J, 2)
@@ -50,18 +58,16 @@ def maximize_cost(columns, iteration_count):
         cost1 = compute_cost(columns)
         if cost1 > cost:
             cost = cost1
-            print('found solution with {} zeroes'.format(cost))
+            print(f'found solution with {cost} zeroes')
         else:
             columns[pos1], columns[pos2] = columns[pos2], columns[pos1]
             J[pos1], J[pos2] = J[pos2], J[pos1]
     return J
 
 
-def run_command(command_line, timeout=1000):
-    import shlex
+def run_command(command, timeout=1000):
     import subprocess
     try:
-        command = shlex.split(command_line)
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output, error = proc.communicate(timeout=timeout)
     except subprocess.TimeoutExpired:
@@ -91,23 +97,39 @@ def parse_read_write_matrix(text):
 def run(args):
     random.seed()
 
+    lpsreach = pathlib.Path('lpsreach')
+    if args.path:
+        lpsreach = args.path.joinpath(lpsreach)
+
     # compute a good variable ordering
-    cmd = 'lpsreach --info --groups={} {}'.format(args.groups, args.lpsfile)
+    cmd = [lpsreach, '--info', f'--groups={args.groups}', args.lpsfile]
     text = run_command(cmd)
     columns = parse_read_write_matrix(text)
     permutation = maximize_cost(columns, args.iterations)
 
     # check the permutation
-    cmd = 'lpsreach --info --groups={} --reorder="{}" {}'.format(args.groups, ' '.join(list(map(str, permutation))), args.lpsfile)
+    [lpsreach, '--info', f'--groups={args.groups}', '--reorder="{}"'.format(' '.join(list(map(str, permutation)))), args.lpsfile]
     text = run_command(cmd)
     columns = parse_read_write_matrix(text)
     cost = compute_cost(columns)
-    print('final solution has {} zeroes'.format(cost))
+    print(f'final solution has {cost} zeroes')
 
     # print the command with optimum reordering
-    cmd = 'lpsreach -v --groups={} --reorder="{}" {}'.format(args.groups, ' '.join(list(map(str, permutation))), args.lpsfile)
-    print(cmd)
-    os.system(cmd)
+    opts = [f'--memory-limit={args.memory_limit}', '--rewriter=jitty']
+    if args.cached:
+        opts.append('--cached')
+    if args.chaining:
+        opts.append('--chaining')
+    if args.saturation:
+        opts.append('--saturation')
+    if args.verbose:
+        opts.append('--verbose')
+
+    cmd = [lpsreach] + opts + [f'--groups={args.groups}', '--reorder={}'.format(' '.join(list(map(str, permutation)))), args.lpsfile]
+    print(' '.join(map(str, cmd)))
+
+    subprocess.run(cmd)
+
 
 
 def main():
@@ -115,6 +137,12 @@ def main():
     cmdline_parser.add_argument('lpsfile', metavar='FILE', type=str, help='an .lps file')
     cmdline_parser.add_argument('iterations', help="the number of iterations of hill climbing", type=int, default=1000, nargs='?')
     cmdline_parser.add_argument('--groups', help="none or simple", default='none')
+    cmdline_parser.add_argument('--cached', help="directly passed to lpsreach", action='store_true')
+    cmdline_parser.add_argument('--chaining', help="directly passed to lpsreach", action='store_true')
+    cmdline_parser.add_argument('--saturation', help="directly passed to lpsreach", action='store_true')
+    cmdline_parser.add_argument('-v', '--verbose', help="directly passed to lpsreac", action='store_true')
+    cmdline_parser.add_argument('-m', '--memory-limit', help="directly passed to lpsreach", type=int, default=3)
+    cmdline_parser.add_argument('--path', help="Path to the mCRL2 installation that should be used", type=dir_path)
     args = cmdline_parser.parse_args()
     run(args)
 
