4581a8c10b4d8b89e95e2e6bb411424c77246d8a
Maurice Laveaux <m.laveaux@tue.nl>
2025-03-17 14:56:24 +0100

    std::backtrace is standard with C++23.

--- cmake/MCRL2AddTarget.cmake
+++ cmake/MCRL2AddTarget.cmake
@@ -55,10 +55,6 @@ function(mcrl2_add_library TARGET_NAME)
   # Install the header files for mCRL2 libraries
   mcrl2_install_header_files(${TARGET_INCLUDE_FILES})
 
-  if (MCRL2_ENABLE_STD_CHECKS_BACKTRACE AND UNIX AND NOT APPLE)
-    target_link_libraries(${TARGET_NAME} PUBLIC -lstdc++_libbacktrace)
-  endif()
-
   # Install the libraries only when we are using jittyc, or shared libraries.
   install(TARGETS ${TARGET_NAME}
     COMPONENT "Libraries"
