98261192f6504e6a206973439c92d167d0e5498b
Maurice Laveaux <m.laveaux@tue.nl>
2025-03-25 14:52:44 +0100

    Resolved several smaller issues with generation

--- doc/sphinx/_extensions/mcrl2_manual.py
+++ doc/sphinx/_extensions/mcrl2_manual.py
@@ -14,11 +14,14 @@ _PWD = os.path.dirname(__file__)
 _MCRL2_TOOL_PATH = os.environ.get("MCRL2_TOOL_PATH")
 _MCRL2_TOOLS = os.environ.get("MCRL2_TOOLS")
 
+
 def log_nonl(msg):
     _LOG.info(term_width_line(msg), nonl=True)
 
+
 def generate_manpage(tool, binpath):
     """Execute the given binary with --generate-xml"""
+    log_nonl(f"Generating manpage for {tool}...")
 
     exe = os.path.join(binpath, tool)
     xml = call(tool, [exe, "--generate-xml"])
@@ -26,7 +29,9 @@ def generate_manpage(tool, binpath):
     dom = ET.fromstring(xml)
     xslt = ET.parse(os.path.join(_PWD, "manual.xsl"))
     transform = ET.XSLT(xslt)
-    return transform(dom)
+
+    return str(transform(dom))
+
 
 def mcrl2_manual_role(
     _name, _rawtext, text, lineno, inliner, _options=None, _content=None
@@ -35,10 +40,15 @@ def mcrl2_manual_role(
     toolname = text
 
     if toolname in _MCRL2_TOOLS:
-        return inliner.parse(generate_manpage(toolname, _MCRL2_TOOL_PATH), lineno, inliner, parent=None)
+        return inliner.parse(
+            generate_manpage(toolname, _MCRL2_TOOL_PATH), lineno, inliner, parent=None
+        )
     else:
-        inliner.document.reporter.severe(f"Tool {toolname} is not part of the build configuration")
+        inliner.document.reporter.severe(
+            f"Tool {toolname} is not part of the build configuration"
+        )
         return ([], [])
+
 
 def setup(app):
     register_canonical_role("mcrl2_manual", mcrl2_manual_role)
