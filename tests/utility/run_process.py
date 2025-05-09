46a28d41319502aae2148cf75bfbd0a04fc4c0c6
Maurice Laveaux <m.laveaux@tue.nl>
2025-05-09 19:34:12 +0200

    Extended tools.py to deal correctly with python scripts.
     * It used to only work on Linux, but now it also works on Windows.

--- tests/utility/run_process.py
+++ tests/utility/run_process.py
@@ -90,7 +90,7 @@ class RunProcess:
         Run the process tool with the given arguments, using at most max_memory MB of memory, and max_time seconds
         """
 
-        if platform.system() == "Windows" and os.path.splitext(tool)[1] == "":
+        if platform.system() == "Windows":
             # Don't display the Windows GPF dialog if the invoked program dies.
             # See comp.os.ms-windows.programmer.win32
             # How to suppress crash notification dialog?, Raymond Chen Jan 14,2004 -
