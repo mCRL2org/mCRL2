7082482374f96cbd3b39ac96fa69e0120ec7f71d
Maurice Laveaux <m.laveaux@tue.nl>
2025-06-11 11:11:42 +0200

    Made this script exit gracefully for the example test.

--- examples/academic/non-atomic_registers/run.py
+++ examples/academic/non-atomic_registers/run.py
@@ -7,10 +7,14 @@ import os
 # Change working dir to the script path
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-model = sys.argv[1]
-property = sys.argv[2]
+if len(sys.argv) < 3:
+    print('Usage: run.py <model> <property>')
+    sys.exit(0)
 
-print(f'Verifying property {property}.mcf for {model}.mcrl2')
+model = sys.argv[1]
+prop = sys.argv[2]
+
+print(f'Verifying property {prop}.mcf for {model}.mcrl2')
 subprocess.run(['mcrl22lps', '-v', '--timings', f'models/{model}.mcrl2', f'{model}.lps'], check=True)
-subprocess.run(['lps2pbes', '-v', '--timings', '-o', 'pbes', '-f', f'properties/{property}.mcf', f'{model}.lps', f'{model}.{property}.pbes'], check=True)
-subprocess.run(['pbessolvesymbolic', '-v', '--timings', '-Q0', '--memory-limit=128', '--cached', '--groups=used', '-rjittyc', '-s2', f'{model}.{property}.pbes'], check=True)
+subprocess.run(['lps2pbes', '-v', '--timings', '-o', 'pbes', '-f', f'properties/{prop}.mcf', f'{model}.lps', f'{model}.{prop}.pbes'], check=True)
+subprocess.run(['pbessolvesymbolic', '-v', '--timings', '-Q0', '--memory-limit=128', '--cached', '--groups=used', '-rjittyc', '-s2', f'{model}.{prop}.pbes'], check=True)
