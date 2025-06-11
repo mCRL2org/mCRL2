cd01000f14aff2043af5b9050bc2224937ed9303
Maurice Laveaux <m.laveaux@tue.nl>
2025-06-11 18:40:34 +0200

    Replaced the generate_mcrl2_code bash script by a Python script

---
+++ scripts/code_generation/generate_mcrl2_code.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+import os
+
+# Change working dir to the script path
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+script_dir = os.path.dirname(__file__)
+
+subprocess.run([sys.executable, os.path.join(script_dir, 'generate_traverser_functions.py')], check=True)
+subprocess.run([sys.executable, os.path.join(script_dir, 'generate_template_overloads.py')], check=True)
+subprocess.run([sys.executable, os.path.join(script_dir, 'generate_traversers.py')], check=True)
+subprocess.run([sys.executable, os.path.join(script_dir, 'generate_classes.py')], check=True)
+subprocess.run([sys.executable, os.path.join(script_dir, 'generate_term_functions.py')], check=True)
