4ed54b8d1bf24bc5ce6677c98ee53ee047580b0c
Maurice Laveaux <m.laveaux@tue.nl>
2025-06-11 13:51:48 +0200

    Extended the example tests with all .mcrl2 files, and check mcrl2ide projects as well.

--- tests/scripts/run_examples.py
+++ tests/scripts/run_examples.py
@@ -5,14 +5,21 @@
 # (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 import argparse
+from enum import Enum
 import os
+from pathlib import Path
 import sys
+import shutil
 import concurrent.futures
 
 # Makes sure that the script can find the modules when ran directly.
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 from tests.utility.run_process import RunProcess, TimeExceededError
 
+class Variant(Enum):
+    Script = 1
+    MCRL2Project = 2
+    MCRL2 = 3
 
 def main():
     """Execute all the run.py scripts in the example directory."""
@@ -33,6 +40,10 @@ def main():
 
     os.environ["PATH"] = args.toolpath + os.pathsep + os.environ["PATH"]
 
+    mcrl22lps_path = shutil.which("mcrl22lps")
+    lps2pbes_path = shutil.which("lps2pbes")
+    mcrl2ide_path = shutil.which("mcrl2ide")
+
     def run_example(path, index):
         print(f"[{index}] Running {path}", flush=True)
 
@@ -49,6 +60,46 @@ def main():
         process = RunProcess(python, arguments, 2000, args.timeout)
         return process.stdout
 
+    def run_mcrl2_file(mcrl22lps_path, path, index):
+        print(f"[{index}] Checking mcrl2 {path}", flush=True)
+
+        process = RunProcess(
+            mcrl22lps_path,
+            ["--check-only", path],
+            2000,
+            args.timeout,
+        )
+        return process.stdout
+
+    def run_mcrl2proj_file(mcrl22lps_path, lps2pbes_path, path, index):
+        print(f"[{index}] Checking mcrl2proj {path}", flush=True)
+
+        mcrl2_path = os.path.splitext(path)[0] + "_spec.mcrl2"
+        lps_path = str(Path(path).with_suffix(".lps"))
+
+        process = RunProcess(
+            mcrl22lps_path,
+            [mcrl2_path, lps_path],
+            2000,
+            args.timeout,
+        )
+
+        output = process.stdout
+
+        # Find properties folder and list property files
+        properties_dir = Path(path).parent / "properties"
+        if properties_dir.exists():
+            for prop in os.listdir(properties_dir):
+                process = RunProcess(
+                    lps2pbes_path,
+                    ["--check-only", f"--formula={os.path.join(properties_dir, prop)}", lps_path],
+                    2000,
+                    args.timeout,
+                )
+                output += process.stdout
+
+        return process.stdout
+
     # Start the linearisation process
     with concurrent.futures.ThreadPoolExecutor(
         max_workers=args.max_workers
@@ -60,12 +111,21 @@ def main():
         ):
             for file in files:
                 if file == "run.py":
-                    run_scripts.append(os.path.join(root, file))
+                    run_scripts.append((Variant.Script, os.path.join(root, file)))
+                elif file.endswith(".mcrl2"):
+                    run_scripts.append((Variant.MCRL2, os.path.join(root, file)))
+                elif file.endswith(".mcrl2proj"):
+                    run_scripts.append((Variant.MCRL2Project, os.path.join(root, file)))
 
         # Submit the jobs
         futures = {}
-        for index, path in enumerate(run_scripts):
-            futures[(executor.submit(run_example, path, index))] = path
+        for index, (variant, path) in enumerate(run_scripts):
+            if variant == Variant.Script:
+                futures[(executor.submit(run_example, path, index))] = path
+            elif variant == Variant.MCRL2:
+                futures[(executor.submit(run_mcrl2_file, mcrl22lps_path, path, index))] = path
+            elif variant == Variant.MCRL2Project:
+                futures[(executor.submit(run_mcrl2proj_file, mcrl22lps_path, lps2pbes_path, path, index))] = path
 
         # Wait for jobs to finish and show a progress bar.
         finished = 0
