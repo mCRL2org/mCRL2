a39b93b98d21fda1097d08350e386861161c6506
Maurice Laveaux <m.laveaux@tue.nl>
2025-06-13 13:47:25 +0200

    On Windows sometimes which find an executable with .EXE and this fails

--- tests/utility/run_process.py
+++ tests/utility/run_process.py
@@ -100,7 +100,7 @@ class RunProcess:
             ctypes.windll.kernel32.SetErrorMode(SEM_NOGPFAULTERRORBOX)
             subprocess_flags = 0x8000000  # win32con.CREATE_NO_WINDOW?
 
-            if not tool.endswith(".exe"):
+            if not tool.lower().endswith(".exe"):
                 tool += ".exe"
         else:
             subprocess_flags = 0
