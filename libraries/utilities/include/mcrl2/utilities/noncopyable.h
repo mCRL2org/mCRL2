89d9f72a0cd6c833908432a48ada2aaaf74a8dd2
Maurice Laveaux <m.laveaux@tue.nl>
2025-07-30 17:21:30 +0200

    Manually fixed some warnings

--- libraries/utilities/include/mcrl2/utilities/noncopyable.h
+++ libraries/utilities/include/mcrl2/utilities/noncopyable.h
@@ -20,6 +20,10 @@ class noncopyable
 {
 public:
   noncopyable() = default;
+  ~noncopyable() = default;
+  
+  noncopyable(noncopyable&&) = default;
+  noncopyable& operator=(noncopyable&&) = default;
   noncopyable(const noncopyable&) = delete;
   noncopyable& operator=(const noncopyable&) = delete;
 };
--- libraries/utilities/include/mcrl2/utilities/stack_array.h
+++ libraries/utilities/include/mcrl2/utilities/stack_array.h
@@ -93,8 +93,9 @@ private:
   NAME ## _reserved_stack_memory
 
 /// \brief Declares a stack_array<TYPE> with the specified NAME that stores SIZE elements type TYPE.
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define MCRL2_DECLARE_STACK_ARRAY(NAME, TYPE, SIZE) \
   TYPE* MCRL2_STACK_ARRAY_NAME(NAME) = MCRL2_SPECIFIC_STACK_ALLOCATOR(TYPE, SIZE); \
-  mcrl2::utilities::stack_array<TYPE> NAME (MCRL2_STACK_ARRAY_NAME(NAME), SIZE)
+  mcrl2::utilities::stack_array<TYPE> NAME (MCRL2_STACK_ARRAY_NAME(NAME), SIZE) 
 
 #endif // MCRL2_UTILITIES_STACK_VECTOR_H
