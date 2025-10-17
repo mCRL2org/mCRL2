475cce28c9c1b169bfdeec5f1dfcd83387746e71
Maurice Laveaux <m.laveaux@tue.nl>
2025-10-17 17:10:28 +0200

    Tried to fix the associativity used for printing

--- libraries/data/include/mcrl2/data/print.h
+++ libraries/data/include/mcrl2/data/print.h
@@ -12,6 +12,8 @@
 #ifndef MCRL2_DATA_PRINT_H
 #define MCRL2_DATA_PRINT_H
 
+#include "mcrl2/data/consistency.h"
+#include "mcrl2/data/data_expression.h"
 #include "mcrl2/data/data_specification.h"
 #include "mcrl2/data/detail/is_untyped.h"
 #include "mcrl2/data/detail/print_utility.h"
@@ -421,10 +423,13 @@ inline int precedence(const data_expression& x)
   return core::detail::max_precedence;
 }
 
+/// These should be consistent with the syntax.
+///
+/// We do not care about the abstractions (quantifiers).
 inline
 bool is_left_associative(const data_expression& x)
 {
-  return !sort_bool::is_implies_application(x) && !sort_list::is_cons_application(x);
+  return !sort_bool::is_implies_application(x) && !sort_list::is_cons_application(x) && !is_and(x) && !is_or(x);
 }
 
 inline
@@ -434,8 +439,9 @@ bool is_right_associative(const data_expression& x)
   {
     return false;
   }
-  const auto& x_ = atermpp::down_cast<application>(x);
-  return !detail::is_minus(x_) && !is_equal_to_application(x);
+
+  // For the binary operators this is one or the other.
+  return !is_left_associative(x);
 }
 
 namespace detail
