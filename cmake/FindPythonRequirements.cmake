39fd130824ff2e6c5363106658f4cbc763de546f
Maurice Laveaux <m.laveaux@tue.nl>
2025-10-17 17:10:15 +0200

    Renamed this to pip3 since that seems to be consistent on every platform

--- cmake/FindPythonRequirements.cmake
+++ cmake/FindPythonRequirements.cmake
@@ -6,7 +6,7 @@ if(Python_FOUND)
   # Execute Python and try to import the module, if this fails then the module cannot be found.
   execute_process(COMMAND ${Python_EXECUTABLE} "check_requirements.py" WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} RESULT_VARIABLE RESULT)
   if(NOT ${RESULT} EQUAL 0)
-      message(FATAL_ERROR "Python dependencies are not correctly installed, or of insufficient version. Please run 'pip install -r requirements.txt' in the mCRL2 source directory.")
+      message(FATAL_ERROR "Python dependencies are not correctly installed, or of insufficient version. Please run 'pip3 install -r requirements.txt' in the mCRL2 source directory.")
   endif()
 else()
   message(FATAL_ERROR "Could not find a suitable python3 interpreter. Ensure that variable Python_EXECUTABLE is set to a python3 interpreter.")
