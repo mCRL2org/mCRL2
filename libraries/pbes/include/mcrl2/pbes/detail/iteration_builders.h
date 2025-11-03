d88e95e39a8f5696c385b4efaa7fddeb10f20951
Jore Booy <j.j.booy@tue.nl>
2025-11-03 13:36:02 +0100

    Add preamble to iteration_builders

--- libraries/pbes/include/mcrl2/pbes/detail/iteration_builders.h
+++ libraries/pbes/include/mcrl2/pbes/detail/iteration_builders.h
@@ -1,3 +1,14 @@
+// Author(s): Jore Booy
+// Copyright: see the accompanying file COPYING or copy at
+// https://github.com/mCRL2org/mCRL2/blob/master/COPYING
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+/// \file mcrl2/pbes/detail/iteration_builders.h
+/// \brief This file provides builders used by pbeschain
+///        and pbesfixpointsolve.
 
 #ifndef MCRL2_PBES_DETAIL_ITERATION_BUILDERS_H
 #define MCRL2_PBES_DETAIL_ITERATION_BUILDERS_H
