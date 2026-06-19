a46f70b7a48fc9c36362a8fb9fe63a76cb8f2357
Maurice Laveaux <m.laveaux@tue.nl>
2026-06-19 09:52:49 +0200

    Removed the myst-parser dependency

--- doc/sphinx/conf.in.py
+++ doc/sphinx/conf.in.py
@@ -48,7 +48,6 @@ extensions = [
     'mcrl2_admonitions',
     'mcrl2_dparser',
     'mcrl2_pygment',
-    'myst_parser',
     'sphinx.ext.ifconfig',
     'sphinx.ext.imgmath',
     'sphinx.ext.todo',
